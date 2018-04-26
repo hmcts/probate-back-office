e66b34bb18d8423135ee27373679b3e5bfd6cb7a
praveenr <praveen.moj@gmail.com>
2018-04-26 18:54:19 +0100

    Corrected a  variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -40,6 +40,9 @@ variable "jenkins_AAD_objectId" {
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
 
 variable "outbound_proxy" {
   default = "http://proxyout.reform.hmcts.net:8080/"
