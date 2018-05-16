5fdce497c1d7dcf52d838f52f522d2e2ee403118
RBaker <rebecca.baker@hmcts.net>
2018-05-16 15:18:14 +0100

    PRO-2696 Adding the rest of the endpoints

--- src/main/java/uk/gov/hmcts/probate/health/HealthConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/health/HealthConfiguration.java
@@ -1,10 +1,16 @@
 package uk.gov.hmcts.probate.health;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.probate.config.EvidenceManagementClientConfiguration;
+import uk.gov.hmcts.probate.config.FeeServiceConfiguration;
 import uk.gov.hmcts.probate.config.PDFServiceConfiguration;
+import uk.gov.hmcts.probate.service.evidencemanagement.upload.UploadRequestBuilder;
 
 @Configuration
 public class HealthConfiguration {
@@ -15,9 +21,50 @@ public class HealthConfiguration {
     @Autowired
     private PDFServiceConfiguration pdfServiceConfiguration;
 
+    @Autowired
+    private FeeServiceConfiguration feeServiceConfiguration;
+
+    @Value("${idam.service.host}")
+    private String idamServiceHost;
+
+    @Value("${idam.user.host}")
+    private String idamUserHost;
+
+    @Value("${evidence.management.host}")
+    private String evidenceManagementHost;
+
+    @Value("${printservice.host}")
+    private String printServiceHost;
+
     @Bean
     public SolsHealthIndicator pdfServiceHealthIndicator() {
         return new SolsHealthIndicator(pdfServiceConfiguration.getUrl(), restTemplate);
+    }
+
+    @Bean
+    public SolsHealthIndicator feeServiceHealthIndicator() {
+        return new SolsHealthIndicator(feeServiceConfiguration.getUrl(), restTemplate);
+    }
+
+    @Bean
+    public SolsHealthIndicator idamServiceHealthIndicator() {
+        return new SolsHealthIndicator(idamServiceHost, restTemplate);
+    }
+
+    @Bean
+    public SolsHealthIndicator idamUserHealthIndicator() {
+        return new SolsHealthIndicator(idamUserHost, restTemplate);
+    }
+
+
+    @Bean
+    public SolsHealthIndicator evidenceManagementHealthIndicator() {
+        return new SolsHealthIndicator(evidenceManagementHost, restTemplate);
+    }
+
+    @Bean
+    public SolsHealthIndicator printServiceHealthIndicator() {
+        return new SolsHealthIndicator(printServiceHost, restTemplate);
     }
 
 }
