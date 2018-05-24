b653f820cf8750154f09eaf79d170824ae74bc54
Dhruv Patel <dhruv.patel20@gmail.com>
2018-05-24 11:09:54 +0100

    PRO-2697: Added updated tests.

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/printservice/SolCcdServicePrintServiceTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/printservice/SolCcdServicePrintServiceTests.java
@@ -1,9 +1,8 @@
-package uk.gov.hmcts.probate.functional.printservice;
+package uk.gov.hmcts.probate.printservice;
 
 import io.restassured.response.Response;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.probate.functional.IntegrationTestBase;
@@ -15,11 +14,9 @@ import static junit.framework.TestCase.assertTrue;
 @RunWith(SerenityRunner.class)
 public class SolCcdServicePrintServiceTests extends IntegrationTestBase {
 
-    @Ignore
     @Test
     public void verifySuccessForGetPrintTemplateDocuments() {
         Response response = SerenityRest.given()
-                .relaxedHTTPSValidation()
                 .headers(utils.getHeadersWithUserId())
                 .body(utils.getJsonFromFile("success.printCaseDetails.json")).
                         when().post("/template/documents");
@@ -33,7 +30,6 @@ public class SolCcdServicePrintServiceTests extends IntegrationTestBase {
     @Test
     public void verifySolsTemplateDetails() {
         Response response = SerenityRest.given()
-                .relaxedHTTPSValidation()
                 .headers(utils.getHeadersWithUserId())
                 .when().get("/template/case-details/sol");
 
@@ -59,39 +55,38 @@ public class SolCcdServicePrintServiceTests extends IntegrationTestBase {
         assertTrue(response.getBody().asString().contains("Executors not applying"));
         assertTrue(response.getBody().asString().contains("Not applying reason:"));
         assertTrue(response.getBody().asString().contains("Additional information"));
-
     }
 
-    @Ignore
     @Test
     public void verifyPaTemplateDetails() {
         Response response = SerenityRest.given()
-                .relaxedHTTPSValidation()
                 .headers(utils.getHeadersWithUserId())
                 .when().get("/template/case-details/pa");
 
         assertEquals(200, response.getStatusCode());
-        assertTrue(response.getBody().asString().contains("Case Data Probate Printout"));
         assertTrue(response.getBody().asString().contains("Case Number:"));
         assertTrue(response.getBody().asString().contains("Jurisdiction:"));
         assertTrue(response.getBody().asString().contains("Case Type:"));
+        assertTrue(response.getBody().asString().contains("Record type:"));
         assertTrue(response.getBody().asString().contains("Date digital application submitted:"));
-        assertTrue(response.getBody().asString().contains("Type of applicant"));
-        assertTrue(response.getBody().asString().contains("Personal"));
+        assertTrue(response.getBody().asString().contains("PA"));
         assertTrue(response.getBody().asString().contains("Is the applicant an executor:"));
         assertTrue(response.getBody().asString().contains("Applicant forename(s):"));
         assertTrue(response.getBody().asString().contains("Applicant surname:"));
-        assertTrue(response.getBody().asString().contains("Applicant address:"));
-        assertTrue(response.getBody().asString().contains("Applicant phone number:"));
-        assertTrue(response.getBody().asString().contains("Applicant email address:"));
-        assertTrue(response.getBody().asString().contains("Deceased forename(s):"));
-        assertTrue(response.getBody().asString().contains("Deceased surname:"));
-        assertTrue(response.getBody().asString().contains("At the time of their death the person who died (domiciled in England or Wales?):"));
-        assertTrue(response.getBody().asString().contains("Deceased address:"));
-        assertTrue(response.getBody().asString().contains("Number of applicants:"));
+        assertTrue(response.getBody().asString().contains("Applicant address"));
+        assertTrue(response.getBody().asString().contains("Applicant phone number"));
+        assertTrue(response.getBody().asString().contains("Applicant email address"));
+        assertTrue(response.getBody().asString().contains("Deceased first name(s):"));
+        assertTrue(response.getBody().asString().contains("Deceased last name:"));
         assertTrue(response.getBody().asString().contains("Deceased date of birth:"));
         assertTrue(response.getBody().asString().contains("Deceased date of death:"));
         assertTrue(response.getBody().asString().contains("Assets in an alias name:"));
+        assertTrue(response.getBody().asString().contains("Names used by the deceased for other assets:"));
+        assertTrue(response.getBody().asString().contains("Deceased was domiciled in England or Wales:"));
+        assertTrue(response.getBody().asString().contains("Executor details"));
+        assertTrue(response.getBody().asString().contains("Number of executors:"));
+        assertTrue(response.getBody().asString().contains("Additional information"));
 
     }
+
 }
