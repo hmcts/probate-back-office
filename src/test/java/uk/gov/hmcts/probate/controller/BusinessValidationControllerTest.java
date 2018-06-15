dacbe82bc68c031e4f7fec78039332e082aa4636
RBaker <rebecca.baker@hmcts.net>
2018-06-15 14:27:37 +0100

    PRO-2882 AppInsights config change for incoming and outgoing calls

--- src/test/java/uk/gov/hmcts/probate/controller/BusinessValidationControllerTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/BusinessValidationControllerTest.java
@@ -8,9 +8,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.probate.insights.AppInsights;
 import uk.gov.hmcts.probate.model.ccd.raw.SolsAddress;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
@@ -24,6 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -76,6 +79,9 @@ public class BusinessValidationControllerTest {
     private MockMvc mockMvc;
 
     private CaseDataBuilder caseDataBuilder;
+
+    @MockBean
+    private AppInsights appInsights;
 
     @Before
     public void setup() {
