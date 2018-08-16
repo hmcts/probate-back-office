cbdbf0b58119edeb3dbb65d81b347b427557ed39
Rabab Rabab <rabab.rabab@hmcts.net>
2018-08-16 15:12:37 +0100

    Added  stop details field to the Transformer Tests

--- src/test/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformerTest.java
+++ src/test/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformerTest.java
@@ -103,6 +103,7 @@ public class CallbackResponseTransformerTest {
     private static final String BO_DOCS_RECEIVED = "Yes";
     private static final String CASE_PRINT = "Yes";
     private static final List<CollectionMember<StopReason>> STOP_REASONS_LIST = Collections.emptyList();
+    private static final String STOP_DETAILS = "Stop Details";
 
     private static final String YES = "Yes";
     private static final Optional<String> ORIGINAL_STATE = Optional.empty();
@@ -173,6 +174,7 @@ public class CallbackResponseTransformerTest {
                 .boEmailDocsReceivedNotificationRequested(BO_DOCS_RECEIVED)
                 .casePrinted(CASE_PRINT)
                 .boCaseStopReasonList(STOP_REASONS_LIST)
+                .boStopDetails(STOP_DETAILS)
                 .willExists(YES)
                 .additionalExecutorsApplying(ADDITIONAL_EXEC_LIST_APP)
                 .additionalExecutorsNotApplying(ADDITIONAL_EXEC_LIST_NOT_APP)
@@ -361,6 +363,7 @@ public class CallbackResponseTransformerTest {
         assertEquals(BO_EMAIL_GRANT_ISSUED, callbackResponse.getData().getBoEmailGrantIssuedNotificationRequested());
         assertEquals(CASE_PRINT, callbackResponse.getData().getCasePrinted());
         assertEquals(STOP_REASONS_LIST, callbackResponse.getData().getBoCaseStopReasonList());
+        assertEquals(STOP_DETAILS, callbackResponse.getData().getBoStopDetails());
 
         assertEquals(ADDITIONAL_EXEC_LIST, callbackResponse.getData().getAdditionalExecutorsApplying());
         assertEquals(ADDITIONAL_EXEC_LIST, callbackResponse.getData().getAdditionalExecutorsNotApplying());
