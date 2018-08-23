1bf3b31f783ed9dee8e73024add751a90aaf5028
RBaker <rebecca.baker@hmcts.net>
2018-08-23 08:40:05 +0100

    PRO-3327 fixed sonarQube issues

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -115,7 +115,7 @@ public class CallbackResponseTransformer {
 
     public CallbackResponse transformCase(CallbackRequest callbackRequest) {
 
-        boolean transform = callbackRequest.getCaseDetails().getData().getApplicationType().equals(ApplicationType.SOLICITOR);
+        boolean transform = callbackRequest.getCaseDetails().getData().getApplicationType()== ApplicationType.SOLICITOR ? true : false;
 
         ResponseCaseData responseCaseData = getResponseCaseData(callbackRequest.getCaseDetails(), transform)
                 .build();
