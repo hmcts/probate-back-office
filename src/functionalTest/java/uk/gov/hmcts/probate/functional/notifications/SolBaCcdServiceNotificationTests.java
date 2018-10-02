87ac91eb7aa2c7d336ace145152d841154a4ced0
RBaker <rebecca.baker@hmcts.net>
2018-10-02 09:34:51 +0100

    PRO-3809 Enhanced functional test further to test conditional logic in emails

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/notifications/SolBaCcdServiceNotificationTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/notifications/SolBaCcdServiceNotificationTests.java
@@ -17,75 +17,87 @@ public class SolBaCcdServiceNotificationTests extends IntegrationTestBase {
 
     private static final String PA_STOP_DETAILS = "PA stop details";
     private static final String SOLS_STOP_DETAILS = "SOLS stop details";
+    private static final String DOCUMENTS_RECEIVED = "/notify/documents-received";
+    private static final String GRANT_ISSUED = "/document/generate-grant";
+    private static final String CASE_STOPPED = "/notify/case-stopped";
+    private static final String BIRMINGHAM_NO = "0121 681 3401";
+    private static final String STOP_URL = "data.probateDocumentsGenerated[0].value.DocumentLink.document_binary_url";
+    private static final String EMAIL_NOTIFICATION_URL = "data.probateNotificationsGenerated[0].value.DocumentLink.document_binary_url";
 
     @Test
     public void verifySolicitorDocumentsReceivedShouldReturnOkResponseCode() {
-        validatePostSuccess("solicitorPayloadNotifications.json", "/notify/documents-received");
+        validatePostSuccess("solicitorPayloadNotifications.json", DOCUMENTS_RECEIVED);
     }
 
     @Test
     public void verifyPersonalApplicantDocumentsReceivedShouldReturnOkResponseCode() {
-        validatePostSuccess("personalPayloadNotifications.json", "/notify/documents-received");
+        validatePostSuccess("personalPayloadNotifications.json", DOCUMENTS_RECEIVED);
+    }
+
+    @Test
+    public void verifyPersonalApplicantDocumentReceivedContentIsOk() {
+        String document = sendEmail("personalPayloadNotifications.json", DOCUMENTS_RECEIVED, EMAIL_NOTIFICATION_URL);
+        verifyPAEmailNotificationReceived(document);
+    }
+
+    @Test
+    public void verifySolicitorApplicantDocumentReceivedContentIsOk() {
+        String document = sendEmail("solicitorPayloadNotifications.json", DOCUMENTS_RECEIVED, EMAIL_NOTIFICATION_URL);
+        verifySolsEmailNotificationReceived(document);
     }
 
     @Test
     public void verifySolicitorGrantIssuedShouldReturnOkResponseCode() {
-        validatePostSuccess("solicitorPayloadNotifications.json", "/document/generate-grant");
+        validatePostSuccess("solicitorPayloadNotifications.json", GRANT_ISSUED);
     }
 
     @Test
     public void verifyPersonalApplicantGrantIssuedShouldReturnOkResponseCode() {
-        validatePostSuccess("personalPayloadNotifications.json", "/document/generate-grant");
+        validatePostSuccess("personalPayloadNotifications.json", GRANT_ISSUED);
+    }
+
+    @Test
+    public void verifyPersonalApplicantGrantIssuedContentIsOk() {
+        String document = sendEmail("personalPayloadNotifications.json", GRANT_ISSUED, EMAIL_NOTIFICATION_URL);
+        verifyPAEmailNotificationReceived(document);
+    }
+
+    @Test
+    public void verifySolicitorApplicantGrantIssuedContentIsOk() {
+        String document = sendEmail("solicitorPayloadNotifications.json", GRANT_ISSUED, EMAIL_NOTIFICATION_URL);
+        verifySolsEmailNotificationReceived(document);
     }
 
     @Test
     public void verifySolicitorCaseStoppedShouldReturnOkResponseCode() {
-
-        String document = sendEmail("solicitorPayloadNotifications.json", "/notify/case-stopped");
-
+        String document = sendEmail("solicitorPayloadNotifications.json", CASE_STOPPED, STOP_URL);
         assertTrue(document.contains(SOLS_STOP_DETAILS));
     }
 
     @Test
     public void verifyPersonalApplicantCaseStoppedShouldReturnOkResponseCode() {
-
-        String document = sendEmail("personalPayloadNotifications.json", "/notify/case-stopped");
-
+        String document = sendEmail("personalPayloadNotifications.json", CASE_STOPPED, STOP_URL);
         assertTrue(document.contains(PA_STOP_DETAILS));
     }
 
     @Test
     public void verifyPersonalApplicantCaseStoppedContentIsOk() {
-        String document = sendEmail("personalPayloadNotifications.json", "/notify/case-stopped");
-
-        assertTrue(document.contains(PA_STOP_DETAILS));
-        assertTrue(document.contains("Birmingham"));
-        assertTrue(document.contains("Executor name 1 Executor Last Name 1"));
-        assertTrue(document.contains("1528365719153338"));
-        assertTrue(document.contains("2000-01-01"));
-        assertTrue(document.contains("Deceased First Name Deceased Last Name"));
+        String document = sendEmail("personalPayloadNotifications.json", CASE_STOPPED, STOP_URL);
+        verifyPAEmailCaseStopped(document);
     }
 
     @Test
     public void verifySolicitorCaseStoppedContentIsOkay() {
-
-        String document = sendEmail("solicitorPayloadNotifications.json", "/notify/case-stopped");
-
-        assertTrue(document.contains(SOLS_STOP_DETAILS));
-        assertTrue(document.contains("Birmingham"));
-        assertTrue(document.contains("1231-3984-3949-0300"));
-        assertTrue(document.contains("name"));
-        assertTrue(document.contains("1528365719153338"));
-        assertTrue(document.contains("2000-01-01"));
-        assertTrue(document.contains("Deceased First Name Deceased Last Name"));
+        String document = sendEmail("solicitorPayloadNotifications.json", CASE_STOPPED, STOP_URL);
+        verifySolsEmailCaseStopped(document);
     }
 
 
-    private String sendEmail(String fileName, String url) {
+    private String sendEmail(String fileName, String url, String jsonDocumentUrl) {
         ResponseBody body = validatePostSuccess(fileName, url);
 
         JsonPath jsonPath = JsonPath.from(body.asString());
-        String documentUrl = jsonPath.get("data.probateDocumentsGenerated[0].value.DocumentLink.document_binary_url");
+        String documentUrl = jsonPath.get(jsonDocumentUrl);
 
         String document = utils.downloadPdfAndParseToString(documentUrl);
         return document;
@@ -102,5 +114,42 @@ public class SolBaCcdServiceNotificationTests extends IntegrationTestBase {
         response.then().assertThat().statusCode(200);
 
         return response.getBody();
+    }
+
+    private void verifyPAEmailNotificationReceived(String document){
+        assertTrue(document.contains("Birmingham"));
+        assertTrue(document.contains("Executor name 1 Executor Last Name 1"));
+        assertTrue(document.contains(BIRMINGHAM_NO));
+    }
+
+    private void verifySolsEmailNotificationReceived(String document){
+        assertTrue(document.contains("1231-3984-3949-0300"));
+        assertTrue(document.contains("Birmingham"));
+        assertTrue(document.contains("name"));
+        assertTrue(document.contains("Deceased First Name Deceased Last Name"));
+        assertTrue(document.contains(BIRMINGHAM_NO));
+    }
+
+    private void verifySolsEmailCaseStopped(String document){
+        assertTrue(document.contains(SOLS_STOP_DETAILS));
+        assertTrue(document.contains("1231-3984-3949-0300"));
+        assertTrue(document.contains("Birmingham"));
+        assertTrue(document.contains("name"));
+        assertTrue(document.contains("1528365719153338"));
+        assertTrue(document.contains("2000-01-01"));
+        assertTrue(document.contains("Deceased First Name Deceased Last Name"));
+        assertTrue(document.contains(BIRMINGHAM_NO));
+        assertTrue(document.contains("SOLS stop details"));
+    }
+
+    private void verifyPAEmailCaseStopped(String document){
+        assertTrue(document.contains(PA_STOP_DETAILS));
+        assertTrue(document.contains("Birmingham"));
+        assertTrue(document.contains("Executor name 1 Executor Last Name 1"));
+        assertTrue(document.contains("1528365719153338"));
+        assertTrue(document.contains("2000-01-01"));
+        assertTrue(document.contains("Deceased First Name Deceased Last Name"));
+        assertTrue(document.contains(BIRMINGHAM_NO));
+        assertTrue(document.contains("PA stop details"));
     }
 }
