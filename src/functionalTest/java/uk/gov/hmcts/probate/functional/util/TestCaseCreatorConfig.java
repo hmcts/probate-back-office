fa5d42744adbbddad86cfdae1df4b4226ef1de72
RBaker <rebecca.baker@hmcts.net>
2018-10-02 10:49:00 +0100

    PRO-2956 PRO-3826 Reverted changes

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/util/TestCaseCreatorConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/util/TestCaseCreatorConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("application.properties")
+@PropertySource("case-creation-demo.properties")
 public class TestCaseCreatorConfig {
 
     @Bean
