7342aa50f16fe04de98e1b0374e586c1054b5d7c
Suneet Kamath <suneet.kamath@hmcts.net>
2018-12-07 10:29:16 +0000

    reinstate unit tests and handle new Exception

--- src/test/java/uk/gov/hmcts/probate/service/template/pdf/PDFGeneratorServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/template/pdf/PDFGeneratorServiceTest.java
@@ -8,20 +8,22 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.probate.config.PDFServiceConfiguration;
+import uk.gov.hmcts.probate.exception.ClientException;
 import uk.gov.hmcts.probate.insights.AppInsights;
 import uk.gov.hmcts.probate.model.evidencemanagement.EvidenceManagementFileUpload;
 import uk.gov.hmcts.probate.service.FileSystemResourceService;
 import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
+import uk.gov.hmcts.reform.pdf.service.client.exception.PDFServiceClientException;
 
 import java.lang.reflect.Field;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT;
@@ -30,16 +32,13 @@ import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT;
 public class PDFGeneratorServiceTest {
 
     @Mock
-    private RestTemplate restTemplateMock;
-
-    @Mock
     private FileSystemResourceService fileSystemResourceServiceMock;
 
     @Mock
     private PDFServiceConfiguration pdfServiceConfiguration;
 
     @Mock
-    private ByteArrayResource byteArrayResourceMock;
+    private PDFServiceClientException pdfServiceClientExceptionMock;
 
     @Mock
     private AppInsights appInsights;
@@ -56,14 +55,28 @@ public class PDFGeneratorServiceTest {
         objectMapper.setAccessible(true);
         objectMapper.set(underTest, new ObjectMapper());
 
+        when(pdfServiceClientExceptionMock.getMessage()).thenReturn("blah");
         when(pdfServiceClient.generateFromHtml(any(), any())).thenReturn("MockedBytes".getBytes());
         when(fileSystemResourceServiceMock.getFileFromResourceAsString(anyString()))
                 .thenReturn("<htmlTemplate>");
     }
 
     @Test
-    public void shouldGeneratePDFWithResponseStatusOK() {
+    public void shouldGeneratePDFWithBytesAndPDFContentType() {
         EvidenceManagementFileUpload result = underTest.generatePdf(LEGAL_STATEMENT, "{\"data\":\"value\"}");
         Assert.assertThat(result.getContentType(), equalTo(MediaType.APPLICATION_PDF));
+        Assert.assertThat(result.getBytes().length, greaterThan(0));
+    }
+
+    @Test(expected = ClientException.class)
+    public void shouldThrowClientException() {
+        when(pdfServiceClient.generateFromHtml(any(byte[].class), anyMap())).thenThrow(pdfServiceClientExceptionMock);
+        underTest.generatePdf(LEGAL_STATEMENT, "{\"data\":\"value\"}");
+    }
+
+    @Test(expected = ClientException.class)
+    public void shouldThrowPDFConnectionException() {
+        when(pdfServiceClient.generateFromHtml(any(), any())).thenThrow(pdfServiceClientExceptionMock);
+        underTest.generatePdf(LEGAL_STATEMENT, "{\"data\":\"value\"}");
     }
 }
