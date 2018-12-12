1fa4c317d866a0767b97c1b893799b4207ca3a90
Rabab Rabab <rabab.rabab@hmcts.net>
2018-12-12 18:43:48 +0000

    temp fix for snoar checks untill the use of address is reactored across cases

--- src/main/java/uk/gov/hmcts/probate/model/ccd/ProbateAddress.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/ProbateAddress.java
@@ -11,23 +11,23 @@ import java.io.Serializable;
 public class ProbateAddress implements Serializable {
 
     @JsonProperty(value = "AddressLine1")
-    private final String addressLine1;
+    private final String proAddressLine1;
 
     @JsonProperty(value = "AddressLine2")
-    private final String addressLine2;
+    private final String proAddressLine2;
 
     @JsonProperty(value = "AddressLine3")
-    private final String addressLine3;
+    private final String proAddressLine3;
 
     @JsonProperty(value = "County")
-    private final String county;
+    private final String proCounty;
 
     @JsonProperty(value = "PostTown")
-    private final String postTown;
+    private final String proPostTown;
 
     @JsonProperty(value = "PostCode")
-    private final String postCode;
+    private final String proPostCode;
 
     @JsonProperty(value = "Country")
-    private final String country;
+    private final String proCountry;
 }
