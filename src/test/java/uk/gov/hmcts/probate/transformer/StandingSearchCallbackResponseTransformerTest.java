39f074f8ccd3fe59f5e1b4aa37b2c87472323461
Suneet Kamath <suneet.kamath@hmcts.net>
2018-12-17 14:51:59 +0000

    PRO-4026: fixes to tests (after rebase) introduced with newer features like elasticsearch

--- src/test/java/uk/gov/hmcts/probate/transformer/StandingSearchCallbackResponseTransformerTest.java
+++ src/test/java/uk/gov/hmcts/probate/transformer/StandingSearchCallbackResponseTransformerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.probate.model.ApplicationType;
 import uk.gov.hmcts.probate.model.DocumentType;
 import uk.gov.hmcts.probate.model.ccd.ProbateAddress;
