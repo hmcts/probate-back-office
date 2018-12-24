f44371711fac79348e7345be881a122b4deed403
sparekh <sanjay.parekh@hmcts.net>
2018-12-24 10:54:32 +0000

    PRO-3704: Cleanup jpa classes

--- src/main/java/uk/gov/hmcts/probate/model/legacy/AdmonWill.java
+++ src/main/java/uk/gov/hmcts/probate/model/legacy/AdmonWill.java
@@ -1,35 +1,49 @@
 package uk.gov.hmcts.probate.model.legacy;
 
+import lombok.Data;
 import org.joda.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Id;
 
+@Data
 public class AdmonWill {
+    @Id
+    private Long id;// bigint
+
     @Column(name = "RK_NUMBER")
     private String rkNumber;// varchar(11)
+
     @Column(name = "PROBATE_NUMBER")
     private String probateNumber;// varchar(11)
+
     @Column(name = "PROBATE_VERSION")
     private Long probateVersion;// int
+
     @Column(name = "DECEASED_ID")
     private Long deceasedId;// bigint,
+
     @Column(name = "DECEASED_FORENAMES")
     private String deceasedForenames;// varchar(50)
+
     @Column(name = "DECEASED_SURNAME")
     private String deceasedSurname;// varchar(30)
+
     @Column(name = "DATE_OF_BIRTH")
     private LocalDate dateOfBirth;// date
+
     @Column(name = "DATE_OF_DEATH1")
     private LocalDate dateOfDeath1;// date
+
     @Column(name = "ALIAS_NAMES")
     private String aliasNames;// varchar(32000)
+
     @Column(name = "RECORD_KEEPERS_TEXT")
     private String recordKeepersText;// varchar(32000)
+
     @Column(name = "CCD_CASE_NO")
     private String ccdCaseNo;// varchar(20)
-    @Column(name = "ID")
-    private Long id;// bigint
+
     @Column(name = "DNM_IND")
     private String dnmInd;// varchar(1)
-
 }
--- src/main/java/uk/gov/hmcts/probate/model/legacy/StandingSearch.java
+++ src/main/java/uk/gov/hmcts/probate/model/legacy/StandingSearch.java
@@ -1,55 +1,82 @@
 package uk.gov.hmcts.probate.model.legacy;
 
+import lombok.Data;
+
 import javax.persistence.Column;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Data
 public class StandingSearch {
+    @Id
+    private Long id; //bigint,
+
     @Column(name = "SS_NUMBER")
-    private String ssNumber; //varchar(11)	,
+    private String ssNumber; //varchar(11),
+
     @Column(name = "PROBATE_NUMBER")
-    private String probateNumber; //varchar(11)	,
+    private String probateNumber; //varchar(11),
+
     @Column(name = "PROBATE_VERSION")
-    private Long probateVersion; //int	,
+    private Long probateVersion; //int,
+
     @Column(name = "DECEASED_ID")
     private Long deceasedId; //bigint,
+
     @Column(name = "DECEASED_FORENAMES")
-    private String deceasedForenames; //varchar(50)	,
+    private String deceasedForenames; //varchar(50),
+
     @Column(name = "DECEASED_SURNAME")
-    private String deceasedSurname; //varchar(50)	,
+    private String deceasedSurname; //varchar(50),
+
     @Column(name = "DATE_OF_BIRTH")
-    private LocalDate dateOfBirth; //date	,
+    private LocalDate dateOfBirth; //date,
+
     @Column(name = "DATE_OF_DEATH1")
-    private LocalDate dateOfDeath1; //date	,
+    private LocalDate dateOfDeath1; //date,
+
     @Column(name = "CCD_CASE_NO")
-    private String ccdCaseNo; //varchar(20)	,
-    @Column(name = "ID")
-    private Long id; //bigint	,
+    private String ccdCaseNo; //varchar(20),
+
     @Column(name = "DECEASED_ADDRESS")
-    private String deceasedAddress; //varchar(500)	,
+    private String deceasedAddress; //varchar(500),
+
     @Column(name = "APPLICANT_ADDRESS")
-    private String applicantAddress; //varchar(500)	,
+    private String applicantAddress; //varchar(500),
+
     @Column(name = "REGISTRY_REG_LOCATION_CODE")
-    private Long registryRegLocationCode; //int	,
+    private Long registryRegLocationCode; //int,
+
     @Column(name = "SS_APPLICANT_FORENAME")
-    private String ssApplicantForename; //varchar(30)	,
+    private String ssApplicantForename; //varchar(30),
+
     @Column(name = "SS_APPLICANT_SURNAME")
-    private String ssApplicantSurname; //varchar(50)	,
+    private String ssApplicantSurname; //varchar(50),
+
     @Column(name = "SS_APPLICANT_HONOURS")
-    private String ssApplicantHonours; //varchar(100)	,
+    private String ssApplicantHonours; //varchar(100),
+
     @Column(name = "SS_APPLICANT_TITLE")
-    private String ssApplicantTitle; //varchar(35)	,
+    private String ssApplicantTitle; //varchar(35),
+
     @Column(name = "SS_DATE_LAST_EXTENDED")
-    private LocalDate ssDateLastExtended; //date	,
+    private LocalDate ssDateLastExtended; //date,
+
     @Column(name = "SS_DATE_OF_ENTRY")
-    private LocalDate ssDateOfEntry; //date	,
+    private LocalDate ssDateOfEntry; //date,
+
     @Column(name = "SS_DATE_OF_EXPIRY")
-    private LocalDate ssDateOfExpiry; //date	,
+    private LocalDate ssDateOfExpiry; //date,
+
     @Column(name = "SS_WITHDRAWN_DATE")
-    private LocalDate ssWithdrawnDate; //date	,
+    private LocalDate ssWithdrawnDate; //date,
+
     @Column(name = "STANDING_SEARCH_TEXT")
-    private String standingSearchText; //varchar(32000)	,
+    private String standingSearchText; //varchar(32000),
+
     @Column(name = "DNM_IND")
-    private String dnmInd; //varchar(1)	,
+    private String dnmInd; //varchar(1),
+
     @Column(name = "ALIAS_NAMES")
     private String aliasNames; //varchar(32000)
 
