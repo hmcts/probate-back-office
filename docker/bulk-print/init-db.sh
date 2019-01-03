8425e90a353d39a9a5e6797d59201ca6dab98080
Ikenna-hmcts <ikenna.ibeanu@hmcts.net>
2019-01-03 10:25:30 +0000

    Added bulk print service container to docker-compose

---
+++ docker/bulk-print/init-db.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Claim Store database
+if [ -z "$LETTER_TRACKING_DB_PASSWORD" ]; then
+  echo "ERROR: Missing environment variables. Set value for 'LETTER_TRACKING_DB_PASSWORD'."
+  exit 1
+fi
+
+psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=letterservice --set PASSWORD=${LETTER_TRACKING_DB_PASSWORD} <<-EOSQL
+  CREATE USER :USERNAME WITH PASSWORD ':PASSWORD';
+  CREATE ROLE :USERNAME WITH PASSWORD ':PASSWORD';
+
+  CREATE DATABASE letter_tracking
+    WITH OWNER = :USERNAME
+    ENCODING = 'UTF-8'
+    CONNECTION LIMIT = -1;
+EOSQL
