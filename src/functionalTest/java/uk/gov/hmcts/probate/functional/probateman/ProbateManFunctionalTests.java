5849fbf4ec0cbbfaa321921f30b250520632018d
Dhruv Patel <dhruv.patel20@gmail.com>
2019-01-25 16:24:14 +0000

    PRO-3704: Functional tests & Removed Auto Config (#245)
    * PRO-3704: Updated functional tests and ensured auto config disabled for CCD api.
    
    * corrected kuuvernaties

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/probateman/ProbateManFunctionalTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/probateman/ProbateManFunctionalTests.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,8 +19,6 @@ import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.probate.functional.IntegrationTestBase;
 
 import java.util.Arrays;
-
-import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SerenityRunner.class)
 public class ProbateManFunctionalTests extends IntegrationTestBase {
@@ -29,15 +28,11 @@ public class ProbateManFunctionalTests extends IntegrationTestBase {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private static final String EMAIL_PLACEHOLDER = "XXXXXXXXXX";
     private static final String PROBATEMAN_DB_PASS = "Probate123";
-    private static final String USER_GROUP_NAME = "caseworker-probate-issuer";
 
     private ObjectMapper objectMapper;
 
     private String email;
-
-    private Long caseId;
 
     private Headers headers;
 
@@ -72,66 +67,63 @@ public class ProbateManFunctionalTests extends IntegrationTestBase {
 
     @Test
     @Sql(scripts = "/scripts/grant_application_insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    //@Sql(scripts = "/scripts/grant_application_clean_up.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(scripts = "/scripts/grant_application_clean_up.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void shouldCreateCcdCaseForGrantApplication() {
-        SerenityRest.given()
+        String actualJson = SerenityRest.given()
             .relaxedHTTPSValidation()
             .headers(headers)
             .when()
-            .post("/probateManTypes/GRANT_APPLICATION/cases/999")
+            .get("/probateManTypes/GRANT_APPLICATION/cases/999")
             .then()
             .assertThat()
             .statusCode(200)
-            .body("case_data.deceasedDateOfDeath", equalTo("2018-01-01"))
-            .body("case_data.primaryApplicantForenames", equalTo("AppFN1 AppFN2"))
-            .body("case_data.primaryApplicantSurname", equalTo("AppSN"))
-            .body("case_data.deceasedDateOfBirth", equalTo("1900-01-01"))
-            .body("case_data.deceasedForenames", equalTo("DeadFN1 DeadFN2"))
-            .body("case_data.deceasedSurname", equalTo("DeadSN"));
+            .extract().body().asString();
+        JSONAssert.assertEquals(utils.getJsonFromFile("/probateman/grantApplicant.json"), actualJson, true);
     }
 
-    //TODO Test with spreadsheet and assert individual fields
     @Test
     @Sql(scripts = "/scripts/caveat_insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/caveat_clean_up.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void shouldCreateCcdCaseForCaveat() {
-        SerenityRest.given()
+        String actualJson = SerenityRest.given()
             .relaxedHTTPSValidation()
             .headers(headers)
             .when()
-            .post("/probateManTypes/CAVEAT/cases/999")
+            .get("/probateManTypes/CAVEAT/cases/999")
             .then()
             .assertThat()
-            .statusCode(200).extract().jsonPath().prettyPrint();
+            .statusCode(200)
+            .extract().body().asString();
+        JSONAssert.assertEquals(utils.getJsonFromFile("/probateman/caveat.json"), actualJson, true);
     }
 
-    //TODO Test with spreadsheet and assert individual fields
     @Test
     @Sql(scripts = "/scripts/standing_search_insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/standing_search_clean_up.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void shouldCreateCcdCaseForStandingSearch() {
-        SerenityRest.given()
+        String actualJson = SerenityRest.given()
             .relaxedHTTPSValidation()
             .headers(headers)
             .when()
-            .post("/probateManTypes/STANDING_SEARCH/cases/999")
+            .get("/probateManTypes/STANDING_SEARCH/cases/999")
             .then()
             .assertThat()
             .statusCode(200).extract().jsonPath().prettyPrint();
+        JSONAssert.assertEquals(utils.getJsonFromFile("/probateman/standingSearch.json"), actualJson, true);
     }
 
-    //TODO Test with spreadsheet and assert individual fields
     @Test
     @Sql(scripts = "/scripts/wills_insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/wills_clean_up.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void shouldCreateCcdCaseForWillLodgement() {
-        SerenityRest.given()
+        String actualJson = SerenityRest.given()
             .relaxedHTTPSValidation()
             .headers(headers)
             .when()
-            .post("/probateManTypes/WILL_LODGEMENT/cases/999")
+            .get("/probateManTypes/WILL_LODGEMENT/cases/999")
             .then()
             .assertThat()
-            .statusCode(200).extract().jsonPath().prettyPrint();
+            .statusCode(200).extract().body().asString();
+        JSONAssert.assertEquals(utils.getJsonFromFile("/probateman/willLodgement.json"), actualJson, true);
     }
 }
--- src/main/java/uk/gov/hmcts/probate/BusinessRulesValidationApplication.java
+++ src/main/java/uk/gov/hmcts/probate/BusinessRulesValidationApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataClientAutoConfiguration;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
-@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class})
+@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class, CoreCaseDataClientAutoConfiguration.class})
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform", "uk.gov.hmcts.reform.sendletter"})
 public class BusinessRulesValidationApplication {
 
