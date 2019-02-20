7e6bb86d0f6bc300b1bf41ba0a468d679ee89578
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2019-02-20 10:23:43 +0000

    PRO-4749: corrected helper script for local import all ss

--- ccdImports/conversionScripts/importAllXLS.sh
+++ ccdImports/conversionScripts/importAllXLS.sh
@@ -9,10 +9,10 @@ echo binFolder = $binFolder
 echo xlsToJsonFolder = $xlsToJsonFolder
 
 ${binFolder}/ccd-add-all-roles.sh
-${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder)/CCD_Probate_Backoffice.xlsx"
-${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder)/CCD_Probate_Caveat.xlsx"
-${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder)/CCD_Probate_LegacyCases.xlsx"
-${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder)/CCD_Probate_LegacySearch.xlsx"
-${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder)/CCD_Probate_StandingSearch.xlsx"
-${binFolder}ccd-import-definition.sh "${xlsToJsonFolder)/xlsToJson/CCD_Probate_WillLodgement.xlsx"
+${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder}/CCD_Probate_Backoffice.xlsx"
+${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder}/CCD_Probate_Caveat.xlsx"
+${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder}/CCD_Probate_Legacy_Cases.xlsx"
+${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder}/CCD_Probate_Legacy_Search.xlsx"
+${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder}/CCD_Probate_Standing_Search.xlsx"
+${binFolder}/ccd-import-definition.sh "${xlsToJsonFolder}/CCD_Probate_Will_Lodgement.xlsx"
 
