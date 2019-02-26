a49d079cdd70f966b98e81a548b7e9ba2b0b3e4a
Doug Rice <Douglas.Rice@hmcts.net>
2019-02-26 14:40:16 +0000

    PRO-4681: Refactoring

--- src/test/config.js
+++ src/test/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    TestE2EFrontendUrl: process.env.TEST_E2E_URL || 'http://localhost:9000',
+    TestE2EFrontendUrl: process.env.TEST_E2E_URL || 'localhost:3000',
     TestRetryFeatures: process.env.RETRY_FEATURES || 0,
     TestRetryScenarios: process.env.RETRY_SCENARIOS || 3,
     TestDocumentToUpload: 'uploadDocuments/test_file_for_document_upload.png',
     TestTimeToWaitForText: 60,
-    TestEnvUser: process.env.BO_TEST_EMAIL_ADDRESS || 'dummy',
-    TestEnvPassword: process.env.BO_TEST_PASSWORD || 'dummy'
+    TestEnvUser: process.env.CW_USER_EMAIL || 'dummy',
+    TestEnvPassword: process.env.CW_USER_PASSWORD || 'dummy'
 };
