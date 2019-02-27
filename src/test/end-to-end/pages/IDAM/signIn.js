0b8315ab1cebd7283dc251e49d7f991c4da6421a
Doug Rice <Douglas.Rice@hmcts.net>
2019-02-27 12:14:10 +0000

    PRO-4681: Refactoring

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -9,11 +9,8 @@ module.exports = function () {
     I.amOnPage('/');
     I.see('Sign in');
 
-    // I.fillField('username', testConfig.TestEnvUser);
-    // I.fillField('password', testConfig.TestEnvPassword);
-
-    I.fillField('username', 'douglasrice1969+cw@gmail.com');
-    I.fillField('password', 'Probate123');
+    I.fillField('username', testConfig.TestEnvUser);
+    I.fillField('password', testConfig.TestEnvPassword);
 
     I.waitForNavigationToComplete('input[value="Sign in"]');
 };
