41a6f71a63a6229bc1d263bbbaacfc053679fe95
RBaker <rebecca.baker@hmcts.net>
2019-03-14 08:30:09 +0000

    PRO-4941 updated to return an empty list

--- src/main/java/uk/gov/hmcts/probate/service/probateman/mapper/SolsAliasNameMapper.java
+++ src/main/java/uk/gov/hmcts/probate/service/probateman/mapper/SolsAliasNameMapper.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.reform.probate.model.cases.CollectionMember;
 import uk.gov.hmcts.reform.probate.model.cases.SolsAliasName;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -17,10 +18,10 @@ public class SolsAliasNameMapper {
     public List<CollectionMember<SolsAliasName>> toCollectionMember(String aliasNames) {
         log.info("Adding aliasNames to collection for legacy case mapping");
         if (aliasNames == null) {
-            return null;
+            Collections.emptyList();
         }
         List<CollectionMember<SolsAliasName>> collectionMemberArrayList = new ArrayList<>();
-        SolsAliasName aliasName = SolsAliasName.builder().build().builder()
+        SolsAliasName aliasName = SolsAliasName.builder()
                 .solsAliasname(aliasNames)
                 .build();
         CollectionMember<SolsAliasName> collectionMember = new CollectionMember(null, aliasName);
