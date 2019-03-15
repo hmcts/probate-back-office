e1eccc2185cad3ce6c3c5703d27e5c8c2179f3e1
Kurt Plaschke <k.plaschke@hull.ac.uk>
2019-03-15 13:14:58 +0000

    PRO-4668: created endpoints for data extract for iron mountain and excela
    Created 2 endpoints for Iron mountain, one that takes in a date as a path param which is then used to search for cases against, currently the story to provide functionality for this is yet to be played so the return of this is simply the cases found - to soon be file generated and sent via FTP, the second IM endpoint takes no path param and instead is the default which will generate for yesterdays date, this is duplicated for excela too where the cases returned are filtered for scanned doc and then emailed to excela. Also created new request model ReturnedCaseDetails that can fit the serialization of case details from ES query

--- src/main/java/uk/gov/hmcts/probate/config/SwaggerConfig.java
+++ src/main/java/uk/gov/hmcts/probate/config/SwaggerConfig.java
@@ -9,11 +9,14 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Collections.singletonList;
 
@@ -24,16 +27,20 @@ public class SwaggerConfig {
     @Bean
     public Docket validationApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .globalOperationParameters(singletonList(parameterBuilder()))
-            .directModelSubstitute(LocalDate.class, java.sql.Date.class)
-            .apiInfo(apiInfo())
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("uk.gov.hmcts.probate.controller"))
-            .paths(PathSelectors.any())
-            .build();
+                .globalOperationParameters(Arrays.asList(serviceAuthorizationParameterBuilder(),
+                        authorizationParameterBuilder()))
+                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+                .apiInfo(apiInfo())
+                .tags(new Tag("tag1", "Tag 1 description."),
+                        new Tag("tag2", "Tag 2 description."),
+                        new Tag("tag2", "Tag 3 description."))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("uk.gov.hmcts.probate.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
-    private Parameter parameterBuilder() {
+    private Parameter serviceAuthorizationParameterBuilder() {
         return new ParameterBuilder()
                 .name("ServiceAuthorization")
                 .description("User authorization header")
@@ -43,12 +50,22 @@ public class SwaggerConfig {
                 .build();
     }
 
+    private Parameter authorizationParameterBuilder() {
+        return new ParameterBuilder()
+                .name("Authorization")
+                .description("Authorization header")
+                .required(true)
+                .parameterType("header")
+                .modelRef(new ModelRef("string"))
+                .build();
+    }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("Solicitor CCD service")
-            .description("Provides data validation and other services")
-            .license("MIT License")
-            .version("1.0")
-            .build();
+                .title("Solicitor CCD service")
+                .description("Provides data validation and other services")
+                .license("MIT License")
+                .version("1.0")
+                .build();
     }
 }
