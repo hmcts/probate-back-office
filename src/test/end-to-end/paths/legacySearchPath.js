5c37fe64706fdf91b4ac1bbcf10dfe3d2cf2055b
Muhammad Husain <muhammad.husain@hmcts.net>
2019-04-01 15:03:36 +0100

    e2e test for Legacy search

--- src/test/end-to-end/paths/legacySearchPath.js
+++ src/test/end-to-end/paths/legacySearchPath.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const testConfig = require('src/test/config');
+const createCaseConfig = require('src/test/end-to-end/pages/createCase/createCaseConfig.json');
 const filterCaseConfig = require('src/test/end-to-end/pages/filterCase/filterCaseConfig');
 
 Feature('Back Office').retry(testConfig.TestRetryFeatures);
@@ -11,8 +12,7 @@ Scenario('Legacy search', async function (I) {
     I.authenticateWithIdamIfAvailable();
     I.filterCase(filterCaseConfig.list1_text, filterCaseConfig.list2_text, filterCaseConfig.list3_text);
     I.selectCase();
-    // TODO: Uncomment the following once we have a solution for data setup
-//    I.legacyCaseSearch();
-//    I.legacyCaseSearch2();
-//    I.legacyCaseSearch3();
+    I.legacyCaseSearch();
+    I.legacyCaseSearch2();
+    I.legacyCaseSearch3();
 }).retry(testConfig.TestRetryScenarios);
