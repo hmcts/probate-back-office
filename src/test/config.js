4d0f68973d8fba23d534032370779e2bb0e639a2
Jack Munkonge <jackmunkonge@gmail.com>
2019-04-02 15:57:41 +0100

    PRO-4611: Fixed config

--- src/test/config.js
+++ src/test/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    TestFrontendUrl: process.env.TEST_E2E_URL || 'https://www-ccd.aat.platform.hmcts.net',
+    TestFrontendUrl: process.env.TEST_E2E_URL || 'localhost:3000',
     TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
     TestRetryFeatures: process.env.RETRY_FEATURES || 0,
     TestRetryScenarios: process.env.RETRY_SCENARIOS || 3,
-    TestPathToRun: './paths/grantOfProbatePath.js',
+    TestPathToRun: './paths/**/*.js',
     TestOutputDir: process.env.E2E_OUTPUT_DIR || './output',
     TestDocumentToUpload: 'uploadDocuments/test_file_for_document_upload.png',
     TestTimeToWaitForText: 60,
-    TestEnvUser: process.env.CW_USER_EMAIL || 'probatebackoffice@gmail.com',
-    TestEnvPassword: process.env.CW_USER_PASSWORD || 'Monday01'
+    TestEnvUser: process.env.CW_USER_EMAIL || 'dummy',
+    TestEnvPassword: process.env.CW_USER_PASSWORD || 'dummy'
 };
