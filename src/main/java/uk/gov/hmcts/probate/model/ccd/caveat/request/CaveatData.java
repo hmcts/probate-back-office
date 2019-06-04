467dd8d9143c7e927af25fbb00b47b6b745d1252
Rabab Rabab <rabab.rabab@hmcts.net>
2019-06-04 10:51:33 +0100

    added and fixed failing tests

--- src/main/java/uk/gov/hmcts/probate/model/ccd/caveat/request/CaveatData.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/caveat/request/CaveatData.java
@@ -151,6 +151,7 @@ public class CaveatData {
             fullAddress += address.getProAddressLine1().isEmpty() ? "" : address.getProAddressLine1() ;
             fullAddress += address.getProAddressLine2().isEmpty() ? "" : ", " + address.getProAddressLine2();
             fullAddress += address.getProAddressLine3().isEmpty() ? "" : ", " + address.getProAddressLine3();
+            fullAddress += address.getProPostTown().isEmpty() ? "" : ", " + address.getProPostTown();
             fullAddress += address.getProCounty().isEmpty() ? "" : ", " + address.getProCounty();
             fullAddress += address.getProPostCode().isEmpty() ? "" : ", " + address.getProPostCode();
             fullAddress += address.getProCountry().isEmpty() ? "" : ", " + address.getProCountry();
--- src/test/java/uk/gov/hmcts/probate/model/ccd/caveat/request/CaveatDataTest.java
+++ src/test/java/uk/gov/hmcts/probate/model/ccd/caveat/request/CaveatDataTest.java
@@ -3,6 +3,9 @@ package uk.gov.hmcts.probate.model.ccd.caveat.request;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import uk.gov.hmcts.probate.model.ccd.ProbateAddress;
+
+import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +16,14 @@ public class CaveatDataTest {
     private static final String CAV_CAVEATOR_FORENAMES = "fName";
     private static final String CAV_CAVEATOR_SURNAME = "sName";
     private static final String CAV_CAVEATOR_EMAIL = "caveator@test.com";
+    private static final String CAV_CAVEATOR_ADDRESS_1 = "15 Hanover Lane";
+    private static final String CAV_CAVEATOR_ADDRESS_2 = "Catford";
+    private static final String CAV_CAVEATOR_ADDRESS_3 = "";
+    private static final String CAV_CAVEATOR_TOWN = "London";
+    private static final String CAV_CAVEATOR_COUNTY = "";
+    private static final String CAV_CAVEATOR_POSTCODE = "1AB 1CD";
+    private static final String CAV_CAVEATOR_COUNTRY = "";
+    private static final LocalDate LOCAL_DATE = LocalDate.of(2000,01,01);
 
     @InjectMocks
     private CaveatData underTest;
@@ -56,4 +67,129 @@ public class CaveatDataTest {
 
         assertEquals("Yes", caveatData.getDefaultValueForEmailNotifications());
     }
+
+    // ...
+
+    @Test
+    public void shouldReturnSubmissionDateFormattedWithST() {
+        final CaveatData caveatData = CaveatData.builder()
+                .applicationSubmittedDate(LOCAL_DATE)
+                .build();
+
+        assertEquals("1st January 2000", caveatData.getApplicationSubmittedDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnSubmissionDateFormattedWithND() {
+        final CaveatData caveatData = CaveatData.builder()
+                .applicationSubmittedDate(LocalDate.of(2000,01,02))
+                .build();
+
+        assertEquals("2nd January 2000", caveatData.getApplicationSubmittedDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnSubmissionDateFormattedWithRD() {
+        final CaveatData caveatData = CaveatData.builder()
+                .applicationSubmittedDate(LocalDate.of(2000,01,03))
+                .build();
+
+        assertEquals("3rd January 2000", caveatData.getApplicationSubmittedDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnSubmissionDateFormattedWithTH() {
+        final CaveatData caveatData = CaveatData.builder()
+                .applicationSubmittedDate(LocalDate.of(2000,01,04))
+                .build();
+
+        assertEquals("4th January 2000", caveatData.getApplicationSubmittedDateFormatted());
+    }
+
+    @Test
+    public void shouldThrowParseException() {
+        final CaveatData caveatData = CaveatData.builder()
+                .applicationSubmittedDate(LocalDate.of(300000, 01, 04))
+                .build();
+
+        assertEquals(null, caveatData.getApplicationSubmittedDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnExpiryDateFormattedWithST() {
+        final CaveatData caveatData = CaveatData.builder()
+                .expiryDate(LOCAL_DATE)
+                .build();
+
+        assertEquals("1st January 2000", caveatData.getExpiryDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnExpiryDateFormattedWithND() {
+        final CaveatData caveatData = CaveatData.builder()
+                .expiryDate(LocalDate.of(2000,01,02))
+                .build();
+
+        assertEquals("2nd January 2000", caveatData.getExpiryDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnExpiryDateFormattedWithRD() {
+        final CaveatData caveatData = CaveatData.builder()
+                .expiryDate(LocalDate.of(2000,01,03))
+                .build();
+
+        assertEquals("3rd January 2000", caveatData.getExpiryDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnExpiryDateFormattedWithTH() {
+        final CaveatData caveatData = CaveatData.builder()
+                .expiryDate(LocalDate.of(2000,01,04))
+                .build();
+
+        assertEquals("4th January 2000", caveatData.getExpiryDateFormatted());
+    }
+
+    @Test
+    public void shouldReturnCaveatorFullAddressLongForCaveat() {
+
+        ProbateAddress caveatorAddress = new ProbateAddress();
+
+        caveatorAddress.setProAddressLine1(CAV_CAVEATOR_ADDRESS_1);
+        caveatorAddress.setProAddressLine2(CAV_CAVEATOR_ADDRESS_2);
+        caveatorAddress.setProAddressLine3(CAV_CAVEATOR_ADDRESS_3);
+        caveatorAddress.setProPostTown(CAV_CAVEATOR_TOWN);
+        caveatorAddress.setProCounty(CAV_CAVEATOR_COUNTY);
+        caveatorAddress.setProPostCode(CAV_CAVEATOR_POSTCODE);
+        caveatorAddress.setProCountry(CAV_CAVEATOR_COUNTRY);
+
+        final CaveatData caveatData = CaveatData.builder()
+                .caveatorAddress(caveatorAddress)
+                .build();
+
+        assertEquals(CAV_CAVEATOR_ADDRESS_1 + ", " + CAV_CAVEATOR_ADDRESS_2 + ", "
+                        + CAV_CAVEATOR_TOWN + ", " + CAV_CAVEATOR_POSTCODE, caveatData.getCaveatorAddressFormatted());
+    }
+
+    @Test
+    public void shouldReturnCaveatorFullAddressShortForCaveat() {
+
+        ProbateAddress caveatorAddress = new ProbateAddress();
+
+        caveatorAddress.setProAddressLine1(CAV_CAVEATOR_ADDRESS_1);
+        caveatorAddress.setProAddressLine2("");
+        caveatorAddress.setProAddressLine3("");
+        caveatorAddress.setProPostTown("");
+        caveatorAddress.setProCounty("");
+        caveatorAddress.setProPostCode(CAV_CAVEATOR_POSTCODE);
+        caveatorAddress.setProCountry("");
+
+        final CaveatData caveatData = CaveatData.builder()
+                .caveatorAddress(caveatorAddress)
+                .build();
+
+        assertEquals(CAV_CAVEATOR_ADDRESS_1 + ", " + CAV_CAVEATOR_POSTCODE, caveatData.getCaveatorAddressFormatted());
+    }
+
 }
