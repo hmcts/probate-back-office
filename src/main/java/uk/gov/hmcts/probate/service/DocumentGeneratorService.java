065dd4e483a10ef278d377aefaf85b54a64c1326
Kurt Plaschke <k.plaschke@hull.ac.uk>
2019-06-28 11:43:39 +0100

    PRO-5609 added letter of admin draft with docmosis
    Added functionality for intestacy grants to generate documets with docmosis setup. addded template reference in yml and added intestacy reissue draft to document types

--- src/main/java/uk/gov/hmcts/probate/service/DocumentGeneratorService.java
+++ src/main/java/uk/gov/hmcts/probate/service/DocumentGeneratorService.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.probate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.probate.config.properties.registries.RegistriesProperties;
-import uk.gov.hmcts.probate.config.properties.registries.Registry;
 import uk.gov.hmcts.probate.model.DocumentType;
 import uk.gov.hmcts.probate.model.ccd.raw.Document;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
@@ -13,20 +11,14 @@ import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.hmcts.probate.service.docmosis.GenericMapperService;
 import uk.gov.hmcts.probate.service.template.pdf.PDFManagementService;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static uk.gov.hmcts.probate.model.Constants.CTSC;
 import static uk.gov.hmcts.probate.model.DocumentType.ADMON_WILL_GRANT_DRAFT;
 import static uk.gov.hmcts.probate.model.DocumentType.DIGITAL_GRANT_DRAFT;
 import static uk.gov.hmcts.probate.model.DocumentType.DIGITAL_GRANT_DRAFT_REISSUE;
 import static uk.gov.hmcts.probate.model.DocumentType.INTESTACY_GRANT_DRAFT;
+import static uk.gov.hmcts.probate.model.DocumentType.INTESTACY_GRANT_DRAFT_REISSUE;
 
 @Slf4j
 @Service
@@ -47,17 +39,23 @@ public class DocumentGeneratorService {
     private static final String CREST_FILE_PATH = "crestImage.txt";
     private static final String SEAL_FILE_PATH = "sealImage.txt";
 
+    private Map<String, Object> images = new HashMap<>();
+
     public Document generateGrantReissueDraft(CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
-        CaseData caseData = caseDetails.getData();
         Document document;
         DocumentType template;
         registryDetailsService.getRegistryDetails(caseDetails);
 
+        images.put(CREST_IMAGE, CREST_FILE_PATH);
+        images.put(SEAL_IMAGE, SEAL_FILE_PATH);
+        Map<String, Object> placeholders = genericMapperService.caseDataWithImages(images, caseDetails);
+        CaseData caseData = caseDetails.getData();
+
         switch (caseData.getCaseType()) {
             case INTESTACY:
-                template = INTESTACY_GRANT_DRAFT;
-                document = pdfManagementService.generateAndUpload(callbackRequest, template);
+                template = INTESTACY_GRANT_DRAFT_REISSUE;
+                document = pdfManagementService.generateDocmosisDocumentAndUpload(placeholders, template, true);
                 log.info("Generated and Uploaded Intestacy grant preview document with template {} for the case id {}",
                         template.getTemplateName(), callbackRequest.getCaseDetails().getId().toString());
                 break;
@@ -73,10 +71,6 @@ public class DocumentGeneratorService {
             case GRANT_OF_PROBATE:
             default:
                 template = DIGITAL_GRANT_DRAFT_REISSUE;
-                Map<String, Object> images = new HashMap<>();
-                images.put(CREST_IMAGE, CREST_FILE_PATH);
-                images.put(SEAL_IMAGE, SEAL_FILE_PATH);
-                Map<String, Object> placeholders = genericMapperService.caseDataWithImages(images, caseDetails);
                 document = pdfManagementService.generateDocmosisDocumentAndUpload(placeholders, template, true);
                 log.info("Generated and Uploaded Grant of Probate preview document with template {} for the case id {}",
                         template.getTemplateName(), callbackRequest.getCaseDetails().getId().toString());
@@ -89,7 +83,8 @@ public class DocumentGeneratorService {
     }
 
     private void expireDrafts(CallbackRequest callbackRequest) {
-        DocumentType[] documentTypes = {DIGITAL_GRANT_DRAFT, INTESTACY_GRANT_DRAFT, ADMON_WILL_GRANT_DRAFT, DIGITAL_GRANT_DRAFT_REISSUE};
+        DocumentType[] documentTypes = {DIGITAL_GRANT_DRAFT, INTESTACY_GRANT_DRAFT, ADMON_WILL_GRANT_DRAFT,
+                                        DIGITAL_GRANT_DRAFT_REISSUE, INTESTACY_GRANT_DRAFT_REISSUE};
         for (DocumentType documentType : documentTypes) {
             documentService.expire(callbackRequest, documentType);
         }
