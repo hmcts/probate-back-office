90c9837f8fb9fbf0e9ba89be174bbb3929dd4875
RBaker <rebecca.baker@hmcts.net>
2019-07-09 15:46:30 +0100

    PRO-5136 PRO-5114 fixed checkstyle error

--- src/main/java/uk/gov/hmcts/probate/exception/BusinessValidationException.java
+++ src/main/java/uk/gov/hmcts/probate/exception/BusinessValidationException.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 public class BusinessValidationException extends RuntimeException {
 
     private String userMessage;
+
     public BusinessValidationException(final String userMessage, final String message) {
         super(message);
         this.userMessage = userMessage;
