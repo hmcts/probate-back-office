5dbadea93aa43cee8d4b980b03b4dc32ad15fc49
RBaker <rebecca.baker@hmcts.net>
2019-08-21 16:54:51 +0100

    refactored notificationservice class and added relevant unit tests

--- src/main/java/uk/gov/hmcts/probate/service/NotificationService.java
+++ src/main/java/uk/gov/hmcts/probate/service/NotificationService.java
@@ -11,38 +11,35 @@ import uk.gov.hmcts.probate.config.properties.registries.Registry;
 import uk.gov.hmcts.probate.exception.BadRequestException;
 import uk.gov.hmcts.probate.exception.InvalidEmailException;
 import uk.gov.hmcts.probate.model.ApplicationType;
-import uk.gov.hmcts.probate.model.CaseType;
 import uk.gov.hmcts.probate.model.DocumentType;
 import uk.gov.hmcts.probate.model.ExecutorsApplyingNotification;
 import uk.gov.hmcts.probate.model.SentEmail;
 import uk.gov.hmcts.probate.model.State;
 import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatData;
 import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatDetails;
-import uk.gov.hmcts.probate.model.ccd.raw.CollectionMember;
 import uk.gov.hmcts.probate.model.ccd.raw.Document;
-import uk.gov.hmcts.probate.model.ccd.raw.ScannedDocument;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.hmcts.probate.model.ccd.raw.request.ReturnedCaseDetails;
 import uk.gov.hmcts.probate.model.ccd.raw.response.CallbackResponse;
+import uk.gov.hmcts.probate.service.notification.CaveatPersonalisationService;
+import uk.gov.hmcts.probate.service.notification.GrantOfRepresentationPersonalisationService;
+import uk.gov.hmcts.probate.service.notification.SentEmailPersonalisationService;
+import uk.gov.hmcts.probate.service.notification.TemplateService;
 import uk.gov.hmcts.probate.service.template.pdf.PDFManagementService;
 import uk.gov.hmcts.probate.validator.EmailAddressNotificationValidationRule;
 import uk.gov.service.notify.NotificationClient;
 import uk.gov.service.notify.NotificationClientException;
 import uk.gov.service.notify.SendEmailResponse;
 
-import java.time.LocalDate;
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.validation.Valid;
 
 import static uk.gov.hmcts.probate.model.Constants.BUSINESS_ERROR;
-import static uk.gov.hmcts.probate.model.Constants.CTSC;
-import static uk.gov.hmcts.probate.model.Constants.DOC_SUBTYPE_WILL;
 import static uk.gov.hmcts.probate.model.DocumentType.SENT_EMAIL;
 import static uk.gov.hmcts.probate.model.State.GRANT_REISSUED;
 
@@ -52,46 +49,25 @@ import static uk.gov.hmcts.probate.model.State.GRANT_REISSUED;
 public class NotificationService {
     @Autowired
     private final EmailAddresses emailAddresses;
+    @Autowired
+    private BusinessValidationMessageService businessValidationMessageService;
     private final NotificationTemplates notificationTemplates;
     private final RegistriesProperties registriesProperties;
     private final NotificationClient notificationClient;
     private final MarkdownTransformationService markdownTransformationService;
     private final PDFManagementService pdfManagementService;
-    private final CaveatQueryService caveatQueryService;
     private final EventValidationService eventValidationService;
     private final List<EmailAddressNotificationValidationRule> emailAddressNotificationValidationRules;
-    @Autowired
-    private BusinessValidationMessageService businessValidationMessageService;
-    private final DateFormatterService dateFormatterService;
-    private final AddressFormatterService addressFormatterService;
+    private final GrantOfRepresentationPersonalisationService grantOfRepresentationPersonalisationService;
+    private final CaveatPersonalisationService caveatPersonalisationService;
+    private final SentEmailPersonalisationService sentEmailPersonalisationService;
+    private final TemplateService templateService;
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM Y HH:mm");
     private static final DateTimeFormatter EXCELA_DATE = DateTimeFormatter.ofPattern("yyyyMMdd");
-    private static final DateTimeFormatter EXCELA_CONTENT_DATE = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     private static final String PERSONALISATION_APPLICANT_NAME = "applicant_name";
-    private static final String PERSONALISATION_DECEASED_NAME = "deceased_name";
-    private static final String PERSONALISATION_SOLICITOR_NAME = "solicitor_name";
-    private static final String PERSONALISATION_SOLICITOR_REFERENCE = "solicitor_reference";
-    private static final String PERSONALISATION_REGISTRY_NAME = "registry_name";
-    private static final String PERSONALISATION_REGISTRY_PHONE = "registry_phone";
-    private static final String PERSONALISATION_CASE_STOP_DETAILS = "case-stop-details";
-    private static final String PERSONALISATION_CASE_STOP_DETAILS_DEC = "boStopDetailsDeclarationParagraph";
-    private static final String PERSONALISATION_CAVEAT_CASE_ID = "caveat_case_id";
-    private static final String PERSONALISATION_DECEASED_DOD = "deceased_dod";
-    private static final String PERSONALISATION_CCD_REFERENCE = "ccd_reference";
-    private static final String PERSONALISATION_CAVEAT_EXPIRY_DATE = "caveat_expiry_date";
-    private static final String PERSONALISATION_MESSAGE_CONTENT = "message_content";
-    private static final String PERSONALISATION_EXCELA_NAME = "excelaName";
-    private static final String PERSONALISATION_CASE_DATA = "caseData";
-    private static final String PERSONALISATION_DATE_CAVEAT_ENTERED = "date_caveat_entered";
-    private static final String PERSONALISATION_CAVEATOR_NAME = "caveator_name";
-    private static final String PERSONALISATION_CAVEATOR_ADDRESS = "caveator_address";
-    private static final String PERSONALISATION_SENT_EMAIL_BODY = "body";
-    private static final String PERSONALISATION_SENT_EMAIL_TO = "to";
-    private static final String PERSONALISATION_SENT_EMAIL_FROM = "from";
-    private static final String PERSONALISATION_SENT_EMAIL_SUBJECT = "subject";
-    private static final String PERSONALISATION_SENT_EMAIL_SENT_ON = "sentOn";
+
 
     public Document sendEmail(State state, CaseDetails caseDetails)
             throws NotificationClientException {
@@ -99,14 +75,14 @@ public class NotificationService {
         CaseData caseData = caseDetails.getData();
         Registry registry = registriesProperties.getRegistries().get(caseData.getRegistryLocation().toLowerCase());
 
-        String templateId = getTemplateId(state, caseData.getApplicationType(), caseData.getRegistryLocation());
+        String templateId = templateService.getTemplateId(state, caseData.getApplicationType(), caseData.getRegistryLocation());
         String emailReplyToId = registry.getEmailReplyToId();
         String emailAddress = getEmail(caseData);
-        Map<String, String> personalisation = getPersonalisation(caseDetails, registry);
+        Map<String, String> personalisation = grantOfRepresentationPersonalisationService.getPersonalisation(caseDetails, registry);
         String reference = caseData.getSolsSolicitorAppReference();
 
         if (state == state.CASE_STOPPED_CAVEAT) {
-            personalisation = getCaveatStopPersonalisation(personalisation, caseData);
+            personalisation = caveatPersonalisationService.getCaveatStopPersonalisation(personalisation, caseData);
         }
 
         if (caseData.getApplicationType().equals(ApplicationType.SOLICITOR)) {
@@ -124,9 +100,9 @@ public class NotificationService {
         CaseData caseData = caseDetails.getData();
         Registry registry = registriesProperties.getRegistries().get(caseData.getRegistryLocation().toLowerCase());
 
-        String templateId = getTemplateId(state, caseData.getApplicationType(), caseData.getRegistryLocation());
+        String templateId = templateService.getTemplateId(state, caseData.getApplicationType(), caseData.getRegistryLocation());
         String emailAddress = executor.getEmail();
-        Map<String, String> personalisation = getPersonalisation(caseDetails, registry);
+        Map<String, String> personalisation = grantOfRepresentationPersonalisationService.getPersonalisation(caseDetails, registry);
         String reference = caseData.getSolsSolicitorAppReference();
         String emailReplyToId = registry.getEmailReplyToId();
 
@@ -144,9 +120,9 @@ public class NotificationService {
         CaveatData caveatData = caveatDetails.getData();
         Registry registry = registriesProperties.getRegistries().get(caveatData.getRegistryLocation().toLowerCase());
 
-        String templateId = getTemplateId(state, caveatData.getApplicationType(), caveatData.getRegistryLocation());
+        String templateId = templateService.getTemplateId(state, caveatData.getApplicationType(), caveatData.getRegistryLocation());
         String emailAddress = caveatData.getCaveatorEmailAddress();
-        Map<String, String> personalisation = getCaveatPersonalisation(caveatDetails, registry);
+        Map<String, String> personalisation = caveatPersonalisationService.getCaveatPersonalisation(caveatDetails, registry);
         String reference = caveatDetails.getId().toString();
 
         SendEmailResponse response;
@@ -172,7 +148,7 @@ public class NotificationService {
             NotificationClientException {
         String templateId = notificationTemplates.getEmail().get(caseDetails.get(0).getData().getApplicationType())
                 .getExcelaData();
-        Map<String, String> personalisation = getExcelaPersonalisation(caseDetails);
+        Map<String, String> personalisation = grantOfRepresentationPersonalisationService.getExcelaPersonalisation(caseDetails);
         String reference = LocalDateTime.now().format(EXCELA_DATE);
 
         SendEmailResponse response;
@@ -226,7 +202,7 @@ public class NotificationService {
                 .subject(response.getSubject())
                 .body(markdownTransformationService.toHtml(response.getBody()))
                 .build();
-        Map<String, Object> placeholders = getPersonalisation(sentEmail);
+        Map<String, Object> placeholders = sentEmailPersonalisationService.getPersonalisation(sentEmail);
         return pdfManagementService.generateDocmosisDocumentAndUpload(placeholders, docType);
     }
 
@@ -255,109 +231,6 @@ public class NotificationService {
         return response;
     }
 
-    private Map<String, Object> getPersonalisation(SentEmail sentEmail) {
-        HashMap<String, Object> personalisation = new HashMap<>();
-        personalisation.put(PERSONALISATION_SENT_EMAIL_BODY, sentEmail.getBody());
-        personalisation.put(PERSONALISATION_SENT_EMAIL_FROM, sentEmail.getFrom());
-        personalisation.put(PERSONALISATION_SENT_EMAIL_TO, sentEmail.getTo());
-        personalisation.put(PERSONALISATION_SENT_EMAIL_SUBJECT, sentEmail.getSubject());
-        personalisation.put(PERSONALISATION_SENT_EMAIL_SENT_ON, sentEmail.getSentOn());
-
-        return personalisation;
-    }
-
-    private Map<String, String> getPersonalisation(CaseDetails caseDetails, Registry registry) {
-        CaseData caseData = caseDetails.getData();
-        HashMap<String, String> personalisation = new HashMap<>();
-        personalisation.put(PERSONALISATION_APPLICANT_NAME, caseData.getPrimaryApplicantFullName());
-        personalisation.put(PERSONALISATION_DECEASED_NAME, caseData.getDeceasedFullName());
-        personalisation.put(PERSONALISATION_SOLICITOR_NAME, caseData.getSolsSOTName());
-        personalisation.put(PERSONALISATION_SOLICITOR_REFERENCE, caseData.getSolsSolicitorAppReference());
-        personalisation.put(PERSONALISATION_REGISTRY_NAME, registry.getName());
-        personalisation.put(PERSONALISATION_REGISTRY_PHONE, registry.getPhone());
-        personalisation.put(PERSONALISATION_CASE_STOP_DETAILS, caseData.getBoStopDetails());
-        personalisation.put(PERSONALISATION_CAVEAT_CASE_ID, caseData.getBoCaseStopCaveatId());
-        personalisation.put(PERSONALISATION_DECEASED_DOD, caseData.getDeceasedDateOfDeathFormatted());
-        personalisation.put(PERSONALISATION_CCD_REFERENCE, caseDetails.getId().toString());
-        personalisation.put(PERSONALISATION_CASE_STOP_DETAILS_DEC, caseData.getBoStopDetailsDeclarationParagraph());
-
-        return personalisation;
-    }
-
-    private Map<String, String> getCaveatStopPersonalisation(Map<String, String> personalisation, CaseData caseData) {
-
-        CaveatData caveatData = caveatQueryService.findCaveatById(CaseType.CAVEAT, caseData.getBoCaseStopCaveatId());
-
-        if (caveatData != null) {
-            personalisation.put(PERSONALISATION_DATE_CAVEAT_ENTERED,
-                    dateFormatterService.formatDate(caveatData.getApplicationSubmittedDate()));
-            personalisation.put(PERSONALISATION_CAVEATOR_NAME, caveatData.getCaveatorFullName());
-            personalisation.put(PERSONALISATION_CAVEATOR_ADDRESS,
-                    addressFormatterService.formatAddress(caveatData.getCaveatorAddress()));
-            personalisation.put(PERSONALISATION_CAVEAT_EXPIRY_DATE,
-                    dateFormatterService.formatCaveatExpiryDate(caveatData.getExpiryDate()));
-        }
-        if (caseData.getApplicationType().equals(ApplicationType.SOLICITOR)) {
-            personalisation.replace(PERSONALISATION_APPLICANT_NAME, caseData.getSolsSOTName());
-        }
-        return personalisation;
-    }
-
-    private Map<String, String> getCaveatPersonalisation(CaveatDetails caveatDetails, Registry registry) {
-        CaveatData caveatData = caveatDetails.getData();
-
-        HashMap<String, String> personalisation = new HashMap<>();
-
-        personalisation.put(PERSONALISATION_APPLICANT_NAME, caveatData.getCaveatorFullName());
-        personalisation.put(PERSONALISATION_DECEASED_NAME, caveatData.getDeceasedFullName());
-        personalisation.put(PERSONALISATION_CCD_REFERENCE, caveatDetails.getId().toString());
-        personalisation.put(PERSONALISATION_MESSAGE_CONTENT, caveatData.getMessageContent());
-        personalisation.put(PERSONALISATION_REGISTRY_NAME, registry.getName());
-        personalisation.put(PERSONALISATION_REGISTRY_PHONE, registry.getPhone());
-        personalisation.put(PERSONALISATION_CAVEAT_EXPIRY_DATE,
-                dateFormatterService.formatCaveatExpiryDate(caveatData.getExpiryDate()));
-
-        return personalisation;
-    }
-
-    private Map<String, String> getExcelaPersonalisation(List<ReturnedCaseDetails> cases) {
-        HashMap<String, String> personalisation = new HashMap<>();
-
-        StringBuilder data = getBuiltData(cases);
-
-        personalisation.put(PERSONALISATION_EXCELA_NAME, LocalDateTime.now().format(EXCELA_DATE) + "will");
-        personalisation.put(PERSONALISATION_CASE_DATA, data.toString());
-
-        return personalisation;
-    }
-
-    private String getTemplateId(State state, ApplicationType applicationType, String registryLocation) {
-        switch (state) {
-            case DOCUMENTS_RECEIVED:
-                return notificationTemplates.getEmail().get(applicationType).getDocumentReceived();
-            case CASE_STOPPED:
-                return notificationTemplates.getEmail().get(applicationType).getCaseStopped();
-            case CASE_STOPPED_CAVEAT:
-                return notificationTemplates.getEmail().get(applicationType).getCaseStoppedCaveat();
-            case GRANT_ISSUED:
-                return notificationTemplates.getEmail().get(applicationType).getGrantIssued();
-            case GRANT_REISSUED:
-                return notificationTemplates.getEmail().get(applicationType).getGrantReissued();
-            case GENERAL_CAVEAT_MESSAGE:
-                return notificationTemplates.getEmail().get(applicationType).getGeneralCaveatMessage();
-            case CASE_STOPPED_REQUEST_INFORMATION:
-                return notificationTemplates.getEmail().get(applicationType).getRequestInformation();
-            case CAVEAT_RAISED:
-                if (registryLocation.equalsIgnoreCase(CTSC)) {
-                    return notificationTemplates.getEmail().get(applicationType).getCaveatRaisedCtsc();
-                } else {
-                    return notificationTemplates.getEmail().get(applicationType).getCaveatRaised();
-                }
-            default:
-                throw new BadRequestException("Unsupported state");
-        }
-    }
-
     private String getEmail(CaseData caseData) {
         switch (caseData.getApplicationType()) {
             case SOLICITOR:
@@ -369,32 +242,4 @@ public class NotificationService {
         }
     }
 
-    private String getWillReferenceNumber(CaseData data) {
-        for (CollectionMember<ScannedDocument> document : data.getScannedDocuments()) {
-            if (document.getValue().getSubtype() != null && document.getValue().getSubtype().equalsIgnoreCase(DOC_SUBTYPE_WILL)) {
-                return document.getValue().getControlNumber();
-            }
-        }
-        return "";
-    }
-
-    private StringBuilder getBuiltData(List<ReturnedCaseDetails> cases) {
-        StringBuilder data = new StringBuilder();
-
-        for (ReturnedCaseDetails currentCase : cases) {
-            data.append(getWillReferenceNumber(currentCase.getData()));
-            data.append(", ");
-            data.append(currentCase.getData().getDeceasedForenames());
-            data.append(" ");
-            data.append(currentCase.getData().getDeceasedSurname());
-            data.append(", ");
-            data.append(EXCELA_CONTENT_DATE.format(currentCase.getData().getDeceasedDateOfBirth()));
-            data.append(", ");
-            data.append(EXCELA_CONTENT_DATE.format(LocalDate.parse(currentCase.getData().getGrantIssuedDate())));
-            data.append(", ");
-            data.append(currentCase.getId().toString());
-            data.append("\n");
-        }
-        return data;
-    }
 }
---
+++ src/test/java/uk/gov/hmcts/probate/service/notification/CaveatPersonalisationServiceTest.java
@@ -0,0 +1,189 @@
+package uk.gov.hmcts.probate.service.notification;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.probate.config.properties.registries.RegistriesProperties;
+import uk.gov.hmcts.probate.config.properties.registries.Registry;
+import uk.gov.hmcts.probate.insights.AppInsights;
+import uk.gov.hmcts.probate.model.ApplicationType;
+import uk.gov.hmcts.probate.model.CaseType;
+import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatData;
+import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatDetails;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
+import uk.gov.hmcts.probate.service.CaveatQueryService;
+import uk.gov.hmcts.probate.service.DateFormatterService;
+import uk.gov.hmcts.probate.service.template.pdf.PDFManagementService;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.service.notify.SendEmailResponse;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaveatPersonalisationServiceTest {
+
+    @Autowired
+    private CaveatPersonalisationService caveatPersonalisationService;
+
+    @MockBean
+    private PDFManagementService pdfManagementService;
+
+    @MockBean
+    private CoreCaseDataApi coreCaseDataApi;
+
+    @MockBean
+    private CaveatQueryService caveatQueryServiceMock;
+
+    @MockBean
+    private AppInsights appInsights;
+
+    @MockBean
+    private SendEmailResponse sendEmailResponse;
+
+    @Mock
+    private RegistriesProperties registriesPropertiesMock;
+
+    @Mock
+    private DateFormatterService dateFormatterService;
+
+    private CaseDetails caseDetails;
+
+    private CaveatDetails caveatDetails;
+
+    Registry registry = new Registry();
+
+    private static final Long ID = 1L;
+    private static final String[] LAST_MODIFIED = {"2018", "1", "1", "0", "0", "0", "0"};
+
+    private static final String PERSONALISATION_APPLICANT_NAME = "applicant_name";
+    private static final String PERSONALISATION_DECEASED_NAME = "deceased_name";
+    private static final String PERSONALISATION_REGISTRY_NAME = "registry_name";
+    private static final String PERSONALISATION_REGISTRY_PHONE = "registry_phone";
+    private static final String PERSONALISATION_CCD_REFERENCE = "ccd_reference";
+    private static final String PERSONALISATION_CAVEAT_EXPIRY_DATE = "caveat_expiry_date";
+    private static final String PERSONALISATION_MESSAGE_CONTENT = "message_content";
+    private static final String PERSONALISATION_DATE_CAVEAT_ENTERED = "date_caveat_entered";
+    private static final String PERSONALISATION_CAVEATOR_NAME = "caveator_name";
+    private static final String PERSONALISATION_CAVEATOR_ADDRESS = "caveator_address";
+
+    HashMap<String, String> personalisation = new HashMap<>();
+    private CaseData caseDataPersonal;
+    private CaseData caseDataSolicitor;
+    private CaveatData returnedCaveatData;
+
+    @Before
+    public void setUp() {
+        registry.setPhone("1234567890");
+        registry.setName("CTSC");
+
+        caseDataPersonal = CaseData.builder()
+                .applicationType(ApplicationType.PERSONAL)
+                .primaryApplicantForenames("first name")
+                .primaryApplicantSurname("surname")
+                .deceasedSurname("deceased surname")
+                .deceasedForenames("deceased forenames")
+                .solsSOTName("sols sot name")
+                .solsSolicitorAppReference("app reference")
+                .boStopDetails("stop details")
+                .boCaseStopCaveatId("123456789012345678")
+                .deceasedDateOfDeath(LocalDate.now())
+                .boStopDetailsDeclarationParagraph("Yes")
+                .build();
+
+        caseDataSolicitor = CaseData.builder()
+                .applicationType(ApplicationType.SOLICITOR)
+                .primaryApplicantForenames("first name")
+                .primaryApplicantSurname("surname")
+                .deceasedSurname("deceased surname")
+                .deceasedForenames("deceased forenames")
+                .solsSOTName("sols sot name")
+                .solsSolicitorAppReference("app reference")
+                .boStopDetails("stop details")
+                .boCaseStopCaveatId("123456789012345678")
+                .deceasedDateOfDeath(LocalDate.now())
+                .boStopDetailsDeclarationParagraph("Yes")
+                .solsSOTName("SotName")
+                .build();
+
+        caseDetails = new CaseDetails(caseDataPersonal, LAST_MODIFIED, ID);
+
+        CaveatData caveatData = CaveatData.builder()
+                .caveatorForenames("cav first name")
+                .caveatorSurname("cav surname")
+                .deceasedForenames("forename")
+                .deceasedSurname("surname")
+                .messageContent("message content")
+                .expiryDate(LocalDate.parse("2000-10-10"))
+                .build();
+
+        returnedCaveatData = CaveatData.builder()
+                .applicationSubmittedDate(LocalDate.parse("2000-10-10"))
+                .caveatorForenames("cav first name")
+                .caveatorSurname("cav surname")
+                .deceasedForenames("forename")
+                .deceasedSurname("surname")
+                .messageContent("message content")
+                .expiryDate(LocalDate.parse("2000-10-10"))
+                .build();
+
+        caveatDetails = new CaveatDetails(caveatData, LAST_MODIFIED, ID);
+
+        when(dateFormatterService.formatCaveatExpiryDate(any())).thenReturn(PERSONALISATION_CAVEAT_EXPIRY_DATE);
+
+    }
+
+    @Test
+    public void getPersonalisationContentIsOk() {
+        Map<String, String> response = caveatPersonalisationService.getCaveatPersonalisation(caveatDetails, registry);
+
+        assertEquals("cav first name cav surname", response.get(PERSONALISATION_APPLICANT_NAME));
+        assertEquals("forename surname", response.get(PERSONALISATION_DECEASED_NAME));
+        assertEquals(caveatDetails.getId().toString(), response.get(PERSONALISATION_CCD_REFERENCE));
+        assertEquals("message content", response.get(PERSONALISATION_MESSAGE_CONTENT));
+        assertEquals("CTSC", response.get(PERSONALISATION_REGISTRY_NAME));
+        assertEquals("1234567890", response.get(PERSONALISATION_REGISTRY_PHONE));
+        assertEquals("10th October 2000", response.get(PERSONALISATION_CAVEAT_EXPIRY_DATE));
+    }
+
+
+    @Test
+    public void getCaveatStopPersonalisationContentIsOk() {
+        when(caveatQueryServiceMock.findCaveatById(CaseType.CAVEAT, caseDataPersonal.getBoCaseStopCaveatId()))
+                .thenReturn(returnedCaveatData);
+
+        Map<String, String> response = caveatPersonalisationService.getCaveatStopPersonalisation(personalisation, caseDataPersonal);
+
+        assertEquals("10th October 2000", response.get(PERSONALISATION_DATE_CAVEAT_ENTERED));
+        assertEquals("cav first name cav surname", response.get(PERSONALISATION_CAVEATOR_NAME));
+        assertEquals("10th October 2000", response.get(PERSONALISATION_CAVEAT_EXPIRY_DATE));
+    }
+
+    @Test
+    public void getCaveatStopPersonalisationContentSolsIsOk() {
+        personalisation.put(PERSONALISATION_APPLICANT_NAME, "name");
+        when(caveatQueryServiceMock.findCaveatById(CaseType.CAVEAT, caseDataSolicitor.getBoCaseStopCaveatId()))
+                .thenReturn(returnedCaveatData);
+
+        Map<String, String> response = caveatPersonalisationService.getCaveatStopPersonalisation(personalisation, caseDataSolicitor);
+
+        assertEquals("10th October 2000", response.get(PERSONALISATION_DATE_CAVEAT_ENTERED));
+        assertEquals("cav first name cav surname", response.get(PERSONALISATION_CAVEATOR_NAME));
+        assertEquals("10th October 2000", response.get(PERSONALISATION_CAVEAT_EXPIRY_DATE));
+        assertEquals("SotName", response.get(PERSONALISATION_APPLICANT_NAME));
+    }
+
+
+}
