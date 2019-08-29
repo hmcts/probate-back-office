64782a234170f84f77062d550ca760db12f6a63f
Dave Jones <dave.jones@hmcts.net>
2019-08-29 14:12:13 +0100

    PRO-5247: Updated to fix docker issues.

--- bin/ccd-add-all-roles.sh
+++ bin/ccd-add-all-roles.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 binFolder=$(dirname "$0")
 
-(${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-issuer ProbateSolCW1@gmail.com)
-(${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-issuer ProbateSolCW2@gmail.com)
-(${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-solicitor ProbateSolicitor1@gmail.com)
-(${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-solicitor ProbateSolicitor2@gmail.com)
-(${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-systemupdate bulkscan+ccd@gmail.com)
+(${binFolder}/idam-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker)
 
 (${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-caseofficer ProbateCaseOfficer@gmail.com)
 (${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-caseadmin ProbateCaseAdmin@gmail.com)
@@ -15,6 +11,7 @@ binFolder=$(dirname "$0")
 
 (${binFolder}/ccd-add-role.sh payment)
 (${binFolder}/ccd-add-role.sh citizen)
+(${binFolder}/ccd-add-role.sh caseworker)
 (${binFolder}/ccd-add-role.sh caseworker-probate)
 (${binFolder}/ccd-add-role.sh caseworker-probate-issuer)
 (${binFolder}/ccd-add-role.sh caseworker-probate-solicitor)
@@ -25,3 +22,16 @@ binFolder=$(dirname "$0")
 (${binFolder}/ccd-add-role.sh caseworker-probate-caseadmin)
 (${binFolder}/ccd-add-role.sh caseworker-probate-registrar)
 (${binFolder}/ccd-add-role.sh caseworker-probate-superuser)
+
+
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-issuer ProbateSolCW1@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-issuer ProbateSolCW2@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-solicitor ProbateSolicitor1@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-solicitor ProbateSolicitor2@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker-probate,caseworker-probate-systemupdate bulkscan+ccd@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-caseofficer ProbateCaseOfficer@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-caseadmin ProbateCaseAdmin@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-registrar ProbateRegistrar@gmail.com)
+(${binFolder}/idam-create-caseworker.sh caseworker,caseworker-probate,caseworker-probate-superuser ProbateSuperuser@gmail.com)
+
+(${binFolder}/idam-create-caseworker.sh citizen testusername@test.com)
