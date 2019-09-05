5171914ed05dda26814fd4e520e28c5230037d40
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-09-05 21:54:10 +0100

    Disable idam security filter altogether

--- src/main/java/uk/gov/hmcts/probate/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/config/SecurityConfiguration.java
@@ -1,22 +1,22 @@
 package uk.gov.hmcts.probate.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import uk.gov.hmcts.probate.exception.handler.AuthenticationExceptionHandler;
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Configuration
-    @ConditionalOnProperty(name = "s2s.enabled", havingValue = "true", matchIfMissing = true)
+    //@Configuration
+    //@ConditionalOnProperty(name = "s2s.enabled", havingValue = "true", matchIfMissing = true)
     public class ServiceOnlySecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
         private final AuthCheckerServiceOnlyFilter filter;
