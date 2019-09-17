c478b92f4bba101cb6f7bdaef5c97f0c02e9c19c
Kurt Plaschke <k.plaschke@hull.ac.uk>
2019-09-17 14:16:26 +0100

    WIP added flow to notify redec, added personalisation that adds file.

---
+++ src/main/java/uk/gov/hmcts/probate/service/RedeclarationNotificationService.java
@@ -0,0 +1,50 @@
+package uk.gov.hmcts.probate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.probate.model.ExecutorsApplyingNotification;
+import uk.gov.hmcts.probate.model.State;
+import uk.gov.hmcts.probate.model.ccd.raw.CollectionMember;
+import uk.gov.hmcts.probate.model.ccd.raw.Document;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
+import uk.gov.hmcts.probate.model.ccd.raw.response.CallbackResponse;
+import uk.gov.hmcts.probate.transformer.CallbackResponseTransformer;
+import uk.gov.hmcts.probate.validator.EmailAddressExecutorsApplyingValidationRule;
+import uk.gov.service.notify.NotificationClientException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static uk.gov.hmcts.probate.model.Constants.YES;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RedeclarationNotificationService {
+
+    private final NotificationService notificationService;
+    private final EmailAddressExecutorsApplyingValidationRule emailAddressExecutorsApplyingValidationRule;
+    private final CallbackResponseTransformer callbackResponseTransformer;
+
+    public CallbackResponse handleRedeclarationNotification(CallbackRequest callbackRequest) {
+
+        log.info("Preparing to send email to executors for redeclaration notification");
+        emailAddressExecutorsApplyingValidationRule.validate(callbackRequest.getCaseDetails());
+
+        List<Document> emailDocument = new ArrayList<>();
+
+        for (CollectionMember<ExecutorsApplyingNotification> executor :
+                callbackRequest.getCaseDetails().getData().getExecutorsApplyingNotifications()) {
+            if (executor.getValue().getNotification().equals(YES)) {
+                try {
+                    emailDocument.add(notificationService.sendEmailWithDocumentAttached(callbackRequest.getCaseDetails(), executor.getValue(), State.REDECLARATION_SOT));
+                } catch (NotificationClientException | IOException e) {
+                    log.error("Error sending email with exception: {}. Has message: {}", e.getClass(), e.getMessage());
+                }
+            }
+        }
+        return callbackResponseTransformer.addDocuments(callbackRequest, emailDocument, null, null);
+    }
+}
