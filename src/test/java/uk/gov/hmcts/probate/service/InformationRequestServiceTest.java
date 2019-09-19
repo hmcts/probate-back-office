ff2f0f21199466d503dd18d06583ec1f35907852
RBaker <rebecca.baker@hmcts.net>
2019-09-19 13:42:51 +0100

    PRO-6210 prevent access to event if its a paper case

--- src/test/java/uk/gov/hmcts/probate/service/InformationRequestServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/InformationRequestServiceTest.java
@@ -18,6 +18,7 @@ import uk.gov.hmcts.probate.model.ccd.raw.response.CallbackResponse;
 import uk.gov.hmcts.probate.model.ccd.raw.response.ResponseCaseData;
 import uk.gov.hmcts.probate.transformer.CallbackResponseTransformer;
 import uk.gov.hmcts.probate.validator.NotificationExecutorsApplyingValidationRule;
+import uk.gov.hmcts.probate.validator.RedeclarationSoTValidationRule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,6 +40,9 @@ public class InformationRequestServiceTest {
 
     @Mock
     private NotificationExecutorsApplyingValidationRule notificationExecutorsApplyingValidationRule;
+
+    @Mock
+    private RedeclarationSoTValidationRule redeclarationSoTValidationRule;
 
     @InjectMocks
     private InformationRequestService informationRequestService;
