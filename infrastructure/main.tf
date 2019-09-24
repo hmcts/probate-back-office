8dc40dd05e27ebae19353a50d3ffe1fa8c1ba2c9
reddy1praveen <32567752+reddy1praveen@users.noreply.github.com>
2019-09-24 10:51:35 +0100

    changed from moj to cnp

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -116,7 +116,7 @@ data "azurerm_key_vault_secret" "idamSecretProbate" {
 
 
 module "probate-back-office" {
-  source = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
+  source = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product = "${var.product}-${var.microservice}"
   location = "${var.location}"
   env = "${var.env}"
