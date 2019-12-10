b84bfa39fed58376165830c9eee2d259bc69ca7b
Dave Jones <dave.jones@hmcts.net>
2019-12-10 12:27:33 +0000

    PRO-6817: Removed imports not being used.

--- src/test/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/OCRFieldAddressMapperTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/OCRFieldAddressMapperTest.java
@@ -3,17 +3,14 @@ package uk.gov.hmcts.probate.service.exceptionrecord.mapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.probate.exception.OCRMappingException;
 import uk.gov.hmcts.probate.model.exceptionrecord.ExceptionRecordOCRFields;
 import uk.gov.hmcts.reform.probate.model.AttorneyNamesAndAddress;
 import uk.gov.hmcts.reform.probate.model.cases.Address;
 import uk.gov.hmcts.reform.probate.model.cases.CollectionMember;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 @Component
 public class OCRFieldAddressMapperTest {
