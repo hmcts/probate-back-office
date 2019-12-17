5051e815c757fe0a066302a3b7e245da47bb4d99
Praveen Reddy <praveen.reddy@hmcts.net>
2019-12-17 12:08:43 +0000

    Corrected S2s Key

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -144,7 +144,7 @@ module "probate-back-office" {
 
     DEPLOYMENT_ENV= "${var.deployment_env}"
 
-    AUTH_PROVIDER_SERVICE_CLIENT_KEY = "${data.azurerm_key_vault_secret.s2s_key.value}"
+    S2S_AUTH_TOTP_SECRET = "${data.azurerm_key_vault_secret.s2s_key.value}"
     PDF_SERVICE_GRANTSIGNATURESECRETKEY = "${data.azurerm_key_vault_secret.pdf_service_grantSignatureKey.value}"
     PDF_SERVICE_GRANTSIGNATUREENCRYPTEDFILE = "${data.azurerm_key_vault_secret.pdf_service_grantSignatureFile.value}"
 
