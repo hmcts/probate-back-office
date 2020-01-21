003cb848a74f1c2de64418221e9b1abc14cb81cb
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-01-21 16:22:54 +0000

    PRO-5389: fix build issue

--- src/main/java/uk/gov/hmcts/probate/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/config/ServiceTokenGeneratorConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.probate.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
