3ab5f909f3c0f6b46686976c6747d0d2764a8d65
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-02-26 12:21:22 +0000

    PRO-5389: reverted to feign

--- src/main/java/uk/gov/hmcts/probate/service/FileTransferService.java
+++ src/main/java/uk/gov/hmcts/probate/service/FileTransferService.java
@@ -1,18 +1,24 @@
 package uk.gov.hmcts.probate.service;
 
+import feign.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import uk.gov.hmcts.probate.exception.BadRequestException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.Optional;
 
 @Slf4j
 public class FileTransferService {
@@ -38,25 +44,47 @@ public class FileTransferService {
     }
 
     public int uploadFile(File file) {
-        String serverUrl = "https://green.blob.core.windows.net/probate-aat/" + file.getName() + "?" +
-            "sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2029-02-10T18:49:08Z&st=2020-02-10T10:49:08Z&spr=https,http" +
-            "&sig=Lbo%2BZwQj0g62ru4lqvgsd8mTnhRzNjzFZxzLHUqIbgw%3D";
+        log.info("Starting file upload to ftp for file:" + file.toPath() + ":" + file.getName());
+        Response response = null;
+        MultipartFile multipartFile = buildMultipartFile(file);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.add("x-ms-blob-type", "BlockBlob");
+        try {
+            response = fileTransferApi.sendFile(
+                multipartFile,
+                targetEnv,
+                file.getName(),
+                SV_VALID_FROM,
+                SS,
+                SRT,
+                SP,
+                SE_SIG_EXPIRY_DATE,
+                ST_SIG_CREATION_DATE,
+                SPR,
+                signature);
+            log.info("File transfer response: {}", response.status());
+            Files.delete(file.toPath());
+        } catch (IOException e) {
+            log.error("Error handling file: " + e.getMessage());
+            throw new BadRequestException("Failed to initiate file transfer request: " + e.getMessage());
+        }
+        return response.status();
+    }
 
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("file", file);
+    private MultipartFile buildMultipartFile(File file) {
+        Optional<FileInputStream> inputStreamOptional = Optional.empty();
+        try {
+            inputStreamOptional = Optional.of(new FileInputStream(file));
+            FileItem fileItem = new DiskFileItem("file", Files.probeContentType(file.toPath()), true,
+                file.getName(), (int) file.length(), file.getParentFile());
+            OutputStream os = fileItem.getOutputStream();
+            IOUtils.copy(inputStreamOptional.get(), os);
+            inputStreamOptional.get().close();
 
+            return new CommonsMultipartFile(fileItem);
+        } catch (IOException e) {
+            log.error("Error building multipart file: " + e.getMessage());
+            throw new BadRequestException("Error building multipart file: " + e.getMessage());
+        }
 
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-        log.info("serverUrl: {}", serverUrl);
-        log.info("requestEntity: {}", requestEntity);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(serverUrl, HttpMethod.PUT, requestEntity, String.class);
-
-        log.info("response: {}", response);
-        return response.getStatusCode().value();
     }
 }
