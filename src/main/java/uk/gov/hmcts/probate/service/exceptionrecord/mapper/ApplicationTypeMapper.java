c32960e896d6fce7e63b9fd88fcca6cf6abd494c
Dave Jones <dave.jones@hmcts.net>
2020-02-27 15:44:00 +0000

    PRO-6828: Updated to add latest work.

---
+++ src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/ApplicationTypeMapper.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.probate.service.exceptionrecord.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.model.exceptionrecord.ExceptionRecordOCRFields;
+import uk.gov.hmcts.probate.service.exceptionrecord.mapper.qualifiers.ToApplicationTypeCaveat;
+import uk.gov.hmcts.reform.probate.model.cases.ApplicationType;
+
+@Slf4j
+@Component
+public class ApplicationTypeMapper {
+
+    @SuppressWarnings("squid:S1168")
+    @ToApplicationTypeCaveat
+    public ApplicationType toApplicationTypeCaveat(ExceptionRecordOCRFields ocrFields) {
+        log.info("Beginning mapping for Application Type (Caveat)");
+        if (StringUtils.isNotBlank(ocrFields.getSolsSolicitorFirmName() )
+                || StringUtils.isNotBlank(ocrFields.getSolsSolicitorRepresentativeName())) {
+            log.info("Found solicitor details returning ApplicationType.SOLICITORS");
+            return ApplicationType.SOLICITORS;
+        }
+
+        log.info("No solicitor details found returning ApplicationType.PERSONAL");
+        return ApplicationType.PERSONAL;
+    }
+}
