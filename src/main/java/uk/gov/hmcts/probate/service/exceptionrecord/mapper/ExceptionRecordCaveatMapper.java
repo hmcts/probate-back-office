fb2f14fbf1ded8fa9c0adef6cb716343fa53f799
Dave Jones <dave.jones@hmcts.net>
2020-03-01 16:59:43 +0000

    PRO-6828: Updated mappings for caveats solicitors.

--- src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/ExceptionRecordCaveatMapper.java
+++ src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/ExceptionRecordCaveatMapper.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.probate.service.exceptionrecord.mapper;
 
+import org.apache.commons.lang.StringUtils;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.ReportingPolicy;
 import uk.gov.hmcts.probate.model.exceptionrecord.ExceptionRecordOCRFields;
@@ -10,17 +13,25 @@ import uk.gov.hmcts.probate.service.exceptionrecord.mapper.qualifiers.ToCaveator
 import uk.gov.hmcts.probate.service.exceptionrecord.mapper.qualifiers.ToDeceasedAddress;
 import uk.gov.hmcts.probate.service.exceptionrecord.mapper.qualifiers.ToDefaultLocalDate;
 import uk.gov.hmcts.probate.service.exceptionrecord.mapper.qualifiers.ToYesOrNo;
+import uk.gov.hmcts.probate.service.exceptionrecord.utils.OCRFieldExtractor;
+import uk.gov.hmcts.reform.probate.model.cases.ApplicationType;
+import uk.gov.hmcts.reform.probate.model.cases.SolsPaymentMethods;
 import uk.gov.hmcts.reform.probate.model.cases.caveat.CaveatData;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",
+        imports = {StringUtils.class, ApplicationType.class},
         uses = {ApplicationTypeMapper.class,
+                OCRFieldAddressMapper.class,
                 OCRFieldDefaultLocalDateFieldMapper.class,
                 OCRFieldYesOrNoMapper.class},
         unmappedTargetPolicy = ReportingPolicy.IGNORE, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface ExceptionRecordCaveatMapper {
     @Mapping(target = "caveatorForenames", source = "caveatorForenames")
     @Mapping(target = "caveatorSurname", source = "caveatorSurnames")
-    @Mapping(target = "caveatorEmailAddress", source = "caveatorEmailAddress")
+    @Mapping(target = "caveatorEmailAddress", expression = "java(StringUtils.isNotBlank(ocrFields.getSolsSolicitorEmail()) ? "
+            + "ocrFields.getSolsSolicitorEmail() : ocrFields.getCaveatorEmailAddress())")
     @Mapping(target = "caveatorAddress", source = "ocrFields", qualifiedBy = {ToCaveatorAddress.class})
     @Mapping(target = "deceasedForenames", source = "deceasedForenames")
     @Mapping(target = "deceasedSurname", source = "deceasedSurname")
@@ -28,8 +39,41 @@ public interface ExceptionRecordCaveatMapper {
     @Mapping(target = "deceasedDateOfDeath", source = "deceasedDateOfDeath", qualifiedBy = {ToDefaultLocalDate.class})
     @Mapping(target = "deceasedDateOfBirth", source = "deceasedDateOfBirth", qualifiedBy = {ToDefaultLocalDate.class})
     @Mapping(target = "deceasedAnyOtherNames", source = "deceasedAnyOtherNames", qualifiedBy = {ToYesOrNo.class})
+    @Mapping(target = "solsSolicitorFirmName", source = "solsSolicitorFirmName")
+    @Mapping(target = "solsSolicitorAppReference", source = "solsSolicitorAppReference")
+    @Mapping(target = "solsFeeAccountNumber", source = "solsFeeAccountNumber")
+    @Mapping(target = "solsPaymentMethods", ignore = true)
+    @Mapping(target = "languagePreferenceWelsh", expression = "java(Boolean.FALSE)")
+    @Mapping(target = "solsSolicitorPhoneNumber", source = "solsSolicitorPhoneNumber")
     @Mapping(target = "caveatRaisedEmailNotificationRequested", expression = "java(Boolean.TRUE)")
     @Mapping(target = "paperForm", expression = "java(Boolean.TRUE)")
     @Mapping(target = "applicationType", source = "ocrFields", qualifiedBy = {ToApplicationTypeCaveat.class})
     CaveatData toCcdData(ExceptionRecordOCRFields ocrFields);
+
+    @AfterMapping
+    default void setSolPaymentMethod(
+            @MappingTarget CaveatData caseData, ExceptionRecordOCRFields ocrField) {
+        if ((caseData.getApplicationType() == ApplicationType.SOLICITORS) &&
+                StringUtils.isNotBlank(caseData.getSolsFeeAccountNumber())) {
+            caseData.setSolsPaymentMethods(SolsPaymentMethods.FEE_ACCOUNT);
+        }
+    }
+
+    @AfterMapping
+    default void setSolsSolicitorRepresentativeName(
+            @MappingTarget CaveatData caseData, ExceptionRecordOCRFields ocrField) {
+        if (StringUtils.isNotBlank(ocrField.getSolsSolicitorRepresentativeName())) {
+            String solicitorFullName = ocrField.getSolsSolicitorRepresentativeName();
+            List<String> names = OCRFieldExtractor.splitFullname(solicitorFullName);
+            if (names.size() > 2) {
+                caseData.setCaveatorSurname(names.get(names.size()-1));
+                caseData.setCaveatorForenames(String.join(" ", names.remove(names.size()-1)));
+            } else if(names.size() == 1) {
+                caseData.setCaveatorForenames(names.get(0));
+            } else {
+                caseData.setCaveatorSurname(names.get(1));
+                caseData.setCaveatorForenames(names.get(0));
+            }
+        }
+    }
 }
--- src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/OCRFieldAddressMapper.java
+++ src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/OCRFieldAddressMapper.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.probate.service.exceptionrecord.mapper;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.model.exceptionrecord.ExceptionRecordOCRFields;
 import uk.gov.hmcts.probate.service.exceptionrecord.mapper.qualifiers.ToAttorneyOnBehalfOfAddress;
@@ -69,15 +70,12 @@ public class OCRFieldAddressMapper {
     @SuppressWarnings("squid:S1168")
     @ToCaveatorAddress
     public Address toCaveatorAddress(ExceptionRecordOCRFields ocrFields) {
-        log.info("Beginning mapping for Caveator Address");
-        this.addressLine1 = ocrFields.getCaveatorAddressLine1();
-        this.addressLine2 = ocrFields.getCaveatorAddressLine2();
-        this.addressLine3 = "";
-        this.postTown = ocrFields.getCaveatorAddressTown();
-        this.county = ocrFields.getCaveatorAddressCounty();
-        this.country = "";
-        this.postCode = ocrFields.getCaveatorAddressPostCode();
-        return buildAddress();
+        if (StringUtils.isNotBlank(ocrFields.getSolsSolicitorAddressLine1()) ||
+                StringUtils.isNotBlank(ocrFields.getSolsSolicitorAddressPostCode())) {
+            return buildSolicitorAddress(ocrFields);
+        } else {
+            return buildCaveatorAddress(ocrFields);
+        }
     }
 
     @SuppressWarnings("squid:S1168")
@@ -91,6 +89,30 @@ public class OCRFieldAddressMapper {
         this.county = ocrFields.getDeceasedAddressCounty();
         this.country = "";
         this.postCode = ocrFields.getDeceasedAddressPostCode();
+        return buildAddress();
+    }
+
+    private Address buildCaveatorAddress(ExceptionRecordOCRFields ocrFields) {
+        log.info("Beginning mapping for Caveator Address");
+        this.addressLine1 = ocrFields.getCaveatorAddressLine1();
+        this.addressLine2 = ocrFields.getCaveatorAddressLine2();
+        this.addressLine3 = "";
+        this.postTown = ocrFields.getCaveatorAddressTown();
+        this.county = ocrFields.getCaveatorAddressCounty();
+        this.country = "";
+        this.postCode = ocrFields.getCaveatorAddressPostCode();
+        return buildAddress();
+    }
+
+    private Address buildSolicitorAddress(ExceptionRecordOCRFields ocrFields) {
+        log.info("Beginning mapping for Solicitor Address");
+        this.addressLine1 = ocrFields.getSolsSolicitorAddressLine1();
+        this.addressLine2 = ocrFields.getSolsSolicitorAddressLine2();
+        this.addressLine3 = "";
+        this.postTown = ocrFields.getSolsSolicitorAddressTown();
+        this.county = ocrFields.getSolsSolicitorAddressCounty();
+        this.country = "";
+        this.postCode = ocrFields.getSolsSolicitorAddressPostCode();
         return buildAddress();
     }
 
