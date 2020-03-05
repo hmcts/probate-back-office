9177c458b6f6e253aa2c88ac16743e76f113fdef
Dave Jones <dave.jones@hmcts.net>
2020-03-05 10:53:19 +0000

    PRO-6102: Update to grant mandatory fields.

--- src/main/java/uk/gov/hmcts/probate/model/ccd/ocr/IntestacySolicitorMandatoryFields.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/ocr/IntestacySolicitorMandatoryFields.java
@@ -1,11 +1,10 @@
 package uk.gov.hmcts.probate.model.ccd.ocr;
 
-public enum GORMandatoryFields {
+public enum IntestacySolicitorMandatoryFields {
     PRIMARY_APPLICANT_FORENAMES("primaryApplicantForenames", "Primary applicant first names"),
     PRIMARY_APPLICANT_SURNAME("primaryApplicantSurname", "Primary applicant last name"),
     PRIMARY_APPLICANT_ADDRESS_LINE1("primaryApplicantAddressLine1", "Primary applicant address line 1"),
     PRIMARY_APPLICANT_ADDRESS_POSTCODE("primaryApplicantAddressPostCode", "Primary applicant postcode"),
-    PRIMARY_APPLICANT_HAS_ALIAS("primaryApplicantHasAlias", "Primary applicant has alias?"),
     DECEASED_FORENAMES("deceasedForenames", "Deceased first names"),
     DECEASED_SURNAME("deceasedSurname", "Deceased last name"),
     DECEASED_ADDRESS_LINE1("deceasedAddressLine1", "Deceased address line 1"),
@@ -15,6 +14,8 @@ public enum GORMandatoryFields {
     DECEASED_ANY_OTHER_NAMES("deceasedAnyOtherNames", "Did the deceased have assets in any other names?"),
     DECEASED_DOMICILE_IN_ENG_WALES("deceasedDomicileInEngWales",
             "Was the deceased domiciled in England or Wales at the time of their death?"),
+    SOLICITOR_FIRM_NAME("solsSolicitorFirmName", "Solicitors Firm name"),
+    SOLICITOR_APP_REFERENCE("solsSolicitorAppReference", "Solictor application reference"),
     IHT_FORM_COMPLETED_ONLINE("ihtFormCompletedOnline", "IHT form completed online?"),
     IHT_GROSS_VALUE("ihtGrossValue", "Gross value of the estate"),
     IHT_NET_VALUE("ihtNetValue", "Net value of the estate");
@@ -22,7 +23,7 @@ public enum GORMandatoryFields {
     private final String key;
     private final String value;
 
-    GORMandatoryFields(String key, String value) {
+    IntestacySolicitorMandatoryFields(String key, String value) {
         this.key = key;
         this.value = value;
     }
