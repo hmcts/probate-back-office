03d646c85e6da5e7f927b04a759c2ed6ab9c609d
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-03-05 16:05:44 +0000

    PRO-7126: completed email journey

--- src/main/java/uk/gov/hmcts/probate/service/GrantDelayedNotificationService.java
+++ src/main/java/uk/gov/hmcts/probate/service/GrantDelayedNotificationService.java
@@ -3,22 +3,15 @@ package uk.gov.hmcts.probate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.probate.model.ExecutorsApplyingNotification;
-import uk.gov.hmcts.probate.model.State;
 import uk.gov.hmcts.probate.model.ccd.raw.CollectionMember;
 import uk.gov.hmcts.probate.model.ccd.raw.Document;
-import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
 import uk.gov.hmcts.probate.model.ccd.raw.request.ReturnedCaseDetails;
-import uk.gov.hmcts.probate.model.ccd.raw.response.CallbackResponse;
+import uk.gov.hmcts.probate.security.SecurityUtils;
 import uk.gov.hmcts.probate.transformer.CallbackResponseTransformer;
 import uk.gov.hmcts.probate.validator.EmailAddressExecutorsApplyingValidationRule;
 import uk.gov.service.notify.NotificationClientException;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-
-import static uk.gov.hmcts.probate.model.Constants.YES;
 
 @Slf4j
 @Service
@@ -29,6 +22,8 @@ public class GrantDelayedNotificationService {
     private final EmailAddressExecutorsApplyingValidationRule emailAddressExecutorsApplyingValidationRule;
     private final CallbackResponseTransformer callbackResponseTransformer;
     private final CaseQueryService caseQueryService;
+    private final CoreCaseDataService coreCaseDataService;
+    private final SecurityUtils securityUtils;
 
     public String handleGrantDelayedNotification(String date) {
         String processedCases = "";
@@ -39,25 +34,24 @@ public class GrantDelayedNotificationService {
         return processedCases;
     }
 
-    private CallbackResponse sendNotificationForCase(ReturnedCaseDetails foundCase) {
+    private String sendNotificationForCase(ReturnedCaseDetails foundCase) {
         log.info("Preparing to send email to executors for grant delayed notification");
         //emailAddressExecutorsApplyingValidationRule.validate(foundCase.getData());
-
+        String caseId = foundCase.getId().toString();
         try {
-            Document emailDocument = notificationService.sendGr antDelayedEmail(foundCase);
-        } catch (NotificationClientException | IOException e) {
+            Document emailDocument = notificationService.sendGrantDelayedEmail(foundCase);
+            foundCase.getData().getProbateNotificationsGenerated()
+                .add(new CollectionMember<>(null, emailDocument));
+            updateFoundCase(foundCase);
+        } catch (NotificationClientException e) {
             log.error("Error sending email with exception: {}. Has message: {}", e.getClass(), e.getMessage());
+            caseId = "*" + caseId + "*";
         }
-        for (CollectionMember<ExecutorsApplyingNotification> executor :
-            
-            callbackRequest.getCaseDetails().getData().getExecutorsApplyingNotifications()) {
-            if (YES.equals(executor.getValue().getNotification())) {
-                try {
-                    emailDocument.add(
-                        notificationService.sendEmailWithDocumentAttached(
-                            callbackRequest.getCaseDetails(), executor.getValue(), State.REDECLARATION_SOT));
-            }
-        }
-        return callbackResponseTransformer.addDocuments(callbackRequest, emailDocument, null, null);
+
+        return caseId;
+    }
+
+    private void updateFoundCase(ReturnedCaseDetails foundCase) {
+        log.info("Updating case for grant delayed, caseId: {}", foundCase.getId());
     }
 }
