e52fcfae314339c412c4fa2dcb96491f54db977a
ruban72 <ruban72@hotmail.com>
2020-03-09 15:18:06 +0000

    PRO-6546: Add new fields to casedata

--- src/test/java/uk/gov/hmcts/probate/controller/CaseDataTestBuilder.java
+++ src/test/java/uk/gov/hmcts/probate/controller/CaseDataTestBuilder.java
@@ -18,9 +18,13 @@ public class CaseDataTestBuilder {
     public static final String SOLICITOR_FIRM_NAME = "Legal Service Ltd";
     public static final String SOLICITOR_FIRM_LINE1 = "Sols Add Line1";
     public static final String SOLICITOR_FIRM_POSTCODE = "SW1E 6EA";
+    private static final String SOLICITOR_FORENAME = "Peter Middlename";
+    private static final String SOLICITOR_SURNAME = "Crouch";
+    private static final String SOLICITOR_JOB_TITLE = "Lawyer";
+    private static final String SOLICITOR_IS_MAIN_APPLICANT = "Yes";
+    private static final String SOLS_NOT_APPLYING_REASON = "Power reserved";
+    private static final String SOLICITOR_APPLYING = "Yes";
     public static final String IHT_FORM = "IHT207";
-    public static final String SOLICITOR_NAME = "Peter Crouch";
-    public static final String SOLICITOR_JOB_TITLE = "Lawyer";
     public static final String PAYMENT_METHOD = "Cheque";
     public static final String WILL_HAS_CODICLIS = "Yes";
     public static final String NUMBER_OF_CODICLIS = "1";
@@ -105,7 +109,12 @@ public class CaseDataTestBuilder {
                 .willNumberOfCodicils(NUMBER_OF_CODICLIS)
                 .ihtFormId(IHT_FORM)
                 .solsSOTNeedToUpdate(NEED_TO_UPDATE)
-                .solsSOTName(SOLICITOR_NAME)
+                .solsSOTForenames(SOLICITOR_FORENAME)
+                .solsSOTSurname(SOLICITOR_SURNAME)
+                .solsSolicitorIsExec(SOLICITOR_APPLYING)
+                .solsSolicitorIsMainApplicant(SOLICITOR_IS_MAIN_APPLICANT)
+                .solsSolicitorIsApplying(SOLICITOR_APPLYING)
+                .solsSolicitorNotApplyingReason(SOLS_NOT_APPLYING_REASON)
                 .solsSOTJobTitle(SOLICITOR_JOB_TITLE)
                 .solsPaymentMethods(PAYMENT_METHOD)
                 .applicationFee(APPLICATION_FEE)
