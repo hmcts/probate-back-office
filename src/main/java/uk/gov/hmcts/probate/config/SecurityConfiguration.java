c8fa555a92c259d661aa36e67b00eb989c15fc8d
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-03-12 15:49:53 +0000

    PRO-7124: changed to split security config

--- src/main/java/uk/gov/hmcts/probate/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/config/SecurityConfiguration.java
@@ -1,86 +1,109 @@
 package uk.gov.hmcts.probate.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import uk.gov.hmcts.probate.exception.handler.AuthenticationExceptionHandler;
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.core.user.User;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceanduser.AuthCheckerServiceAndUserFilter;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    private AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
-    private AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter;
+    @Configuration
+    @Order(1)
+    public static class AuthCheckerServiceAndUSerFilterConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    public SecurityConfiguration(RequestAuthorizer<Service> serviceRequestAuthorizer,
-                                 AuthenticationManager authenticationManager,
-                                 RequestAuthorizer<User> userRequestAuthorizer) {
-        authCheckerServiceAndUserFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
-        authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
-        authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
-        authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
+        private final AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
 
+        public AuthCheckerServiceAndUSerFilterConfigurerAdapter(RequestAuthorizer<User> userRequestAuthorizer,
+                                                                RequestAuthorizer<Service> serviceRequestAuthorizer,
+                                                                AuthenticationManager authenticationManager) {
+            authCheckerServiceAndUserFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
+            authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+                .requestMatchers()
+                .antMatchers("/notify/grant-delayed-scheduled")
+                .and()
+                .addFilter(authCheckerServiceAndUserFilter)
+                .csrf().disable()
+                .formLogin().disable()
+                .logout().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        }
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui.html",
-            "/swagger-resources/**",
-            "/webjars/springfox-swagger-ui/**",
-            "/v2/api-docs",
-            "/health",
-            "/health/liveness",
-            "/info",
-            "/data-extract/**",
-            "/");
+    @Configuration
+    public static class AuthCheckerServiceOnlyFilterConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        private AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter;
+
+        public AuthCheckerServiceOnlyFilterConfigurerAdapter(RequestAuthorizer<Service> serviceRequestAuthorizer,
+                                                             AuthenticationManager authenticationManager) {
+            authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
+            authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
+        }
+
+        @Override
+        @Order(2)
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+                .requestMatchers()
+                .antMatchers("/swagger-ui.html")
+                .antMatchers("/swagger-resources/**")
+                .antMatchers("/webjars/springfox-swagger-ui/**")
+                .antMatchers("/v2/api-docs")
+                .antMatchers("/health", "/health/liveness")
+                .antMatchers("/info")
+                .antMatchers("/case/**")
+                .antMatchers("/case-matching/**")
+                .antMatchers("/caveat/**")
+                .antMatchers("/data-extract/**")
+                .antMatchers("/document/**")
+                .antMatchers("/transform-exception-record")
+                .antMatchers("/grant/**")
+                .antMatchers("/nextsteps/**")
+                .antMatchers("/notify/**")
+                .antMatchers("/forms/**")
+                .antMatchers("/template/**")
+                .antMatchers("/probateManTypes/**")
+                .antMatchers("/legacy/**")
+                .antMatchers("/standing-search/**")
+                .and()
+                .addFilter(authCheckerServiceOnlyFilter)
+                .csrf().disable()
+                .formLogin().disable()
+                .logout().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        }
+
+        public void configure(WebSecurity web) {
+            web.ignoring().antMatchers("/swagger-ui.html",
+                "/swagger-resources/**",
+                "/webjars/springfox-swagger-ui/**",
+                "/v2/api-docs",
+                "/health",
+                "/health/liveness",
+                "/info",
+                "/data-extract/**",
+                "/");
+        }
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
 
-        http
-            .requestMatchers()
-            .antMatchers("/notify/grant-delayed-scheduled")
-            .and()
-            .addFilter(authCheckerServiceAndUserFilter)
-            .requestMatchers()
-            .antMatchers("/swagger-ui.html")
-            .antMatchers("/swagger-resources/**")
-            .antMatchers("/webjars/springfox-swagger-ui/**")
-            .antMatchers("/v2/api-docs")
-            .antMatchers("/health", "/health/liveness")
-            .antMatchers("/info")
-            .antMatchers("/case/**")
-            .antMatchers("/case-matching/**")
-            .antMatchers("/caveat/**")
-            .antMatchers("/data-extract/**")
-            .antMatchers("/document/**")
-            .antMatchers("/transform-exception-record")
-            .antMatchers("/grant/**")
-            .antMatchers("/nextsteps/**")
-            .antMatchers("/notify/**")
-            .antMatchers("/forms/**")
-            .antMatchers("/template/**")
-            .antMatchers("/probateManTypes/**")
-            .antMatchers("/legacy/**")
-            .antMatchers("/standing-search/**")
-            .and()
-            .addFilter(authCheckerServiceOnlyFilter)
-            .csrf().disable()
-            .formLogin().disable()
-            .logout().disable()
-            .authorizeRequests()
-            .anyRequest().authenticated();
-
-    }
 }
