fb7a0f18c31b58d58c99bb3c6d2bf6f12d68defa
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-04-23 12:01:44 +0100

    PRO-7311: added warnings to OCR exception

--- src/main/java/uk/gov/hmcts/probate/exception/OCRMappingException.java
+++ src/main/java/uk/gov/hmcts/probate/exception/OCRMappingException.java
@@ -3,15 +3,25 @@ package uk.gov.hmcts.probate.exception;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class OCRMappingException extends RuntimeException {
+
+    List<String> warnings = new ArrayList<>();
 
     public OCRMappingException(String message) {
         super(message);
     }
 
-    public OCRMappingException(String message, Throwable t) {
-        super(message, t);
+    public OCRMappingException(String message, List<String> warnings) {
+        super(message);
+        this.warnings = warnings;
+    }
+    
+    public List<String> getWarnings() {
+        return warnings;
     }
 }
