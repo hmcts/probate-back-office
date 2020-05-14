a0f9c67f00978c7bd125a312d7dac2bce576214d
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-05-14 16:01:06 +0100

    PRO-7366: fix test failure

--- src/test/java/uk/gov/hmcts/probate/service/CaseQueryServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/CaseQueryServiceTest.java
@@ -59,9 +59,8 @@ public class CaseQueryServiceTest {
         MockitoAnnotations.initMocks(this);
 
         when(serviceAuthTokenGenerator.generate()).thenReturn("Bearer 321");
-        when(idamAuthenticateUserService.getIdamOauth2Token()).thenReturn("Bearer 123");
+        when(idamAuthenticateUserService.getIdamOauth2Toke  n()).thenReturn("Bearer 123");
         when(headers.getAuthorizationHeaders()).thenReturn(new HttpHeaders());
-        when(headers.getElasticSearchAuthorizationHeaders()).thenReturn(new HttpHeaders());
 
         when(ccdDataStoreAPIConfiguration.getHost()).thenReturn("http://localhost");
         when(ccdDataStoreAPIConfiguration.getCaseMatchingPath()).thenReturn("/path");
