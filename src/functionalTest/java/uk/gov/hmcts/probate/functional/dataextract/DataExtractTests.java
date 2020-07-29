ebed1e49ca6e19bfc06e8ac47fc0d3308448320a
Megan <meganaliceslattery@gmail.com>
2020-07-29 14:14:50 +0100

    Apply fix to data extract tests

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/dataextract/DataExtractTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/dataextract/DataExtractTests.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.probate.functional.dataextract;
 
 import io.restassured.RestAssured;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.thucydides.core.annotations.Pending;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +10,7 @@ import uk.gov.hmcts.probate.functional.IntegrationTestBase;
 @RunWith(SpringIntegrationSerenityRunner.class)
 public class DataExtractTests extends IntegrationTestBase {
     private static final String IRONMOUNTAIN_URL = "/data-extract/iron-mountain";
-    private static final String EXCELA_URL = "/data-extract/excela";
+    private static final String EXCELA_URL = "/data-extract/exela";
 
     @Value("${probate.caseworker.email}")
     private String email;
@@ -22,63 +21,58 @@ public class DataExtractTests extends IntegrationTestBase {
     @Value("${probate.caseworker.id}")
     private Integer id;
 
-    @Pending
     @Test
-    public void verifyValidRequestReturnsOkStatusForIronMountain() {
+    public void verifyValidDateRequestReturnsOkStatusForIronMountain() {
         RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
-                password, id))
+                password, id)).queryParam("date", "2019-02-03")
                 .when()
                 .post(IRONMOUNTAIN_URL)
                 .then().assertThat().statusCode(200);
     }
 
-    @Pending
     @Test
-    public void verifyValidDateRequestReturnsOkStatusForIronMountain() {
+    public void verifyNoDateFormatReturnsBadRequestForIronMountain() {
         RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
                 password, id))
                 .when()
-                .post(IRONMOUNTAIN_URL + "/2019-02-03")
-                .then().assertThat().statusCode(200);
-    }
-
-    @Pending
-    @Test
-    public void verifyIncorrectDateFormatReturnsBadRequestForIronMountain() {
-        RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
-                password, id))
-                .when()
-                .post(IRONMOUNTAIN_URL + "/2019-2-2")
+                .post(IRONMOUNTAIN_URL)
                 .then().assertThat().statusCode(400);
     }
 
-    @Pending
     @Test
-    public void verifyValidRequestReturnsOkStatusForExcela() {
+    public void verifyIncorrectDateFormatReturnsBadRequestForIronMountain() {
         RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
-                password, id))
+                password, id)).queryParam("date", "2019-2-2")
+                .when()
+                .post(IRONMOUNTAIN_URL)
+                .then().assertThat().statusCode(400);
+    }
+
+    @Test
+    public void verifyValidDateRequestReturnsOkStatusForExcela() {
+        RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
+                password, id)).queryParam("date", "2019-02-03")
                 .when()
                 .post(EXCELA_URL)
                 .then().assertThat().statusCode(200);
     }
 
-    @Pending
     @Test
-    public void verifyValidDateRequestReturnsOkStatusForExcela() {
+    public void verifyNoDateFormatReturnsBadRequestForExcela() {
         RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
                 password, id))
                 .when()
-                .post(EXCELA_URL + "/2019-02-03")
-                .then().assertThat().statusCode(200);
+                .post(EXCELA_URL)
+                .then().assertThat().statusCode(400);
     }
 
-    @Pending
     @Test
     public void verifyIncorrectDateFormatReturnsBadRequestForExcela() {
         RestAssured.given().relaxedHTTPSValidation().headers(utils.getHeaders(email,
-                password, id))
+                password, id)).queryParam("date", "2019-2-2")
                 .when()
-                .post(EXCELA_URL + "/2019-2-2")
+                .post(EXCELA_URL)
                 .then().assertThat().statusCode(400);
     }
+
 }
