5b2770e26ee2e38eee4e1c389ac4415165d5c819
KleoG <kleogeorgiou@hotmail.com>
2020-08-05 10:42:56 +0100

    Added/updated functional tests for bulk scanning automation

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/bulkscanning/SolBaCcdServiceBulkScanningTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/bulkscanning/SolBaCcdServiceBulkScanningTests.java
@@ -281,6 +281,147 @@ public class SolBaCcdServiceBulkScanningTests extends IntegrationTestBase {
         transformExceptionPostUnprocessed(jsonRequest, jsonResponse);
     }
 
+    @Test
+    public void testTransformPA8AReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordPA8AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputPA8A.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCombinedCitizenPA8AReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CaveatCallbackResponseTransformer.dateTimeFormatter);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombCitizenPA8AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputCombCitizenPA8A.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCombinedSolicitorPA8AReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CaveatCallbackResponseTransformer.dateTimeFormatter);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombSolicitorPA8AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputCombSolicitorPA8A.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformPA1PReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordPA1PAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputPA1P.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCombinedCitizenPA1PReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombCitizenPA1PAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputCombCitizenPA1P.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCombinedSolicitorPA1PReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombSolicitorPA1PAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputCombSolicitorPA1P.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformPA1AReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordPA1AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputPA1A.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testUpdateCaseExtendCaveatPA8AReturnSuccessfulAutomatedJSON() throws IOException {
+        String currentDate = LocalDate.now().format(CaveatCallbackResponseTransformer.dateTimeFormatter);
+        String expiryDate7DaysFromNow = LocalDate.now().plusDays(7).format(CaveatCallbackResponseTransformer.dateTimeFormatter);
+        String expectedExpiryDate6MonthsFromNow = LocalDate.now().plusDays(7).plusMonths(6).format(CaveatCallbackResponseTransformer.dateTimeFormatter);
+        String expiryDate = "\"expiryDate\":\"" + expiryDate7DaysFromNow + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanUpdateCaseExceptionRecordExtendExpiryPA8AAutomated.json");
+        jsonRequest = jsonRequest.replaceAll("\"expiryDate\":\"[0-9-]+\"", expiryDate);
+        JsonPath jsonPath = fetchJsonPathUpdatedCaveatDetailsFromCaseFromException(jsonRequest);
+
+        // Unable to use static file as documents are generated in the response, picking out specific values instead.
+        Assert.assertEquals("Correct applicationType", "Personal", jsonPath.get("case_update_details.case_data.applicationType"));
+        Assert.assertEquals("Correct paperForm", "Yes", jsonPath.get("case_update_details.case_data.paperForm"));
+        Assert.assertEquals("Correct expiry date", expectedExpiryDate6MonthsFromNow, jsonPath.get("case_update_details.case_data.expiryDate"));
+        Assert.assertEquals("Correct registry", "ctsc", jsonPath.get("case_update_details.case_data.registryLocation"));
+
+        // Checked Scanned Documents
+        Assert.assertEquals("Correct number scanned docs", 2, jsonPath.getList("case_update_details.case_data.scannedDocuments").size());
+        Assert.assertEquals("Correct DCN Scan Doc 1", "19365040100100002", jsonPath.get("case_update_details.case_data.scannedDocuments[0].value.controlNumber"));
+        Assert.assertEquals("Correct DCN Scan Doc 2", "123135453645", jsonPath.get("case_update_details.case_data.scannedDocuments[1].value.controlNumber"));
+
+        // Checked Generated Notification Documents
+        Assert.assertEquals("Correct number generated notifications", 2, jsonPath.getList("case_update_details.case_data.notificationsGenerated").size());
+        Assert.assertEquals("Correct DocumentType Doc 1", "sentEmail", jsonPath.get("case_update_details.case_data.notificationsGenerated[0].value.DocumentType"));
+        Assert.assertEquals("Correct DocumentType Doc 2", "sentEmail", jsonPath.get("case_update_details.case_data.notificationsGenerated[1].value.DocumentType"));
+    }
+
+    @Test
+    public void testUpdateCaseExtendCaveatPA8AReturnExpiredErrorAutomatedJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanUpdateCaseExceptionRecordExtendExpiryPA8AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanUpdateCaseExceptionRecordExpiredCaveatErrorPA8A.json");
+        updateCaseFromExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testUpdateCaseExtendCaveatPA8AReturnOutsideOneMonthExpiryErrorAutomatedJSON() {
+        String expiryDate3MonthsFromNow = LocalDate.now().plusMonths(3).format(CaveatCallbackResponseTransformer.dateTimeFormatter);
+        String expireDate = "\"expiryDate\":\"" + expiryDate3MonthsFromNow + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanUpdateCaseExceptionRecordExtendExpiryPA8AAutomated.json");
+        jsonRequest = jsonRequest.replaceAll("\"expiryDate\":\"[0-9-]+\"", expireDate);
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanUpdateCaseExceptionRecordExpiryOutsideOneMonthErrorPA8A.json");
+        updateCaseFromExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCombinedCitizenPA1AReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombCitizenPA1AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputCombCitizenPA1A.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCombinedSolicitorPA1AReturnSuccessfulAutomatedJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombSolicitorPA1AAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputCombSolicitorPA1A.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformPA8AReturnTransformErrorAutomatedJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordErrorAutomated.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputError.json");
+        transformExceptionPostUnprocessed(jsonRequest, jsonResponse);
+    }
+
     private void transformExceptionPostUnprocessed(String bodyText, String containsText) {
         RestAssured.given()
             .relaxedHTTPSValidation()
