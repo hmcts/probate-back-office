74443bced3c749feeae0f063ef91082e7ace4b2a
KleoG <kleogeorgiou@hotmail.com>
2020-08-20 15:10:41 +0100

    Added functional tests and payloads

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/bulkscanning/SolBaCcdServiceBulkScanningTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/bulkscanning/SolBaCcdServiceBulkScanningTests.java
@@ -201,6 +201,23 @@ public class SolBaCcdServiceBulkScanningTests extends IntegrationTestBase {
     }
 
     @Test
+    public void testTransformSolicitorPA1PReturnSuccessfulJSON() {
+        String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
+        String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordSolicitorPA1P.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputSolicitorPA1P.json");
+        jsonResponse = jsonResponse.replaceAll("\"applicationSubmittedDate\":\"[0-9-]+\"", applicationSubmittedDate);
+        transformExceptionPostSuccess(jsonRequest, jsonResponse);
+    }
+
+    @Test
+    public void testTransformCitizenPA1PReturnUnprocessedJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordPA1PUnprocessed.json");
+        jsonResponse = utils.getJsonFromFile("expectedBulkScanTransformExceptionRecordOutputPA1PUnprocessed.json");
+        transformExceptionPostUnprocessed(jsonRequest, jsonResponse);
+    }
+
+    @Test
     public void testTransformPA1AReturnSuccessfulJSON() {
         String currentDate = LocalDate.now().format(CCD_DATE_FORMAT);
         String applicationSubmittedDate = "\"applicationSubmittedDate\":\"" + currentDate + "\"";
@@ -222,33 +239,6 @@ public class SolBaCcdServiceBulkScanningTests extends IntegrationTestBase {
 
         // Unable to use static file as documents are generated in the response, picking out specific values instead.
         Assert.assertEquals("Correct applicationType", "Personal", jsonPath.get("case_update_details.case_data.applicationType"));
-        Assert.assertEquals("Correct paperForm", "Yes", jsonPath.get("case_update_details.case_data.paperForm"));
-        Assert.assertEquals("Correct expiry date", expectedExpiryDate6MonthsFromNow, jsonPath.get("case_update_details.case_data.expiryDate"));
-        Assert.assertEquals("Correct registry", "ctsc", jsonPath.get("case_update_details.case_data.registryLocation"));
-
-        // Checked Scanned Documents
-        Assert.assertEquals("Correct number scanned docs", 2, jsonPath.getList("case_update_details.case_data.scannedDocuments").size());
-        Assert.assertEquals("Correct DCN Scan Doc 1", "19365040100100002", jsonPath.get("case_update_details.case_data.scannedDocuments[0].value.controlNumber"));
-        Assert.assertEquals("Correct DCN Scan Doc 2", "123135453645", jsonPath.get("case_update_details.case_data.scannedDocuments[1].value.controlNumber"));
-
-        // Checked Generated Notification Documents
-        Assert.assertEquals("Correct number generated notifications", 2, jsonPath.getList("case_update_details.case_data.notificationsGenerated").size());
-        Assert.assertEquals("Correct DocumentType Doc 1", "sentEmail", jsonPath.get("case_update_details.case_data.notificationsGenerated[0].value.DocumentType"));
-        Assert.assertEquals("Correct DocumentType Doc 2", "sentEmail", jsonPath.get("case_update_details.case_data.notificationsGenerated[1].value.DocumentType"));
-    }
-
-    @Test
-    public void testUpdateCaseExtendCaveatSolicitorPA8AReturnSuccessfulJSON() throws IOException {
-        String currentDate = LocalDate.now().format(CaveatCallbackResponseTransformer.dateTimeFormatter);
-        String expiryDate7DaysFromNow = LocalDate.now().plusDays(7).format(CaveatCallbackResponseTransformer.dateTimeFormatter);
-        String expectedExpiryDate6MonthsFromNow = LocalDate.now().plusDays(7).plusMonths(6).format(CaveatCallbackResponseTransformer.dateTimeFormatter);
-        String expiryDate = "\"expiryDate\":\"" + expiryDate7DaysFromNow + "\"";
-        jsonRequest = utils.getJsonFromFile("bulkScanUpdateCaseExceptionRecordExtendExpirySolicitorPA8A.json");
-        jsonRequest = jsonRequest.replaceAll("\"expiryDate\":\"[0-9-]+\"", expiryDate);
-        JsonPath jsonPath = fetchJsonPathUpdatedCaveatDetailsFromCaseFromException(jsonRequest);
-
-        // Unable to use static file as documents are generated in the response, picking out specific values instead.
-        Assert.assertEquals("Correct applicationType", "Solicitor", jsonPath.get("case_update_details.case_data.applicationType"));
         Assert.assertEquals("Correct paperForm", "Yes", jsonPath.get("case_update_details.case_data.paperForm"));
         Assert.assertEquals("Correct expiry date", expectedExpiryDate6MonthsFromNow, jsonPath.get("case_update_details.case_data.expiryDate"));
         Assert.assertEquals("Correct registry", "ctsc", jsonPath.get("case_update_details.case_data.registryLocation"));
@@ -463,6 +453,30 @@ public class SolBaCcdServiceBulkScanningTests extends IntegrationTestBase {
         transformExceptionPostUnprocessed(jsonRequest, jsonResponse);
     }
 
+    @Test
+    public void testTransformPA1AReturnTransformForbiddenJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombSolicitorPA1AAutomated.json");
+        transformExceptionPostForbidden(jsonRequest);
+    }
+
+    @Test
+    public void testUpdatePA1AReturnTransformForbiddenJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombSolicitorPA1AAutomated.json");
+        updateExceptionPostForbidden(jsonRequest);
+    }
+
+    @Test
+    public void testTransformPA8AReturnTransformForbiddenJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordPA8A.json");
+        transformExceptionPostForbidden(jsonRequest);
+    }
+
+    @Test
+    public void testUpdatePA8AReturnTransformForbiddenJSON() {
+        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordPA8A.json");
+        updateExceptionPostForbidden(jsonRequest);
+    }
+
     private void transformExceptionPostUnprocessed(String bodyText, String containsText) {
         RestAssured.given()
             .relaxedHTTPSValidation()
@@ -472,4 +486,39 @@ public class SolBaCcdServiceBulkScanningTests extends IntegrationTestBase {
             .then().assertThat().statusCode(422)
             .and().content(containsString(containsText));
     }
+
+    private void transformExceptionPostForbidden(String bodyText) {
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders("dummyServiceToken"))
+                .body(bodyText)
+                .when().post(TRANSFORM_EXCEPTON_RECORD)
+                .then().assertThat().statusCode(403);
+    }
+
+
+    private void updateExceptionPostForbidden(String bodyText) {
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders("dummyServiceToken"))
+                .body(bodyText)
+                .when().post(UPDATE_CASE_FROM_EXCEPTON_RECORD)
+                .then().assertThat().statusCode(403);
+    }
+
+
+//    @Test
+//    public void test401test() {
+//        jsonRequest = utils.getJsonFromFile("bulkScanTransformExceptionRecordCombSolicitorPA1AAutomated.json");
+//        test401(jsonRequest);
+//    }
+//
+//    private void test401(String bodyText) {
+//        RestAssured.given()
+//                .relaxedHTTPSValidation()
+//                .headers(utils.getHeadersTEST())
+//                .body(bodyText)
+//                .when().post(TRANSFORM_EXCEPTON_RECORD)
+//                .then().assertThat().statusCode(401);
+//    }
 }
