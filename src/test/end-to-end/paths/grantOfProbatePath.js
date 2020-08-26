cf5d38b49319335e645693dc92fce28eb4f4ea98
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2020-08-26 16:28:12 +0100

    misspelling

--- src/test/end-to-end/paths/grantOfProbatePath.js
+++ src/test/end-to-end/paths/grantOfProbatePath.js
@@ -22,7 +22,7 @@ const documentUploadTabConfig = require('src/test/end-to-end/pages/caseDetails/g
 const examChecklistTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/examChecklistTabConfig');
 const grantNotificationsTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/grantNotificationsTabConfig');
 const historyTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/historyTabConfig');
-const copiesTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/CopiesTabConfig');
+const copiesTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/copiesTabConfig');
 
 const applicantDetailsUpdateTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/applicantDetailsUpdateTabConfig');
 const caseDetailsUpdateTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/caseDetailsUpdateTabConfig');
