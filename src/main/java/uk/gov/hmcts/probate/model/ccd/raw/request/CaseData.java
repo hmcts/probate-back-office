477aa83e180fd64b117886b932f3d45996ba41ee
KleoG <kleogeorgiou@hotmail.com>
2020-09-14 13:10:41 +0100

    Updated layout of intestacy journey. Notblank check is now not done to siblings question. Removed the word siblings from questions and error messages. Added DUMMY_VALUE_TO_HIDE_FIELD field show condition.

--- src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseData.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseData.java
@@ -233,7 +233,6 @@ public class CaseData {
     @NotBlank(groups = {ApplicationIntestacyGroup.class}, message = "{solsMinorityInterestIsNull}")
     private final String solsMinorityInterest;
 
-    @NotBlank(groups = {ApplicationIntestacyGroup.class}, message = "{solsApplicantSiblingsIsNull}")
     private final String solsApplicantSiblings;
 
     private final String boEmailDocsReceivedNotificationRequested;
