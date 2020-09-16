5113d1c6d71e2bd86cb2ba315fd644621b15110f
Linus Norton <linusnorton@gmail.com>
2020-09-16 15:08:15 +0100

    more debug

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -7,6 +7,10 @@ REDIRECT_URI=${IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
 CLIENT_ID="ccd_gateway"
 CLIENT_SECRET=${API_GATEWAY_IDAM_SECRET:-ccd_gateway_secret}
 
+echo curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | docker run --rm --interactive stedolan/jq -r .access_token
+
+curl -v ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d ""
+
 code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | docker run --rm --interactive stedolan/jq -r .code)
 
 curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | docker run --rm --interactive stedolan/jq -r .access_token
