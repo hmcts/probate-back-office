966c56609122d3ff51306e3f4c4aa5007d3098a1
Jack Munkonge <jackmunkonge@gmail.com>
2020-10-29 09:57:48 +0000

    Remove redundant code

--- src/main/java/uk/gov/hmcts/probate/htmlRendering/HeaderRenderer.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.probate.htmlRendering;
-
-import static java.lang.String.format;
-
-public class HeaderRenderer {
-    public String render(String headerText) {
-        return format("## %s", headerText);
-    }
-}
