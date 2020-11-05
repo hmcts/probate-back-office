8cd291c697bb82b43dd14c17ab7d74d2fe3c244a
Vincent Holt <vleholt@yahoo.co.uk>
2020-11-05 10:16:33 +0000

    remove commented code

--- src/main/java/uk/gov/hmcts/probate/model/htmlTemplate/StatusTagHtmlTemplate.java
+++ src/main/java/uk/gov/hmcts/probate/model/htmlTemplate/StatusTagHtmlTemplate.java
@@ -2,13 +2,4 @@ package uk.gov.hmcts.probate.model.htmlTemplate;
 
 public class StatusTagHtmlTemplate {
     public static String statusTag = "<p><img align=\"right\" height=\"25px\" src=\"<imgSrc/>\" alt=\"<imgAlt/>\" /></p>\n";
-            /*
-            "<table class=\"govuk-table\">\n" +
-        "<tbody class=\"govuk-table__body\">\n" +
-        "<tr class=\"govuk-table__row\">\n" +
-        "<td class=\"govuk-table__cell govuk-table__cell--numeric\"><img height=\"25px\" src=\"<imgSrc/>\" alt=\"<imgAlt/>\" /></td>\n" +
-        "</tr>\n" +
-        "</tbody>\n" +
-        "</table>";*/
-
 }
