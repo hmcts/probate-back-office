c6157f92875643c98bf7f7c4b5d98fb87a9d845e
Vincent Holt <vleholt@yahoo.co.uk>
2020-11-12 17:08:08 +0000

    Prepare data for first func test

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/caseprogress/SolCcdCaseProgressTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/caseprogress/SolCcdCaseProgressTests.java
@@ -1,2 +1,19 @@
-package uk.gov.hmcts.probate.functional.caseprogress;public class SolCcdCaseProgressTests {
+package uk.gov.hmcts.probate.functional.caseprogress;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.runner.RunWith;
+import uk.gov.hmcts.probate.functional.IntegrationTestBase;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+public class SolCcdCaseProgressTests extends IntegrationTestBase  {
+
+    private void validatePostSuccess(String jsonFileName, String URL) {
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeadersWithUserId())
+                .body(utils.getJsonFromFile(jsonFileName))
+                .when().post(URL)
+                .then().assertThat().statusCode(200);
+    }
 }
