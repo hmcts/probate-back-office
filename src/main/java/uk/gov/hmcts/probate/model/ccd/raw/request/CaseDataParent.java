1668e96817c46d513fe8405aa84b68d29a463ac9
Megan <meganaliceslattery@gmail.com>
2020-11-17 12:57:44 +0000

    Add parent class

---
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseDataParent.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.probate.model.ccd.raw.request;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@SuperBuilder
+@Data
+public class CaseDataParent {
+}
