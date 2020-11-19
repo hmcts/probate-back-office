4c03b009238dd08346bb4a81f3e3f2c621d9aa80
stelios <stelios.kalorkotis@hotmail.com>
2020-11-19 20:23:51 +0000

    Functional tests updates.

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/documents/SolBaCcdServiceDocumentsTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/documents/SolBaCcdServiceDocumentsTests.java
@@ -256,9 +256,18 @@ public class SolBaCcdServiceDocumentsTests extends IntegrationTestBase {
     @Test
     public void verifySuccessForWillLodgementForCardiff() {
         CaseData caseData = CaseData.builder().build();
-        String response = generateNonProbateDocument(DEFAULT_WILL_NO_DOCS_PAYLOAD, GENERATE_DEPOSIT_RECEIPT);
+        Response jsonResponse = RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeadersWithUserId())
+                .body(utils.getJsonFromFile("solicitorPayloadNotificationsGopCardiff.json"))
+                .when().post("/document/generate-deposit-receipt").andReturn();
 
-        String expectedText = utils.getJsonFromFile("gopGrantDraftForCardiffResponse.txt");
+        JsonPath jsonPath = JsonPath.from(jsonResponse.getBody().asString());
+        String documentUrl = jsonPath.get("data.documentsGenerated[0].value.DocumentLink.document_binary_url");
+        String response = utils.downloadPdfAndParseToString(documentUrl);
+        response = response.replace("\n", "").replace("\r", "");
+
+        String expectedText = utils.getJsonFromFile("willLodgementDepositReceiptResponse.txt");
         expectedText = expectedText.replace("\n", "").replace("\r", "");
         expectedText = expectedText.replaceAll("19th November 2020", caseData.convertDate(LocalDate.now()));
 
