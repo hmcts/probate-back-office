1a3da4188e3dc5a16693cfd9e563a0755f796d1e
Vincent Holt <vleholt@yahoo.co.uk>
2020-12-04 23:51:02 +0000

    increase the idam wait

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -16,5 +16,5 @@ module.exports = async function (useProfessionalUser, isAlreadyAtSignOnPage) {
     await I.fillField('username', useProfessionalUser ? testConfig.TestEnvProfUser : testConfig.TestEnvUser);
     await I.fillField('password', useProfessionalUser ? testConfig.TestEnvProfPassword : testConfig.TestEnvPassword);
 
-    await I.waitForNavigationToComplete('input[type="submit"]');
+    await I.waitForNavigationToComplete('input[type="submit"]', 10);
 };
