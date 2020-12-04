2ef9eaae21dcb3791afdab95c583ff46957bad31
Vincent Holt <vleholt@yahoo.co.uk>
2020-12-04 16:51:58 +0000

    add wait for next step ddl to be enabled

--- src/test/end-to-end/pages/nextStep/nextStep.js
+++ src/test/end-to-end/pages/nextStep/nextStep.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
+const testConfig = require('src/test/config.js');
 
 module.exports = async function (nextStep) {
 
     const I = this;
 
+    await I.waitForEnabled({css: '#next-step'}, testConfig.TestTimeToWaitForText || 60);
     await I.selectOption('#next-step', nextStep);
     await I.waitForNavigationToComplete(commonConfig.goButton);
 };
