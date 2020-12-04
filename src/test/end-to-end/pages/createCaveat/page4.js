beef8aff6e7c7fbc618f1c2d2b0c9383697bb5ff
Vincent Holt <vleholt@yahoo.co.uk>
2020-12-04 16:37:24 +0000

    wait for datefield to be enabled

--- src/test/end-to-end/pages/createCaveat/page4.js
+++ src/test/end-to-end/pages/createCaveat/page4.js
@@ -10,6 +10,7 @@ module.exports = async function (crud) {
 
     if (crud === 'update') {
         await I.waitForText(createCaveatConfig.page4_amend_waitForText, testConfig.TestTimeToWaitForText);
+        await I.waitForEnabled({css: '#expiryDate-day'}, testConfig.TestTimeToWaitForText);
 
         await I.fillField('#expiryDate-day', createCaveatConfig.page4_caveatExpiryDate_day_update);
         await I.fillField('#expiryDate-month', createCaveatConfig.page4_caveatExpiryDate_month_update);
