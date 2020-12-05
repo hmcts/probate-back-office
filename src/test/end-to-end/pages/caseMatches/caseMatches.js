d112f2695f6068050509d2ad316b28f088926f86
Vincent Holt <vleholt@yahoo.co.uk>
2020-12-05 08:06:07 +0000

    commonise case matching code

--- src/test/end-to-end/pages/caseMatches/caseMatches.js
+++ src/test/end-to-end/pages/caseMatches/caseMatches.js
@@ -3,15 +3,17 @@
 const testConfig = require('src/test/config.js');
 const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 
-module.exports = async function (caseRef, caseMatchesConfig, nextStepName) {
+module.exports = async function (caseRef, caseMatchesConfig, nextStepName, retainFirstItem=true) {
 
     const I = this;
     caseMatchesConfig.waitForText = nextStepName;
     await I.waitForText(caseMatchesConfig.waitForText, testConfig.TestTimeToWaitForText);
 
-    await I.see(caseRef);
+    await I.waitForText(caseRef, testConfig.TestTimeToWaitForText);
 
-    await I.waitForElement('#caseMatches_0_0');
+    await I.waitForElement('#caseMatches_0_0', testConfig.TestTimeToWaitForText);
+
+    await I.waitForVisible({css: '#caseMatches_0_valid-Yes'}, testConfig.TestTimeToWaitForText);
 
     const btnLocator = {css: 'button.button-secondary[aria-label^="Remove Possible case matches"]'};
     const actionBtnLocator = {css: 'button.action-button[title="Remove"]'};
@@ -23,7 +25,7 @@ module.exports = async function (caseRef, caseMatchesConfig, nextStepName) {
         // -1 to ignore previous button at bottom of page
         /* eslint-disable no-await-in-loop */
         const btnLocatorLastChild = {css: `${btnLocator.css}:last-child`};
-        for (let i = 0; i < numOfElements - 1; i++) {
+        for (let i = retainFirstItem ? 1 : 0; i < numOfElements; i++) {
             await I.waitForElement(btnLocatorLastChild);
             await I.click(btnLocatorLastChild);
             await I.waitForElement(actionBtnLocator);
@@ -38,5 +40,4 @@ module.exports = async function (caseRef, caseMatchesConfig, nextStepName) {
 
     await I.waitForEnabled(commonConfig.continueButton);
     await I.waitForNavigationToComplete(commonConfig.continueButton);
-
 };
