516c05c908fb20caf59420fa98e3e4d04450564f
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-12-10 14:03:50 +0000

    DTSPB-947: updated FTs + CTs5

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/notifications/SolBaCcdServiceNotificationTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/notifications/SolBaCcdServiceNotificationTests.java
@@ -373,8 +373,7 @@ public class SolBaCcdServiceNotificationTests extends IntegrationTestBase {
     }
 
     private void verifyPAEmailInformationRequestRedec(String document) {
-        log.info("document:"+document);
-        assertTrue(document.contains("solicitor@probate-test.com"));
+        assertTrue(document.contains("primary@probate-test.com"));
         assertTrue(document.contains("Deceased First Name Deceased Last Name"));
         assertTrue(document.contains("Birmingham"));
         assertTrue(document.contains(BIRMINGHAM_NO));
