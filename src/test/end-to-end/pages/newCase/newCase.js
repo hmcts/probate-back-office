421357affa4431db5911f465bfbc8a708a87a573
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-12-11 16:45:17 +0000

    update await async on new case

--- src/test/end-to-end/pages/newCase/newCase.js
+++ src/test/end-to-end/pages/newCase/newCase.js
@@ -3,12 +3,12 @@
 const testConfig = require('src/test/config');
 const newCaseConfig = require('./newCaseConfig');
 
-module.exports = function () {
+module.exports = async function () {
 
     const I = this;
 
-    I.waitForText(newCaseConfig.waitForText, testConfig.TestTimeToWaitForText);
+    await I.waitForText(newCaseConfig.waitForText, testConfig.TestTimeToWaitForText);
 
     // eslint-disable-next-line
-    I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator);
+    await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator);
 };
