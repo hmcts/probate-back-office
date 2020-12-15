62303796d7483102caabb232c674448397d48aff
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-12-15 14:55:32 +0000

    DTSPB-963: updated PaymentFee processing for creaidt account payment

--- src/main/java/uk/gov/hmcts/probate/service/payments/CreditAccountPaymentTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/service/payments/CreditAccountPaymentTransformer.java
@@ -1,45 +1,68 @@
 package uk.gov.hmcts.probate.service.payments;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.hmcts.probate.model.fee.FeesResponse;
 import uk.gov.hmcts.probate.model.payments.CreditAccountPayment;
 import uk.gov.hmcts.probate.model.payments.PaymentFee;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CreditAccountPaymentTransformer {
+    private static final String PROBATE_SOLICITOR_PAYMENT_DESCRIPTION = "Probate Solicitor payment";
+
+    private PaymentFeeBuilder paymentFeeBuilder;
+    
+    @Value("${fee.api.service}")
+    private String service;
+
+    @Value("${payment.currency}")
+    private String currency;
+
+    @Value("${payment.siteId}")
+    private String siteId;
 
     public CreditAccountPayment transform(CaseDetails caseDetails, FeesResponse feesResponse) {
-        List<PaymentFee> paymentFees = buildFees(feesResponse);
+        List<PaymentFee> paymentFees = buildFees(caseDetails.getData(), feesResponse);
         CreditAccountPayment creditAccountPayment = CreditAccountPayment.builder()
             .accountNumber(caseDetails.getData().getSolsPBANumber().getValue().getCode())
             .fees(paymentFees)
-            //.caseReference()
-            //.amount(getTotalAmount(paymentFees))
+            .caseReference(caseDetails.getData().getSolsSolicitorAppReference())
+            .amount(feesResponse.getTotalAmount())
             .ccdCaseNumber(caseDetails.getId().toString())
-            .currency("GBP")
-            .description("desc")
-            .organisationName("org")
-            .service("probate_backend")
-            .siteId("siteId")
+            .currency(currency)
+            .description(PROBATE_SOLICITOR_PAYMENT_DESCRIPTION)
+            .organisationName(caseDetails.getData().getSolsSolicitorFirmName())
+            .service(service)
+            .siteId(siteId)
             .customerReference(caseDetails.getData().getSolsSolicitorAppReference())
             .build();
-        
+
         return creditAccountPayment;
     }
 
-    private List<PaymentFee> buildFees(FeesResponse feesResponse) {
+    
+    private List<PaymentFee> buildFees(CaseData caseData, FeesResponse feesResponse) {
         ArrayList<PaymentFee> paymentFees = new ArrayList<>();
-        PaymentFee applicationFee = PaymentFee.buildPaymentFee(feesResponse.getApplicationFeeResponse());
-        PaymentFee ukCopiesFee = PaymentFee.buildPaymentFee(feesResponse.getUkCopiesFeeResponse());
-        PaymentFee overseasCopiesFee = PaymentFee.buildPaymentFee(feesResponse.getOverseasCopiesFeeResponse());
+        PaymentFee applicationFee = paymentFeeBuilder.buildPaymentFee(feesResponse.getApplicationFeeResponse(), BigDecimal.ONE);
         paymentFees.add(applicationFee);
-        paymentFees.add(ukCopiesFee);
-        paymentFees.add(overseasCopiesFee);
+
+        if (caseData.getExtraCopiesOfGrant() > 0) {
+            PaymentFee ukCopiesFee = paymentFeeBuilder.buildPaymentFee(feesResponse.getUkCopiesFeeResponse(),
+                BigDecimal.valueOf(caseData.getExtraCopiesOfGrant()));
+            paymentFees.add(ukCopiesFee);
+        }
+        if (caseData.getExtraCopiesOfGrant() > 0) {
+            PaymentFee overseasCopiesFee = paymentFeeBuilder.buildPaymentFee(feesResponse.getOverseasCopiesFeeResponse(),
+                BigDecimal.valueOf(caseData.getOutsideUKGrantCopies()));
+            paymentFees.add(overseasCopiesFee);
+        }
 
         return paymentFees;
     }
---
+++ src/test/java/uk/gov/hmcts/probate/service/payments/PaymentFeeBuilderTest.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.probate.service.payments;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.probate.config.FeeServiceConfiguration;
+import uk.gov.hmcts.probate.model.fee.FeeResponse;
+import uk.gov.hmcts.probate.model.payments.PaymentFee;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class PaymentFeeBuilderTest {
+    @Mock
+    private FeeServiceConfiguration feeServiceConfiguration;
+
+    @InjectMocks
+    private PaymentFeeBuilder paymentFeeBuilder;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        when(feeServiceConfiguration.getJurisdiction1()).thenReturn("feeJurisdiction1");
+        when(feeServiceConfiguration.getJurisdiction2()).thenReturn("feeJurisdiction2");
+    }
+
+    @Test
+    public void shouldBuildPaymentFeeFromFeeResponse() {
+        FeeResponse feeResponse = FeeResponse.builder().feeAmount(BigDecimal.TEN).code("feeCode").description("feeDescription").version(
+            "feeVersion").build();
+        BigDecimal volume = BigDecimal.valueOf(2);
+        PaymentFee paymentFee = paymentFeeBuilder.buildPaymentFee(feeResponse, volume);
+        assertEquals("feeCode", paymentFee.getCode());
+        assertEquals("feeDescription", paymentFee.getDescription());
+        assertEquals("feeJurisdiction1", paymentFee.getJurisdiction1());
+        assertEquals("feeJurisdiction2", paymentFee.getJurisdiction2());
+        assertEquals("feeVersion", paymentFee.getVersion());
+        assertEquals(20, paymentFee.getCalculated_amount().longValue());
+        assertEquals(10, paymentFee.getFee_amount().longValue());
+        assertEquals(2, paymentFee.getVolume().longValue());
+
+    }
+
+}
