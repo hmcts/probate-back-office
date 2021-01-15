95fa0580fe3f11720f5d56c750110bf884e1776a
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-01-15 11:32:54 +0000

    update config

--- src/test/end-to-end/paths/solicitor/applyGrantOfProbateMultiExecutor.js
+++ src/test/end-to-end/paths/solicitor/applyGrantOfProbateMultiExecutor.js
@@ -26,7 +26,7 @@ Scenario('Solicitor - Apply Grant of probate Multi Executor', async function (I)
     let nextStepName = 'Deceased details';
     let endState = 'Application created';
     await I.selectNewCase();
-    await I.selectCaseTypeOptions(createCaseConfig.list1_text, createCaseConfig.list2_text_gor, createCaseConfig.list3_text_gor);
+    await I.selectCaseTypeOptions(createCaseConfig.list1_text, createCaseConfig.list2_text_gor, createCaseConfig.list3_text_solGor);
     await I.applyForProbatePage1();
     await I.applyForProbatePage2();
     await I.cyaPage();
