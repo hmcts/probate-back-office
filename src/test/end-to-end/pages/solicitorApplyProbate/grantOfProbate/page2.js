bc4a536da44f8f0c1a0830ede2e2ce09accb1b47
Megan <meganaliceslattery@gmail.com>
2021-01-18 13:16:40 +0000

    Add trust corp fields to page2 of grantOfProbate sols

--- src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page2.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page2.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const grantOfProbateConfig = require('./grantOfProbate');
+const testConfig = require('src/test/config.js');
 const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 
 module.exports = async function () {
@@ -11,5 +12,21 @@ module.exports = async function () {
     await I.waitForClickable({css: '#titleAndClearingType-TCTTrustCorpResWithApp'});
     await I.click({css: '#titleAndClearingType-TCTTrustCorpResWithApp'});
 
+    await I.waitForElement('#trustCorpName');
+    await I.fillField('#trustCorpName', grantOfProbateConfig.page2_nameOfTrustCorp);
+    await I.fillField('#actingTrustCorpName', grantOfProbateConfig.page2_executorFirstName);
+    await I.fillField('#positionInTrustCorp', grantOfProbateConfig.page2_positionInTrustCorp);
+
+    await I.click(`#othersActingForTrustCorp-${grantOfProbateConfig.optionYes}`);
+    await I.waitForText(grantOfProbateConfig.page2_waitForAdditionPerson, testConfig.TestTimeToWaitForText);
+    await I.click('#othersActingForTrustCorpList > div > button');
+    await I.fillField('#othersActingForTrustCorpList_0_otherActingForTrustCorpName', grantOfProbateConfig.page2_executorFirstName);
+    await I.fillField('#othersActingForTrustCorpList_0_otherActingForTrustCorpPosition', grantOfProbateConfig.page2_positionInTrustCorp);
+
+    await I.fillField('#lodgementAddress', grantOfProbateConfig.page2_lodgementAddress);
+    await I.fillField('#lodgementAddress', grantOfProbateConfig.page2_lodgementAddress);
+    await I.fillField('#lodgementDate-day', grantOfProbateConfig.page2_lodgementDate_day);
+    await I.fillField('#lodgementDate-month', grantOfProbateConfig.page2_lodgementDate_month);
+    await I.fillField('#lodgementDate-year', grantOfProbateConfig.page2_lodgementDate_year);
     await I.waitForNavigationToComplete(commonConfig.continueButton);
 };
