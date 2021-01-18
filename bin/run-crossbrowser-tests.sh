2035462eec40a61505ab56355ea4d3053d9c8e0e
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-01-18 16:13:27 +0000

    update click tab

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -12,6 +12,9 @@ if [[ "$BROWSER_GROUP" == "" ]]
 then
     EXIT_STATUS=0
     BROWSER_GROUP=chrome yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+    BROWSER_GROUP=firefox yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+    BROWSER_GROUP=safari yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+    BROWSER_GROUP=microsoft yarn test-crossbrowser-e2e || EXIT_STATUS=$?
     echo EXIT_STATUS: $EXIT_STATUS
     exit $EXIT_STATUS
 else
