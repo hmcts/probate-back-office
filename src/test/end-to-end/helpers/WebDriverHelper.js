22ab4fade5eb89811a6c8d3f234dfe9609691290
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-01-19 17:13:55 +0000

    update force click

--- src/test/end-to-end/helpers/WebDriverHelper.js
+++ src/test/end-to-end/helpers/WebDriverHelper.js
@@ -57,11 +57,28 @@ class WebDriverHelper extends Helper {
         await Promise.resolve();
     }
 
+
     async clickTab(tabTitle) {
+
         const helper = this.helpers.WebDriver;
         const tabXPath = `//div[text()='${tabTitle}']`;
-        await helper.waitForClickable(tabXPath, testConfig.TestTimeToWaitForText);
-        await helper.forceClick(tabXPath);
+        const elements = await helper._locateClickable(tabXPath);
+        const selector = elements[0].selector;
+
+        helper.executeScript(function (el) {
+            (
+                function(expression, parentElement) {
+                    const r = [];
+                    const x = document.evaluate(expression, parentElement ||
+                      document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+                    for (let i = 0, l = x.snapshotLength; i < l; i++) {
+                        r.push(x.snapshotItem(i));
+                    }
+                    return r;
+                }
+            )(el)[0].click();
+        }, selector);
     }
 }
+
 module.exports = WebDriverHelper;
