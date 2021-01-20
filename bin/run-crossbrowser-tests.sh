69fcebb6b58590cd717556ed833373490e704e35
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-01-20 08:36:39 +0000

    remove exit status

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -16,7 +16,6 @@ then
     BROWSER_GROUP=safari yarn test-crossbrowser-e2e || EXIT_STATUS=$?
     BROWSER_GROUP=microsoft yarn test-crossbrowser-e2e || EXIT_STATUS=$?
     echo EXIT_STATUS: $EXIT_STATUS
-    exit $EXIT_STATUS
 else
     # Compatible with Jenkins parallel crossbrowser pipeline
     yarn test-crossbrowser-e2e
