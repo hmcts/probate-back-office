6f2498fbb9a61de76dc2fca2bffcc56a627b551b
Vincent Holt <vleholt@yahoo.co.uk>
2021-01-27 10:15:30 +0000

    remove trailing comma

--- src/test/end-to-end/pages/steps.js
+++ src/test/end-to-end/pages/steps.js
@@ -80,6 +80,6 @@ module.exports = function () {
         caveatApplicationDetailsPage2: steps.solicitorApplyCaveat.applicationDetails.page2,
         completeCaveatApplicationPage1: steps.solicitorApplyCaveat.completeApplication.page1,
         completeCaveatApplicationPage2: steps.solicitorApplyCaveat.completeApplication.page2,
-        completeCaveatApplicationPage3: steps.solicitorApplyCaveat.completeApplication.page3,
+        completeCaveatApplicationPage3: steps.solicitorApplyCaveat.completeApplication.page3
     });
 };
