7a0693ba4c27f2e43436576801f184016e01c93b
Vincent Holt <vleholt@yahoo.co.uk>
2021-01-29 21:37:26 +0000

    amend wait test to allow for exui difference

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -9,15 +9,7 @@ module.exports = async function (useProfessionalUser, isAlreadyAtSignOnPage) {
         await I.amOnLoadedPage('/');
     }
 
-    const un = testConfig.TestEnvProfUser.substr(0, testConfig.TestEnvProfUser.length - 1);
-    const x = process.env.PROF_USER_EMAIL.substr(0, process.env.PROF_USER_EMAIL.length - 1);
-
-    /* eslint-disable no-console */
-    console.info (`1 = ${un}`);
-    console.info (`2 = ${x}`);
-
-    const textToWaitFor = useProfessionalUser ? 'Sign in or create an account' : 'Sign in';
-    await I.waitForText(textToWaitFor);
+    await I.waitForText('Sign in');
 
     await I.fillField('#username', useProfessionalUser ? testConfig.TestEnvProfUser : testConfig.TestEnvUser);
     await I.fillField('#password', useProfessionalUser ? testConfig.TestEnvProfPassword : testConfig.TestEnvPassword);
