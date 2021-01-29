368feb323126f18587bfcae88f8f78701de9840e
Vincent Holt <vleholt@yahoo.co.uk>
2021-01-29 17:21:28 +0000

    add console output

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -9,6 +9,13 @@ module.exports = async function (useProfessionalUser, isAlreadyAtSignOnPage) {
         await I.amOnLoadedPage('/');
     }
 
+    const un = testConfig.TestEnvProfUser.substr(0, testConfig.TestEnvProfUser.length - 1);
+    const x = process.env.PROF_USER_EMAIL.substr(0, process.env.PROF_USER_EMAIL.length - 1);
+
+    /* eslint-disable no-console */
+    console.info (`1 = ${un}`);
+    console.info (`2 = ${x}`);
+
     const textToWaitFor = useProfessionalUser ? 'Sign in or create an account' : 'Sign in';
     await I.waitForText(textToWaitFor);
 
