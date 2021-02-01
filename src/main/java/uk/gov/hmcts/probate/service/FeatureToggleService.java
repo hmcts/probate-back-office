403eb613099f939129c947c57f2f267500a766be
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-02-01 14:39:22 +0000

    DTSPB-1338 removed the unused import

--- src/main/java/uk/gov/hmcts/probate/service/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/probate/service/FeatureToggleService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class FeatureToggleService {
 
