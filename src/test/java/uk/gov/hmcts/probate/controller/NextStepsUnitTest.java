ecdceb099481a8b8e9847d35df33af3f2b6cb780
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-02-11 17:03:49 +0000

    DTSPB-1087 check style and java plugin (#1321)
    * DTSPB-1087 Initial commit
    
    * DTSPB-1087 resolved checkstyle for few more files
    
    * DTSPB-1087 Completes the checkstyle issues for CheckstyleMain task
    
    * DTSPB-1087 initial checkstyle fixes for tests
    
    * DTSPB-1087 fixes for test and testCreator checkstyles
    
    * DTSPB-1087 resolved caseCreator and functional test checkstyles
    
    * DTSPB-1087 more functional test checkstyles fixed
    
    * DTSPB-1087 all checkstyles errors fixed
    
    * DTSPB-1087 correct changes related to utility classes
    
    * DTSPB-1087 updated BulkPrintServiceTest which were failing in pipeline
    
    * DTSPB-1087 removing checkstyle.xml as it would be taken care by the hmcts java plugin
    
    * DTSPB-1087 add owasp dependency checker plugin and update version of spring-dependency-management plgin version
    
    * Rewritten the dependency supression file and removed the owasp plugin from build.gradle

--- src/test/java/uk/gov/hmcts/probate/controller/NextStepsUnitTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/NextStepsUnitTest.java
@@ -81,8 +81,9 @@ public class NextStepsUnitTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        underTest = new NextStepsController(ccdBeanTransformerMock, confirmationResponseServiceMock, callbackResponseTransformerMock,
-                objectMapperMock, feeServiceMock, stateChangeServiceMock);
+        underTest = new NextStepsController(ccdBeanTransformerMock, confirmationResponseServiceMock,
+            callbackResponseTransformerMock,
+            objectMapperMock, feeServiceMock, stateChangeServiceMock);
 
         when(callbackRequestMock.getCaseDetails()).thenReturn(caseDetailsMock);
         when(caseDetailsMock.getData()).thenReturn(caseDataMock);
@@ -96,11 +97,12 @@ public class NextStepsUnitTest {
         when(ccdDataMock.getFee()).thenReturn(feeMock);
         when(feeServiceMock.getTotalFee(null, 0L, 0L)).thenReturn(feeServiceResponseMock);
         when(callbackResponseTransformerMock
-                .transformForSolicitorComplete(callbackRequestMock, feeServiceResponseMock)).thenReturn(callbackResponseMock);
+            .transformForSolicitorComplete(callbackRequestMock, feeServiceResponseMock))
+            .thenReturn(callbackResponseMock);
 
 
         ResponseEntity<CallbackResponse> response = underTest.validate(callbackRequestMock,
-                bindingResultMock, httpServletRequestMock);
+            bindingResultMock, httpServletRequestMock);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), is(callbackResponseMock));
@@ -113,7 +115,7 @@ public class NextStepsUnitTest {
         when(stateChangeServiceMock.getChangedStateForCaseReview(caseDataMock)).thenReturn(Optional.empty());
 
         ResponseEntity<CallbackResponse> response = underTest.validate(callbackRequestMock,
-                bindingResultMock, httpServletRequestMock);
+            bindingResultMock, httpServletRequestMock);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), is(callbackResponseMock));
@@ -127,7 +129,7 @@ public class NextStepsUnitTest {
         when(objectMapperMock.writeValueAsString(callbackRequestMock)).thenThrow(JsonProcessingException.class);
 
         ResponseEntity<CallbackResponse> response = underTest.validate(callbackRequestMock,
-                bindingResultMock, httpServletRequestMock);
+            bindingResultMock, httpServletRequestMock);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), is(callbackResponseMock));
@@ -138,11 +140,11 @@ public class NextStepsUnitTest {
         Optional<String> newState = Optional.of("changedState");
         when(stateChangeServiceMock.getChangedStateForCaseReview(caseDataMock)).thenReturn(newState);
         when(callbackResponseTransformerMock.transformWithConditionalStateChange(callbackRequestMock, newState))
-                .thenReturn(callbackResponseMock);
+            .thenReturn(callbackResponseMock);
 
 
         ResponseEntity<CallbackResponse> response = underTest.validate(callbackRequestMock,
-                bindingResultMock, httpServletRequestMock);
+            bindingResultMock, httpServletRequestMock);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), is(callbackResponseMock));
