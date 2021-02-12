85f1783368ffdc754fe7a4529b2d732ea6f0aeaf
stelios <stelios.kalorkotis@hotmail.com>
2021-02-12 13:43:38 +0000

    DataExtractController updated.

--- src/main/java/uk/gov/hmcts/probate/controller/DataExtractController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/DataExtractController.java
@@ -81,7 +81,7 @@ public class DataExtractController {
     }
 
     @ApiOperation(value = "Initiate Exela data extract", notes = " Date MUST be in format 'yyyy-MM-dd'")
-    @PostMapping(path = "/exela")
+    @PostMapping(path = "/exela/{fromDate}/{toDate}")
     public ResponseEntity initiateExelaExtractDateRange(@ApiParam(value = "Date to find cases against", required = true)
                                                @RequestParam("fromDate") String fromDate,
                                                 @RequestParam("toDate") String toDate) {
