182c2b4fa06f2a6a552322292486e77ec545d4b2
itoulou <touloumbadjiani@gmail.com>
2021-02-12 11:41:52 +0000

    fix unit test build failures

--- src/test/java/uk/gov/hmcts/probate/validator/IHTFourHundredDateValidationRuleTest.java
+++ src/test/java/uk/gov/hmcts/probate/validator/IHTFourHundredDateValidationRuleTest.java
@@ -55,8 +55,8 @@ public class IHTFourHundredDateValidationRuleTest {
             underTest.validate(caseDetails);
         })
                 .isInstanceOf(BusinessValidationException.class)
-                .hasMessage("Case ID 12345678987654321: IHT400421 date ("+ invalidDateAfter20DaysBeforeToday+")"
-                        +" needs to be before 20 days before current date ("+validDate+")");
+                .hasMessage("Case ID 12345678987654321: IHT400421 date (" + invalidDateAfter20DaysBeforeToday
+                        + ") needs to be before 20 days before current date (" + validDate + ")");
     }
 
     @Test
@@ -67,6 +67,6 @@ public class IHTFourHundredDateValidationRuleTest {
         })
                 .isInstanceOf(BusinessValidationException.class)
                 .hasMessage("Case ID 12345678987654321: IHT400421 date ("
-                        +invalidDateInFuture+") needs to be in the past");
+                        + invalidDateInFuture + ") needs to be in the past");
     }
 }
