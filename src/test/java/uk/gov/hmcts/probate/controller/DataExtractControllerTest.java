b80999ac21c74c1b64aa42da1f691bfd6eef42f1
stelios <stelios.kalorkotis@hotmail.com>
2021-02-15 12:56:06 +0000

    Test updated.

--- src/test/java/uk/gov/hmcts/probate/controller/DataExtractControllerTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/DataExtractControllerTest.java
@@ -117,7 +117,7 @@ public class DataExtractControllerTest {
 
     @Test
     public void exelaShouldReturnOkResponseOnValidDateFormat() throws Exception {
-        mockMvc.perform(post("/data-extract/exela?date=2019-02-13"))
+        mockMvc.perform(post("/data-extract/exela?fromDate=2019-02-13&toDate=2019-02-13"))
             .andExpect(status().isAccepted())
             .andExpect(content().string("Exela data extract finished"));
     }
