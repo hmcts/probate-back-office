8d2b05c10b6918d03682269a522f01421912ae43
Ferdi <ferdicocelli@hotmail.com>
2021-02-18 12:03:22 +0000

    DTSPB-1454-Custom-ordering-e2e-check-for-each-casetype - further improvements

--- src/test/end-to-end/codecept.conf.js
+++ src/test/end-to-end/codecept.conf.js
@@ -24,8 +24,7 @@ exports.config = {
                     '--no-sandbox',
                     '--allow-running-insecure-content',
                     '--ignore-certificate-errors',
-                    '--proxy-server=proxyout.reform.hmcts.net:8080',
-                    '--proxy-bypass-list=*beta*LB.reform.hmcts.net',
+
                     '--window-size=1440,1400'
                 ]
             },
