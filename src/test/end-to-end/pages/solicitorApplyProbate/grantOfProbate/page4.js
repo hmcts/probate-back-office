c63d1d76c1e300436977b95fa3ddfb599b29594a
Megan <meganaliceslattery@gmail.com>
2021-02-23 15:43:02 +0000

    Check applying and not applying lists are displayed e2e

--- src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page4.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page4.js
@@ -14,6 +14,9 @@ module.exports = async function (isSolicitorApplying = false) {
 
         await I.waitForText(grantOfProbateConfig.page2_waitForAdditionalExecutor, testConfig.TestTimeToWaitForText);
 
+        await I.waitForText(grantOfProbateConfig.page4_previouslyIdentifiedApplyingExecutors, testConfig.TestTimeToWaitForText);
+        await I.waitForText(grantOfProbateConfig.page4_previouslyIdentifiedNotApplyingExecutors, testConfig.TestTimeToWaitForText);
+
         await I.click('#solsAdditionalExecutorList > div > button');
 
         await I.fillField('#solsAdditionalExecutorList_0_additionalExecForenames', grantOfProbateConfig.page2_executorFirstName);
@@ -53,20 +56,6 @@ module.exports = async function (isSolicitorApplying = false) {
         }
 
     } else {
-        await I.fillField('#primaryApplicantForenames', grantOfProbateConfig.page2_primaryApplicantFirstName);
-        await I.fillField('#primaryApplicantSurname', grantOfProbateConfig.page2_primaryApplicantSurname);
-        await I.click(`#primaryApplicantHasAlias-${grantOfProbateConfig.optionYes}`);
-        await I.fillField('#solsExecutorAliasNames', grantOfProbateConfig.page2_primaryApplicantWillName);
-        await I.click(`#primaryApplicantIsApplying-${grantOfProbateConfig.optionYes}`);
-
-        await I.click(grantOfProbateConfig.UKpostcodeLink);
-        await I.fillField('#primaryApplicantAddress_AddressLine1', grantOfProbateConfig.address_line1);
-        await I.fillField('#primaryApplicantAddress_AddressLine2', grantOfProbateConfig.address_line2);
-        await I.fillField('#primaryApplicantAddress_AddressLine3', grantOfProbateConfig.address_line3);
-        await I.fillField('#primaryApplicantAddress_PostTown', grantOfProbateConfig.address_town);
-        await I.fillField('#primaryApplicantAddress_County', grantOfProbateConfig.address_county);
-        await I.fillField('#primaryApplicantAddress_PostCode', grantOfProbateConfig.address_postcode);
-        await I.fillField('#primaryApplicantAddress_Country', grantOfProbateConfig.address_country);
 
         await I.click(`#otherExecutorExists-${grantOfProbateConfig.optionNo}`);
 
