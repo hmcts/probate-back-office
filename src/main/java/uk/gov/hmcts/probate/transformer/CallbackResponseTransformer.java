2be685cdab02aa89351276ab6203889fb2361e5f
KleoG <kleogeorgiou@hotmail.com>
2021-02-24 22:57:55 +0000

    DTSPB-1304 - checkstyles fix

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -521,21 +521,21 @@ public class CallbackResponseTransformer {
         String executorNames = "The Executor" + plural + " ";
         String professionalName = caseDetails.getData().getSolsSOTName();
 
-        String confirmSOT = "By signing the Statement of Truth by ticking the boxes below, I, " + professionalName +
-                " confirm the following:\n\n" +
-                "I, " + professionalName + ", have provided a copy of this application to the Executor" + plural +
-                " named below.\n\n" +
-                "I, " + professionalName + ", have informed the Executor"  + plural +
-                " that in signing the Statement of Truth I am confirming that the Executor "  + plural +
-                " believe "  + plural + " the facts set out in this legal statement are true.\n\n" +
-                "I, " + professionalName + ", have informed the Executor"   + plural +
-                " of the consequences if it should subsequently appear that the Executor "  + plural +
-                " did not have an honest belief in the facts set out in the legal statement.\n\n" +
-                "I, " + professionalName + ", have been authorised but the Executor"  + plural +
-                " to sign the Statement of Truth.\n\n" +
-                "I, " + professionalName + ", understand that proceedings for contempt of court may be brought " +
-                "against anyone who makes, or causes to be made, a false statement in a document verified by a " +
-                "statement of truth without an honest belief in its truth.\n";
+        String confirmSOT = "By signing the Statement of Truth by ticking the boxes below, I, " + professionalName
+                + " confirm the following:\n\n"
+                + "I, " + professionalName + ", have provided a copy of this application to the Executor" + plural
+                + " named below.\n\n"
+                + "I, " + professionalName + ", have informed the Executor"  + plural
+                + " that in signing the Statement of Truth I am confirming that the Executor "  + plural
+                + " believe "  + plural + " the facts set out in this legal statement are true.\n\n"
+                + "I, " + professionalName + ", have informed the Executor"   + plural
+                + " of the consequences if it should subsequently appear that the Executor "  + plural
+                + " did not have an honest belief in the facts set out in the legal statement.\n\n"
+                + "I, " + professionalName + ", have been authorised but the Executor"  + plural
+                + " to sign the Statement of Truth.\n\n"
+                + "I, " + professionalName + ", understand that proceedings for contempt of court may be brought "
+                + "against anyone who makes, or causes to be made, a false statement in a document verified by a "
+                + "statement of truth without an honest belief in its truth.\n";
 
         // if there are no executors then fill the space in with the professional users name (primary applicant)
         executorNames = listOfApplyingExecs.isEmpty() ? executorNames + professionalName + ": " :
