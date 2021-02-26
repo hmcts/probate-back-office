4da846ff9fbd242126b844660f3bf5ebe4936bea
itoulou <touloumbadjiani@gmail.com>
2021-02-26 13:11:45 +0000

    fix format issue, class can now be initalised

--- src/main/java/uk/gov/hmcts/probate/model/htmltemplate/SendDocumentsDetailsHtmlTemplate.java
+++ src/main/java/uk/gov/hmcts/probate/model/htmltemplate/SendDocumentsDetailsHtmlTemplate.java
@@ -6,7 +6,7 @@ import static java.lang.String.format;
 
 public class SendDocumentsDetailsHtmlTemplate {
     public static final String DOC_DETAILS =
-            format("%s<br/><ul><li>%s</li><li>%s</li><li>%s</li></ul>",
+            format("%s<br/><ul><li>%s</li><li>%s</li></ul>",
                     PageTextConstants.DOCUMENT_NOW_SEND_US,
                     PageTextConstants.DOCUMENT_YOUR_REF_NUM,
                     PageTextConstants.DOCUMENT_LEGAL_STATEMENT_PHOTOCOPY);
