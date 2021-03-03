3d55e40ed0b1ae3779a98f107da9c5ced5c68913
Megan <meganaliceslattery@gmail.com>
2021-03-03 16:15:56 +0000

    Add new trust corps legal statement html
    Amend

--- src/main/java/uk/gov/hmcts/probate/controller/BusinessValidationController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/BusinessValidationController.java
@@ -53,7 +53,7 @@ import java.util.Optional;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT_ADMON;
 import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT_INTESTACY;
-import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT_PROBATE;
+import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT_PROBATE_TRUST_CORPS;
 import static uk.gov.hmcts.probate.model.State.APPLICATION_RECEIVED;
 import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantType.Constants.ADMON_WILL_NAME;
 import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantType.Constants.GRANT_OF_PROBATE_NAME;
@@ -131,7 +131,7 @@ public class BusinessValidationController {
 
             Optional<String> newState =
                 stateChangeService.getChangedStateForProbateUpdate(callbackRequest.getCaseDetails().getData());
-            response = getCallbackResponseForGenerateAndUpload(callbackRequest, newState, LEGAL_STATEMENT_PROBATE,
+            response = getCallbackResponseForGenerateAndUpload(callbackRequest, newState, LEGAL_STATEMENT_PROBATE_TRUST_CORPS,
                 GRANT_OF_PROBATE_NAME);
         }
         return ResponseEntity.ok(response);
