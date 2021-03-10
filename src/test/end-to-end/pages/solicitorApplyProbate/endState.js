09138ba87b6ccd33c0cea6061d30bae5468d2f29
Arun <arun.kumar@hmcts.net>
2021-03-10 13:15:26 +0000

    DTSPB-1534  Downgraded the codecept version

--- src/test/end-to-end/pages/solicitorApplyProbate/endState.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/endState.js
@@ -4,7 +4,6 @@ const testConfig = require('src/test/config.js');
 
 module.exports = async function (endState) {
     const I = this;
-    pause();
     await I.waitForText('Event History');
     await I.click('Event History');
     await I.waitForText(endState, testConfig.TestTimeToWaitForText);
