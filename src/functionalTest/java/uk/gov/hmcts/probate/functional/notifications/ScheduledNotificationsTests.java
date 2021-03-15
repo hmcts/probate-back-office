cd1252fd31d65867c685289e48bc67886f3e7297
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-03-15 12:51:34 +0000

    DTSPB-1488: added more logging2

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/notifications/ScheduledNotificationsTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/notifications/ScheduledNotificationsTests.java
@@ -87,11 +87,12 @@ public class ScheduledNotificationsTests extends IntegrationTestBase {
         String baseCaseJson = utils.getJsonFromFile(APPLY_FOR_GRANT_PAYLOAD);
         String grantDocCaseJson = utils.replaceAttribute(baseCaseJson, EVENT_PARM, EVENT_APPLY);
         String applyforGrantPaperApplicationManResponse = utils.createCaseAsCaseworker(grantDocCaseJson, EVENT_APPLY);
+        log.info("applyforGrantPaperApplicationManResponse:" + applyforGrantPaperApplicationManResponse);
         JsonPath jsonPathApply = JsonPath.from(applyforGrantPaperApplicationManResponse);
         String caseId = jsonPathApply.get("id").toString();
         log.info("caseId:" + caseId);
 
-        String updateGrantDocCaseJson = utils.replaceAttribute(baseCaseJson, EVENT_PARM, EVENT_PRINT_CASE);
+        String updateGrantDocCaseJson = utils.replaceAttribute(baseCaseJson, EVENT_APPLY, EVENT_PRINT_CASE);
         String printCaseUpdateResponse = utils.updateCaseAsCaseworker(updateGrantDocCaseJson, EVENT_PRINT_CASE, caseId);
         log.info("printCaseUpdateResponse:" + printCaseUpdateResponse);
 
