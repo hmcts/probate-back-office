5585d394654b427cb824e7243089fe984cf286a6
Arun <arun.kumar@hmcts.net>
2021-03-15 13:48:10 +0000

    DTSPB-1534  Fix the E2E Test

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -9,19 +9,19 @@ module.exports = async function (jurisdiction, caseType, event) {
     await I.waitForText(createCaseConfig.waitForText, testConfig.TestTimeToWaitForText || 60);
     //In saucelabs this page is not able to load so waiting for more time
     if (testConfig.TestForXUI) {
-        await I.wait(120);
+        await I.wait(180);
 
     }
     if (testConfig.TestForCrossBrowser) {
         await I.wait(5);
     }
-    await I.waitForEnabled({css: '#cc-jurisdiction'}, 90);
+    await I.waitForEnabled({css: '#cc-jurisdiction'}, );
     await I.retry(5).selectOption('#cc-jurisdiction', jurisdiction);
-    await I.waitForEnabled({css: '#cc-case-type'}, 90);
+    await I.waitForEnabled({css: '#cc-case-type'}, 120);
     await I.retry(5).selectOption('#cc-case-type', caseType);
-    await I.waitForEnabled({css: '#cc-event'}, 90);
+    await I.waitForEnabled({css: '#cc-event'}, 120);
     await I.retry(5).selectOption('#cc-event', event);
 
-    await I.waitForEnabled(createCaseConfig.startButton, 90);
+    await I.waitForEnabled(createCaseConfig.startButton, 120);
     await I.waitForNavigationToComplete(createCaseConfig.startButton);
 };
