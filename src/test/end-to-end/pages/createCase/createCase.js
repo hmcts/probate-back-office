4481f0ffccdc22d4c088c55d3a49a9c67dd16b27
Arun <arun.kumar@hmcts.net>
2021-03-15 13:52:22 +0000

    DTSPB-1534  Fix the E2E Test

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -10,12 +10,11 @@ module.exports = async function (jurisdiction, caseType, event) {
     //In saucelabs this page is not able to load so waiting for more time
     if (testConfig.TestForXUI) {
         await I.wait(180);
-
     }
     if (testConfig.TestForCrossBrowser) {
         await I.wait(5);
     }
-    await I.waitForEnabled({css: '#cc-jurisdiction'}, );
+    await I.waitForEnabled({css: '#cc-jurisdiction'}, 120);
     await I.retry(5).selectOption('#cc-jurisdiction', jurisdiction);
     await I.waitForEnabled({css: '#cc-case-type'}, 120);
     await I.retry(5).selectOption('#cc-case-type', caseType);
