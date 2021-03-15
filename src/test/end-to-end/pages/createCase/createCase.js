886427bf1333d4738e7cecf24e91849fdea2ac84
Arun <arun.kumar@hmcts.net>
2021-03-15 12:11:05 +0000

    DTSPB-1534  Fix the E2E Test

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -11,13 +11,13 @@ module.exports = async function (jurisdiction, caseType, event) {
     if (testConfig.TestForCrossBrowser) {
         await I.wait(5);
     }
-    await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.TestTimeToWaitForText || 60);
+    await I.waitForEnabled({css: '#cc-jurisdiction'});
     await I.retry(5).selectOption('#cc-jurisdiction', jurisdiction);
-    await I.waitForEnabled({css: '#cc-case-type'}, testConfig.TestTimeToWaitForText || 60);
+    await I.waitForEnabled({css: '#cc-case-type'});
     await I.retry(5).selectOption('#cc-case-type', caseType);
-    await I.waitForEnabled({css: '#cc-event'}, testConfig.TestTimeToWaitForText || 60);
+    await I.waitForEnabled({css: '#cc-event'});
     await I.retry(5).selectOption('#cc-event', event);
 
-    await I.waitForEnabled(createCaseConfig.startButton, testConfig.TestTimeToWaitForText || 60);
+    await I.waitForEnabled(createCaseConfig.startButton);
     await I.waitForNavigationToComplete(createCaseConfig.startButton);
 };
