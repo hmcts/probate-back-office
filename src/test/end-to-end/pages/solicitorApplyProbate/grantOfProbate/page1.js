ac293dfd7f82019d6a9eea301a5f14a8f6996930
Vincent Holt <vleholt@yahoo.co.uk>
2021-03-19 11:18:28 +0000

    start on e2e

--- src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page1.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page1.js
@@ -9,6 +9,11 @@ module.exports = async function () {
     await I.runAccessibilityTest();
 
     await I.click(`#willAccessOriginal-${grantOfProbateConfig.optionYes}`);
+
+    await I.fillField('#originalWillSignedDate-day', grantOfProbateConfig.page1_originalWillSignedDate_day);
+    await I.fillField('#originalWillSignedDate-month', grantOfProbateConfig.page1_originalWillSignedDate_month);
+    await I.fillField('#originalWillSignedDate-year', grantOfProbateConfig.page1_originalWillSignedDate_year);
+
     await I.click(`#willHasCodicils-${grantOfProbateConfig.optionNo}`);
 
     await I.waitForNavigationToComplete(commonConfig.continueButton);
