d2a228207dbbd204092f30dc9d6e2284ff7b19f4
Vincent Holt <vleholt@yahoo.co.uk>
2021-03-24 16:40:23 +0000

    attend paperform yes e2e test

--- src/test/end-to-end/pages/createGrantOfProbateSolicitor/page6.js
+++ src/test/end-to-end/pages/createGrantOfProbateSolicitor/page6.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const testConfig = require('src/test/config');
-const createGrantOfProbateConfig = require('./createGrantOfProbateConfig');
 const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 
-module.exports = async function (crud) {
+module.exports = async function (crud, createGrantOfProbateConfig) {
 
     const I = this;
 
@@ -13,25 +12,33 @@ module.exports = async function (crud) {
         await I.click(`#willExists-${createGrantOfProbateConfig.page6_willExistsYes}`);
         await I.click(`#willDatedBeforeApril-${createGrantOfProbateConfig.page6_willDatedBeforeAprilYes}`);
         await I.click(`#willAccessOriginal-${createGrantOfProbateConfig.page6_willAccessOriginalYes}`);
-
-        await I.fillField('#originalWillSignedDate-day', createGrantOfProbateConfig.page6_originalWillSignedDate_day);
-        await I.fillField('#originalWillSignedDate-month', createGrantOfProbateConfig.page6_originalWillSignedDate_month);
-        await I.fillField('#originalWillSignedDate-year', createGrantOfProbateConfig.page6_originalWillSignedDate_year);
-
         await I.click(`#willHasCodicils-${createGrantOfProbateConfig.page6_willHasCodicilsYes}`);
 
-        const addBtn = {css: '#codicilAddedDateList button'};
-        await I.waitForVisible(addBtn);
-        await I.scrollTo(addBtn);
-        await I.waitForClickable(addBtn);
-        await I.click(addBtn);
-        if (!testConfig.TestAutoDelayEnabled) {
-            await I.wait(0.25);
-        }
+        if (createGrantOfProbateConfig.page1_paperForm === 'No') {
+            await I.fillField('#originalWillSignedDate-day', createGrantOfProbateConfig.page6_originalWillSignedDate_day);
+            await I.fillField('#originalWillSignedDate-month', createGrantOfProbateConfig.page6_originalWillSignedDate_month);
+            await I.fillField('#originalWillSignedDate-year', createGrantOfProbateConfig.page6_originalWillSignedDate_year);
 
-        await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-day'}, createGrantOfProbateConfig.page6_codicilDate_day);
-        await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-month'}, createGrantOfProbateConfig.page6_codicilDate_month);
-        await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-year'}, createGrantOfProbateConfig.page6_codicilDate_year);
+            const addBtn = {css: '#codicilAddedDateList button'};
+            await I.waitForVisible(addBtn);
+            await I.scrollTo(addBtn);
+            await I.waitForClickable(addBtn);
+            await I.click(addBtn);
+            if (!testConfig.TestAutoDelayEnabled) {
+                await I.wait(0.25);
+            }
+
+            await I.waitForVisible({css: '#codicilAddedDateList_0_dateCodicilAdded-day'});
+            await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-day'}, createGrantOfProbateConfig.page6_codicilDate_day);
+            await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-month'}, createGrantOfProbateConfig.page6_codicilDate_month);
+            await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-year'}, createGrantOfProbateConfig.page6_codicilDate_year);
+
+        } else {
+            await I.dontSeeElement('#originalWillSignedDate-day');
+            await I.dontSeeElement('#originalWillSignedDate-month');
+            await I.dontSeeElement('#originalWillSignedDate-year');
+            await I.dontSeeElement('#codicilAddedDateList button');
+        }
 
         await I.click({css: `#willsOutsideOfUK-${createGrantOfProbateConfig.page6_willsOutsideOfUKYes}`});
         await I.click({css: `#deceasedEnterMarriageOrCP-${createGrantOfProbateConfig.page6_deceasedEnterMarriageOrCPYes}`});
@@ -55,7 +62,9 @@ module.exports = async function (crud) {
 
         //        complete these when completing cw amend story
         //        await I.waitForElement({css: '#codicilAddedDateList_0_dateCodicilAdded-day'});
+        // if (createGrantOfProbateConfig.page1_paperForm === 'No') {
         //        await I.fillField({css: '#codicilAddedDateList_0_dateCodicilAdded-day'}, createGrantOfProbateConfig.page6_codicilDate_day_update);
+        // }
     }
 
     await I.waitForNavigationToComplete(commonConfig.continueButton);
