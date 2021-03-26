f08653ff23374215fef2dbe89ff34d240efb9921
Megan <meganaliceslattery@gmail.com>
2021-03-26 11:42:40 +0000

    Add reset transformer

---
+++ src/main/java/uk/gov/hmcts/probate/transformer/ResetTransformer.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.probate.transformer;
+
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
+import uk.gov.hmcts.probate.model.ccd.raw.response.ResponseCaseData;
+
+public class ResetTransformer {
+
+    public void resetTitleAndClearingFields(CaseData caseData, ResponseCaseData.ResponseCaseDataBuilder<?, ?> builder) {
+        if (caseData)
+
+    }
+
+}
