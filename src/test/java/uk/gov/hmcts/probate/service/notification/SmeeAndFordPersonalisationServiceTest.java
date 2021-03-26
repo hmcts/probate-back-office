83152df76e1966c16eeea85afbb60efbe5272821
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-03-26 12:06:45 +0000

    DTSPB-1296: corrected checkstyle

--- src/test/java/uk/gov/hmcts/probate/service/notification/SmeeAndFordPersonalisationServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/notification/SmeeAndFordPersonalisationServiceTest.java
@@ -62,8 +62,8 @@ public class SmeeAndFordPersonalisationServiceTest {
         MockitoAnnotations.initMocks(this);
         
         when(fileSystemResourceService.getFileFromResourceAsString("templates/dataExtracts/SmeeAndFordHeaderRow.csv"))
-            .thenReturn("col1,col2,col3,col4,col5,col6,col7,col8,col9,col10," +
-                "col11,col12,col13,col14,col15,col16,col17,col18,col19");
+            .thenReturn("col1,col2,col3,col4,col5,col6,col7,col8,col9,col10," 
+                + "col11,col12,col13,col14,col15,col16,col17,col18,col19");
     }
 
     private CaseData.CaseDataBuilder getCaseDataBuilder(ApplicationType applicationType, boolean hasScanned, 
