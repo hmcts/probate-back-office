6c24c6e10ba3f99eb7371b48938e2603c1e38d89
Arun <arun.kumar@hmcts.net>
2021-03-26 09:44:06 +0000

    DTSPB-1534  Trying out the hard coded user id and password.

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -14,14 +14,17 @@ export E2E_OUTPUT_DIR='./functional-output/xui'
 
 
 export TEST_E2E_URL=${TEST_CCD_E2E_URL}
-export E2E_TEST_PATH="./paths/caseworker/**/*.js"
-export TEST_USER_EMAIL=${CW_USER_EMAIL}
-export TEST_USER_PASSWORD=${CW_USER_PASSWORD}
-export PROF_USER_EMAIL=${SOL_USER_EMAIL}
-export PROF_USER_PASSWORD=${SOL_USER_PASSWORD}
+export E2E_TEST_PATH="./paths/caseworker/**/caseProgressStandardPath.js"
+export TEST_USER_EMAIL="probatebackoffice@gmail.com"
+export TEST_USER_PASSWORD="Monday01"
+
+
+export PROF_USER_EMAIL="probatesolicitorpreprod@gmail.com"
+export PROF_USER_PASSWORD="Monday01"
+echo  PROF_USER_EMAIL
+echo  PROF_USER_PASSWORD
 export TESTS_FOR_XUI_SERVICE='false'
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/ccd'
 
 yarn test:functional
-
