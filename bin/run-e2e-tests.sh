82bd079777e1fe6a89b3c0ccc84039709e8ef604
Arun <arun.kumar@hmcts.net>
2021-03-26 15:28:20 +0000

    DTSPB-1534  Fixed the E2E Test

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -14,7 +14,7 @@ export E2E_OUTPUT_DIR='./functional-output/xui'
 
 
 export TEST_E2E_URL=${TEST_CCD_E2E_URL}
-export E2E_TEST_PATH="./paths/caseworker/**/caseProgress*.js"
+export E2E_TEST_PATH="./paths/caseworker/**/*.js"
 export TEST_USER_EMAIL="probatebackoffice@gmail.com"
 export TEST_USER_PASSWORD="Monday01"
 
