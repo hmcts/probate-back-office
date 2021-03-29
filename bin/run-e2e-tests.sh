241e1aa26aa956fdd9c6e62dda0debf0a2e4965b
Arun <arun.kumar@hmcts.net>
2021-03-29 14:41:34 +0100

    Merge branch 'master' of github.com:hmcts/probate-back-office into solicitor-E2E-Test-Fix
    # Conflicts:
    #	bin/run-e2e-tests.sh
    #	src/test/config.js

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -11,7 +11,7 @@ export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_XUI_SERVICE='true'
 export TESTS_FOR_ACCESSIBILITY='true'
 export E2E_OUTPUT_DIR='./functional-output/xui'
-
+yarn test:functional
 
 export TEST_E2E_URL=${TEST_CCD_E2E_URL}
 export E2E_TEST_PATH="./paths/caseworker/**/*.js"
@@ -25,4 +25,4 @@ export TESTS_FOR_XUI_SERVICE='false'
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/ccd'
 
-yarn test:functional
+//yarn test:functional
--- src/test/config.js
+++ src/test/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TestBackOfficeUrl: process.env.TEST_E2E_URL || 'http://localhost:3451',
-    TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
+    TestBackOfficeUrl: process.env.TEST_E2E_URL || 'https://manage-case.aat.platform.hmcts.net/ ',
+    TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || true,
     TestRetryFeatures: process.env.RETRY_FEATURES || 0,
     TestRetryScenarios: process.env.RETRY_SCENARIOS || 2,
     TestPathToRun: process.env.E2E_TEST_PATH || './paths/**/*.js',
@@ -8,11 +8,12 @@ module.exports = {
     TestDocumentToUpload: 'uploadDocuments/test_file_for_document_upload.png',
     TestTimeToWaitForText: parseInt(process.env.BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT || 120),
     TestAutoDelayEnabled: process.env.E2E_AUTO_DELAY_ENABLED === 'true',
-    TestEnvUser: process.env.TEST_USER_EMAIL || 'ProbateSolCW1@gmail.com',
-    TestEnvPassword: process.env.TEST_USER_PASSWORD || 'Pa55word11',
-    TestEnvProfUser: process.env.PROF_USER_EMAIL || 'ProbateSolicitor1@gmail.com',
-    TestEnvProfPassword: process.env.PROF_USER_PASSWORD || 'Pa55word11',
-    TestForXUI: process.env.TESTS_FOR_XUI_SERVICE === 'true',
+    TestAutoDelayEnabled: process.env.E2E_AUTO_DELAY_ENABLED === 'true',
+    TestEnvUser: process.env.TEST_USER_EMAIL || 'probatebackoffice@gmail.com',
+    TestEnvPassword: process.env.TEST_USER_PASSWORD || 'Monday01',
+    TestEnvProfUser: process.env.PROF_USER_EMAIL || 'probatesolicitorpreprod@gmail.com',
+    TestEnvProfPassword: process.env.PROF_USER_PASSWORD || 'Monday01',
+    TestForXUI: true,
     TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
     TestForCrossBrowser: process.env.TESTS_FOR_CROSS_BROWSER === 'true'
 };
