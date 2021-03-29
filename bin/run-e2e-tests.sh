16359d28f75bd8c5c6bd8da76518df3ff6c9b181
Arun <arun.kumar@hmcts.net>
2021-03-29 16:05:11 +0100

    DTSPB-1534 Fix Solicitor E2E Test

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -3,10 +3,10 @@ set -ex
 
 export TEST_E2E_URL=${TEST_XUI_E2E_URL}
 export E2E_TEST_PATH="./paths/solicitor/**/*.js"
-export TEST_USER_EMAIL=${SOL_USER_EMAIL}
-export TEST_USER_PASSWORD=${SOL_USER_PASSWORD}
-export PROF_USER_EMAIL=${SOL_USER_EMAIL}
-export PROF_USER_PASSWORD=${SOL_USER_PASSWORD}
+eexport PROF_USER_PASSWORD="Monday01"
+export TESTS_FOR_XUI_SERVICE='false'
+export TESTS_FOR_ACCESSIBILITY='false'
+export E2E_OUTPUT_DIR='./functional-output/ccd'
 export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_XUI_SERVICE='true'
 export TESTS_FOR_ACCESSIBILITY='true'
