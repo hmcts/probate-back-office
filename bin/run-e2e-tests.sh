ec59bd49999491909b971047d5a21928fecc9f83
arun-hmcts <73122505+arun-hmcts@users.noreply.github.com>
2021-03-29 18:50:02 +0100

    DTSPB-1534 Fix Solicitor E2E Test (#1453)
    * DTSPB-1534 Fix Solicitor E2E Test

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -3,14 +3,15 @@ set -ex
 
 export TEST_E2E_URL=${TEST_XUI_E2E_URL}
 export E2E_TEST_PATH="./paths/solicitor/**/*.js"
-export TEST_USER_EMAIL=${SOL_USER_EMAIL}
-export TEST_USER_PASSWORD=${SOL_USER_PASSWORD}
-export PROF_USER_EMAIL=${SOL_USER_EMAIL}
-export PROF_USER_PASSWORD=${SOL_USER_PASSWORD}
+export TEST_USER_EMAIL="probatebackoffice@gmail.com"
+export TEST_USER_PASSWORD="Monday01"
+export PROF_USER_EMAIL="probatesolicitorpreprod@gmail.com"
+export PROF_USER_PASSWORD="Monday01"
 export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_XUI_SERVICE='true'
 export TESTS_FOR_ACCESSIBILITY='true'
 export E2E_OUTPUT_DIR='./functional-output/xui'
+yarn test:functional
 
 
 export TEST_E2E_URL=${TEST_CCD_E2E_URL}
