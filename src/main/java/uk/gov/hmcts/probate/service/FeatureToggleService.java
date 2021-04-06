4e1a2fca7ca317916230e817bcf751384f120675
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-04-06 10:40:07 +0100

    missing the import log

--- src/main/java/uk/gov/hmcts/probate/service/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/probate/service/FeatureToggleService.java
@@ -5,7 +5,9 @@ import com.launchdarkly.sdk.server.LDClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class FeatureToggleService {
 
