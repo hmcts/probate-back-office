ebee842cf172e7722bfa07dbee0f8519ecfa2ac2
Vincent Holt <vleholt@yahoo.co.uk>
2021-04-09 20:02:09 +0100

    fix cw e2e test

--- src/main/java/uk/gov/hmcts/probate/transformer/solicitorexecutors/SolicitorApplicationCompletionTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/solicitorexecutors/SolicitorApplicationCompletionTransformer.java
@@ -2,16 +2,13 @@ package uk.gov.hmcts.probate.transformer.solicitorexecutors;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.probate.model.ccd.raw.AdditionalExecutorApplying;
-import uk.gov.hmcts.probate.model.ccd.raw.AdditionalExecutorNotApplying;
-import uk.gov.hmcts.probate.model.ccd.raw.CollectionMember;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 import uk.gov.hmcts.probate.service.solicitorexecutor.ExecutorListMapperService;
 
-import java.util.List;
-
 @Component
 @Slf4j
+// Handles some casedata mappings for when a solicitor application becomes a case
+// for caseworker or solicitor journeys
 public class SolicitorJourneyCompletionTransformer extends LegalStatementExecutorTransformer {
 
     public SolicitorJourneyCompletionTransformer(ExecutorListMapperService executorListMapperService) {
