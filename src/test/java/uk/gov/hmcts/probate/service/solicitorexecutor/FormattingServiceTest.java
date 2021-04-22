164212aa1f4f5d406fe4d781a196bea6ab99a6e0
Vincent Holt <vleholt@yahoo.co.uk>
2021-04-22 09:17:51 +0100

    amend dependency

--- src/test/java/uk/gov/hmcts/probate/service/solicitorexecutor/FormattingServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/solicitorexecutor/FormattingServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FormattingServiceTest {
