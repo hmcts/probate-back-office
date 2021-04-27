abacc50b13463529c36e9d4d63c937df77b152b3
stelios <stelios.kalorkotis@hotmail.com>
2021-04-27 16:14:18 +0100

    update sot page for admon will.

--- src/test/end-to-end/pages/solicitorApplyProbate/completeApplication/page1.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/completeApplication/page1.js
@@ -6,24 +6,16 @@ const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 module.exports = async function (willType = 'WillLeft') {
     const I = this;
     await I.waitForElement('#solsSOTNeedToUpdate');
+    await I.waitForVisible('#solsReviewLegalStatement1');
+    await I.waitForVisible('#solsReviewLegalStatement2');
+    await I.waitForVisible('#solsReviewLegalStatement3');
+    await I.waitForVisible('#solsReviewLegalStatement4');
     await I.runAccessibilityTest();
     if (willType === 'WillLeftAnnexed') {
-        await I.waitForVisible('#solsReviewLegalStatementAdmon1');
-        await I.waitForVisible('#solsReviewLegalStatementAdmon2');
-        await I.waitForVisible('#solsReviewLegalStatementAdmon3');
-        await I.waitForVisible('#solsReviewLegalStatement4');
         await I.see(completeApplicationConfig.page1_AdmonWilllegalStmtLink);
     } else if (willType === 'NoWill') {
-        await I.waitForVisible('#solsReviewLegalStatement1');
-        await I.waitForVisible('#solsReviewLegalStatement2');
-        await I.waitForVisible('#solsReviewLegalStatement3');
-        await I.waitForVisible('#solsReviewLegalStatement4');
         await I.see(completeApplicationConfig.page1_NoWilllegalStmtLink);
     } else {
-        await I.waitForVisible('#solsReviewLegalStatement1');
-        await I.waitForVisible('#solsReviewLegalStatement2');
-        await I.waitForVisible('#solsReviewLegalStatement3');
-        await I.waitForVisible('#solsReviewLegalStatement4');
         await I.see(completeApplicationConfig.page1_legalStmtLink);
     }
 
