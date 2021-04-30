c25d0e2cede73910ec091525681e1c3ad47f6942
Vincent Holt <vleholt@yahoo.co.uk>
2021-04-30 13:27:28 +0100

    eslint e2e test code

--- src/test/end-to-end/paths/solicitor/applyGrantOfProbateNoWill.js
+++ src/test/end-to-end/paths/solicitor/applyGrantOfProbateNoWill.js
@@ -85,7 +85,7 @@ Scenario('02 - Solicitor - Apply Grant of probate - No Will (Intestacy)', async 
     await I.completeApplicationPage5();
     await I.completeApplicationPage6();
     await I.completeApplicationPage7();
-    
+
     await I.seeEndState(endState);
     await I.seeCaseDetails(caseRef, historyTabConfig, {}, nextStepName, endState);
     await I.seeCaseDetails(caseRef, copiesTabConfig, completeApplicationConfig);
