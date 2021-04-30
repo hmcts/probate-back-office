d744a255d43f4e6278e061515c264aeecd1a9dc0
AbdullahShahHMCTS <77341369+AbdullahShahHMCTS@users.noreply.github.com>
2021-04-30 16:11:11 +0100

    Dtspb 1222 fix merge issues (#1519)
    * DTSPB-1222 Rework to push the DTSPB-1222 related changes pushed to master properly
    
    * Added back the changes for DTSPB-1222
    
    * Update e2e test
    
    Co-authored-by: isha2040 <58481974+isha2040@users.noreply.github.com>
    Co-authored-by: abdullahshah97 <abdullah-shah97@hotmail.com>

--- src/test/end-to-end/pages/solicitorApplyProbate/admonWillDetails/page2.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/admonWillDetails/page2.js
@@ -5,15 +5,9 @@ const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 
 module.exports = async function (updateAddressManually) {
     const I = this;
-    await I.waitForElement('#solsEntitledMinority');
     await I.runAccessibilityTest();
 
-    await I.click(`#solsEntitledMinority-${admonWillDetailsConfig.optionNo}`);
-    await I.click(`#solsDiedOrNotApplying-${admonWillDetailsConfig.optionYes}`);
-    await I.click(`#solsResiduary-${admonWillDetailsConfig.optionYes}`);
-    await I.waitForElement('#solsResiduaryType');
-    await I.selectOption('#solsResiduaryType', admonWillDetailsConfig.page2_legateeAndDevisee);
-    await I.click(`#solsLifeInterest-${admonWillDetailsConfig.optionNo}`);
+
 
     await I.fillField('#primaryApplicantForenames', admonWillDetailsConfig.applicant_firstname);
     await I.fillField('#primaryApplicantSurname', admonWillDetailsConfig.applicant_lastname);
