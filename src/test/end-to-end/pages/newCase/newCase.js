08922107166610f67a5f079e8cd319a4c297b823
Ferdi <ferdicocelli@hotmail.com>
2021-05-07 11:55:36 +0100

    DTSPB-710-new-branch- Added a saveScreenshot function to take a snapshot of potential error causing build failure on Jenkins nightly pipeline

--- src/test/end-to-end/pages/newCase/newCase.js
+++ src/test/end-to-end/pages/newCase/newCase.js
@@ -7,7 +7,16 @@ module.exports = async function () {
 
     const I = this;
 
+    await I.saveScreenshot('debugNightly.png', true);
     await I.waitForText(newCaseConfig.waitForText, testConfig.TestTimeToWaitForText);
-    // eslint-disable-next-line
-    await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator,120);
+
+    try {
+        // eslint-disable-next-line
+        await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator, 120);
+    } catch (e) {
+        await I.saveScreenshot('debugNightlyInCatchBlock.png', true);
+        throw e;
+    }
+    await I.saveScreenshot('debugNightlyAfterExecution.png', true);
+
 };
