ecaddbb1fe31016cb238b04eeee83d7fb3f4583e
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-05-17 10:17:12 +0100

    DTSPB-1526: Improve test

--- src/test/java/uk/gov/hmcts/probate/controller/LifeEventControllerTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/LifeEventControllerTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.probate.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,12 +15,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.probate.insights.AppInsights;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.hmcts.probate.security.SecurityUtils;
 import uk.gov.hmcts.probate.service.LifeEventService;
 import uk.gov.hmcts.probate.util.TestUtils;
-import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,9 +43,6 @@ public class LifeEventControllerTest {
     @MockBean
     private AppInsights appInsights;
 
-    @MockBean
-    private CoreCaseDataApi coreCaseDataApi;
-
     @Autowired
     private WebApplicationContext webApplicationContext;
 
@@ -49,6 +52,9 @@ public class LifeEventControllerTest {
     @MockBean
     private SecurityUtils securityUtils;
 
+    @Captor
+    private ArgumentCaptor<CaseDetails> caseDetailsArgumentCaptor;
+    
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
@@ -57,13 +63,21 @@ public class LifeEventControllerTest {
     @Test
     public void lifeEventUpdateShouldReturnDataPayloadOkResponseCode() throws Exception {
 
-        String taskListPayload = testUtils.getStringFromFile("standingSearchPayload.json");
+        String payload = testUtils.getStringFromFile("lifeEventPayload.json");
 
         mockMvc.perform(post("/lifeevent/update")
-                .content(taskListPayload)
+                .content(payload)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("data")));
+        
+        verify(lifeEventService).verifyDeathRecord(caseDetailsArgumentCaptor.capture(), any());
+        final CaseDetails caseDetailsArgumentCaptorValue = caseDetailsArgumentCaptor.getValue();
+        assertThat(caseDetailsArgumentCaptorValue.getId()).isEqualTo(1621002468661478L);
+        final CaseData data = caseDetailsArgumentCaptorValue.getData();
+        assertThat(data.getDeceasedForenames()).isEqualTo("John");
+        assertThat(data.getDeceasedSurname()).isEqualTo("Cook");
+        assertThat(data.getDeceasedDateOfDeath().toString()).isEqualTo("2006-11-16");
+        verify(securityUtils).getSecurityDTO();
     }
-
 }
