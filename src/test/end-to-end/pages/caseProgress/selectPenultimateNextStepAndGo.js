c5e01f835604248048f20d4416c587e25f8cae5e
Vincent Holt <vleholt@yahoo.co.uk>
2021-05-17 13:16:34 +0100

    add clicking of penultimate option in next steps ddl

---
+++ src/test/end-to-end/pages/caseProgress/selectPenultimateNextStepAndGo.js
@@ -0,0 +1,13 @@
+'use strict';
+const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
+
+module.exports = async function () {
+    const I = this;
+    await I.waitForElement({css: '#next-step'});
+
+    const penultimateOpt = await I.grabValueFrom({css: '#next-step option:nth-last-child(2)'}); 
+    await I.selectOption({css: '#next-step'}, penultimateOpt);
+    await I.waitForEnabled({css: commonConfig.goButton});
+    
+    await I.waitForNavigationToComplete(commonConfig.goButton);
+};
--- src/test/end-to-end/pages/newCase/newCase.js
+++ src/test/end-to-end/pages/newCase/newCase.js
@@ -8,13 +8,5 @@ module.exports = async function () {
     const I = this;
 
     await I.waitForText(newCaseConfig.waitForText, testConfig.TestTimeToWaitForText);
-    I.logPageHtml(true);
-
-    try {
-        await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator, 120);
-    } catch (e) {
-        I.logPageHtml(false, e);
-        throw e;
-    }
-    I.logPageHtml(false);
+    await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator, 120);
 };
