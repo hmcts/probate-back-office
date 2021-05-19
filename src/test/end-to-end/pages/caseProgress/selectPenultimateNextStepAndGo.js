acf1538d7cc4900e1a3d69eefa8ec08e4900bac0
Vincent Holt <vleholt@yahoo.co.uk>
2021-05-19 15:47:46 +0100

    delint

--- src/test/end-to-end/pages/caseProgress/selectPenultimateNextStepAndGo.js
+++ src/test/end-to-end/pages/caseProgress/selectPenultimateNextStepAndGo.js
@@ -5,9 +5,9 @@ module.exports = async function () {
     const I = this;
     await I.waitForElement({css: '#next-step'});
 
-    const penultimateOpt = await I.grabValueFrom({css: '#next-step option:nth-last-child(2)'}); 
+    const penultimateOpt = await I.grabValueFrom({css: '#next-step option:nth-last-child(2)'});
     await I.selectOption({css: '#next-step'}, penultimateOpt);
     await I.waitForEnabled({css: commonConfig.goButton});
-    
+
     await I.waitForNavigationToComplete(commonConfig.goButton);
 };
