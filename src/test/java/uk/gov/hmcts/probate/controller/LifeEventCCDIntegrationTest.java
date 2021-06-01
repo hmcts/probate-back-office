06fcbf4729154f3ccf314d65078618aed4bb149b
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-06-01 19:01:53 +0100

    Remove lev properties from test application.yml

--- src/test/java/uk/gov/hmcts/probate/controller/LifeEventCCDIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/LifeEventCCDIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -35,16 +36,15 @@ import static org.awaitility.Awaitility.await;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = {
-        "core_case_data.api.url=localhost:9400/ccd",
-        "idam.s2s-auth.url=http://localhost:9400/idam"
-})
+@SpringBootTest()
+@TestPropertySource("classpath:LifeEventCCDIntegrationTest.properties")
 @AutoConfigureMockMvc
 public class LifeEventCCDIntegrationTest {
 
     private static WireMockServer wireMockServer;
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(9400);
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -61,18 +61,18 @@ public class LifeEventCCDIntegrationTest {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
         wireMockRule.stubFor(post(urlEqualTo("/token"))
-                .willReturn(okJson("{ \"access_token\": \"dummyToken\"}")));
-        
+            .willReturn(okJson("{ \"access_token\": \"dummyToken\"}")));
+
         wireMockRule.stubFor(post(urlEqualTo("/idam/lease"))
-                .willReturn(ok("idamToken")));
-        
+            .willReturn(ok("idamToken")));
+
         wireMockRule.stubFor(get(urlPathMatching("/ccd/citizens/jurisdictions/.*"))
-                .willReturn(okJson("{ \"token\": \"dummyCcdToken\"}")));
+            .willReturn(okJson("{ \"token\": \"dummyCcdToken\"}")));
 
         wireMockRule.stubFor(post(urlPathMatching("/ccd/citizens/.*"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                )
+            .willReturn(aResponse()
+                .withStatus(200)
+            )
         );
     }
 
@@ -81,32 +81,32 @@ public class LifeEventCCDIntegrationTest {
     public void shouldUpdateCCDIfSingleRecordReturned() throws Exception {
 
         wireMockRule.stubFor(get(urlPathMatching("/api/.*"))
-                .willReturn(okJson(
-                        "[\n"
-                                + "    {\n"
-                                + "        \"id\": 500035096,\n"
-                                + "        \"deceased\": {\n"
-                                + "            \"forenames\": \"John TEST\",\n"
-                                + "            \"surname\": \"COOK\",\n"
-                                + "            \"dateOfBirth\": \"1901-01-01\",\n"
-                                + "            \"dateOfDeath\": \"2006-11-16\"\n"
-                                + "        }\n"
-                                + "    }\n"
-                                + "]"))
+            .willReturn(okJson(
+                "[\n"
+                    + "    {\n"
+                    + "        \"id\": 500035096,\n"
+                    + "        \"deceased\": {\n"
+                    + "            \"forenames\": \"John TEST\",\n"
+                    + "            \"surname\": \"COOK\",\n"
+                    + "            \"dateOfBirth\": \"1901-01-01\",\n"
+                    + "            \"dateOfDeath\": \"2006-11-16\"\n"
+                    + "        }\n"
+                    + "    }\n"
+                    + "]"))
         );
 
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-                .post("/lifeevent/update")
-                .content(testUtils.getStringFromFile("lifeEventPayload.json"))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+            .post("/lifeevent/update")
+            .content(testUtils.getStringFromFile("lifeEventPayload.json"))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
 
         await()
-                .atMost(2, SECONDS)
-                .untilAsserted(() ->
-                        verify(postRequestedFor(urlEqualTo(
-                                "/ccd/citizens/jurisdictions/PROBATE/case-types/GrantOfRepresentation"
-                                       + "/cases/1621002468661478/events?ignore-warning=false"))));
+            .atMost(2, SECONDS)
+            .untilAsserted(() ->
+                verify(postRequestedFor(urlEqualTo(
+                    "/ccd/citizens/jurisdictions/PROBATE/case-types/GrantOfRepresentation"
+                        + "/cases/1621002468661478/events?ignore-warning=false"))));
 
     }
 
@@ -114,21 +114,21 @@ public class LifeEventCCDIntegrationTest {
     public void shouldNotUpdateCCDIfNoRecordsReturned() throws Exception {
 
         wireMockRule.stubFor(get(urlPathMatching("/api/.*"))
-                .willReturn(okJson(
-                        "[]"))
+            .willReturn(okJson(
+                "[]"))
         );
 
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-                .post("/lifeevent/update")
-                .content(testUtils.getStringFromFile("lifeEventPayload.json"))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+            .post("/lifeevent/update")
+            .content(testUtils.getStringFromFile("lifeEventPayload.json"))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
 
         await().during(2, SECONDS)
-                .untilAsserted(() ->
-                        verify(0, postRequestedFor(urlEqualTo(
-                                "/ccd/citizens/jurisdictions/PROBATE/case-types/GrantOfRepresentation/"
-                                       + "cases/1621002468661478/events?ignore-warning=false"))));
+            .untilAsserted(() ->
+                verify(0, postRequestedFor(urlEqualTo(
+                    "/ccd/citizens/jurisdictions/PROBATE/case-types/GrantOfRepresentation/"
+                        + "cases/1621002468661478/events?ignore-warning=false"))));
 
     }
 
@@ -136,39 +136,39 @@ public class LifeEventCCDIntegrationTest {
     public void shouldNotUpdateCCDIfMultipleRecordsReturned() throws Exception {
 
         wireMockRule.stubFor(get(urlPathMatching("/api/.*"))
-                .willReturn(okJson(
-                        "[\n"
-                                + "    {\n"
-                                + "        \"id\": 500035096,\n"
-                                + "        \"deceased\": {\n"
-                                + "            \"forenames\": \"John TEST\",\n"
-                                + "            \"surname\": \"COOK\",\n"
-                                + "            \"dateOfBirth\": \"1901-01-01\",\n"
-                                + "            \"dateOfDeath\": \"2006-11-16\"\n"
-                                + "        }\n"
-                                + "    },\n"
-                                + "    {\n"
-                                + "        \"id\": 500035096,\n"
-                                + "        \"deceased\": {\n"
-                                + "            \"forenames\": \"John Another TEST\",\n"
-                                + "            \"surname\": \"COOK\",\n"
-                                + "            \"dateOfBirth\": \"1901-01-01\",\n"
-                                + "            \"dateOfDeath\": \"2006-11-16\"\n"
-                                + "        }\n"
-                                + "    }\n"
-                                + "]"))
+            .willReturn(okJson(
+                "[\n"
+                    + "    {\n"
+                    + "        \"id\": 500035096,\n"
+                    + "        \"deceased\": {\n"
+                    + "            \"forenames\": \"John TEST\",\n"
+                    + "            \"surname\": \"COOK\",\n"
+                    + "            \"dateOfBirth\": \"1901-01-01\",\n"
+                    + "            \"dateOfDeath\": \"2006-11-16\"\n"
+                    + "        }\n"
+                    + "    },\n"
+                    + "    {\n"
+                    + "        \"id\": 500035096,\n"
+                    + "        \"deceased\": {\n"
+                    + "            \"forenames\": \"John Another TEST\",\n"
+                    + "            \"surname\": \"COOK\",\n"
+                    + "            \"dateOfBirth\": \"1901-01-01\",\n"
+                    + "            \"dateOfDeath\": \"2006-11-16\"\n"
+                    + "        }\n"
+                    + "    }\n"
+                    + "]"))
         );
 
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post("/lifeevent/update")
-                .content(testUtils.getStringFromFile("lifeEventPayload.json"))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+            .content(testUtils.getStringFromFile("lifeEventPayload.json"))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
 
         await().during(2, SECONDS)
-                .untilAsserted(() ->
-                        verify(0, postRequestedFor(
-                                urlEqualTo("/ccd/citizens/jurisdictions/PROBATE/case-types/GrantOfRepresentation"
-                                       + "/cases/1621002468661478/events?ignore-warning=false"))));
+            .untilAsserted(() ->
+                verify(0, postRequestedFor(
+                    urlEqualTo("/ccd/citizens/jurisdictions/PROBATE/case-types/GrantOfRepresentation"
+                        + "/cases/1621002468661478/events?ignore-warning=false"))));
 
     }
 }
