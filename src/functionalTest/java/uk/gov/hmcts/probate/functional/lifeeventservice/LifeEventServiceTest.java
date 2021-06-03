861f19f6032a97ddd59b4e3eaa273a2e153db4e5
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-06-03 10:06:29 +0100

    Initiate build to pick changed secret Dtspb 1526 (#1481)
    * DTSPB-1526: Life Event Verification
    
    * DTSPB-1526: Bump LEV client to correct package names
    
    * DTSPB-1526: give test meaningful name
    
    * DTSPB-1526: bump lev client to add logging
    
    * DTSPB-1526: add logging
    
    * DTSPB-1526: fix rebase issue
    
    * DTSPB-1526: Add log messages
    
    * DTSPB-1526: Remove redundant log info
    
    * DTSPB-1526: Improve test
    
    * DTSPB-1526: Add integration test
    
    * DTSPB-1526: Fix rebase issue
    
    * DTSPB-1526: bump probate-commons
    
    * Suppress dependency warnings
    
    * Remove lev properties from test application.yml
    
    * Remove lev properties from test applicaiton.yml

---
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/lifeeventservice/LifeEventServiceTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.probate.functional.lifeeventservice;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.gov.hmcts.probate.functional.IntegrationTestBase;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+public class LifeEventServiceTest extends IntegrationTestBase {
+
+    @Test
+    public void shouldReturn200() {
+        final String jsonFromFile = utils.getJsonFromFile("caseprogress/01-appCreated.json");
+        
+        Response response = RestAssured.given()
+            .relaxedHTTPSValidation()
+            .headers(utils.getHeadersWithUserId())
+            .body(jsonFromFile)
+            .when().post("/lifeevent/update");
+
+        assertEquals(200, response.getStatusCode());
+    }
+}
+
+
