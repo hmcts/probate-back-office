4ac11d84dac25e6b1b4fc4ae138037ae7cdc8338
stelios <stelios.kalorkotis@hotmail.com>
2021-06-08 14:10:45 +0100

    DTSPB-1997 added changes for DTSPB-2004 and DTSPB-2002

--- src/main/java/uk/gov/hmcts/probate/changerule/SolsExecutorRule.java
+++ src/main/java/uk/gov/hmcts/probate/changerule/SolsExecutorRule.java
@@ -3,7 +3,8 @@ package uk.gov.hmcts.probate.changerule;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 
-import static uk.gov.hmcts.probate.model.Constants.GRANT_TYPE_PROBATE;
+import static uk.gov.hmcts.probate.model.Constants.GRANT_TYPE_ADMON;
+import static uk.gov.hmcts.probate.model.Constants.GRANT_TYPE_INTESTACY;
 import static uk.gov.hmcts.probate.model.Constants.NO;
 import static uk.gov.hmcts.probate.model.Constants.YES;
 
@@ -13,10 +14,26 @@ public class SolsExecutorRule implements ChangeRule {
 
     @Override
     public boolean isChangeNeeded(CaseData caseData) {
-        return (!GRANT_TYPE_PROBATE.equals(caseData.getSolsWillType())
+
+        if (GRANT_TYPE_ADMON.equals(caseData.getSolsWillType())
             && YES.equals(caseData.getSolsSolicitorIsExec())
             && NO.equals(caseData.getSolsSolicitorIsApplying())
-            && caseData.getSolsSolicitorNotApplyingReason().matches("PowerReserved"));
+            && caseData.getSolsSolicitorNotApplyingReason().matches("Renunciation")) {
+            return false;
+
+        } else if (GRANT_TYPE_ADMON.equals(caseData.getSolsWillType())
+            && NO.equals(caseData.getSolsSolicitorIsExec())
+            && NO.equals(caseData.getSolsSolicitorIsApplying())) {
+            return false;
+            
+        } else if (GRANT_TYPE_INTESTACY.equals(caseData.getSolsWillType())
+            && NO.equals(caseData.getSolsSolicitorIsExec())
+            && NO.equals(caseData.getSolsSolicitorIsApplying())) {
+            return false;
+
+        } else {
+            return true;
+        }
     }
 
     @Override
