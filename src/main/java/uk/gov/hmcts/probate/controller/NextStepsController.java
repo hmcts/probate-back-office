2a49031f7e212569f8ef6514568a44e7492dfbe0
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-06-10 12:26:52 +0100

    DTSPB-2061: force error scenarios - MUST REVERT

--- src/main/java/uk/gov/hmcts/probate/controller/NextStepsController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/NextStepsController.java
@@ -100,8 +100,17 @@ public class NextStepsController {
                     eventValidationService.validatePaymentResponse(callbackRequest.getCaseDetails(),
                         paymentResponse, creditAccountPaymentValidationRule);
                 if (creditPaymentResponse.getErrors().isEmpty()) {
-                    callbackResponse = callbackResponseTransformer.transformForSolicitorComplete(callbackRequest,
-                        feesResponse, paymentResponse);
+                    if (ccdData.getFee().getExtraCopiesOfGrant() == 10) {
+                        //dont complete the event in ccd - pay succesful in PAY
+                        throw new RuntimeException("break the flow before completing to ccd");
+                    } else  if (ccdData.getFee().getExtraCopiesOfGrant() == 20) {
+                        //complete event without RC details in ccd - pay succesful in PAY
+                        callbackResponse = callbackResponseTransformer.transformForSolicitorComplete(callbackRequest,
+                            feesResponse, null);
+                    } else {
+                        callbackResponse = callbackResponseTransformer.transformForSolicitorComplete(callbackRequest,
+                            feesResponse, paymentResponse);
+                    }
                 } else {
                     callbackResponse = creditPaymentResponse;
                 }
