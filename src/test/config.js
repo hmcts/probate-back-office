953a77771b7965da54b70a67297eeac68ebffe36
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-06-10 16:12:02 +0100

    DTSPB-261: show browser MUST REVERT

--- src/test/config.js
+++ src/test/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TestBackOfficeUrl: process.env.TEST_E2E_URL || 'http://localhost:3451',
-    TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
+    TestShowBrowserWindow: true,
     TestRetryFeatures: process.env.RETRY_FEATURES || 0,
     TestRetryScenarios: process.env.RETRY_SCENARIOS || 2,
     TestPathToRun: process.env.E2E_TEST_PATH || './paths/**/*.js',
