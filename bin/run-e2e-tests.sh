7b7a798189803f45b19f3b59b749c2287ccd5416
stelios <stelios.kalorkotis@hotmail.com>
2021-06-21 13:54:17 +0100

    DTSPB-2122 use secrets instead of fixed strings.

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -3,10 +3,10 @@ set -ex
 
 export TEST_E2E_URL=${TEST_XUI_E2E_URL}
 export E2E_TEST_PATH="./paths/solicitor/**/*.js"
-export TEST_USER_EMAIL="probatebackoffice@gmail.com"
-export TEST_USER_PASSWORD="Monday01"
-export PROF_USER_EMAIL="probatesolicitortestorgtest1@gmail.com"
-export PROF_USER_PASSWORD="Probate123"
+export TEST_USER_EMAIL=${SOL_USER_EMAIL}
+export TEST_USER_PASSWORD=${SOL_USER_PASSWORD}
+export PROF_USER_EMAIL=${SOL_USER_EMAIL}
+export PROF_USER_PASSWORD=${SOL_USER_PASSWORD}
 export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_XUI_SERVICE='true'
 export TESTS_FOR_ACCESSIBILITY='true'
@@ -16,12 +16,10 @@ yarn test:functional
 
 export TEST_E2E_URL=${TEST_CCD_E2E_URL}
 export E2E_TEST_PATH="./paths/caseworker/**/*.js"
-export TEST_USER_EMAIL="probatebackoffice@gmail.com"
-export TEST_USER_PASSWORD="Monday01"
-
-
-export PROF_USER_EMAIL="probatesolicitorpreprod@gmail.com"
-export PROF_USER_PASSWORD="Monday01"
+export TEST_USER_EMAIL=${CW_USER_EMAIL}
+export TEST_USER_PASSWORD=${CW_USER_PASSWORD}
+export PROF_USER_EMAIL=${SOL_USER_EMAIL}
+export PROF_USER_PASSWORD=${SOL_USER_PASSWORD}
 export TESTS_FOR_XUI_SERVICE='false'
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/ccd'
