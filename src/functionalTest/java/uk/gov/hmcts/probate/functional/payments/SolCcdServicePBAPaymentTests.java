26929091fb7e4e44e6564e9ba79c4c57687671a8
Vincent Holt <vleholt@yahoo.co.uk>
2021-06-22 12:22:37 +0100

    ignores removed and revert to probatesolicitor1/2

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/payments/SolCcdServicePBAPaymentTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/payments/SolCcdServicePBAPaymentTests.java
@@ -4,7 +4,6 @@ package uk.gov.hmcts.probate.functional.payments;
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.thucydides.core.annotations.Pending;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,10 +40,7 @@ public class SolCcdServicePBAPaymentTests extends IntegrationTestBase {
         assertFalse(responseBody.contains("\"payments\": ["));
     }
 
-    // currently failing - come back to it at some point
-    // failing in FeeService, getApplicationFeeResponse, restTemplate.getForEntity(uri, FeeResponse.class)
     @Test
-    @Ignore
     public void shouldValidatePBAPayment() {
         validatePostRequestSuccessForPBAs("/nextsteps/validate", "solicitorPDFPayloadProbateAccountSuccess.json",
             "\"payments\":[", "\"reference\":\"RC-", "\"method\":\"pba\"");
@@ -66,20 +62,14 @@ public class SolCcdServicePBAPaymentTests extends IntegrationTestBase {
             "Your account is on hold");
     }
 
-    // currently failing - come back to it at some point
-    // failing in FeeService, getApplicationFeeResponse, restTemplate.getForEntity(uri, FeeResponse.class)
     @Test
-    @Ignore
     public void shouldValidatePaymentAccountDeleted() {
         validatePostRequestSuccessForPBAs("/nextsteps/validate",
             "solicitorPDFPayloadProbateAccountDeleted.json",
             "Your account is deleted");
     }
 
-    // currently failing - come back to it at some point
-    // failing in FeeService, getApplicationFeeResponse, restTemplate.getForEntity(uri, FeeResponse.class)
     @Test
-    @Ignore
     public void shouldValidatePaymentInsufficientFunds() {
         validatePostRequestSuccessForPBAs("/nextsteps/validate",
             "solicitorPDFPayloadProbateCopiesForInsufficientFunds.json",
