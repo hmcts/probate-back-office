217bd7a14eeebab9c0079e239ed63dc6933d6e35
Vincent Holt <vleholt@yahoo.co.uk>
2021-07-01 13:46:54 +0100

    reject cookies, add extra wait for multi exec, and attend sol entries in package.json to run in xui

--- src/test/end-to-end/pages/newCase/newCase.js
+++ src/test/end-to-end/pages/newCase/newCase.js
@@ -8,5 +8,16 @@ module.exports = async function () {
     const I = this;
 
     await I.waitForText(newCaseConfig.waitForText, testConfig.TestTimeToWaitForText);
-    await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator, 120);
+    const numVisibleCookieBannerEls = await I.grabNumberOfVisibleElements({css: 'body exui-root xuilib-cookie-banner'});
+    if (numVisibleCookieBannerEls > 0) {
+        // just reject additional cookies
+        const rejectLocator = {css: 'button.govuk-button[value="reject"]'};
+        await I.waitForEnabled(rejectLocator);
+        await I.click(rejectLocator);
+    }
+    await I.waitForEnabled({css: testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator});
+    await I.waitForNavigationToComplete(testConfig.TestForXUI ? newCaseConfig.xuiCreateCaseLocator : newCaseConfig.ccduilCreateCaselocator);
+    if (testConfig.TestForXUI) {
+        await I.wait(0.5);
+    }
 };
