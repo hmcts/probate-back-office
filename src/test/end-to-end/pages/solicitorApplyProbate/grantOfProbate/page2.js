0452cd735c6e8cf89480f990a64f51c490f50ae2
vinceholt <vleholt@yahoo.co.uk>
2021-07-02 10:52:33 +0100

    DTSPB-2156 - run solicitor e2e tests locally using XUI (#1636)
    * reject cookies, add extra wait for multi exec, and attend sol entries in package.json to run in xui
    
    * fix issue trying to click radio btn before its ready

--- src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page2.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/page2.js
@@ -36,7 +36,7 @@ module.exports = async function (isSolcitorMainApplicant = false) {
         if (!testConfig.TestAutoDelayEnabled) {
             // only valid for local dev where we need it to run as fast as poss to minimise
             // lost dev time
-            await I.wait(0.25);
+            await I.wait(0.5);
         }
         await I.fillField('#solsAdditionalExecutorList_0_additionalExecLastname', grantOfProbateConfig.page2_executorSurname);
         await I.click(`#solsAdditionalExecutorList_0_additionalExecNameOnWill_${grantOfProbateConfig.optionYes}`);
