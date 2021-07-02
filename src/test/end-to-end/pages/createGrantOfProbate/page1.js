cbe73a1be0ca5c0096e1eccde5a7b52fb4c832c9
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-07-02 09:08:38 +0100

    FIx failing e2e

--- src/test/end-to-end/pages/createGrantOfProbate/page1.js
+++ src/test/end-to-end/pages/createGrantOfProbate/page1.js
@@ -36,7 +36,7 @@ module.exports = async function (crud) {
         await I.selectOption('#selectionList', createGrantOfProbateConfig.page1_list5_update_option);
         await I.waitForNavigationToComplete(commonConfig.continueButton);
         await I.fillField('#boWillMessage', createGrantOfProbateConfig.page1_boWillMessage);
-        await I.click({css: `#caseHandedOffToLegacySite-${createGrantOfProbateConfig.page1_optionNo}`});
+        await I.click({css: `#caseHandedOffToLegacySite_${createGrantOfProbateConfig.page1_optionNo}`});
 
     }
 
