50d5aaf493382552aee33d508305a6b53d4eae48
Vincent Holt <vleholt@yahoo.co.uk>
2021-07-08 15:39:17 +0100

    fix e2e test issues

--- src/test/end-to-end/helpers/JSWait.js
+++ src/test/end-to-end/helpers/JSWait.js
@@ -22,14 +22,14 @@ class JSWait extends codecept_helper {
 
             await Promise.all([
                 // wait for a max of 1 min (override default of max 1 sec), but will return as soon as ready within that timeframe
-                helper.page.waitForNavigation({waitUntil: ['domcontentloaded', 'networkidle0']}, 60), // The promise resolves after navigation has finished
+                helper.page.waitForNavigation(), // The promise resolves after navigation has finished
                 helper.page.goto(url)
             ]);
         } else {
             // wait for a max of 1 min (override default of max 1 sec), but will return as soon as ready within that timeframe
             await helper.amOnPage(url, 60);
-            await helper.waitInUrl(url, 60);
         }
+        await helper.waitInUrl(url, 60);
     }
 }
 
