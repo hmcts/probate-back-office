9ea432a93c8fd6eb36f46c232b1eb4d778b9c80e
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-07-14 15:42:14 +0100

    DTSPB-1916: corrected sol gop2 test

--- src/test/end-to-end/pages/solicitorApplyProbate/intestacyDetails/page2.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/intestacyDetails/page2.js
@@ -8,9 +8,9 @@ module.exports = async function () {
     await I.waitForElement('#solsMinorityInterest');
     await I.runAccessibilityTest();
 
-    await I.click(`#solsApplicantRelationshipToDeceased_${intestacyDetailsConfig.page2_child}`);
+    await I.click(`#solsApplicantRelationshipToDeceased-${intestacyDetailsConfig.page2_child}`);
     await I.click(`#solsApplicantSiblings_${intestacyDetailsConfig.optionNo}`);
-    await I.click(`#deceasedMaritalStatus_${intestacyDetailsConfig.page2_maritalstatus}`);
+    await I.click(`#deceasedMaritalStatus-${intestacyDetailsConfig.page2_maritalstatus}`);
     await I.click(`#solsMinorityInterest_${intestacyDetailsConfig.optionNo}`);
 
     await I.waitForNavigationToComplete(commonConfig.continueButton);
