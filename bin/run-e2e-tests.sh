cca7a9eba8d6f9e09c4804096c429c8b7b71240c
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-07-15 15:55:20 +0100

    DTSPB-1916: 2 cp failing

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -2,7 +2,7 @@
 set -ex
 
 export TEST_E2E_URL=${TEST_XUI_E2E_URL}
-export E2E_TEST_PATH="./paths/singleuser/*.js"
+export E2E_TEST_PATH="./paths/multiuser/*.js"
 export TEST_USER_EMAIL="probatecaseworker@gmail.com"
 export TEST_USER_PASSWORD="Monday01"
 export PROF_USER_EMAIL="probatesolicitortestorgtest1@gmail.com"
@@ -15,5 +15,5 @@ export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT=30
 export RETRY_SCENARIOS=2
 yarn test:functional
 
-export E2E_TEST_PATH="./paths/multiuser/*.js"
+export E2E_TEST_PATH="./paths/singleuser/*.js"
 yarn test:functional
