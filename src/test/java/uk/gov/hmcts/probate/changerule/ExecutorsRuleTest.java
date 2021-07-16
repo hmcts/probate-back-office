6bc02d8bf54bd187234aa63b7367689444550c89
Vincent Holt <vleholt@yahoo.co.uk>
2021-07-16 11:05:32 +0100

    add additional unit test for better coverage

--- src/test/java/uk/gov/hmcts/probate/changerule/ExecutorsRuleTest.java
+++ src/test/java/uk/gov/hmcts/probate/changerule/ExecutorsRuleTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static uk.gov.hmcts.probate.model.Constants.TITLE_AND_CLEARING_PARTNER_OTHERS_RENOUNCING;
+import static uk.gov.hmcts.probate.model.Constants.TITLE_AND_CLEARING_TRUST_CORP;
 
 public class ExecutorsRuleTest {
     private static final String YES = "Yes";
@@ -111,6 +112,21 @@ public class ExecutorsRuleTest {
         List<CollectionMember<AdditionalExecutorPartners>> additionalExecutorsPartnersList = new ArrayList<>();
         additionalExecutorsPartnersList.add(additionalPartnerExecutors1Mock);
         when(caseDataMock.getOtherPartnersApplyingAsExecutors()).thenReturn(additionalExecutorsPartnersList);
+
+        assertFalse(undertest.isChangeNeeded(caseDataMock));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenExecutorListIsNullAndPrimaryApplicantIsNotApplyingTc() {
+        when(caseDataMock.getSolsAdditionalExecutorList()).thenReturn(null);
+        when(caseDataMock.getPrimaryApplicantIsApplying()).thenReturn(NO);
+        when(caseDataMock.getOtherPartnersApplyingAsExecutors()).thenReturn(null);
+        when(caseDataMock.getTitleAndClearingType()).thenReturn(TITLE_AND_CLEARING_TRUST_CORP);
+
+        when(additionalTrustCorpExecutors1Mock.getValue()).thenReturn(additionalTrustCorpExecutor1Mock);
+        List<CollectionMember<AdditionalExecutorTrustCorps>> additionalExecutorsTrustCorpList = new ArrayList<>();
+        additionalExecutorsTrustCorpList.add(additionalTrustCorpExecutors1Mock);
+        when(caseDataMock.getAdditionalExecutorsTrustCorpList()).thenReturn(additionalExecutorsTrustCorpList);
 
         assertFalse(undertest.isChangeNeeded(caseDataMock));
     }
