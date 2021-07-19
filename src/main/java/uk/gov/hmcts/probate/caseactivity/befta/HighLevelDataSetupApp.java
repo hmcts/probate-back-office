1be43c1cc02a17138ded390990fc502a233688d3
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-07-19 12:16:16 +0100

    Fix failing build

--- src/main/java/uk/gov/hmcts/probate/caseactivity/befta/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/probate/caseactivity/befta/HighLevelDataSetupApp.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.probate.caseactivity.befta;
 
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
+
 public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
         super(dataSetupEnvironment);
