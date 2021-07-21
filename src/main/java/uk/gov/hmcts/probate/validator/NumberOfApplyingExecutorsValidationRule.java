96c321aa9a0061ca787ef6442879f9adfd86082c
Stelios Kalorkotis <62909070+stelioskalo@users.noreply.github.com>
2021-07-21 14:23:20 +0100

    Dtspb 2225 fix validator for execs (#1672)
    * DTSPB-2225 fix validator
    
    * DTSPB-2225 fix validator
    
    * DTSPB-2225 modified validator
    
    * DTSPB-2225 UTs added.
    
    Co-authored-by: stelios <stelios.kalorkotis@hotmail.com>

--- src/main/java/uk/gov/hmcts/probate/validator/NumberOfApplyingExecutorsValidationRule.java
+++ src/main/java/uk/gov/hmcts/probate/validator/NumberOfApplyingExecutorsValidationRule.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static uk.gov.hmcts.probate.model.Constants.BUSINESS_ERROR;
+import static uk.gov.hmcts.probate.model.Constants.YES;
 
 @Component
 @RequiredArgsConstructor
@@ -38,6 +39,10 @@ public class NumberOfApplyingExecutorsValidationRule implements SolExecutorDetai
     private List<String> getErrorCodeForInvalidNumberOfApplyingExecutors(CCDData ccdData) {
         List<String> allErrorCodes = new ArrayList<>();
         long countApplying = ccdData.getExecutors().stream().filter(Executor::isApplying).count();
+
+        if (ccdData.getSolsSolicitorIsApplying() != null && YES.equals(ccdData.getSolsSolicitorIsApplying())) {
+            countApplying++;
+        }
 
         if (countApplying > MAX_EXECUTORS) {
             allErrorCodes.add(TOO_MANY_EXECUTORS);
