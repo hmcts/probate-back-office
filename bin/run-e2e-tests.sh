ef7a131dca45316695a0fbae489bf5e2d72fc8c8
Vincent Holt <vleholt@yahoo.co.uk>
2021-07-29 23:46:13 +0100

    add caseworker tests

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -42,7 +42,7 @@ export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/ccd'
 export RETRY_SCENARIOS=2
 
-# yarn test:functional
+yarn test:functional
 
 ################
 
