fb0591cc798aea4e1e21b766288c1560cc09e3ec
Vincent Holt <vleholt@yahoo.co.uk>
2021-07-29 12:59:02 +0100

    fix eslint issues

--- src/test/end-to-end/pages/markForIssue/markForIssue.js
+++ src/test/end-to-end/pages/markForIssue/markForIssue.js
@@ -10,8 +10,8 @@ module.exports = async function (caseRef) {
     await I.waitForText(markForIssueConfig.waitForText, testConfig.WaitForTextTimeout);
 
     await I.see(caseRef);
-    await I.waitForElement({css: `#boExaminationChecklistQ1_${markForIssueConfig.list1_text}`});    
-    await I.waitForEnabled({css: `#boExaminationChecklistQ1_${markForIssueConfig.list1_text}`});    
+    await I.waitForElement({css: `#boExaminationChecklistQ1_${markForIssueConfig.list1_text}`});
+    await I.waitForEnabled({css: `#boExaminationChecklistQ1_${markForIssueConfig.list1_text}`});
     await I.click(`#boExaminationChecklistQ1_${markForIssueConfig.list1_text}`);
     await I.click(`#boExaminationChecklistQ2_${markForIssueConfig.list2_text}`);
     await I.click(`#boExaminationChecklistRequestQA_${markForIssueConfig.list3_text}`);
