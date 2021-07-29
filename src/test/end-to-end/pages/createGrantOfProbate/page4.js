c30809a915a973f0b7761b573c8cc85ce135d3e3
Vincent Holt <vleholt@yahoo.co.uk>
2021-07-29 13:32:08 +0100

    e2e fix

--- src/test/end-to-end/pages/createGrantOfProbate/page4.js
+++ src/test/end-to-end/pages/createGrantOfProbate/page4.js
@@ -52,6 +52,7 @@ module.exports = async function (crud, unique_deceased_user) {
     if (crud === 'update') {
         await I.waitForText(createGrantOfProbateConfig.page4_amend_waitForText, testConfig.WaitForTextTimeout);
 
+        await I.waitForEnabled({css: '#selectionList'});
         await I.selectOption('#selectionList', createGrantOfProbateConfig.page4_list1_update_option);
         await I.waitForNavigationToComplete(commonConfig.continueButton);
 
