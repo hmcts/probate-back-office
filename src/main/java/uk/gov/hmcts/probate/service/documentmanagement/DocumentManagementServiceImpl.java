f01e54ab437025e49970af7d4c8343087d276626
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-08-17 15:26:19 +0100

    DTSPB-1238: added logging doc upload2

--- src/main/java/uk/gov/hmcts/probate/service/documentmanagement/DocumentManagementServiceImpl.java
+++ src/main/java/uk/gov/hmcts/probate/service/documentmanagement/DocumentManagementServiceImpl.java
@@ -40,11 +40,12 @@ public class DocumentManagementServiceImpl implements DocumentManagementService 
             documentType);
 
         SecurityDTO securityDTO = securityUtils.getSecurityDTO();
-        String auth = securityDTO.getAuthorisation();
+        String auth = "Bearer " + securityDTO.getAuthorisation();
         String s2s = securityDTO.getServiceAuthorisation();
 
         log.info("auth:" + auth);
         log.info("s2s:" + s2s);
+        
         return caseDocumentClient.uploadDocuments(auth, s2s, documentUploadRequest.getCaseTypeId(),
             documentUploadRequest.getJurisdictionId(), documentUploadRequest.getFiles(), PRIVATE);
     }
