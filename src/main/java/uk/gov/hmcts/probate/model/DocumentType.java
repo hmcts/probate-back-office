61fb669748ca3dc93205e059bb806a6763f2be57
abdullahshah97 <abdullah-shah97@hotmail.com>
2021-11-02 10:48:20 +0000

    updated the trust corps document to show legalStatementProbate.pdf

--- src/main/java/uk/gov/hmcts/probate/model/DocumentType.java
+++ src/main/java/uk/gov/hmcts/probate/model/DocumentType.java
@@ -12,7 +12,7 @@ public enum DocumentType {
     LEGAL_STATEMENT_PROBATE("legalStatementProbate"),
 
     @JsonProperty("legalStatement")
-    LEGAL_STATEMENT_PROBATE_TRUST_CORPS("legalStatementGrantOfProbate"),
+    LEGAL_STATEMENT_PROBATE_TRUST_CORPS("legalStatementProbate"),
 
     @JsonProperty("legalStatement")
     LEGAL_STATEMENT_INTESTACY("legalStatementIntestacy"),
