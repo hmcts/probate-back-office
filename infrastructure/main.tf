5bb5801dae48fbad88b48077cc15e472ce823a47
itoulou <touloumbadjiani@gmail.com>
2021-11-03 13:24:35 +0000

    add missing variables as em-stiching

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -65,3 +65,15 @@ resource "azurerm_key_vault_secret" "POSTGRES-DATABASE-V11" {
   value = module.db-v11.postgresql_database
   key_vault_id = module.local_key_vault.key_vault_id
 }
+
+module "local_key_vault" {
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product = local.app_full_name
+  env = var.env
+  tenant_id = var.tenant_id
+  object_id = var.jenkins_AAD_objectId
+  resource_group_name = "${local.app_full_name}-${var.env}"
+  product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
+  common_tags = var.common_tags
+  managed_identity_object_ids = [data.azurerm_user_assigned_identity.rpa-shared-identity.principal_id]
+}
