e053c032e5b1331352f78bbec3479d8813c311a0
miteshc <mitesh.chudasama@hmcts.net>
2021-12-14 13:00:15 +0000

    DTSPB-2344-Post-Jan-IHT400421-e2e-changes

--- src/test/end-to-end/pages/solicitorApplyProbate/deceasedDetails/page1.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/deceasedDetails/page1.js
@@ -14,9 +14,23 @@ module.exports = async function () {
     await I.fillField('#deceasedDateOfBirth-month', deceasedDetailsConfig.page1_dateOfBirth_month);
     await I.fillField('#deceasedDateOfBirth-year', deceasedDetailsConfig.page1_dateOfBirth_year);
 
+    await I.click(`#deceasedDomicileInEngWales_${deceasedDetailsConfig.optionYes}`);
+
     await I.fillField('#deceasedDateOfDeath-day', deceasedDetailsConfig.page1_dateOfDeath_day);
     await I.fillField('#deceasedDateOfDeath-month', deceasedDetailsConfig.page1_dateOfDeath_month);
     await I.fillField('#deceasedDateOfDeath-year', deceasedDetailsConfig.page1_dateOfDeath_year);
+
+    await I.click(deceasedDetailsConfig.UKpostcodeLink);
+
+    await I.fillField('#deceasedAddress__detailAddressLine1', deceasedDetailsConfig.address_line1);
+    await I.fillField('#deceasedAddress__detailAddressLine2', deceasedDetailsConfig.address_line2);
+    await I.fillField('#deceasedAddress__detailAddressLine3', deceasedDetailsConfig.address_line3);
+    await I.fillField('#deceasedAddress__detailPostTown', deceasedDetailsConfig.address_town);
+    await I.fillField('#deceasedAddress__detailCounty', deceasedDetailsConfig.address_county);
+    await I.fillField('#deceasedAddress__detailPostCode', deceasedDetailsConfig.address_postcode);
+    await I.fillField('#deceasedAddress__detailCountry', deceasedDetailsConfig.address_country);
+
+    await I.click(`#deceasedAnyOtherNames_${deceasedDetailsConfig.optionNo}`);
 
     await I.waitForNavigationToComplete(commonConfig.continueButton, true);
 };
--- src/test/end-to-end/pages/solicitorApplyProbate/deceasedDetails/page2.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/deceasedDetails/page2.js
@@ -6,27 +6,36 @@ const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 module.exports = async function () {
     const I = this;
 
-    await I.waitForElement('#deceasedDomicileInEngWales');
+    //await I.waitForElement('#deceasedDomicileInEngWales');
     await I.runAccessibilityTest();
 
-    await I.click(`#deceasedDomicileInEngWales_${deceasedDetailsConfig.optionYes}`);
-    await I.click(`#deceasedAnyOtherNames_${deceasedDetailsConfig.optionNo}`);
+    await I.click(`#ihtFormEstateValuesCompleted_${deceasedDetailsConfig.optionYes}`);
+    await I.waitForText(deceasedDetailsConfig.page2_whichIHTFormsLabel);
+    await I.waitForText(deceasedDetailsConfig.page2_IHT207Label);
+    await I.waitForText(deceasedDetailsConfig.page2_IHT400421Label);
 
-    await I.click(deceasedDetailsConfig.UKpostcodeLink);
+    //await I.click(`#page2_IHTOption`);
+    await I.click({css: `#ihtFormEstate-${deceasedDetailsConfig.page2_IHTOption}`})
 
-    await I.fillField('#deceasedAddress__detailAddressLine1', deceasedDetailsConfig.address_line1);
+
+    //await I.click(`#deceasedDomicileInEngWales_${deceasedDetailsConfig.optionYes}`);
+    //await I.click(`#deceasedAnyOtherNames_${deceasedDetailsConfig.optionNo}`);
+
+    //await I.click(deceasedDetailsConfig.UKpostcodeLink);
+
+    /*await I.fillField('#deceasedAddress__detailAddressLine1', deceasedDetailsConfig.address_line1);
     await I.fillField('#deceasedAddress__detailAddressLine2', deceasedDetailsConfig.address_line2);
     await I.fillField('#deceasedAddress__detailAddressLine3', deceasedDetailsConfig.address_line3);
     await I.fillField('#deceasedAddress__detailPostTown', deceasedDetailsConfig.address_town);
     await I.fillField('#deceasedAddress__detailCounty', deceasedDetailsConfig.address_county);
     await I.fillField('#deceasedAddress__detailPostCode', deceasedDetailsConfig.address_postcode);
-    await I.fillField('#deceasedAddress__detailCountry', deceasedDetailsConfig.address_country);
+    await I.fillField('#deceasedAddress__detailCountry', deceasedDetailsConfig.address_country);*/
 
-    await I.waitForText(deceasedDetailsConfig.page2_solsIHT205FormLabel);
+    /*await I.waitForText(deceasedDetailsConfig.page2_solsIHT205FormLabel);
     await I.waitForText(deceasedDetailsConfig.page2_solsIHT207FormLabel);
     await I.waitForText(deceasedDetailsConfig.page2_solsIHT400421FormLabel);
     await I.waitForText(deceasedDetailsConfig.page2_solsIHTDNUFormLabel);
-    await I.click({css: `#ihtFormId-${deceasedDetailsConfig.page2_IHTOption}`});
+    await I.click({css: `#ihtFormId-${deceasedDetailsConfig.page2_IHTOption}`});*/
     await I.fillField('#ihtGrossValue', deceasedDetailsConfig.page2_ihtGrossValue);
     await I.fillField('#ihtNetValue', deceasedDetailsConfig.page2_ihtNetValue);
 
