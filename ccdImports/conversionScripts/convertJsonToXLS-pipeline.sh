79fbf4307b92e3c0f85838e4858c1a6224880a45
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-12-15 15:47:19 +0000

    Correct the repo name for definition-processor

--- ccdImports/conversionScripts/convertJsonToXLS-pipeline.sh
+++ ccdImports/conversionScripts/convertJsonToXLS-pipeline.sh
@@ -60,7 +60,7 @@ docker run --rm --name json2xlsx \
     -v ${definition_tmp_dir}:/tmp/ccd-definition \
     -v ${definition_tmp_out_file}:/tmp/ccd-definition.xlsx \
     -e CCD_DEF_CASE_SERVICE_BASE_URL=${CCD_DEF_CASE_SERVICE_BASE_URL} \
-    hmctspublic.azurecr.io/ccd/ccd-definition-processor:latest \
+    hmctspublic.azurecr.io/ccd/definition-processor:latest \
     json2xlsx -D /tmp/ccd-definition -o /tmp/ccd-definition.xlsx
 
 cp "$definition_tmp_out_file"  "$ccd_definition_json_output_dir_name"
