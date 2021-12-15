4670c116aecadde6804fdcdc64bdeb988571ce43
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-12-15 12:59:17 +0000

    UPdated to jenkins locations for file processing

--- ccdImports/conversionScripts/convertJsonToXLS-pipeline.sh
+++ ccdImports/conversionScripts/convertJsonToXLS-pipeline.sh
@@ -21,20 +21,46 @@ ccd_definition_json_output_dir_absolute_path=$(to-abs-path "$1")
 ccd_definition_json_output_dir_name=$(echo ${ccd_definition_json_output_dir_absolute_path##*/})
 ccd_definition_excel_output_file=$(to-abs-path "${excel_output_directory}/${ccd_definition_json_output_dir_name}.xlsx")
 
-echo $ccd_definition_json_output_dir_absolute_path
-echo $ccd_definition_json_output_dir_name
-echo $ccd_definition_excel_output_file
-echo $excel_output_directory
+definition_input_dir=${ccd_definition_json_output_dir_absolute_path}
 
-echo $ccd_definition_excel_output_file
+echo ccd_definition_json_output_dir_absolute_path = $ccd_definition_json_output_dir_absolute_path
+echo ccd_definition_json_output_dir_name = $ccd_definition_json_output_dir_name
+echo ccd_definition_excel_output_file = $ccd_definition_excel_output_file
+echo excel_output_directory = $excel_output_directory
 
-if [[ ! -e ${ccd_definition_excel_output_file} ]]; then
-   touch ${ccd_definition_excel_output_file}
-fi
+
+#echo ccd_definition_json_output_dir_absolute_path = /opt/jenkins/workspace/bate_probate-back-office_PR-1778/ccdImports/configFiles/CCD_Probate_Backoffice
+#echo ccd_definition_json_output_dir_name = /opt/jenkins/workspace/bate_probate-back-office_PR-1778/ccdImports/conversionScripts/build/jsonToXLS-preview/CCD_Probate_Backoffice.xlsx
+#echo ccd_definition_excel_output_file = .././ccdImports/conversionScripts/build/jsonToXLS-preview
+#echo excel_output_directory = /opt/jenkins/workspace/bate_probate-back-office_PR-1778/ccdImports/conversionScripts/build/jsonToXLS-preview/CCD_Probate_Backoffice.xlsx
+
+#
+#echo $ccd_definition_excel_output_file
+#
+#if [[ ! -e ${ccd_definition_excel_output_file} ]]; then
+#   touch ${ccd_definition_excel_output_file}
+#fi
+
+
+[[ ! -d /tmp/jenkins-agent ]] && mkdir -p /tmp/jenkins-agent
+definition_tmp=$(mktemp -d /tmp/jenkins-agent/probate.XXXXXX)
+definition_tmp_dir="$definition_tmp/ccd-definition"
+mkdir -p "$definition_tmp_dir"
+cp -a ${definition_input_dir}/* "$definition_tmp_dir"
+definition_tmp_out_dir="${definition_tmp_dir}/build/ccd-development-config"
+[[ ! -d "$definition_tmp_out_dir" ]] && mkdir -p "$definition_tmp_out_dir"
+
+definition_tmp_out_file="${definition_tmp_out_dir}/ccd-probate-dev.xlsx"
+[[ ! -e "$definition_tmp_out_file" ]] && touch "$definition_tmp_out_file"
+
+echo definition_tmp_out_file = definition_tmp_out_file
+echo definition_tmp_dir = definition_tmp_dir
 
 docker run --rm --name json2xlsx \
-    -v ${ccd_definition_json_output_dir_absolute_path}:/tmp/ccd-definition \
-    -v ${ccd_definition_excel_output_file}:/tmp/ccd-definition.xlsx \
-    -e CCD_DEF_CASE_SERVICE_BASE_URL \
+    -v ${definition_tmp_dir}:/tmp/ccd-definition \
+    -v ${definition_tmp_out_file}:/tmp/ccd-definition.xlsx \
+    -e CCD_DEF_CASE_SERVICE_BASE_URL =${CCD_DEF_CASE_SERVICE_BASE_URL} \
     hmcts/ccd-definition-processor:latest \
     json2xlsx -D /tmp/ccd-definition -o /tmp/ccd-definition.xlsx
+
+cp "$definition_tmp_out_file"  "$ccd_definition_json_output_dir_name"
