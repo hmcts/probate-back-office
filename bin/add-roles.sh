4aa658832a33284a962ac0a08a9f506d15056911
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-12-16 16:31:51 +0000

    Added the missing roles

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -24,3 +24,20 @@ ${dir}/idam-role.sh ccd-import
 ${dir}/idam-create-caseworker.sh "ccd-import" "ccd.docker.default@hmcts.net"
 
 ${dir}/idam-role.sh "prd-aac-system"
+${dir}/ccd-add-role.sh "prd-aac-system"
+
+${dir}/ccd-add-role.sh payment
+${dir}/ccd-add-role.sh citizen
+${dir}/ccd-add-role.sh caseworker
+${dir}/ccd-add-role.sh caseworker-probate
+${dir}/ccd-add-role.sh caseworker-probate-issuer
+${dir}/ccd-add-role.sh caseworker-probate-solicitor
+${dir}/ccd-add-role.sh caseworker-probate-systemupdate
+${dir}/ccd-add-role.sh caseworker-probate-pcqextractor
+${dir}/ccd-add-role.sh caseworker-probate-caseofficer
+${dir}/ccd-add-role.sh caseworker-probate-caseadmin
+${dir}/ccd-add-role.sh caseworker-probate-registrar
+${dir}/ccd-add-role.sh caseworker-probate-superuser
+${dir}/ccd-add-role.sh caseworker-probate-scheduler
+${dir}/ccd-add-role.sh caseworker-probate-charity
+${dir}/ccd-add-role.sh caseworker-probate-bulkscan
