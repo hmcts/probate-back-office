a46f6b54e0ad643eed55d49c445bf9103abcbcb5
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-12-16 14:23:05 +0000

    Remove the additional status in idam role script

--- bin/idam-role.sh
+++ bin/idam-role.sh
@@ -10,17 +10,8 @@ if [ -z "$role" ]
 fi
 authToken=$(curl -s -X POST "${IDAM_URI}/loginUser" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "password=Ref0rmIsFun&username=idamOwner@hmcts.net" | docker run --rm --interactive stedolan/jq -r .api_auth_token)
 
-STATUS=curl --request POST \
+curl --request POST \
   --url "${IDAM_URI}/roles" \
   --header "Authorization: AdminApiAuthToken ${authToken}" \
   --header "Content-Type: application/json" \
   --data '{"id": "'${role}'","name": "'${role}'","description": "'${role}'","assignableRoles": [],"conflictingRoles": []}'
-
-if [ $STATUS -eq 201 ]; then
-  echo "Role created sucessfully"
-elif [ $STATUS -eq 409 ]; then
-  echo "Role already exists!"
-else
-  echo "ERROR: HTTPCODE = $STATUS"
-  exit 1
-fi
