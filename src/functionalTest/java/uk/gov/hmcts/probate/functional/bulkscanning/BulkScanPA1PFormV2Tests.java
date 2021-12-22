aca9bf30b7c60522d3e28b4e87c97c47c8c29589
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-12-22 12:22:47 +0000

    DTSPB-2444: initial commit

---
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/bulkscanning/BulkScanPA1PFormV2Tests.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.probate.functional.bulkscanning;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.gov.hmcts.probate.functional.IntegrationTestBase;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+public class BulkScanPA1PFormV2Tests extends IntegrationTestBase {
+
+    private static final String SUCCESS = "SUCCESS";
+    private static final String VALIDATE_OCR_DATA = "/forms/%s/validate-ocr";
+    private static final String PA1P = "PA1P";
+    private static final String WARNINGS = "WARNINGS";
+    private static final String NQV_MISSING = "Net qualifying value (ihtEstateNetQualifyingValue) is mandatory.";
+    private String jsonRequest;
+
+    @Before
+    public void setUp() {
+        initialiseConfig();
+    }
+
+    @Test
+    public void testPost2022AllMandatoryFieldsPresentReturnNoWarning() {
+        jsonRequest = utils.getJsonFromFile("bulkScan/version2/Post2022AllMandatoryFilled.json");
+        validateOCRDataPostSuccess(PA1P, jsonRequest, SUCCESS, null, 0, 0);
+    }
+
+    @Test
+    public void testPost2022MissingMandatoryFieldsPresentReturnSomeWarnings() {
+        jsonRequest = utils.getJsonFromFile("bulkScan/version2/Post2022MissingNVQ.json");
+        validateOCRDataPostSuccess(PA1P, jsonRequest, WARNINGS, NQV_MISSING, 1, 1);
+    }
+
+    private void validateOCRDataPostSuccess(String formName, String bodyText, String containsText,
+                                            String warningMessage, int warningSize, int warningItem) {
+        RestAssured.given()
+            .config(config)
+            .relaxedHTTPSValidation()
+            .headers(utils.getHeaders())
+            .body(bodyText)
+            .when().post(String.format(VALIDATE_OCR_DATA, formName))
+            .then().assertThat().statusCode(200)
+            .and().body("warnings", hasSize(warningSize))
+            .and().body("warnings[" + warningItem + "]", equalTo(warningMessage))
+            .and().body(containsString(containsText));
+    }
+
+}
