444d661cb9ff220552271f8ce42ead9140fae633
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-01-04 15:12:29 +0000

    DTSPB-2528: remove v2 code

--- src/main/java/uk/gov/hmcts/probate/service/ocr/pa1p/PA1PCitizenMandatoryFieldsValidator.java
+++ src/main/java/uk/gov/hmcts/probate/service/ocr/pa1p/PA1PCitizenMandatoryFieldsValidator.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.probate.service.ocr.pa1p;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.keyvalue.DefaultKeyValue;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.probate.model.ccd.ocr.GORCitizenMandatoryFields;
 
@@ -19,15 +18,8 @@ import static uk.gov.hmcts.probate.service.ocr.CCDMandatoryFieldKeys.MANDATORY_F
 @RequiredArgsConstructor
 public class PA1PCitizenMandatoryFieldsValidator {
 
-    private static final DefaultKeyValue IHT_207_COMPLETED = new DefaultKeyValue("iht207completed",
-        "Did you complete an IHT400 and IHT421 form?");
-    private static final DefaultKeyValue IHT_205_COMPLETED_ONLINE = new DefaultKeyValue("iht205completedOnline",
-        "Did you complete the IHT205 online with HMRC?");
-    private static final DefaultKeyValue DIED_AFTER_SWITCH_DATE = new DefaultKeyValue("deceasedDiedOnAfterSwitchDate",
-        "Did the deceased die on or after 1 January 2022?");
-
     public void addWarnings(Map<String, String> ocrFieldValues, List<String> warnings) {
-        Stream.of(GORCitizenMandatoryFields.values()).filter(field -> field.isVersion1()).forEach(field -> {
+        Stream.of(GORCitizenMandatoryFields.values()).forEach(field -> {
             log.info("Checking {} against ocr fields", field.getKey());
             if (!ocrFieldValues.containsKey(field.getKey())) {
                 log.warn(MANDATORY_FIELD_NOT_FOUND_LOG, field.getKey());
