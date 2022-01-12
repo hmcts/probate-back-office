413c08debd6d488aadefe4432d5ac5bace14c3ce
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2022-01-12 13:43:53 +0000

    DTSPB-2540: re-enable LEV FT (#1804)
    * DTSPB-2540: re-enable LEV FT
    
    * DTSPB-2540: rearrange imports

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/lifeeventservice/LifeEventServiceTest.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/lifeeventservice/LifeEventServiceTest.java
@@ -4,16 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import java.util.List;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.thucydides.core.annotations.Pending;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.probate.functional.IntegrationTestBase;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
 import uk.gov.hmcts.probate.model.ccd.raw.response.CallbackResponse;
 import uk.gov.hmcts.probate.model.ccd.raw.response.ResponseCaseData;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,15 +22,14 @@ public class LifeEventServiceTest extends IntegrationTestBase {
     public void shouldReturn200() {
         final String jsonFromFile = utils.getJsonFromFile("caseprogress/01-appCreatedSolDtls.json");
         Response response = RestAssured.given()
-            .relaxedHTTPSValidation()
-            .headers(utils.getHeadersWithUserId())
-            .body(jsonFromFile)
-            .when().post("/lifeevent/update");
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeadersWithUserId())
+                .body(jsonFromFile)
+                .when().post("/lifeevent/update");
 
         assertEquals(200, response.getStatusCode());
     }
 
-    @Pending
     @Test
     public void shouldAddDeathRecordWhenManualUpdateAboutToStart() throws JsonProcessingException {
         final String jsonFromFile = utils.getJsonFromFile("lifeEvent/manualUpdateAboutToStart.json");
@@ -40,20 +37,19 @@ public class LifeEventServiceTest extends IntegrationTestBase {
         objectMapper.findAndRegisterModules();
         final CallbackRequest callbackRequest = objectMapper.readValue(jsonFromFile, CallbackRequest.class);
         final CallbackResponse callbackResponse = RestAssured.given()
-            .relaxedHTTPSValidation()
-            .headers(utils.getHeadersWithUserId())
-            .body(callbackRequest)
-            .when().post("/lifeevent/manualUpdateAboutToStart")
-            .then()
-            .statusCode(200)
-            .extract()
-            .as(CallbackResponse.class);
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeadersWithUserId())
+                .body(callbackRequest)
+                .when().post("/lifeevent/manualUpdateAboutToStart")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(CallbackResponse.class);
 
         final ResponseCaseData caseData = callbackResponse.getData();
         assertEquals(1, caseData.getDeathRecords().size());
     }
 
-    @Pending
     @Test
     public void shouldReturnErrorManualUpdateAboutToStart() throws JsonProcessingException {
         final String jsonFromFile = utils.getJsonFromFile("lifeEvent/manualUpdateAboutToStartNonExistent.json");
@@ -61,14 +57,14 @@ public class LifeEventServiceTest extends IntegrationTestBase {
         objectMapper.findAndRegisterModules();
         final CallbackRequest callbackRequest = objectMapper.readValue(jsonFromFile, CallbackRequest.class);
         final CallbackResponse callbackResponse = RestAssured.given()
-            .relaxedHTTPSValidation()
-            .headers(utils.getHeadersWithUserId())
-            .body(callbackRequest)
-            .when().post("/lifeevent/manualUpdateAboutToStart")
-            .then()
-            .statusCode(200)
-            .extract()
-            .as(CallbackResponse.class);
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeadersWithUserId())
+                .body(callbackRequest)
+                .when().post("/lifeevent/manualUpdateAboutToStart")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(CallbackResponse.class);
 
         final List<String> errors = callbackResponse.getErrors();
         assertEquals(1, errors.size());
