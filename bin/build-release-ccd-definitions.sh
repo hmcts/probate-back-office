7381785c2f938d5e87ad8c927f63bb7158f0fbc3
isha2040 <58481974+isha2040@users.noreply.github.com>
2022-03-07 15:05:19 +0000

    DTSPB-2219 PR Clean up

--- bin/build-release-ccd-definitions.sh
+++ bin/build-release-ccd-definitions.sh
@@ -2,18 +2,11 @@
 
 set -eu
 
-environment=${1:-prod1}
+environment=${1:-prod}
 
-if [[ ${environment} != "prod1" && ${environment} != "aat" && ${environment} != "demo" && ${environment} != "ithc" && ${environment} != "perftest" ]]; then
+if [[ ${environment} != "prod" && ${environment} != "aat" && ${environment} != "demo" && ${environment} != "ithc" && ${environment} != "perftest" ]]; then
   echo "Environment '${environment}' is not supported!"
   exit 1
 fi
 
-if [[ ${environment} == "prod1" ]]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*-testing.json"
-else
-  excludedFilenamePatterns="-e UserProfile.json,*-prod.json"
-fi
-
 .././ccdImports/conversionScripts/createAllXLS-pipeline.sh probate-back-office-${1}.service.core-compute-${1}.internal ${1}
-#./ccdImports/conversionScripts/importAllXLS.sh "${environment}"
