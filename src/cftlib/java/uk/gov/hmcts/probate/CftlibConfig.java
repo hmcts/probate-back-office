25bf900794a4025a926270c8703e9548708032b0
Alex McAusland <alex.mcausland@gmail.com>
2022-04-14 14:27:42 +0100

    Demo cftlib integration

---
+++ src/cftlib/java/uk/gov/hmcts/probate/CftlibConfig.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.probate;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
+import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
+
+@Component
+public class CftlibConfig implements CFTLibConfigurer {
+  @Override
+  public void configure(CFTLib lib) throws Exception {
+    lib.createIdamUser("test@user.com", "caseworker", "caseworker-probate", "caseworker-probate-solicitor");
+    lib.createRoles(
+      "caseworker-probate",
+      "caseworker-probate-superuser",
+      "caseworker-probate-registrar",
+      "caseworker-probate-issuer",
+      "caseworker-probate-pcqextractor",
+      "caseworker-probate-solicitor",
+      "caseworker-probate-caseadmin",
+      "caseworker-probate-caseofficer",
+      "caseworker-probate-systemupdate",
+      "caseworker-probate-scheduler",
+      "caseworker-probate-charity"
+    );
+    lib.importDefinition(new File("jsontoXLS/CCD_Probate_Backoffice.xlsx"));
+  }
+}
