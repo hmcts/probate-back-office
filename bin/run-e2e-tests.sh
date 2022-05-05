e65d806d953d8102eb692fbe60637605b9b39c9d
sunilrochani <sunil.rochani@hotmail.com>
2022-05-05 09:06:14 +0100

    DTSPB-2760: Fix nightly full functional on PRs

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -9,7 +9,7 @@ export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/solicitor/xui'
 export RETRY_SCENARIOS=2
-export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT = 120
+export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT='120'
 
 yarn test:functional
 
@@ -23,7 +23,7 @@ export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/caseworker/xui'
 export RETRY_SCENARIOS=2
-export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT = 120
+export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT='120'
 
 yarn test:functional
 
@@ -38,7 +38,7 @@ export SOL_USER_PASSWORD=${SOL_USER_PASSWORD}
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/caseprogress/xui'
 export RETRY_SCENARIOS=2
-export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT = 120
+export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT='120'
 
 yarn test:functional
 
@@ -54,6 +54,6 @@ export E2E_AUTO_DELAY_ENABLED='true'
 export TESTS_FOR_ACCESSIBILITY='false'
 export E2E_OUTPUT_DIR='./functional-output/multiuser/xui'
 export RETRY_SCENARIOS=2
-export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT = 120
+export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT='120'
 
 # yarn test:functional
