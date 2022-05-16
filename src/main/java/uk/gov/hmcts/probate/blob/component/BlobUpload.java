93665efa0cf2dbcad82f3053ebeef2c0750c46ae
sunilrochani <sunil.rochani@hotmail.com>
2022-05-16 20:15:10 +0100

    DTSPB-2686: Ability to compress daily grants, wills and re-issued grants into a compressed file for sharing with party Smee & Ford

--- src/main/java/uk/gov/hmcts/probate/blob/component/BlobUpload.java
+++ src/main/java/uk/gov/hmcts/probate/blob/component/BlobUpload.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.logging.Logger;
 
 import static java.util.Objects.nonNull;
