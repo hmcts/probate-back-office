082430a06a6f5597123bd80750117d9034f3f25d
DelythJustice <delyth.jones11@justice.gov.uk>
2022-06-17 09:59:03 +0100

    DTSPB-2776 moved cftlibconfig

--- src/cftlib/java/uk/gov/hmcts/probate/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/probate/CftLibConfig.java
@@ -1,5 +1,4 @@
-package uk.gov.hmcts.cftlib;
-
+package uk.gov.hmcts.probate;
 import java.io.File;
 
 import org.springframework.stereotype.Component;
@@ -11,8 +10,11 @@ import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
 public class CftLibConfig implements CFTLibConfigurer {
     @Override
     public void configure(CFTLib lib) throws Exception {
-        lib.createIdamUser("test@user.com", "caseworker", "caseworker-probate", "caseworker-probate-solicitor");
+
+        lib.createIdamUser("testCW@user.com", "caseworker", "caseworker-probate", "caseworker-probate-solicitor");
+        lib.createIdamUser("testAdmin@user.com", "caseworker-probate-caseadmin");
         lib.createRoles(
+            "caseworker",
             "caseworker-probate",
             "caseworker-probate-superuser",
             "caseworker-probate-registrar",
@@ -23,8 +25,15 @@ public class CftLibConfig implements CFTLibConfigurer {
             "caseworker-probate-caseofficer",
             "caseworker-probate-systemupdate",
             "caseworker-probate-scheduler",
-            "caseworker-probate-charity"
+            "caseworker-probate-charity",
+            "citizen",
+            "caseworker-caa"
         );
         lib.importDefinition(new File("jsontoXLS/CCD_Probate_Backoffice.xlsx"));
+      //  lib.importDefinition(new File("jsontoXLS/CCD_Probate_Caveat.xlsx"));
+      //  lib.importDefinition(new File("jsontoXLS/CCD_Probate_Legacy_Cases.xlsx"));
+      //  lib.importDefinition(new File("jsontoXLS/CCD_Probate_Legacy_Search.xlsx"));
+       // lib.importDefinition(new File("jsontoXLS/CCD_Probate_Will_Lodgement.xlsx"));
+      //  lib.importDefinition(new File("jsontoXLS/CCD_Probate_Standing_Search.xlsx"));
     }
 }
