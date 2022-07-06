f3a4555a9184ac5c455ae60356272aa7802be154
DelythJustice <delyth.jones11@justice.gov.uk>
2022-07-06 10:35:01 +0100

    DTSPB-1603 test fix

--- src/test/end-to-end/pages/utility/getCaseRefFromUrl.js
+++ src/test/end-to-end/pages/utility/getCaseRefFromUrl.js
@@ -6,7 +6,9 @@ module.exports = async function () {
     await I.wait(testConfig.GetCaseRefFromUrlDelay);
     const url = await I.grabCurrentUrl();
 
-    return url.replace('#Event%20History', '').split('/')
+    return url.replace('#Event%20History', '')
+        .replace('#Case%20Progress', '')
+        .split('/')
         .pop()
         .match(/.{4}/g)
         .join('-');
