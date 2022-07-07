a4a6a00efff5d74dda5b9a44ed6936267834c4e1
sunilrochani <sunil.rochani@hotmail.com>
2022-07-07 09:41:23 +0100

    DTSPB-2887: Investigation to show a case can be moved automatically in and out of Dormant

--- src/main/java/uk/gov/hmcts/probate/service/DormantCaseService.java
+++ src/main/java/uk/gov/hmcts/probate/service/DormantCaseService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.probate.model.ccd.CcdCaseType;
 import uk.gov.hmcts.probate.model.ccd.EventId;
-import uk.gov.hmcts.probate.model.ccd.raw.Document;
 import uk.gov.hmcts.probate.model.ccd.raw.request.ReturnedCaseDetails;
 import uk.gov.hmcts.probate.security.SecurityUtils;
 import uk.gov.hmcts.probate.service.ccd.CcdClientApi;
@@ -23,11 +22,9 @@ public class DormantCaseService {
 
     public void makeCasesDormant(String fromDate, String toDate) {
         log.info("Make Dormant for date: {}", fromDate);
-        //TODO: this qry is actually wrong - it should NOT include already dormant cases
         List<ReturnedCaseDetails> cases = caseQueryService.findCaseToBeMadeDormant(fromDate, fromDate);
         log.info("Found {} cases with dated document for Make Dormant", cases.size());
         for (ReturnedCaseDetails returnedCaseDetails : cases) {
-            //TODO: we dont need to set the SEH flag here
             GrantOfRepresentationData grantOfRepresentationData = GrantOfRepresentationData.builder()
                     .build();
             ccdClientApi.updateCaseAsCaseworker(CcdCaseType.GRANT_OF_REPRESENTATION,
@@ -44,8 +41,6 @@ public class DormantCaseService {
         log.info("Found {} cases with dated document for Reactivate Dormant", cases.size());
         for (ReturnedCaseDetails returnedCaseDetails : cases) {
             GrantOfRepresentationData grantOfRepresentationData = GrantOfRepresentationData.builder()
-                    //TODO: this will actually be handled by the callback of the reactivate event
-                    //.evidenceHandled(Boolean.FALSE)
                     .build();
             ccdClientApi.updateCaseAsCaseworker(CcdCaseType.GRANT_OF_REPRESENTATION,
                     returnedCaseDetails.getId().toString(),
