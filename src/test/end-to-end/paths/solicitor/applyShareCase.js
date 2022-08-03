274f0820d0036e2f2d14c6b1bf67f753c809c16a
AhsanZX97 <ahsan97@hotmail.co.uk>
2022-08-03 13:55:34 +0100

    tests for aat only will now ignore local and demo links

--- src/test/end-to-end/paths/solicitor/applyShareCase.js
+++ src/test/end-to-end/paths/solicitor/applyShareCase.js
@@ -8,26 +8,28 @@ Feature('Solicitor - Share A Case').retry(testConfig.TestRetryFeatures);
 const scenarioName = 'Solicitor - Share A Case';
 
 Scenario(scenarioName, async function ({I}, SAC) {
-    const isSolicitorNamedExecutor = true;
-    const isSolicitorApplyingExecutor = true;
+    if(testConfig.TestBackOfficeUrl.includes("aat")) {
+        const isSolicitorNamedExecutor = true;
+        const isSolicitorApplyingExecutor = true;
 
-    await I.logInfo(scenarioName, 'Login as PP user 1');
-    await I.authenticateUserShareCase(true);
+        await I.logInfo(scenarioName, 'Login as PP user 1');
+        await I.authenticateUserShareCase(true);
 
-    let nextStepName = 'Deceased details';
-    let endState = 'Application created';
+        let nextStepName = 'Deceased details';
+        let endState = 'Application created';
 
-    await I.logInfo(scenarioName, nextStepName);
-    await I.selectNewCase();
-    await I.selectCaseTypeOptions(createCaseConfig.list2_text_gor, createCaseConfig.list3_text_solGor);
-    await I.applyForProbatePage1();
-    await I.applyForProbatePage2(isSolicitorNamedExecutor, isSolicitorApplyingExecutor);
-    await I.cyaPage();
+        await I.logInfo(scenarioName, nextStepName);
+        await I.selectNewCase();
+        await I.selectCaseTypeOptions(createCaseConfig.list2_text_gor, createCaseConfig.list3_text_solGor);
+        await I.applyForProbatePage1();
+        await I.applyForProbatePage2(isSolicitorNamedExecutor, isSolicitorApplyingExecutor);
+        await I.cyaPage();
 
-    await I.shareCaseSelection(false);
+        await I.shareCaseSelection(false);
 
-    await I.logInfo(scenarioName, 'Login as PP user 2');
-    await I.authenticateUserShareCase();
-    await I.verifyShareCase();
+        await I.logInfo(scenarioName, 'Login as PP user 2');
+        await I.authenticateUserShareCase();
+        await I.verifyShareCase();
+    }
 
 }).retry(testConfig.TestRetryScenarios);
