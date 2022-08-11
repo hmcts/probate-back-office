dd513b2da0fa6effef5a0c3a072216f52add0948
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2022-08-11 09:31:34 +0100

    DTSPB-2958: try and fix callback

--- bin/build-preview-ccd-definitions.sh
+++ bin/build-preview-ccd-definitions.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-.././ccdImports/conversionScripts/createAllXLS-pipeline.sh probate-back-office-pr-${1}.service.core-compute-preview.internal "preview"
+.././ccdImports/conversionScripts/createAllXLS-pipeline.sh probate-back-office-pr-${1} "preview"
