c6dafe75ca78fdf31a6d5a65f7ecefe4e4e5da19
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2022-09-15 12:31:38 +0100

    DTSPB-2932 fix minor style problem

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -111,7 +111,7 @@ public class CallbackResponseTransformer {
     private static final String CASE_PRINTED = "CasePrinted";
     private static final String READY_FOR_EXAMINATION = "BOReadyForExamination";
     private static final String EXAMINING = "BOExamining";
-    private static final String DEFAULT_DATE_OF_DEATHTYPE="diedOn";
+    private static final String DEFAULT_DATE_OF_DEATHTYPE = "diedOn";
 
     private static final String SOL_AS_EXEC_ID = "solicitor";
     private static final String PBA_PAYMENT_METHOD = "pba";
@@ -148,6 +148,7 @@ public class CallbackResponseTransformer {
         ResponseCaseDataBuilder<?, ?> builder = ResponseCaseData.builder().dateOfDeathType(DEFAULT_DATE_OF_DEATHTYPE);
         return transformResponse(builder.build());
     }
+
     public CallbackResponse defaultIhtEstateFromDateOfDeath(CallbackRequest callbackRequest) {
         ResponseCaseDataBuilder<?,?> responseCaseDataBuilder = getResponseCaseData(callbackRequest.getCaseDetails(),
             true);
