4639035557b5aa4481c1dadbee9e34161fea2b0c
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-09-27 15:33:09 +0100

    DTSPB-3021: improvements

--- bin/run-e2e-tests-pr.sh
+++ bin/run-e2e-tests-pr.sh
@@ -41,19 +41,3 @@ export RETRY_SCENARIOS=0
 export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT='200'
 
 yarn test:functional
-
-################
-
-export TEST_E2E_URL=${TEST_E2E_URL}
-export E2E_TEST_PATH="./paths/multiuser/**/*.js"
-export CW_USER_EMAIL=${CW_USER_EMAIL}
-export CW_USER_PASSWORD=${CW_USER_PASSWORD}
-export SOL_USER_EMAIL=${SOL_USER_EMAIL}
-export SOL_USER_PASSWORD=${SOL_USER_PASSWORD}
-export E2E_AUTO_DELAY_ENABLED='true'
-export TESTS_FOR_ACCESSIBILITY='false'
-export E2E_OUTPUT_DIR='./functional-output/multiuser/xui'
-export RETRY_SCENARIOS=0
-export BO_E2E_TEST_TIME_TO_WAIT_FOR_TEXT='200'
-
-# yarn test:functional
