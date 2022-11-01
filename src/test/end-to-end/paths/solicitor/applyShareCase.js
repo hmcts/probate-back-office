8e07dd46aae3de8a3c8ffbf102846d5ae9df6f0c
hrishi-moj <105309362+hrishi-moj@users.noreply.github.com>
2022-11-01 11:46:07 +0000

    BO nightly test fix variables changed

--- src/test/end-to-end/paths/solicitor/applyShareCase.js
+++ src/test/end-to-end/paths/solicitor/applyShareCase.js
@@ -10,8 +10,8 @@ Scenario(scenarioName, async function ({I}) {
 
     const isSolicitorNamedExecutor = true;
     const isSolicitorApplyingExecutor = true;
-    await I.logInfo(scenarioName, 'Login as PP user 1');
-    await I.logInfo(scenarioName, 'Create and share a case with PP user 2');
+    //  await I.logInfo(scenarioName, 'Login as PP user 1');
+    // await I.logInfo(scenarioName, 'Create and share a case with PP user 2');
     await I.authenticateUserShareCase(true);
     const nextStepName = 'Deceased details';
     await I.logInfo(scenarioName, nextStepName);
@@ -20,20 +20,20 @@ Scenario(scenarioName, async function ({I}) {
     await I.applyForProbatePage1();
     await I.applyForProbatePage2(isSolicitorNamedExecutor, isSolicitorApplyingExecutor);
     await I.cyaPage();
-    const caseRef = await I.grabTextFrom('//div[@class="column-one-half"]//ccd-case-header');
-    const caseIdShareCase = caseRef.replace(/#/g, '');
-    const caseRefNumber = caseRef.replace(/\D/g, '');
-    await I.shareCaseSelection(caseRefNumber);
-    await I.logInfo(scenarioName, 'Login as PP user 2');
+    const sacCaseRef = await I.grabTextFrom('//div[@class="column-one-half"]//ccd-case-header');
+    const caseIdShareCase = sacCaseRef.replace(/#/g, '');
+    const sacCaseRefNumber = sacCaseRef.replace(/\D/g, '');
+    await I.shareCaseSelection(sacCaseRefNumber);
+    // await I.logInfo(scenarioName, 'Login as PP user 2');
     await I.authenticateUserShareCase(false);
-    await I.verifyShareCase(caseRefNumber);
-    await I.logInfo(scenarioName, 'Login as PP user 1');
-    await I.logInfo(scenarioName, 'Verify Case ' + caseRef + ' is not shared with  PP user 1');
+    await I.verifyShareCase(sacCaseRefNumber);
+    // await I.logInfo(scenarioName, 'Login as PP user 1');
+    // await I.logInfo(scenarioName, 'Verify Case ' + caseRef + ' is not shared with  PP user 1');
     await I.authenticateUserShareCase(true);
-    await I.shareCaseVerifyUserRemove(caseRefNumber);
-    await I.logInfo(scenarioName, 'Login as PP user 2');
-    await I.logInfo(scenarioName, 'Delete the case ' + caseRef + ' and sign out');
+    await I.shareCaseVerifyUserRemove(sacCaseRefNumber);
+    // await I.logInfo(scenarioName, 'Login as PP user 2');
+    // await I.logInfo(scenarioName, 'Delete the case ' + caseRef + ' and sign out');
     await I.authenticateUserShareCase(false);
-    await I.shareCaseDelete(caseIdShareCase, caseRef);
+    await I.shareCaseDelete(caseIdShareCase, sacCaseRef);
 
 }).retry(testConfig.TestRetryScenarios);
