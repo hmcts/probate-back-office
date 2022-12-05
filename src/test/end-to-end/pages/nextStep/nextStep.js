34c4ff00399984e0cb24ea949e686ba2c7b6151e
Marian.Irudayanathan <marian.irudayanathan@justice.gov.uk>
2022-12-05 16:11:46 +0000

    Added assertion in Next Steps selection

--- src/test/end-to-end/pages/nextStep/nextStep.js
+++ src/test/end-to-end/pages/nextStep/nextStep.js
@@ -7,6 +7,12 @@ module.exports = async function (nextStep) {
 
     const I = this;
 
+    await I.dontSee('Print the case');
+    await I.dontSee('Mark as ready for examination');
+    await I.dontSee('Find matches (Examining)');
+    await I.dontSee('Examine case');
+    await I.dontSee('Mark as ready to issue');
+    await I.see(nextStep);
     await I.waitForEnabled({css: '#next-step'}, testConfig.WaitForTextTimeout || 60);
     await I.selectOption('#next-step', nextStep);
     await I.waitForEnabled(commonConfig.submitButton, testConfig.WaitForTextTimeout || 60);
