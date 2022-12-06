09c047ad751ae36cb0b4d112b02dc62db27a5bba
Iswarya Pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2022-12-06 10:11:26 +0000

    Dtspb 2886 automate case move out from dormant (#2101)
    * DTSPB-2800 Create Dormant state in the State.jsom
    
    * DTSPB-2800 add Dormant State to AuthorisationCaseState.json
    
    * DTSPB-2800 add getUserByAuthTokenAndServiceSecurityDTO() to SecurityUtils.java
    
    * DTSPB-2808 add manual reactivate dormant case event
    
    * fix checkstyle issues
    
    * remove unused variables
    
    * remove unused imports
    
    * DTSPB-2886 update evidenceHandled to No when reactivateDormantCases
    
    * add unit test for new endpoint
    
    * rename url variable
    
    * remove unused imports
    
    * DTSPB-2886 Removed toDate and added unit tests to make dormant
    
    * DTSPB-2886:Removed toDate from Reactivate Dormant
    
    * DTSPB-2886:Modified Elastic search query
    
    * rename event name and fix hse bug
    
    * amend test
    
    * DTSPB-2886:Added tests
    
    * DTSPB-2886 Added commons tag
    
    * refactor code to mirror spike
    
    * remove unused import
    
    * amend test
    
    * suppress dep issue
    
    * remove suppression and upgrade psql jar
    
    * remove suppression and upgrade psql jar
    
    * test case stoppped service methid
    
    * add FT
    
    * fix checkstyle
    
    * DTSPB-2886 Added movedIntoDormantDateTime and scheduler code
    
    * DTSPB-2886 Changed event name
    
    * DTSPB-2886 Pointed to commons tag
    
    * DTSPB-2886 Removed controller code
    
    * DTSPB-2886 Removed unused code
    
    * DTSPB-2886 Added review comments
    
    * DTSPB-2886 Removed sonar code smells
    
    * DTSPB-2886 Removed sonar code smells
    
    * DTSPB-2886 Added time constants in app.yaml
    
    * DTSPB-2886 Added roles and try catch block
    
    * Update CaseField.json
    
    * DTSPB-2886 Added from date for testing
    
    * DTSPB-2886 Removing script in ES query for testing purpose
    
    * DTSPB-2886 Added Reactivate functionality in code
    
    * DTSPB-2886 Modified for testing need to be reverted to 1
    
    * Change label of event and remove stop reason question
    
    * DTSPB-2886 Rebase master
    
    * DTSPB-2886 Fixed build error
    
    * DTSPB-3071 Added Adhoc date for ReactivateDormantCasesTask
    
    * DTSPB-2886 updated commons and properties
    
    * DTSPB-2886 Rename field
    
    * DTSPB-2886 Suppression xml
    
    * DTSPB-2886 modify last date
    
    * DTSPB-2886-2800 Revert Dormant Dates
    
    * DTSPB-2886-2800 update common tag version
    
    Co-authored-by: MarcusHMCTS <Marcus.Ho@justice.gov.uk>
    Co-authored-by: itoulou <touloumbadjiani@gmail.com>

--- src/test/java/uk/gov/hmcts/probate/service/dataextract/ExelaDataExtractServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/dataextract/ExelaDataExtractServiceTest.java
@@ -22,6 +22,7 @@ import uk.gov.hmcts.probate.service.NotificationService;
 import uk.gov.service.notify.NotificationClientException;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,7 +51,7 @@ class ExelaDataExtractServiceTest {
     @Captor
     private ArgumentCaptor<List<ReturnedCaseDetails>> filteredCasesCaptor;
 
-    private static final String[] LAST_MODIFIED = {"2018", "1", "1", "0", "0", "0", "0"};
+    private static final LocalDateTime LAST_MODIFIED = LocalDateTime.now(ZoneOffset.UTC).minusYears(2);
     private CaseData caseData1;
     private CaseData caseData2;
 
