9f648810604534c7d6980a51d48312e8832c5c1f
hrishi-moj <105309362+hrishi-moj@users.noreply.github.com>
2023-01-03 10:18:03 +0000

    CVE Surpress and pathway for caseworker

--- src/test/end-to-end/paths/caseworker/pr_specific/grantOfProbatePathEECwEx.js
+++ src/test/end-to-end/paths/caseworker/pr_specific/grantOfProbatePathEECwEx.js
@@ -1,6 +1,5 @@
 'use strict';
 const dateFns = require('date-fns');
-
 const testConfig = require('src/test/config');
 const createCaseConfig = require('src/test/end-to-end/pages/createCase/createCaseConfig');
 const caseMatchesConfig = require('src/test/end-to-end/pages/caseMatches/grantOfProbate/caseMatchesConfigEE');
@@ -8,15 +7,11 @@ const createGrantOfProbateConfig = require('src/test/end-to-end/pages/createGran
 const documentUploadConfig = require('src/test/end-to-end/pages/documentUpload/grantOfProbate/documentUploadConfig');
 const eventSummaryConfig = require('src/test/end-to-end/pages/eventSummary/eventSummaryConfig');
 const issueGrantConfig = require('src/test/end-to-end/pages/issueGrant/issueGrantConfig');
-const markForExaminationConfig = require('src/test/end-to-end/pages/markForExamination/markForExaminationConfig');
-const markForIssueConfig = require('src/test/end-to-end/pages/markForIssue/markForIssueConfig');
 const applicantDetailsTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/applicantDetailsTabConfigEE');
 const caseDetailsTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/caseDetailsTabConfigEE');
 const caseMatchesTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/caseMatchesTabConfig');
 const deceasedTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/deceasedTabConfigEE');
-const docNotificationsTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/docNotificationsTabConfig');
 const documentUploadTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/documentUploadTabConfig');
-const examChecklistTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/examChecklistTabConfig');
 const grantNotificationsTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/grantNotificationsTabConfig');
 const historyTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/historyTabConfig');
 const copiesTabConfig = require('src/test/end-to-end/pages/caseDetails/grantOfProbate/copiesTabConfig');
