3dd01b6a53097db2753569931c73634be71a1081
danielbe11 <daniel.bell@solirius.com>
2023-01-10 09:34:56 +0000

    Removed unnecessary cftlib config

--- src/cftlib/java/uk/gov/hmcts/probate/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/probate/CftLibConfig.java
@@ -1,22 +1,15 @@
 package uk.gov.hmcts.probate;
 
-import java.io.File;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
+
+import java.io.File;
 
 @Component
 public class CftLibConfig implements CFTLibConfigurer {
     @Override
     public void configure(CFTLib lib) throws Exception {
-
-        lib.createIdamUser("testCW@user.com", "caseworker", "caseworker-probate", "caseworker-probate-solicitor");
-        lib.createIdamUser("testAdmin@user.com", "caseworker-probate-caseadmin");
-        lib.createIdamUser("data.store.idam.system.user@gmail.com","caseworker");
-
-        lib.createIdamUser("probatesolicitortestorgtest1@gmail.com","caseworker");
-        lib.createProfile("probatesolicitortestorgtest1@gmail.com", "PROBATE", "GrantOfRepresentation", "Pending");
-
         lib.createRoles(
             "caseworker",
             "caseworker-probate",
