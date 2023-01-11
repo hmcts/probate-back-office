aa67dd9ac6636fbe22352d2978437f7397d930d0
hrishi-moj <105309362+hrishi-moj@users.noreply.github.com>
2023-01-11 16:41:42 +0000

    Increased wait timing in codecept config

--- src/test/end-to-end/codecept.conf.js
+++ src/test/end-to-end/codecept.conf.js
@@ -9,7 +9,7 @@ exports.config = {
             'getPageTimeout': 60000,
             'show': testConfig.TestShowBrowserWindow,
             'waitForNavigation': ['domcontentloaded', 'networkidle0'],
-            'waitForAction': 600,
+            'waitForAction': 2000,
             'chrome': {
                 'ignoreHTTPSErrors': true,
                 'ignore-certificate-errors': true,
