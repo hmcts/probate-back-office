1c612b1992026f07c550539e4b0fb4e331796041
hrishi-moj <105309362+hrishi-moj@users.noreply.github.com>
2023-01-12 23:02:32 +0000

    removed spaces and changed test cases in folder

--- src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/verifyTitleAndClearingTypeOptions.js
+++ src/test/end-to-end/pages/solicitorApplyProbate/grantOfProbate/verifyTitleAndClearingTypeOptions.js
@@ -4,6 +4,7 @@ module.exports = async function () {
     const I = this;
     const opts = ['TCTPartSuccPowerRes', 'TCTPartPowerRes', 'TCTSolePrinSucc', 'TCTSolePrin', 'TCTPartSuccAllRenouncing',
         'TCTPartAllRenouncing', 'TCTTrustCorpResWithSDJ', 'TCTTrustCorpResWithApp', 'TCTPartSuccOthersRenouncing', 'TCTPartOthersRenouncing', 'TCTNoT'];
+    await I.wait(4);
     for (let i = 0; i < opts.length; i++) {
         // eslint-disable-next-line no-await-in-loop
         await I.verifyTitleAndClearingTypeOption(opts[i]);
