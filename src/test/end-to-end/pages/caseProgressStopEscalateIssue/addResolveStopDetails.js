fe448f828ecc42429b671508a092a38504f80c2c
Marian.Irudayanathan <marian.irudayanathan@justice.gov.uk>
2023-01-13 14:47:38 +0000

    Resolve Stop updated

--- src/test/end-to-end/pages/caseProgressStopEscalateIssue/addResolveStopDetails.js
+++ src/test/end-to-end/pages/caseProgressStopEscalateIssue/addResolveStopDetails.js
@@ -5,11 +5,6 @@ const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 module.exports = async function () {
     const I = this;
     await I.waitForElement({css: '#resolveStopState'});
-    await I.selectOption({css: '#resolveStopState'}, '1: CasePrinted');
-    await I.waitForEnabled({css: 'div.panel button'});
-    await I.click({css: 'div.panel button'});
-    await I.waitForElement({css: '#boCaseStopReasonList_0_caseStopReason'});
-    await I.selectOption({css: '#boCaseStopReasonList_0_caseStopReason'}, '1: ExecNotAccountedFor');
-
+    await I.selectOption({css: '#resolveStopState'}, '4: CasePrinted');
     await I.waitForNavigationToComplete(commonConfig.continueButton);
 };
