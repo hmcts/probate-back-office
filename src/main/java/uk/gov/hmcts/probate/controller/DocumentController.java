8dd2285a281fce3f29bbd214f56d6afa09220936
nilay <nilay.patel@hmcts.net>
2023-01-18 14:09:58 +0000

    DTSPB-3242 scenario 1 and 2

--- src/main/java/uk/gov/hmcts/probate/controller/DocumentController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/DocumentController.java
@@ -354,7 +354,19 @@ public class DocumentController {
 
     @PostMapping(path = "/evidenceAdded", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CallbackResponse> evidenceAdded(@RequestBody CallbackRequest callbackRequest) {
-        evidenceUploadService.updateLastEvidenceAddedDate(callbackRequest.getCaseDetails());
+        if (callbackRequest.getCaseDetails().getData().getGrantStoppedDate() != null) {
+            log.info("Case is stopped.");
+            log.info("getDocumentUploadedAfterCaseStopped has value {}", callbackRequest.getCaseDetails().getData().getDocumentUploadedAfterCaseStopped());
+            if (callbackRequest.getCaseDetails().getData().getDocumentUploadedAfterCaseStopped().equalsIgnoreCase("No") || callbackRequest.getCaseDetails().getData().getDocumentUploadedAfterCaseStopped() == null) {
+                evidenceUploadService.setDocumentUploadedAfterCaseStopped(callbackRequest.getCaseDetails(), "Yes");
+                evidenceUploadService.updateLastEvidenceAddedDate(callbackRequest.getCaseDetails());
+            } else {
+                log.info("A document has already been uploaded since case was stopped so no need to update lastEvidenceAddedDate");
+            }
+        } else {
+            log.info("Case is ongoing.");
+            evidenceUploadService.updateLastEvidenceAddedDate(callbackRequest.getCaseDetails());
+        }
         CallbackResponse response = callbackResponseTransformer.transformCase(callbackRequest);
         return ResponseEntity.ok(response);
     }
