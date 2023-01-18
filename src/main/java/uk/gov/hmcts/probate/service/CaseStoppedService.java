73a61837ddb3136c3fcef201415a1bfa9ad7b6e7
nilay <nilay.patel@hmcts.net>
2023-01-18 14:26:01 +0000

    DTSPB-3242 scenarios 1-4 for uploading documents

--- src/main/java/uk/gov/hmcts/probate/service/CaseStoppedService.java
+++ src/main/java/uk/gov/hmcts/probate/service/CaseStoppedService.java
@@ -25,6 +25,8 @@ public class CaseStoppedService {
     }
 
     public void caseResolved(CaseDetails caseDetails) {
+        log.info("Setting documentUploadedAfterCaseStopped to No");
+        caseDetails.getData().setDocumentUploadedAfterCaseStopped("No");
         log.info("Case resolved: {} ", caseDetails.getId());
 
         addToGrantDelay(caseDetails);
