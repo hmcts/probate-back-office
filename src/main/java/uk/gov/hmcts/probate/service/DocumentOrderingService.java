b348a2ce1ec0313aef29717a7903e2e0fb84f188
nilay <nilay.patel@hmcts.net>
2023-02-07 18:18:51 +0000

    DTSPB-3202 Ordering scanned documents service

---
+++ src/main/java/uk/gov/hmcts/probate/service/DocumentOrderingService.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.probate.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.probate.model.DocumentType;
+import uk.gov.hmcts.probate.model.ccd.raw.CollectionMember;
+import uk.gov.hmcts.probate.model.ccd.raw.Document;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
+import uk.gov.hmcts.probate.service.documentmanagement.DocumentManagementService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class DocumentOrderingService {
+    public void orderScannedDocuments(CallbackRequest callbackRequest) {
+        CaseDetails caseDetails = callbackRequest.getCaseDetails();
+        CaseData caseData = caseDetails.getData();
+        callbackRequest.getCaseDetails().getData().getScannedDocuments()
+            .sort((o1,o2) -> {
+                String[] orderOfDocs = new String[]{"coversheet", "form", "will", "forensic_sheets", "supporting_documents", "iht", "pps_legal_statement", "cherished", "other"};
+                int compare1 = 0;
+                int compare2 = 0;
+                for (int i = 0; i < orderOfDocs.length; i++) {
+                    if (o1.getValue().getType().equals(orderOfDocs[i])) {
+                        compare1 = i;
+                    }
+                    if (o2.getValue().getType().equals(orderOfDocs[i])) {
+                        compare2 = i;
+                    }
+                }
+                return compare1-compare2;
+            });
+    }
+}
