bee7c6121efe903c84eee7a09fd3a99385245941
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2023-03-08 09:09:14 +0000

    DTSPB-1172: try to add aac pod - for casePrinted7

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -11,7 +11,7 @@ echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-probate-back-office-pr-${pr}.preview.platform.hmcts.net"
-echo "export CCD_DEF_AAC_MANAGER_CASE_URL=http://aac-manage-case-assignment-aat.service.core-compute-aat.internal"
+echo "export CCD_DEF_AAC_MANAGER_CASE_URL=https://aac-probate-back-office-pr-${pr}.preview.platform.hmcts.net"
 
 # definition placeholders
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://probate-back-office-pr-${pr}.preview.platform.hmcts.net"
