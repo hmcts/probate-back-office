ed59163b72a03d127472ee1d0208af6fbec71f57
nilay <nilay.patel@hmcts.net>
2023-03-17 10:04:00 +0000

    DTSPB-3314 e2e fix

--- src/test/end-to-end/pages/caseProgressStopEscalateIssue/addCaseStoppedAgainReason.js
+++ src/test/end-to-end/pages/caseProgressStopEscalateIssue/addCaseStoppedAgainReason.js
@@ -6,8 +6,8 @@ module.exports = async function () {
     const I = this;
     await I.waitForElement({css: '#boCaseStopReasonList_0_caseStopReason'});
     await I.selectOption({css: '#boCaseStopReasonList_0_caseStopReason'}, '25: DocumentsRequired');
-    await I.waitForElement({css: '#boCaseStopReasonList_0_caseStopSubReasonMyStopReason'});
-    await I.selectOption({css: '#boCaseStopReasonList_0_caseStopSubReasonMyStopReason'}, 'PA11');
+    await I.waitForElement({css: '#boCaseStopReasonList_0_caseStopSubReasonDocRequired'});
+    await I.selectOption({css: '#boCaseStopReasonList_0_caseStopSubReasonDocRequired'}, 'PA11');
 
     await I.waitForNavigationToComplete(commonConfig.continueButton);
 };
