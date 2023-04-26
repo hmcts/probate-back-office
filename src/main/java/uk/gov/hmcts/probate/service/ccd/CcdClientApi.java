4e3dff831660314cdfd905b64326c62f43910c9c
DelythJustice <delyth.jones11@justice.gov.uk>
2023-04-26 18:59:37 +0100

    DTSPB-3549 bugfix compare to nearest millisecond as we only store to millisecond

--- src/main/java/uk/gov/hmcts/probate/service/ccd/CcdClientApi.java
+++ src/main/java/uk/gov/hmcts/probate/service/ccd/CcdClientApi.java
@@ -17,6 +17,7 @@ import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.probate.model.cases.CaseData;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -93,7 +94,7 @@ public class CcdClientApi implements CoreCaseDataService {
                 eventId.getName()
         );
         //check case not updated in-between DTSPB-3367
-        if (startEventResponse.getCaseDetails().getLastModified().isAfter(lastModified)) {
+        if (startEventResponse.getCaseDetails().getLastModified().truncatedTo(ChronoUnit.MILLIS).isAfter(lastModified)) {
             throw new ConcurrentDataUpdateException(
                 String.format("caseId: %s not updated as working with out of date case details", caseId));
         }
