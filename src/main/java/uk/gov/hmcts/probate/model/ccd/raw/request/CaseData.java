33870d409ba7fd85f247cc0741576e4effd972cc
Marcus Ho <99657446+MarcusHMCTS@users.noreply.github.com>
2023-06-21 11:24:38 +0100

    Dtspb 3438 3 superuser change case state (#2331)
    * DTSPB-3438 Superuser change case state
    
    * DTSPB-3438 Superuser change case state
    
    * DTSPB-3438 Superuser change case state add test cases
    
    * DTSPB-3438 Superuser change case state add merge with master
    
    * DTSPB-3438 Superuser change case state add merge with master
    
    * DTSPB-3438 Superuser change case state add merge with master
    
    * DTSPB-3438 Superuser adding tests tp CallbackResponseTransformer & BusinessValidationUnitTest
    
    * DTSPB-3438 fixing style issue
    
    * DTSPB-3438 fixing Sonar issue with Optional.isPresent()
    
    * DTSPB-3438 remove case Created and Case Matching(Examining) from dropdown list
    
    * DTSPB-3438 replace currentApplication state name with CCD Metadata
    
    * DTSPB-3438 Remove duplicate test & no use code
    
    * DTSPB-3438 fix merge to master issue
    
    * DTSPB-3438 add Superuser e2e test
    
    * DTSPB-3438 fix CVE suppression
    
    * DTSPB-3438 fix e2e test
    
    * DTSPB-3438-2 testing
    
    * DTSPB-3438-2 fix CVE
    
    * DTSPB-3438 fix CVE
    
    * DTSPB-3438 add change state event Read for CW
    
    * DTSPB-3438 update java chart to 4.0.13
    
    * Bumping chart version/ fixing aliases
    
    * DTSPB-3438 update ccd chart to 8.0.27
    
    * DTSPB-3438 update logstash image
    
    * DTSPB-3438 suppress CVE 2022-45688
    
    * Bumping chart version/ fixing aliases
    
    * DTSPB-3438 fix CVE suppression
    
    * DTSPB-3438 fix CVE-2023-20860
    
    * DTSPB-3438 fix CVE-2023-20860
    
    * DTSPB-3438 fix cve suppression
    
    * DTSPB-3438 remove Dormant state
    
    * DTSPB-3482 reorder the state on CaseEvent.json
    
    * DTSPB-3438 create superuser change case state 3 branch
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: FeliTam <feli.tam@justice.gov.uk>

--- src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseData.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseData.java
@@ -457,6 +457,7 @@ public class CaseData extends CaseDataParent {
     private final String bulkPrintPdfSize;
     private final String dateOfDeathType;
     private final String resolveStopState;
+    private final String transferToState;
     private final String orderNeeded;
     private final List<CollectionMember<Reissue>> reissueReason;
     private final String reissueReasonNotation;
