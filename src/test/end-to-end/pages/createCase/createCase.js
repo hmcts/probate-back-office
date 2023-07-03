4abf2599d8d2bd9085b9195aa31fa3edbae95484
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-07-03 16:26:22 +0100

    DTSPB-3615 change to waitForElement 300 Sec

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -22,9 +22,8 @@ module.exports = async function (caseType, event) {
     await I.waitForText(createCaseConfig.waitForText, testConfig.WaitForTextTimeout || 60);
     await I.wait(testConfig.CreateCaseDelay);
     await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.WaitForTextTimeout || 60);
-    console.log('waitForSelector cc-jurisdiction option[value=PROBATE]..............................');
     //await I.waitForSelector({css: '#cc-jurisdiction option[value=PROBATE]'});
-    await I.waitForElementVisible({css: '#cc-jurisdiction option[value=PROBATE]'});
+    await I.waitForElement({css: '#cc-jurisdiction option[value=PROBATE]'},300);
     await I.selectOption('#cc-jurisdiction', 'PROBATE');
     await I.wait(testConfig.CreateCaseDelay);
     await I.waitForEnabled({css: '#cc-case-type'}, testConfig.WaitForTextTimeout || 60);
