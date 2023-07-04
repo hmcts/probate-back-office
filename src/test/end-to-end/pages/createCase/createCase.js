1701e753540fdf8f4b90043d99b7875efc728001
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-07-04 16:38:34 +0100

    DTSPB-3615 rollback change at createCase.js

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -2,19 +2,7 @@
 
 const testConfig = require('src/test/config.js');
 const createCaseConfig = require('./createCaseConfig');
-async function waitForElementVisible(selector, timeout = 15000) {
-    const start = Date.now();
 
-    while (Date.now() - start < timeout) {
-        const el = document.querySelector(selector);
-        if (el) {
-            return el;
-        }
-        await new Promise(resolve => setTimeout(resolve, 1000));
-    }
-
-    return null;
-}
 module.exports = async function (caseType, event) {
 
     const I = this;
@@ -22,8 +10,7 @@ module.exports = async function (caseType, event) {
     await I.waitForText(createCaseConfig.waitForText, testConfig.WaitForTextTimeout || 60);
     await I.wait(testConfig.CreateCaseDelay);
     await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.WaitForTextTimeout || 60);
-    //await I.waitForSelector({css: '#cc-jurisdiction option[value=PROBATE]'});
-    await I.waitForElement({css: '#cc-jurisdiction option[value=PROBATE]'},400);
+    await I.waitForElement({css: '#cc-jurisdiction option[value=PROBATE]'},testConfig.WaitForTextTimeout || 60);
     await I.selectOption('#cc-jurisdiction', 'PROBATE');
     await I.wait(testConfig.CreateCaseDelay);
     await I.waitForEnabled({css: '#cc-case-type'}, testConfig.WaitForTextTimeout || 60);
