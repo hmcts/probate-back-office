f955fc7956401e2554332b92396465a081820650
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-07-07 16:42:01 +0100

    DTSPB-3615  add while loop on newCase.js & createCase.js

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -6,10 +6,15 @@ const createCaseConfig = require('./createCaseConfig');
 module.exports = async function (caseType, event) {
 
     const I = this;
+    var numElementFound =0;
     await I.wait(testConfig.CreateCaseDelay);
-    await I.waitForText(createCaseConfig.waitForText, testConfig.WaitForTextTimeout || 60);
-    await I.wait(testConfig.CreateCaseDelay);
-    await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.WaitForTextTimeout || 60);
+    do{
+        I.amOnLoadedPage(`${testConfig.TestBackOfficeUrl}/cases/case-filter`);
+        await I.wait(testConfig.CreateCaseDelay);
+        await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.WaitForTextTimeout || 60);
+        await I.wait(testConfig.CreateCaseDelay);
+        numElementFound = await I.grabNumberOfVisibleElements({css: `#cc-jurisdiction option[value=PROBATE]`});
+    }while(numElementFound <= 0)
     await I.waitForElement({css: '#cc-jurisdiction option[value=PROBATE]'}, testConfig.WaitForTextTimeout || 60);
     await I.selectOption('#cc-jurisdiction', 'PROBATE');
     await I.wait(testConfig.CreateCaseDelay);
--- src/test/end-to-end/pages/newCase/newCase.js
+++ src/test/end-to-end/pages/newCase/newCase.js
@@ -6,13 +6,18 @@ const newCaseConfig = require('./newCaseConfig');
 module.exports = async function () {
 
     const I = this;
-
-    await I.wait(testConfig.CreateCaseDelay);
-    await I.waitForText(newCaseConfig.waitForText, testConfig.WaitForTextTimeout);
-    await I.rejectCookies();
-
     const locator = newCaseConfig.xuiCreateCaseLocator;
-    await I.waitForEnabled({css: locator});
     await I.wait(testConfig.CreateCaseDelay);
+    var numElementFound = 0;
+    do{
+        I.amOnLoadedPage(`${testConfig.TestBackOfficeUrl}/cases`);
+        await I.wait(testConfig.CreateCaseDelay);
+        await I.waitForText(newCaseConfig.waitForText, testConfig.WaitForTextTimeout);
+        await I.rejectCookies();
+
+        await I.waitForEnabled({css: locator});
+        await I.wait(testConfig.CreateCaseDelay);
+        numElementFound = await I.grabNumberOfVisibleElements(locator);
+    }while(numElementFound<=0);
     await I.waitForNavigationToComplete(locator);
 };
