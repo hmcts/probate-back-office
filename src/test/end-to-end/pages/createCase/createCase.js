7d2425c42c82589bc887ecd4c58815fab72fd9ca
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-07-07 15:15:01 +0100

    DTSPB-3615  remove while loop

--- src/test/end-to-end/pages/createCase/createCase.js
+++ src/test/end-to-end/pages/createCase/createCase.js
@@ -6,15 +6,10 @@ const createCaseConfig = require('./createCaseConfig');
 module.exports = async function (caseType, event) {
 
     const I = this;
-    var numElementFound = 0;
-    do{
-        I.amOnLoadedPage(`${testConfig.TestBackOfficeUrl}/cases/case-filter`);
-        await I.wait(testConfig.CreateCaseDelay);
-        await I.waitForText(createCaseConfig.waitForText, testConfig.WaitForTextTimeout || 60);
-        await I.wait(testConfig.CreateCaseDelay);
-        await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.WaitForTextTimeout || 60);
-        numElementFound = await I.grabNumberOfVisibleElements({css: `#cc-jurisdiction option[value=PROBATE]`});
-    }while(numElementFound <= 0)
+    await I.wait(testConfig.CreateCaseDelay);
+    await I.waitForText(createCaseConfig.waitForText, testConfig.WaitForTextTimeout || 60);
+    await I.wait(testConfig.CreateCaseDelay);
+    await I.waitForEnabled({css: '#cc-jurisdiction'}, testConfig.WaitForTextTimeout || 60);
     await I.waitForElement({css: '#cc-jurisdiction option[value=PROBATE]'}, testConfig.WaitForTextTimeout || 60);
     await I.selectOption('#cc-jurisdiction', 'PROBATE');
     await I.wait(testConfig.CreateCaseDelay);
@@ -24,7 +19,6 @@ module.exports = async function (caseType, event) {
     await I.waitForEnabled({css: '#cc-event'}, testConfig.WaitForTextTimeout || 60);
     await I.retry(5).selectOption('#cc-event', event);
     await I.wait(testConfig.CreateCaseDelay);
-
     await I.waitForEnabled(createCaseConfig.startButton, testConfig.WaitForTextTimeout || 60);
     await I.waitForNavigationToComplete(createCaseConfig.startButton);
     await I.wait(testConfig.CreateCaseDelay);
