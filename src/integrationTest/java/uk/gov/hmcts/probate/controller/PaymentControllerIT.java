160523915f10b21a86678c47e0b42ccfe046b4b4
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2023-08-07 11:02:59 +0100

    fix unused import

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/PaymentControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/PaymentControllerIT.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.probate.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
