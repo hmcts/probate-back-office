7cce448b755051601372b1e996792ae19af7336a
DelythJustice <delyth.jones11@justice.gov.uk>
2023-08-09 12:24:37 +0100

    DTSPB-3541

--- src/test/java/uk/gov/hmcts/probate/service/EmailWithFileServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/EmailWithFileServiceTest.java
@@ -2,31 +2,14 @@ package uk.gov.hmcts.probate.service;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.time.temporal.ChronoUnit;
-import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.probate.config.notifications.EmailAddresses;
-import uk.gov.service.notify.NotificationClient;
-import uk.gov.service.notify.NotificationClientException;
-import uk.gov.service.notify.RetentionPeriodDuration;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -37,12 +20,12 @@ class EmailWithFileServiceTest {
 
 
     /**
-     * Method under test: {@link EmailWithFileService#emailFile(File)}
+     * Method under test: {@link EmailWithFileService#emailFile(File)}.
      */
     @Test
     void testEmailFileFail() {
         assertFalse(
             emailWithFileService.emailFile(Paths.get(System.getProperty("java.io.tmpdir"), "test.txt").toFile()));
         assertFalse(emailWithFileService.emailFile(Paths.get(System.getProperty("java.io.tmpdir"), "").toFile()));
-       }
+    }
 }
