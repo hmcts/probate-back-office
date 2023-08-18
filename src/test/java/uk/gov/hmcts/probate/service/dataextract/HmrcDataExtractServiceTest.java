40f15b33fdab016b719d4de217c4045af3ef92d9
DelythJustice <97962563+DelythJustice@users.noreply.github.com>
2023-08-18 13:31:42 +0100

    DTSPB-3541 hmrc to blob (#2351)
    * DTSPB-3541 hmrc extract via notify
    
    * DTSPB-3541
    
    * Bumping chart version/ fixing aliases
    
    * DTSPB-3541 format
    
    * DTSPB-3541 format
    
    * DTSPB-3541 removed my email put in slack one fix application.yml for tests
    
    * DTSPB-3541 ! instead of == fix
    
    * DTSPB-3541 add unit test
    
    * DTSPB-3541
    
    * DTSPB-3541 coverage
    
    * DTSPB-3541 coverage
    
    * DTSPB-3541
    
    * DTSPB-3541 checkstyle
    
    * DTSPB-3541 secret
    
    * DTSPB-3541 format maybe
    
    * DTSPB-3541 templateid change
    
    * DTSPB-3541 templateid change
    
    * DTSPB-3541 revert typo
    
    * DTSPB-3541 remove email, remove unused mailer
    
    * DTSPB-3541 missed test changes after param change and using dummy email
    
    * DTSPB-3541 rest of missed test changes after param change
    
    * Bumping chart version/ fixing aliases
    
    * DTSPB-3541 error in queryservice doesn't get logged
    
    * DTSPB-3541
    
    * DTSPB-3541 move code so the new notify is called alongside the globalscape upload.
    
    * DTSPB-3541 format
    
    * DTSPB-3541 call the email before the globalscape - as that doesn't work in lowr envs anyway
    
    * DTSPB-3451 cope with ; separated email list - need to send separate emails cos that's how notify works
    
    * DTSPB-3451 cope with ; separated email list - need to send separate emails cos that's how notify works
    
    * DTSPB-3451 setting file retention to 6months
    
    * DTSPB-3451 try encoding
    
    * DTSPB-3451 took out encoding
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/probate/service/dataextract/HmrcDataExtractServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/dataextract/HmrcDataExtractServiceTest.java
@@ -14,14 +14,19 @@ import uk.gov.hmcts.probate.model.ccd.raw.ScannedDocument;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 import uk.gov.hmcts.probate.model.ccd.raw.request.ReturnedCaseDetails;
 import uk.gov.hmcts.probate.service.CaseQueryService;
+import uk.gov.hmcts.probate.service.EmailWithFileService;
+import uk.gov.hmcts.probate.service.FileSystemResourceService;
 import uk.gov.hmcts.probate.service.FileTransferService;
 import uk.gov.hmcts.probate.service.filebuilder.FileExtractDateFormatter;
 import uk.gov.hmcts.probate.service.filebuilder.HmrcFileService;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -39,15 +44,20 @@ class HmrcDataExtractServiceTest {
     private HmrcFileService hmrcFileService;
     @Mock
     private FileExtractDateFormatter fileExtractDateFormatter;
+    @Mock
+    private EmailWithFileService emailWithFileService;
 
     @InjectMocks
     private HmrcDataExtractService hmrcDataExtractService;
+
+    private FileSystemResourceService fileSystemResourceService;
+
 
     private static final LocalDateTime LAST_MODIFIED = LocalDateTime.now(ZoneOffset.UTC).minusYears(2);
     private CaseData caseData;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws FileNotFoundException {
         MockitoAnnotations.openMocks(this);
 
         CollectionMember<ScannedDocument> scannedDocument = new CollectionMember<>(new ScannedDocument("1",
@@ -65,6 +75,11 @@ class HmrcDataExtractServiceTest {
             .scannedDocuments(scannedDocuments)
             .build();
 
+        File file = new File(UUID.randomUUID().toString());
+        when(hmrcFileService.createHmrcFile(any(), any())).thenReturn(file);
+
+
+        when(emailWithFileService.emailFile(any(), any())).thenReturn(true);
         when(fileTransferService.uploadFile(any())).thenReturn(HttpStatus.CREATED.value());
     }
 
@@ -103,7 +118,7 @@ class HmrcDataExtractServiceTest {
 
         hmrcDataExtractService.performHmrcExtractFromDate("2000-10-30", "2000-12-31");
 
-        verify(fileTransferService).uploadFile(any());
+        verify(emailWithFileService, times(1)).emailFile(any(), any());
         verify(fileExtractDateFormatter).getHmrcFormattedFileDate(anyString(), any());
         verify(hmrcFileService).createHmrcFile(any(), anyString());
     }
@@ -116,7 +131,7 @@ class HmrcDataExtractServiceTest {
 
         hmrcDataExtractService.performHmrcExtractFromDate("2000-10-30", "2000-12-31");
 
-        verify(fileTransferService, times(1)).uploadFile(any());
+        verify(emailWithFileService, times(1)).emailFile(any(), any());
         verify(fileExtractDateFormatter, times(1)).getHmrcFormattedFileDate(anyString(), any());
         verify(hmrcFileService, times(1)).createHmrcFile(any(), anyString());
     }
@@ -143,7 +158,7 @@ class HmrcDataExtractServiceTest {
             List<ReturnedCaseDetails> returnedCases = new ImmutableList.Builder<ReturnedCaseDetails>().add(new
                     ReturnedCaseDetails(caseData, LAST_MODIFIED, 1L)).build();
             when(caseQueryService.findGrantIssuedCasesWithGrantIssuedDate(any(), any())).thenReturn(returnedCases);
-            when(fileTransferService.uploadFile(any())).thenReturn(HttpStatus.SERVICE_UNAVAILABLE.value());
+            when(emailWithFileService.emailFile(any(), any())).thenReturn(false);
             when(caseQueryService.findCaseStateWithinDateRangeHMRC(any(), any())).thenReturn(returnedCases);
 
             hmrcDataExtractService.performHmrcExtractFromDate("2000-12-31", "2001-01-01");
