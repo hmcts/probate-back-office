242ba7978068f7aff1257af263e591a5c48a7d8a
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2023-08-24 10:20:17 +0100

    DTSPB-3589 changed exception name

--- src/main/java/uk/gov/hmcts/probate/service/fee/FeeService.java
+++ src/main/java/uk/gov/hmcts/probate/service/fee/FeeService.java
@@ -52,7 +52,7 @@ public class FeeService {
             ukCopiesFeeResponse = getCopiesFeeResponse(ukCopies);
             nonUkCopiesFeeResponse = getCopiesFeeResponse(nonUkCopies);
         } catch (Exception e) {
-            throw new SocketException("Exception while calling Fee register service");
+            throw new SocketException("Timeout occurred while calling Fee register service");
         }
 
 
