ea103d56818ac25409a48ff75960a943dba787d8
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2023-09-04 11:00:30 +0100

    Revert "DTSPB-3694 Changed exception"
    This reverts commit dc130e28acfa4a3ded77251574cc30d95fc5ed0f.

--- src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/ExceptionRecordService.java
+++ src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/ExceptionRecordService.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-import uk.gov.hmcts.probate.exception.OCRException;
 import uk.gov.hmcts.probate.exception.OCRMappingException;
 import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatCallbackRequest;
 import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatDetails;
@@ -144,7 +143,7 @@ public class ExceptionRecordService {
 
         } catch (Exception e) {
             log.error("Error transforming Grant of Representation case from Exception Record", e);
-            throw new OCRException(e.getMessage());
+            throw new OCRMappingException(e.getMessage());
         }
     }
 
