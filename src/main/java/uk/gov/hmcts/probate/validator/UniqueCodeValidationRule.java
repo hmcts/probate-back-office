ce91e7c4d1521c1b458ffcdda032ae9dad194013
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2023-10-09 09:16:04 +0100

    DTSPB-3784 Added null check

--- src/main/java/uk/gov/hmcts/probate/validator/UniqueCodeValidationRule.java
+++ src/main/java/uk/gov/hmcts/probate/validator/UniqueCodeValidationRule.java
@@ -22,12 +22,14 @@ public class UniqueCodeValidationRule {
 
     public void validate(CaseDetails caseDetails) {
         CaseData caseData = caseDetails.getData();
-        if (!caseData.getUniqueProbateCodeId().matches(UNIQUE_CODE_REGEX_PATTERN)
-                || !removeSpaces(caseData.getUniqueProbateCodeId())) {
-            String userMessage = businessValidationMessageRetriever
-                    .getMessage("uniqueProbateCode", null, Locale.UK);
-            throw new BusinessValidationException(userMessage,
-                    "Unique Probate code is invalid: " + caseDetails.getId());
+        if (caseData.getUniqueProbateCodeId() != null) {
+            if (!caseData.getUniqueProbateCodeId().matches(UNIQUE_CODE_REGEX_PATTERN)
+                    || !removeSpaces(caseData.getUniqueProbateCodeId())) {
+                String userMessage = businessValidationMessageRetriever
+                        .getMessage("uniqueProbateCode", null, Locale.UK);
+                throw new BusinessValidationException(userMessage,
+                        "Unique Probate code is invalid: " + caseDetails.getId());
+            }
         }
     }
 
