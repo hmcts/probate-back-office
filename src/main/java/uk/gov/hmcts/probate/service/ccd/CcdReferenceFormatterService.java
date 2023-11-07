5af916b088cf9cd29ac26516fa015c8113ce3be2
DelythJustice <delyth.jones11@justice.gov.uk>
2023-11-07 17:30:56 +0000

    DTSPB-3866 fix munged caveatReference

--- src/main/java/uk/gov/hmcts/probate/service/ccd/CcdReferenceFormatterService.java
+++ src/main/java/uk/gov/hmcts/probate/service/ccd/CcdReferenceFormatterService.java
@@ -10,9 +10,18 @@ import org.springframework.stereotype.Service;
 public class CcdReferenceFormatterService {
 
     public String getFormattedCaseReference(String caseId) {
-        return "#" + caseId.substring(0, 4) + "-"
+        if (isAlreadyFormattedWithHyphens(caseId)) {
+            return "#" + caseId;
+        } else {
+            return "#" + caseId.substring(0, 4) + "-"
                 + caseId.substring(4, 8) + "-"
                 + caseId.substring(8, 12) + "-"
                 + caseId.substring(12, 16);
+        }
+    }
+    public static boolean isAlreadyFormattedWithHyphens(String input) {
+        // Define a regular expression to match the specified format
+        String regex = "\\d{4}-\\d{4}-\\d{4}-\\d{4}";
+        return input.matches(regex);
     }
 }
--- src/test/java/uk/gov/hmcts/probate/service/ccd/CcdReferenceFormatterServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/ccd/CcdReferenceFormatterServiceTest.java
@@ -1,25 +1,45 @@
 package uk.gov.hmcts.probate.service.ccd;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class CcdReferenceFormatterServiceTest {
 
     private CcdReferenceFormatterService underTest = new CcdReferenceFormatterService();
 
-    @Test
-    void shouldGetFormattedCaseReference() {
-        String reference = "1234567890123456";
-        String formattedResponse = underTest.getFormattedCaseReference(reference);
-        assertEquals("#1234-5678-9012-3456", formattedResponse);
+    private static Stream<Arguments> caseReferenceTests() {
+        return Stream.of(arguments("1234567890123456", "#1234-5678-9012-3456"),
+            arguments("1234-5678-9012-3456", "#1234-5678-9012-3456"),
+            arguments("123456789012345678", "#1234-5678-9012-3456")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("caseReferenceTests")
+    void shouldGetFormattedCaseReference(String referenceIn, String referenceOut) {
+        String formattedResponse = underTest.getFormattedCaseReference(referenceIn);
+        assertEquals(referenceOut, formattedResponse);
     }
 
     @Test
-    void shouldGetFormattedCaseReferenceMax16() {
-        String reference = "123456789012345678";
-        String formattedResponse = underTest.getFormattedCaseReference(reference);
-        assertEquals("#1234-5678-9012-3456", formattedResponse);
+    void shouldReturnTrueWhenReferenceIsAlreadyFormatted() {
+        String reference = "1234-5678-9012-3456";
+        boolean formattedResponse = underTest.isAlreadyFormattedWithHyphens(reference);
+        assertEquals(true, formattedResponse);
+    }
+
+    @Test
+    void shouldReturnFalseWhenReferenceIsNotAlreadyFormatted() {
+        String reference = "1234567890123456";
+        boolean formattedResponse = underTest.isAlreadyFormattedWithHyphens(reference);
+        assertEquals(false, formattedResponse);
     }
 
 }
