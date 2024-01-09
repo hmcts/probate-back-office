c3d87938a505a8f3f4aa74d4ab23e2c14dced626
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-01-09 11:01:38 +0000

    DTSPB-3783 Skip FT

---
+++ src/functionalTest/empty/java/uk/gov/hmcts/probate/functional/TestContextConfiguration.java
@@ -0,0 +1,54 @@
+package java.uk.gov.hmcts.probate.functional;
+
+import feign.Feign;
+import feign.jackson.JacksonEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+@ComponentScan("uk.gov.hmcts.probate.functional")
+@PropertySource("application.properties")
+public class TestContextConfiguration {
+
+    @Value("${datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${datasource.password}")
+    private String datasourcePassword;
+
+    @Bean
+    public ServiceAuthTokenGenerator serviceAuthTokenGenerator(
+                                                              @Value("${service.auth.provider.base.url}") String s2sUrl,
+                                                              @Value("${s2s-auth.totp_secret}") String secret,
+                                                              @Value("${service.name}") String microservice) {
+        final ServiceAuthorisationApi serviceAuthorisationApi = Feign.builder()
+                .encoder(new JacksonEncoder())
+                .contract(new SpringMvcContract())
+                .target(ServiceAuthorisationApi.class, s2sUrl);
+        return new ServiceAuthTokenGenerator(secret, microservice, serviceAuthorisationApi);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(datasourceUsername);
+        dataSource.setPassword(datasourcePassword);
+        return dataSource;
+    }
+}
