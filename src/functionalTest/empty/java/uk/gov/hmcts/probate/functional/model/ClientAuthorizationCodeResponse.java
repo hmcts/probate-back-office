4367fbe36a3f24dfd90e380da04027886337b960
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-01-09 11:21:23 +0000

    DTSPB-3783 Skip FT

---
+++ src/functionalTest/empty/java/uk/gov/hmcts/probate/functional/model/ClientAuthorizationCodeResponse.java
@@ -0,0 +1,11 @@
+package java.uk.gov.hmcts.probate.functional.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClientAuthorizationCodeResponse {
+    public String code;
+
+    ClientAuthorizationCodeResponse() {
+    }
+}
