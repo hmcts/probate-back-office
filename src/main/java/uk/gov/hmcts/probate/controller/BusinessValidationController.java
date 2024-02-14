5083ad334f0df460a639e66a25660d33f92a3c66
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-02-14 17:11:11 +0000

    DTSPB-4008 Clear form data

--- src/main/java/uk/gov/hmcts/probate/controller/BusinessValidationController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/BusinessValidationController.java
@@ -206,6 +206,7 @@ public class BusinessValidationController {
         CallbackResponse response = eventValidationService.validateRequest(callbackRequest, allValidationRules);
         CaseDetails details = callbackRequest.getCaseDetails();
         if (response.getErrors().isEmpty()) {
+            caseDataTransformer.transformFormCaseData(request);
             if (YES.equals(details.getData().getHmrcLetterId()) || null == details.getData().getHmrcLetterId()) {
                 Optional<String> newState =
                         stateChangeService.getChangedStateForGrantType(callbackRequest.getCaseDetails().getData());
