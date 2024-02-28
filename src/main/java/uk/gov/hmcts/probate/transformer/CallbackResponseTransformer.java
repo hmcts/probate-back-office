d460993059ce101429964e1321d6e41b94e1588b
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-02-28 18:39:36 +0000

    DTSPB-3933 fix channelChoice issue with solicitor

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -63,6 +63,7 @@ import static uk.gov.hmcts.probate.model.Constants.GRANT_TYPE_PROBATE;
 import static uk.gov.hmcts.probate.model.Constants.LATEST_SCHEMA_VERSION;
 import static uk.gov.hmcts.probate.model.Constants.NO;
 import static uk.gov.hmcts.probate.model.Constants.YES;
+import static uk.gov.hmcts.probate.model.Constants.CHANNEL_CHOICE_DIGITAL;
 import static uk.gov.hmcts.probate.model.DocumentType.ADMON_WILL_GRANT;
 import static uk.gov.hmcts.probate.model.DocumentType.ADMON_WILL_GRANT_REISSUE;
 import static uk.gov.hmcts.probate.model.DocumentType.ASSEMBLED_LETTER;
@@ -1577,7 +1578,11 @@ public class CallbackResponseTransformer {
                     .paperForm(ANSWER_NO);
         }
 
-        builder.channelChoice(caseData.getChannelChoice());
+        if (caseData.getChannelChoice() == null) {
+            builder.channelChoice(CHANNEL_CHOICE_DIGITAL);
+        } else {
+            builder.channelChoice(caseData.getChannelChoice());
+        }
 
         if (willExists(caseData)) {
             builder
