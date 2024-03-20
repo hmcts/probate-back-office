2b96d80f9134d523c00fd8eb0a86bc180e006c2b
MarianIrudayanathan <108797124+MarianIrudayanathan@users.noreply.github.com>
2024-03-20 17:33:13 +0000

    e2e - Crossbrowser tests for BO nightly (#2526)
    * e2e - Crossbrowser tests for BO nightly
    
    * e2e - Crossbrowser tests for BO nightly
    
    * e2e - Crossbrowser tests for BO nightly

--- src/test/end-to-end/pages/ihtPages/ihtPage3.js
+++ src/test/end-to-end/pages/ihtPages/ihtPage3.js
@@ -1,11 +1,18 @@
 'use strict';
 const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 // deceased details part 2
-module.exports = async function (ihtGrossValue, ihtNetValue) {
+module.exports = async function (ihtGrossValue, ihtNetValue, whichIHTFormsCompleted) {
     const I = this;
     await I.waitForElement({css: '#ihtGrossValue'});
     await I.fillField({css: '#ihtGrossValue'}, ihtGrossValue);
-    await I.waitForElement({css: '#ihtNetValue'});
-    await I.fillField({css: '#ihtNetValue'}, ihtNetValue);
+    if (whichIHTFormsCompleted === 'IHT400') {
+        await I.waitForElement({css: '#ihtFormNetValue'});
+        await I.fillField({css: '#ihtFormNetValue'}, ihtNetValue);
+    }
+    else {
+        await I.waitForElement({css: '#ihtNetValue'});
+        await I.fillField({css: '#ihtNetValue'}, ihtNetValue);
+    }
+
     await I.waitForNavigationToComplete(commonConfig.continueButton);
 };
