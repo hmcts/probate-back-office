e58da8f33fda51046830f30f422c0a34739cdb7e
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-03-22 18:01:07 +0000

    DTSPB-3621 Opid token

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/SolCCDServiceAuthTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/SolCCDServiceAuthTokenGenerator.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.probate.functional.model.ClientAuthorizationCodeResponse;
 import uk.gov.hmcts.probate.functional.model.ClientAuthorizationResponse;
 import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 
