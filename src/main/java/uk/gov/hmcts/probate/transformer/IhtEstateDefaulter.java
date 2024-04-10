be4ff58494b17c365c9869371c7a54c205d74bd0
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-04-10 12:43:56 +0100

    DTSPB-4051 Need to revert

--- src/main/java/uk/gov/hmcts/probate/transformer/IhtEstateDefaulter.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/IhtEstateDefaulter.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.probate.transformer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
@@ -14,6 +15,7 @@ import static uk.gov.hmcts.probate.model.Constants.YES;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class IhtEstateDefaulter {
     private static final String SWITCH_DATE_FORMATTER_PATTERN = "yyyy-MM-dd";
 
@@ -25,8 +27,18 @@ public class IhtEstateDefaulter {
                                              ResponseCaseData.ResponseCaseDataBuilder<?, ?> responseCaseDataBuilder) {
 
         LocalDate dod = data.getDeceasedDateOfDeath();
+        log.info("caseData {}", data);
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(SWITCH_DATE_FORMATTER_PATTERN);
         LocalDate switchDate = LocalDate.parse(ihtEstateSwitchDate, dateFormatter);
+        if (dod.isBefore(switchDate)) {
+            log.info("Putting form estate to null");
+            responseCaseDataBuilder.ihtFormEstate(null);
+        }
+        else {
+            log.info("Putting form id to null");
+            responseCaseDataBuilder.ihtFormId(null);
+        }
         responseCaseDataBuilder.dateOfDeathAfterEstateSwitch(!dod.isBefore(switchDate) ? YES : NO);
+        log.info("response {}", responseCaseDataBuilder);
     }
 }
