c0df9f850ec9dbd28d2d9b2879bb58e188f4d93f
FeliTam <feli.tam@justice.gov.uk>
2024-05-01 07:41:25 +0100

    fix checkstyle

--- src/test/java/uk/gov/hmcts/probate/service/EmailWithFileServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/EmailWithFileServiceTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verifyNoInteractions;
