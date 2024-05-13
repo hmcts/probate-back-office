e26cb726b9514807f54e9a00ed9a7aec313881e2
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-05-13 17:43:43 +0100

    DTSPB-3885 fix review issue

--- src/main/java/uk/gov/hmcts/probate/validator/ApplicationSubmittedDateValidationRule.java
+++ src/main/java/uk/gov/hmcts/probate/validator/ApplicationSubmittedDateValidationRule.java
@@ -2,48 +2,40 @@ package uk.gov.hmcts.probate.validator;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.probate.exception.BusinessValidationException;
-import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
-import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
-import uk.gov.hmcts.probate.service.BusinessValidationMessageRetriever;
+import uk.gov.hmcts.probate.exception.model.FieldErrorResponse;
+import uk.gov.hmcts.probate.model.ccd.CCDData;
+
+import uk.gov.hmcts.probate.service.BusinessValidationMessageService;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Locale;
+import java.util.*;
+
+import static uk.gov.hmcts.probate.model.Constants.BUSINESS_ERROR;
 
 @Component
 @RequiredArgsConstructor
-public class ApplicationSubmittedDateValidationRule implements CaseDetailsValidationRule {
+public class ApplicationSubmittedDateValidationRule implements CaseworkerAmendAndCreateValidationRule {
 
-    private final BusinessValidationMessageRetriever businessValidationMessageRetriever;
+    private final BusinessValidationMessageService businessValidationMessageService;
 
-    public void validate(CaseDetails caseDetails) {
-        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.UK);
-        CaseData caseData = caseDetails.getData();
-        String newApplicationSubmittedDate = caseData.getApplicationSubmittedDate();
-        LocalDate dod = caseData.getDeceasedDateOfDeath();
-        LocalDate applicationSubmittedDate = LocalDate.parse(newApplicationSubmittedDate, dateTimeFormatter);
+    @Override
+    public List<FieldErrorResponse> validate(CCDData ccdData) {
+        Set<FieldErrorResponse> errors = new HashSet<>();
+        LocalDate dod = ccdData.getDeceasedDateOfDeath();
+        LocalDate applicationSubmittedDate = ccdData.getCaseSubmissionDate();
         try {
 
             if (applicationSubmittedDate.isAfter(LocalDate.now())) {
-                String userMessage = businessValidationMessageRetriever.getMessage(
-                        "applicationSubmittedDateIsInTheFuture", null, Locale.UK);
-                throw new BusinessValidationException(userMessage,
-                        "Application Submitted Date cannot be a future date for case: " + caseDetails.getId());
+                errors.add(businessValidationMessageService.generateError(BUSINESS_ERROR, "applicationSubmittedDateIsInTheFuture"));
             }
             if (dod.isAfter(applicationSubmittedDate)) {
-                String userMessage = businessValidationMessageRetriever.getMessage(
-                        "dodIsAfterApplicationSubmittedDate", null, Locale.UK);
-                throw new BusinessValidationException(userMessage,
-                        "Date of Death cannot be after Application Submitted Date for case: " + caseDetails.getId());
+                errors.add(businessValidationMessageService.generateError(BUSINESS_ERROR, "dodIsAfterApplicationSubmittedDate"));
             }
         } catch (DateTimeParseException dtpe) {
-            String userMessage = businessValidationMessageRetriever.getMessage("dobOverrideDateInvalid",
-                    null, Locale.UK);
-            throw new BusinessValidationException(userMessage,
-                    "Application Submitted Date is invalid format for case: " + caseDetails.getId());
+            errors.add(businessValidationMessageService.generateError(BUSINESS_ERROR, "ApplicationSubmittedDateInvalid"));
         }
+        return new ArrayList<>(errors);
     }
 
 }
