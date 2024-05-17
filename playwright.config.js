54ef6d370a109e8911f8394a815bfaa3e09669b1
Marian.Irudayanathan <marian.irudayanathan@justice.gov.uk>
2024-05-17 13:13:35 +0100

    DTSPB-4080 : Fix Lint Issues

--- playwright.config.js
+++ playwright.config.js
@@ -37,17 +37,17 @@ module.exports = defineConfig({
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            use: {...devices['Desktop Chrome']},
         },
 
         {
             name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
+            use: {...devices['Desktop Firefox']},
         },
 
         {
             name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
+            use: {...devices['Desktop Safari']},
         },
 
         //* Test against mobile viewports. */
