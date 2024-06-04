d63805c6528d192bb52b15624061e6c51d0c7fa7
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-06-04 09:54:47 +0100

    DTSPB-3883: Adjust formVersion3 ocrFields to build with exceptedEstate and not deceasedDiedOnAfterSwitchDate

--- src/test/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/OCRFieldIhtFormEstateValuesCompletedMapperTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/exceptionrecord/mapper/OCRFieldIhtFormEstateValuesCompletedMapperTest.java
@@ -51,7 +51,7 @@ class OCRFieldIhtFormEstateValuesCompletedMapperTest {
     void shouldReturnFalseWhenIhtEstateFieldsAreAllPresentFormVersion3() {
         ExceptionRecordOCRFields ocrFields = ExceptionRecordOCRFields.builder()
             .formVersion("3")
-            .deceasedDiedOnAfterSwitchDate("True")
+            .exceptedEstate("True")
             .deceasedDateOfDeath(POST_EE_DECEASED_DATE_OF_DEATH)
             .ihtEstateGrossValue("1000000")
             .ihtEstateNetValue("900000")
