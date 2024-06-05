cd4a2c7d456a31d80212cdd1eed74b91dfc1020b
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-06-05 15:09:37 +0100

    DTSPB-4183: Move from codecept to Playwright directory

---
+++ src/test/PlaywrightTest/Pages/utility/getCaseRefFromUrl.js
@@ -0,0 +1,16 @@
+'use strict';
+const testConfig = require('src/test/PlaywrightTest/Configs/config.js');
+
+module.exports = async function () {
+    const I = this;
+    await I.wait(testConfig.GetCaseRefFromUrlDelay);
+    const url = await I.grabCurrentUrl();
+
+    return url
+        .replace('#Event%20History', '')
+        .replace('#Case%20Progress', '')
+        .split('/')
+        .pop()
+        .match(/.{4}/g)
+        .join('-');
+};
