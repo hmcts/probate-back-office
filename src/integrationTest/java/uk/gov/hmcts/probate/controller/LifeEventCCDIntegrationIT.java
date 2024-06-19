f333a79657f7d7e01abbfa60ab2163b7335a640c
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-06-19 15:31:00 +0100

    delete no use import at LifeEventCCDIntegrationIT.java

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/LifeEventCCDIntegrationIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/LifeEventCCDIntegrationIT.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
