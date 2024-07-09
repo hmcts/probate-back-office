5e0e349a05aa81b07d40b219833272513452aecd
abbasiTahir <tahir.abbasi@justice.gov.uk>
2024-07-09 14:01:46 +0100

    DTSPB-4180 Removed unwanted import

--- src/integrationTest/java/uk/gov/hmcts/probate/service/NotificationServiceIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/service/NotificationServiceIT.java
@@ -64,7 +64,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
