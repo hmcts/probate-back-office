18dfc4070281f76dae2ec5f189f64b82315fe238
Marcus Ho <99657446+MarcusHMCTS@users.noreply.github.com>
2024-07-11 11:25:29 +0100

    Dtspb 4182 bulk scan document type validation (#2589)
    * DTSPB-4182 add bulkscan document type validation
    
    * DTSPB-4182 add unit tests
    
    * DTSPB-4182 add null check to the validator
    
    * DTSPB-4182 remove typo
    
    * DTSPB-4182 update to InputScannedDoc validation
    
    * DTSPB-4182 fix exception error Message
    
    * update @Ignore to @Disabled on tests
    
    * rollback incorrect change
    
    * rollback incorrect change
    
    * merged with Master

--- src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/utils/ExceptionRecordCaseDataValidator.java
+++ src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/utils/ExceptionRecordCaseDataValidator.java
@@ -1,12 +1,30 @@
 package uk.gov.hmcts.probate.service.exceptionrecord.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.exception.OCRMappingException;
+import uk.gov.hmcts.probate.model.CaseType;
+import uk.gov.hmcts.probate.model.exceptionrecord.InputScannedDoc;
 import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantOfRepresentationData;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_CHERISHED;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_COVERSHEET;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_FORM;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_PPS_LEGAL_STATEMENT;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_SUPPORTING_DOCUMENTS;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_WILL;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_IHT;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_FORENSIC_SHEETS;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_OTHER;
 
 @Slf4j
 @Component
@@ -21,6 +39,21 @@ public class ExceptionRecordCaseDataValidator {
     private static final String IHT_NETQUALIFYING_VALUE_GREATER_THAN_ESTATE_GROSS_VAlUE =
             "Net qualifying value can't be greater than the gross amount";
     private static final String IHT_VALDIATION_ERROR = "IHT Values validation error";
+    private static final String INVALID_SCANNED_DOCUMENT_TYPE_ERROR = "Invalid scanned Document Type Error "
+            + "for case type '%s': [%s]";
+
+    private static final Map<String, List<CaseType>> allowScannedDocumentTypes =
+            Map.of(DOC_TYPE_WILL, singletonList(CaseType.GRANT_OF_REPRESENTATION),
+                    DOC_TYPE_IHT, singletonList(CaseType.GRANT_OF_REPRESENTATION),
+                    DOC_TYPE_FORENSIC_SHEETS, singletonList(CaseType.GRANT_OF_REPRESENTATION),
+                    DOC_TYPE_SUPPORTING_DOCUMENTS, singletonList(CaseType.GRANT_OF_REPRESENTATION),
+                    DOC_TYPE_PPS_LEGAL_STATEMENT, singletonList(CaseType.GRANT_OF_REPRESENTATION),
+                    DOC_TYPE_COVERSHEET, List.of(CaseType.GRANT_OF_REPRESENTATION, CaseType.CAVEAT),
+                    DOC_TYPE_CHERISHED, List.of(CaseType.GRANT_OF_REPRESENTATION, CaseType.CAVEAT),
+                    DOC_TYPE_OTHER, List.of(CaseType.GRANT_OF_REPRESENTATION, CaseType.CAVEAT),
+                    DOC_TYPE_FORM, List.of(CaseType.GRANT_OF_REPRESENTATION, CaseType.CAVEAT)
+            );
+
 
     private ExceptionRecordCaseDataValidator() {
     }
@@ -55,6 +88,35 @@ public class ExceptionRecordCaseDataValidator {
         }
         if (!errorMessages.isEmpty()) {
             throw new OCRMappingException(IHT_VALDIATION_ERROR, errorMessages);
+        }
+    }
+
+
+    public static void validateInputScannedDocumentTypes(List<InputScannedDoc>
+                                                            scannedDocuments,CaseType caseType) {
+
+        List<String> disallowedDocTypesFound =
+                scannedDocuments
+                        .stream()
+                        .filter(Objects::nonNull)
+                        .filter(inputScannedDoc ->
+                                !allowScannedDocumentTypes.containsKey(inputScannedDoc.type)
+                                        || (allowScannedDocumentTypes.containsKey(inputScannedDoc.type
+                                )
+                                        && !allowScannedDocumentTypes.get(inputScannedDoc.type)
+                                        .contains(caseType)))
+                        .map(inputScannedDoc -> inputScannedDoc.type)
+                        .collect(toList());
+
+        if (!disallowedDocTypesFound.isEmpty()) {
+
+            String errorMessage = String.format(
+                    INVALID_SCANNED_DOCUMENT_TYPE_ERROR,
+                    caseType,
+                    StringUtils.join(disallowedDocTypesFound, ", ")
+            );
+
+            throw new OCRMappingException(errorMessage, Arrays.asList(errorMessage));
         }
     }
 }
