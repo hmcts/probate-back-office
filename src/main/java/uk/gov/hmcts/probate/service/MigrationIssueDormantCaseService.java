4f20bef50317e13040709f99c7f5ae1e28f127a9
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-07-15 12:29:55 +0100

    DTSPB-4243 Unused import

--- src/main/java/uk/gov/hmcts/probate/service/MigrationIssueDormantCaseService.java
+++ src/main/java/uk/gov/hmcts/probate/service/MigrationIssueDormantCaseService.java
@@ -15,7 +15,6 @@ import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantOfRepr
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
