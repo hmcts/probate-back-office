ebfd356ee43960603a455d9eb5db713b7cf676c4
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-07-26 11:11:31 +0100

    fix sonar to Remove this redundant "@Qualifier" annotation and rely on the @Bean method

--- src/main/java/uk/gov/hmcts/probate/config/AuthCheckerConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/config/AuthCheckerConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Qualifier;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
