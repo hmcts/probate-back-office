b798b31198cdb459112c065a03c36d8c33f60f82
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-09-04 18:39:49 +0100

    DTSPB-4315 Added logs

--- src/main/java/uk/gov/hmcts/probate/service/payments/PaymentsService.java
+++ src/main/java/uk/gov/hmcts/probate/service/payments/PaymentsService.java
@@ -70,6 +70,8 @@ public class PaymentsService {
     public String createServiceRequest(ServiceRequestDto serviceRequestDto) {
         SecurityDTO securityDTO = securityUtils.getSecurityDTO();
         log.info("securityDTO:{}", securityDTO);
+        List<String> roles= securityUtils.getRoles(securityDTO.getAuthorisation());
+        log.info("roles:{}", roles);
         String serviceRequestResponse = serviceRequestClient.createServiceRequest(securityDTO.getAuthorisation(),
                 securityDTO.getServiceAuthorisation(), serviceRequestDto);
         DocumentContext jsonContext = JsonPath.parse(serviceRequestResponse);
