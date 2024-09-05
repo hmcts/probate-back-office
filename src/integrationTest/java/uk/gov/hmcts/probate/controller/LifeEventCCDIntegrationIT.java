d1c17b02a250ae735a5632f8622335b8984d1463
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-09-05 21:39:45 +0100

    DTSPB-4335 FIX Integration Test

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/LifeEventCCDIntegrationIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/LifeEventCCDIntegrationIT.java
@@ -19,7 +19,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.probate.insights.AppInsights;
+import uk.gov.hmcts.probate.security.SecurityDTO;
+import uk.gov.hmcts.probate.security.SecurityUtils;
 import uk.gov.hmcts.probate.util.TestUtils;
+
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
@@ -32,6 +36,8 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -48,6 +54,8 @@ class LifeEventCCDIntegrationIT {
     private TestUtils testUtils;
     @MockBean
     private AppInsights appInsights;
+    @MockBean
+    private SecurityUtils securityUtils;
     @Autowired
     private WebApplicationContext webApplicationContext;
 
@@ -82,6 +90,12 @@ class LifeEventCCDIntegrationIT {
                 .withStatus(200)
             )
         );
+        SecurityDTO securityDTO = SecurityDTO.builder()
+                .authorisation("AUTH_TOKEN")
+                .serviceAuthorisation("serviceAuth")
+                .build();
+        when(securityUtils.getSecurityDTO()).thenReturn(securityDTO);
+        when(securityUtils.getRoles(anyString())).thenReturn(List.of("citizen"));
     }
 
     @AfterEach
