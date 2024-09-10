08ee6870bd304f3635f8efd08c905bf01e69922a
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-09-10 09:16:57 +0100

    add debug log

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/caseprogress/CaseProgressTestsBase.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/caseprogress/CaseProgressTestsBase.java
@@ -98,8 +98,7 @@ public abstract class CaseProgressTestsBase extends IntegrationTestBase {
 
         // make sure tasklist controller update in db works when called separately,
         // which happens prior to first state change
-        log.info(jsonFile + "<--*****indexOf-->" + jsonFile.indexOf("/application-updated-html.txt"));
-        if (jsonFile.indexOf("application-updated-html.txt") >= 0) {
+        if (jsonFile.indexOf("02-appUpdated.json") >= 0) {
             log.info("*****-->" + removeCrLfs(taskList));
         }
         assertEquals(removeCrLfs(expected), removeCrLfs(taskList));
