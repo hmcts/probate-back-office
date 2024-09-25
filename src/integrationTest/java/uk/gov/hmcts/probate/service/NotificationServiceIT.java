ebdb4c91b0620aaa90175819b892a1057510ce7b
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-09-25 21:12:21 +0100

    DTSPB-4267 Unused package

--- src/integrationTest/java/uk/gov/hmcts/probate/service/NotificationServiceIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/service/NotificationServiceIT.java
@@ -67,7 +67,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
