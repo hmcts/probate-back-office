f67fe9afd32d31330156a96e7fe9471dcc258fcb
Tom Saunders <tom.saunders@justice.gov.uk>
2024-10-07 11:33:02 +0100

    Testcases for markdown filtering.
    This list of formats does not actually completely cover all the forms accepted by
    Notify - we don't cover the:
    \^ inset text
    format, since that is not part of commonmark. There is also the concern documented
    in DTSPB-4380 which affects the wider Notify application so will not be detailed
    here.

--- src/test/java/uk/gov/hmcts/probate/validator/PersonalisationValidationRuleTest.java
+++ src/test/java/uk/gov/hmcts/probate/validator/PersonalisationValidationRuleTest.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.probate.validator;
 
+import org.commonmark.parser.Parser;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import uk.gov.hmcts.probate.service.MarkdownValidatorService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,46 +19,81 @@ import java.util.function.Function;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class PersonalisationValidationRuleTest {
+
+    @Spy
+    Parser markdownParserSpy = Parser.builder().build();
+
+    @Spy
+    MarkdownValidatorService markdownValidatorService;
 
     @InjectMocks
     private PersonalisationValidationRule personalisationValidationRule;
 
+    AutoCloseable closableMocks;
+
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        closableMocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closableMocks.close();
     }
 
     @Test
-    void shouldReturnFielNameWithHyperLinkWhereValidatePersonilization() {
+    void givenMixedValidAndInvalidInputs_whenValidated_thenReturnsInvalidFields() {
         final Map<String, Object> personalisation = new HashMap<>();
+
         personalisation.put("valid", "Valid text");
-        personalisation.put("single_link", "Some text [example](http://example.com)");
-        personalisation.put("bypass_using_match_before", "[) [example](http://example.com)");
-        personalisation.put("bypass_with_early_rsqb", "[example]](http://example.com)");
-        personalisation.put("with_bold", "bold **text**");
-        personalisation.put("with_ital", "ital *text*");
-        personalisation.put("with_bold_alt", "bold __text__");
-        personalisation.put("with_ital_alt", "ital _text_");
+
+        // Documented as supported by Notify https://www.notifications.service.gov.uk/using-notify/formatting
         personalisation.put("heading1", "# heading1");
         personalisation.put("heading2", "## heading2");
         personalisation.put("heading1_alt", "heading1\n========");
         personalisation.put("heading2_alt", "heading2\n--------");
-        personalisation.put("blockquote", "> blockquote");
         personalisation.put("list", "* list");
         personalisation.put("list_alt", "- list");
         personalisation.put("numlist", "1. list");
         personalisation.put("numlist_alt", "1) list");
         personalisation.put("hrule", "---");
         personalisation.put("hrule_alt", "***");
+
+        // Documented as unsupported by Notify
+        personalisation.put("with_bold", "bold **text**");
+        personalisation.put("with_ital", "ital *text*");
+        personalisation.put("with_bold_alt", "bold __text__");
+        personalisation.put("with_ital_alt", "ital _text_");
         personalisation.put("inline_code", "`inline code`");
         personalisation.put("code_block", "```\ncode\nblock\n```");
         personalisation.put("code_block_alt", "    code\n    block");
 
+        // Not explicitly documented as supported or unsupported (the documentation refers to "inset text"
+        // but uses: " ^ inset text" as the example. Unclear if this is just a nonstandard alternative, but
+        // a stock markdown implementation will not catch it. This is likely because `>` would be removed
+        // by any html filtering.
+        personalisation.put("block_quote", "> block_quote");
+
+        // Link handling
+        personalisation.put("single_link", "Some text [example](http://example.com)");
+        personalisation.put("bypass_using_match_before", "[) [example](http://example.com)");
+        personalisation.put("bypass_with_early_rsqb", "[example\\]](http://example.com)");
+        personalisation.put("link_with_ref", "Some text [link][1]\n\nMore text\n\n[1]: http://example.com");
+
+        // Images (not mentioned in Notify documentation, but seems like we shouldn't be permitting this
+        personalisation.put("single_image", "Some text ![example](http://example.com/img.png)");
+        personalisation.put("image_with_ref", "Some text ![link][1]\n\nMore text\n\n[1]: http://example.com/img.png");
+
+
         final List<String> result = personalisationValidationRule.validatePersonalisation(personalisation);
 
-        final Function<String, Executable> assertContains = (name) -> {
+        final Function<String, Executable> assertContains = name -> {
             return () -> assertTrue(result.contains(name), "result did not contain " + name);
         };
 
@@ -66,6 +105,45 @@ class PersonalisationValidationRuleTest {
             }
         }
 
+        assertAll(assertions);
+    }
+
+    @Test
+    void givenInput_whenValidatorFlags_thenRejected() {
+        final var key = "key";
+        final var personalisation = Map.ofEntries(Map.entry(key, "value"));
+
+        final var visitorMock = mock(MarkdownValidatorService.NontextVisitor.class);
+        when(markdownValidatorService.getNontextVisitor(key)).thenReturn(visitorMock);
+        when(visitorMock.isInvalid()).thenReturn(true);
+
+        final var result = personalisationValidationRule.validatePersonalisation(personalisation);
+
+        final List<Executable> assertions = List.of(
+                () -> verify(markdownValidatorService).getNontextVisitor(any()),
+                () -> verify(visitorMock).isInvalid(),
+                () -> assertEquals(1, result.size(), "Expected validation to fail"),
+                () -> assertTrue(result.contains(key), "Expected key to be present in failure list")
+        );
+        assertAll(assertions);
+    }
+
+    @Test
+    void givenInput_whenValidatorPasses_thenAccepted() {
+        final var key = "key";
+        final var personalisation = Map.ofEntries(Map.entry(key, "value"));
+
+        final var visitorMock = mock(MarkdownValidatorService.NontextVisitor.class);
+        when(markdownValidatorService.getNontextVisitor(key)).thenReturn(visitorMock);
+        when(visitorMock.isInvalid()).thenReturn(false);
+
+        final var result = personalisationValidationRule.validatePersonalisation(personalisation);
+
+        final List<Executable> assertions = List.of(
+                () -> verify(markdownValidatorService).getNontextVisitor(any()),
+                () -> verify(visitorMock).isInvalid(),
+                () -> assertEquals(0, result.size(), "Expected validation to pass")
+        );
         assertAll(assertions);
     }
 
