242a6078dac6f16b827c6b66f2ba49a2b387f17b
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2024-10-07 08:48:54 +0100

    add debug code to integration tests

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/documents/WelshGrantGenerationTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/documents/WelshGrantGenerationTests.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ExtendWith(SerenityJUnit5Extension.class)
 public class WelshGrantGenerationTests extends DocumentGenerationTestBase {
 
@@ -58,6 +60,8 @@ public class WelshGrantGenerationTests extends DocumentGenerationTestBase {
         String expectedText = removeCrLfs(utils.getJsonFromFile(expectedFile));
         expectedText = expectedText.replaceAll("1st August 2021", utils.formatDate(LocalDate.now()));
         expectedText = expectedText.replaceAll("1 Awst 2021", utils.convertToWelsh(LocalDate.now()));
+        log.info("expectedFile *****>>:" + expectedText.trim());
+        log.info("response.trim() *****>>:" + response.trim());
 
         assertEquals(expectedText.trim(), response.trim());
     }
