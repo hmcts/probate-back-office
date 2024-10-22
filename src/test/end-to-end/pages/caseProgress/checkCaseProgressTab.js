01c55bb903d16d8d5b3cede6db7e29dc8a67d117
Elife Toplu <elife.toplu@justice.gov.uk>
2024-10-22 16:02:06 +0100

    Updated Web Element List

--- src/test/end-to-end/pages/caseProgress/checkCaseProgressTab.js
+++ src/test/end-to-end/pages/caseProgress/checkCaseProgressTab.js
@@ -13,35 +13,35 @@ module.exports = async function (opts) {
     await I.wait(3);
     const texts = await I.grabTextFromAll('markdown  p.govuk-body-s');
     await I.wait(3);
-    assert (texts.length === 39);
-    console.log(texts.length());
-    assert (texts[1] === 'These steps are to be completed by the Probate practitioner.');
-    assert (texts[3] === 'Add Probate practitioner details');
-    assert (texts[5] === 'Add deceased details');
-    assert (texts[7] === 'Add application details');
-    assert (texts[9] === 'These steps are to be completed by the Probate practitioner.');
-    assert (texts[11] === 'Review and sign legal statement and submit application');
-    assert (texts[12] === 'The legal statement is generated. You can review, change any details, then sign and submit your application.');
-    assert (texts[15] === 'Make payment');
+    assert (texts.length === 37);
+    assert (texts[0] === 'These steps are to be completed by the Probate practitioner.');
+    assert (texts[2] === 'Add Probate practitioner details');
+    assert (texts[4] === 'Add deceased details');
+    assert (texts[6] === 'Add application details');
+    assert (texts[8] === 'These steps are to be completed by the Probate practitioner.');
+    assert (texts[10] === 'Review and sign legal statement and submit application');
+    assert (texts[11] === 'The legal statement is generated. You can review, change any details, then sign and submit your application.');
+    assert (texts[14] === 'Make payment');
     await I.wait(3);
     if (texts[17] === '') {
         assert (texts[17] === '');
     } else {
         assert (texts[17] === 'Once payment is made, you\'ll need to refresh the page or re-enter the case for the payment status to update.');
     }
-    assert (texts[19] === 'Send documents\n');
-    assert (texts[20] === 'These steps are completed by HM Courts and Tribunals Service staff. It can take a few weeks before the review starts.');
-    assert (texts[22] === 'Authenticate documents');
-    assert (texts[23] === 'We will authenticate your documents and match them with your application.');
-    assert (texts[26] === 'Examine application');
-    assert (texts[27] === 'We review your application for incomplete information or problems and validate it against other cases or caveats. After the review we prepare the grant.');
-    assert (texts[28] === 'Your application will update through any of these case states as it is reviewed by our team:');
-    assert (texts[32] === 'This step is completed by HM Courts and Tribunals Service staff.');
-    assert (texts[34] === 'Issue grant of representation');
-    assert (texts[35] === 'The grant will be delivered in the post a few days after issuing.');
+
+    assert (texts[18].split("\n")[0] === 'Send documents');
+    assert (texts[19] === 'These steps are completed by HM Courts and Tribunals Service staff. It can take a few weeks before the review starts.');
+    assert (texts[21] === 'Authenticate documents');
+    assert (texts[22] === 'We will authenticate your documents and match them with your application.');
+    assert (texts[25] === 'Examine application');
+    assert (texts[26] === 'We review your application for incomplete information or problems and validate it against other cases or caveats. After the review we prepare the grant.');
+    assert (texts[27] === 'Your application will update through any of these case states as it is reviewed by our team:');
+    assert (texts[31] === 'This step is completed by HM Courts and Tribunals Service staff.');
+    assert (texts[33] === 'Issue grant of representation');
+    assert (texts[34] === 'The grant will be delivered in the post a few days after issuing.');
 
     if (opts.linkText && opts.linkUrl) {
-        await I.seeNumberOfVisibleElements('p.govuk-body-s a', 1);
+        await I.seeNumberOfVisibleElements('p.govuk-body-s a', 2);
         const lnkTxt = await I.grabTextFrom('p.govuk-body-s a');
         assert (lnkTxt === opts.linkText); //assert deceased details has a link
 
