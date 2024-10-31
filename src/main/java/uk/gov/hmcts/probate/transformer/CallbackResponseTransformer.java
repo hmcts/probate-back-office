806150120040d80608ed839a224593cb3255f724
FeliTam <feli.tam@justice.gov.uk>
2024-10-31 17:14:24 +0000

    DTSPB-4266 skip informationNeededByPost question for issued case, use by post  notification template

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -1353,7 +1353,8 @@ public class CallbackResponseTransformer {
     }
 
     private boolean nothingSubmitted(CaseData caseData) {
-        return caseData.getCitizenResponse().isEmpty() && caseData.getCitizenDocumentsUploaded().isEmpty();
+        return (caseData.getCitizenResponse() == null || caseData.getCitizenResponse().isEmpty()) &&
+                (caseData.getCitizenDocumentsUploaded() == null || caseData.getCitizenDocumentsUploaded().isEmpty());
     }
 
     private ResponseCaseDataBuilder<?, ?> getCaseCreatorResponseCaseBuilder(CaseData caseData,
