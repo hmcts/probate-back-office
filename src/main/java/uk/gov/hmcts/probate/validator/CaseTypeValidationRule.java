9df22f751ba0a4d6f57f9f4002016286d2fe29c5
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-11-20 19:29:02 +0000

    DTSPB-4293 Added validation for Ad Colligenda case type selection

---
+++ src/main/java/uk/gov/hmcts/probate/validator/CaseTypeValidationRule.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.probate.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.exception.BusinessValidationException;
+import uk.gov.hmcts.probate.model.DocumentCaseType;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
+import uk.gov.hmcts.probate.service.BusinessValidationMessageRetriever;
+
+import java.util.Locale;
+
+import static uk.gov.hmcts.probate.model.Constants.YES;
+import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_APPLICABLE_VALUE;
+
+
+@Component
+@RequiredArgsConstructor
+public class CaseTypeValidationRule {
+
+    private final BusinessValidationMessageRetriever businessValidationMessageRetriever;
+
+    public void validate(CaseDetails caseDetails) {
+        CaseData caseData = caseDetails.getData();
+        if (DocumentCaseType.AD_COLLIGENDA_BONA.getCaseType().equals(caseData.getCaseType())) {
+            String userMessage = businessValidationMessageRetriever
+                    .getMessage("invalidCaseTypeSelection", null, Locale.UK);
+            throw new BusinessValidationException(userMessage,
+                    "Ad Colligenda Bona selection is invalid: " + caseDetails.getId());
+        }
+    }
+}
--- src/test/java/uk/gov/hmcts/probate/controller/BusinessValidationUnitTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/BusinessValidationUnitTest.java
@@ -38,6 +38,7 @@ import uk.gov.hmcts.probate.transformer.HandOffLegacyTransformer;
 import uk.gov.hmcts.probate.transformer.reset.ResetCaseDataTransformer;
 import uk.gov.hmcts.probate.transformer.solicitorexecutors.LegalStatementExecutorTransformer;
 import uk.gov.hmcts.probate.transformer.solicitorexecutors.SolicitorApplicationCompletionTransformer;
+import uk.gov.hmcts.probate.validator.CaseTypeValidationRule;
 import uk.gov.hmcts.probate.validator.CaseworkerAmendAndCreateValidationRule;
 import uk.gov.hmcts.probate.validator.CaseworkersSolicitorPostcodeValidationRule;
 import uk.gov.hmcts.probate.validator.CheckListAmendCaseValidationRule;
@@ -179,6 +180,8 @@ class BusinessValidationUnitTest {
     private Pre1900DOBValidationRule pre1900DOBValidationRuleMock;
     @Mock
     private BusinessValidationMessageService businessValidationMessageServiceMock;
+    @Mock
+    private CaseTypeValidationRule caseTypeValidationRule;
 
 
     @Mock
@@ -221,6 +224,7 @@ class BusinessValidationUnitTest {
             handOffLegacyTransformer,
             registrarDirectionServiceMock,
             pre1900DOBValidationRuleMock,
+            caseTypeValidationRule,
             businessValidationMessageServiceMock);
 
         when(httpServletRequest.getRequestURI()).thenReturn("/test-uri");
@@ -894,6 +898,7 @@ class BusinessValidationUnitTest {
                 bindingResultMock);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        verify(caseTypeValidationRule, times(1)).validate(caseDetailsMock);
         verify(caseDataTransformerMock).transformCaseDataForEvidenceHandledForManualCreateByCW(callbackRequestMock);
     }
 
