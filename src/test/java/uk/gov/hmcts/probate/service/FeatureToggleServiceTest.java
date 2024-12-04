a78100f09726759729aa095195c2254330ed0f09
Tom Saunders <tom.saunders@justice.gov.uk>
2024-12-04 10:55:42 +0000

    FeatureToggle test.

---
+++ src/test/java/uk/gov/hmcts/probate/service/FeatureToggleServiceTest.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.probate.service;
+
+import org.junit.jupiter.api.function.Executable;
+import uk.gov.hmcts.probate.service.FeatureToggleService.DocGen;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class FeatureToggleServiceTest {
+    @Test
+    void testDocGenMapping() {
+        final BiFunction<String, DocGen, Executable> genAssert = (v, e) -> {
+            final DocGen a = DocGen.fromString(v);
+            return () -> assertEquals(e, a, "Expected [" + v + "] to give [" + e + "]");
+        };
+
+        assertAll(
+                genAssert.apply("master", DocGen.MASTER),
+                genAssert.apply("pr", DocGen.PR),
+                genAssert.apply("html", DocGen.HTML),
+                genAssert.apply("html_proc", DocGen.HTML_PROC),
+                genAssert.apply("other", DocGen.MASTER)
+        );
+    }
+}
