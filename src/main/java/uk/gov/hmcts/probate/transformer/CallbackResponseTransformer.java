0d614246fa9391dda261583c44f89ab14968a1e0
Tom Saunders <tom.saunders@justice.gov.uk>
2024-12-09 12:17:09 +0000

    Add handling for persisting amendedLegalStatement in case data etc.

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -1180,6 +1180,7 @@ public class CallbackResponseTransformer {
             .deceasedAnyChildren(caseData.getDeceasedAnyChildren())
             .deceasedHasAssetsOutsideUK(caseData.getDeceasedHasAssetsOutsideUK())
             .statementOfTruthDocument(caseData.getStatementOfTruthDocument())
+            .amendedLegalStatement(caseData.getAmendedLegalStatement())
             .boStopDetailsDeclarationParagraph(caseData.getBoStopDetailsDeclarationParagraph())
             .executorsApplyingNotifications(caseData.getExecutorsApplyingNotifications())
             .boEmailRequestInfoNotification(caseData.getBoEmailRequestInfoNotification())
