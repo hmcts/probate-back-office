203aff001ec096ff1e703c7fcacc890fd321b5ef
Marcus Ho <99657446+MarcusHMCTS@users.noreply.github.com>
2024-12-19 11:35:52 +0000

    DTSPB-4373 Send Sealed and Certified Email (#2926)
    * DTSPB-4373 Send Sealed and Certified Email
    
    * DTSPB-4373 fix DocumentControllerIT tests
    
    * DTSPB-4373 add re-issue with send sealed and certified email
    
    * DTSPB-4373 add sealed and Certified email
    
    * DTSPB-4373 update sealed and Certified email at application.yml
    
    * DTSPB-4373 fix review issue
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: Feli Tam <feli.tam@justice.gov.uk>

--- src/main/java/uk/gov/hmcts/probate/service/NotificationService.java
+++ src/main/java/uk/gov/hmcts/probate/service/NotificationService.java
@@ -171,6 +171,27 @@ public class NotificationService {
         return getGeneratedDocument(previewResponse, getEmail(caseData), SENT_EMAIL);
     }
 
+    public Document sendSealedAndCertifiedEmail(CaseDetails caseDetails) throws NotificationClientException {
+        CaseData caseData = caseDetails.getData();
+        String reference = caseDetails.getId().toString();
+        String deceasedName = caseData.getDeceasedFullName();
+
+        String templateId = notificationTemplates.getEmail().get(LanguagePreference.ENGLISH)
+                .get(caseData.getApplicationType()).getSealedAndCertified();
+        Map<String, Object> personalisation =
+                grantOfRepresentationPersonalisationService.getSealedAndCertifiedPersonalisation(caseDetails.getId(),
+                         deceasedName);
+        doCommonNotificationServiceHandling(personalisation, caseDetails.getId());
+
+        log.info("Sealed And Certified get the email response for case {}", caseDetails.getId());
+
+        SendEmailResponse response = notificationClientService.sendEmail(templateId,
+                emailAddresses.getSealedAndCertifiedEmail(), personalisation, reference);
+
+        log.info("Send Sealed And Certified completed for case {}", caseDetails.getId());
+        return getGeneratedSentEmailDocument(response, emailAddresses.getSealedAndCertifiedEmail(), SENT_EMAIL);
+    }
+
     public Document sendNocEmail(State state, CaseDetails caseDetails) throws NotificationClientException {
         CaseData caseData = caseDetails.getData();
         Registry registry = registriesProperties.getRegistries().get(caseData.getRegistryLocation().toLowerCase());
