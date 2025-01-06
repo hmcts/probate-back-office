58cfc5cd82e400ba239226b776505915c8a84fe9
Tom Saunders <tom.saunders@justice.gov.uk>
2025-01-06 16:48:20 +0000

    Intentionally introduce 'race' behaviour.

--- src/main/java/uk/gov/hmcts/probate/service/InformationRequestCorrespondenceService.java
+++ src/main/java/uk/gov/hmcts/probate/service/InformationRequestCorrespondenceService.java
@@ -17,12 +17,20 @@ import static uk.gov.hmcts.probate.model.State.CASE_STOPPED_REQUEST_INFORMATION;
 public class InformationRequestCorrespondenceService {
 
     private final NotificationService notificationService;
+    private Document previous = null;
 
     public List<Document> emailInformationRequest(CaseDetails caseDetails) {
         try {
             final Document notification = notificationService.sendEmail(CASE_STOPPED_REQUEST_INFORMATION, caseDetails);
             log.info("Successful response for request for information email for case id {} ", caseDetails.getId());
-            return List.of(notification);
+            final List<Document> rVal;
+            if (previous != null) {
+                rVal = List.of(notification, previous);
+            } else {
+                rVal = List.of(notification);
+            }
+            previous = notification;
+            return rVal;
         } catch (NotificationClientException e) {
             log.error(e.getMessage());
             // this feels wrong - do we not want to alert the caller that the email sending has failed?
