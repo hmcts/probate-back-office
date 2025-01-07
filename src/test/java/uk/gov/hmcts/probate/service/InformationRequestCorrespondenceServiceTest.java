468f8ace2c66458cef174aaae29d53b117a99903
Tom Saunders <tom.saunders@justice.gov.uk>
2025-01-07 12:56:29 +0000

    Add some logging for simplicitity.

--- src/test/java/uk/gov/hmcts/probate/service/InformationRequestCorrespondenceServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/InformationRequestCorrespondenceServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.MockitoAnnotations;
 import uk.gov.hmcts.probate.model.DocumentType;
 import uk.gov.hmcts.probate.model.State;
 import uk.gov.hmcts.probate.model.ccd.raw.Document;
+import uk.gov.hmcts.probate.model.ccd.raw.DocumentLink;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.service.notify.NotificationClientException;
@@ -19,8 +20,15 @@ class InformationRequestCorrespondenceServiceTest {
 
     private static final String[] LAST_MODIFIED = {"2018", "1", "2", "0", "0", "0", "0"};
     private static final Long ID = 123456789L;
+    private static final DocumentLink GENERIC_DOCUMENT_LINK =
+            DocumentLink.builder()
+                    .documentUrl("GENERIC_URL")
+                    .build();
     private static final Document GENERIC_DOCUMENT =
-        Document.builder().documentType(DocumentType.SENT_EMAIL).build();
+        Document.builder()
+                .documentType(DocumentType.SENT_EMAIL)
+                .documentLink(GENERIC_DOCUMENT_LINK)
+                .build();
     @InjectMocks
     private InformationRequestCorrespondenceService informationRequestCorrespondenceService;
     @Mock
