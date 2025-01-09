a10aee6d90aeb6b11c4a6826d5c4ad192d07e3ed
Tom Saunders <tom.saunders@justice.gov.uk>
2025-01-09 12:02:52 +0000

    Add new exception for markdown errors.
    We probably shouldn't use an exception provided by a dependency for this.

--- src/test/java/uk/gov/hmcts/probate/exception/handler/DefaultExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/probate/exception/handler/DefaultExceptionHandlerTest.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.probate.exception.ClientException;
 import uk.gov.hmcts.probate.exception.ConnectionException;
 import uk.gov.hmcts.probate.exception.NotFoundException;
 import uk.gov.hmcts.probate.exception.OCRMappingException;
+import uk.gov.hmcts.probate.exception.RequestInformationParameterException;
 import uk.gov.hmcts.probate.exception.SocketException;
 import uk.gov.hmcts.probate.exception.TextFileBuilderException;
 import uk.gov.hmcts.probate.exception.model.ErrorResponse;
@@ -22,6 +23,7 @@ import uk.gov.service.notify.NotificationClientException;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -189,5 +191,20 @@ class DefaultExceptionHandlerTest {
         assertEquals(OK, response.getStatusCode());
         assertEquals(1, response.getBody().getErrors().size());
         assertEquals(EXCEPTION_MESSAGE, response.getBody().getErrors().get(0));
+    }
+
+    @Test
+    void shouldReturnMarkdownError() {
+        RequestInformationParameterException ex = mock(RequestInformationParameterException.class);
+        when(ex.getMessage()).thenReturn("");
+        when(ex.getUserMessage()).thenReturn(EXCEPTION_MESSAGE);
+
+        ResponseEntity<CallbackResponse> response = underTest.handle(ex);
+
+        assertEquals(OK, response.getStatusCode(),
+                "Expected HTTP OK from RequestInformationParameterException handler");
+        assertEquals(1, response.getBody().getErrors().size(), "Expected one error");
+        assertEquals(EXCEPTION_MESSAGE, response.getBody().getErrors().get(0),
+                "Expected error to be extracted from exception");
     }
 }
