de7589da68bd91946b8645a70514065a7f81d867
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2025-01-15 12:37:02 +0000

    create iron mountain extract task for k8s cron

---
+++ src/main/java/uk/gov/hmcts/probate/schedule/IronMountainExtractTask.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.probate.schedule;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.service.dataextract.DataExtractDateValidator;
+import uk.gov.hmcts.probate.service.dataextract.IronMountainDataExtractService;
+import uk.gov.hmcts.reform.probate.model.client.ApiClientException;
+
+import java.time.LocalDate;
+
+import static uk.gov.hmcts.probate.model.Constants.DATE_FORMAT;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class IronMountainExtractTask implements Runnable {
+
+    private final DataExtractDateValidator dataExtractDateValidator;
+    private final IronMountainDataExtractService ironMountainDataExtractService;
+
+    @Value("${adhocSchedulerJobDate}")
+    public String adHocJobFromDate;
+
+    @Override
+    public void run() {
+        log.info("Scheduled task IronMountainExtractTask started to extract data for IronMountain");
+        String fromDate = DATE_FORMAT.format(LocalDate.now().minusDays(1L));
+        if (StringUtils.isNotEmpty(adHocJobFromDate)) {
+            fromDate = adHocJobFromDate;
+            log.info("Running IronMountainDataExtractTask with Adhoc dates {}", fromDate);
+        }
+        log.info("Calling perform iron mountain data extract from date {}", fromDate);
+        try {
+            dataExtractDateValidator.dateValidator(fromDate);
+            log.info("Perform iron mountain data extract from date started");
+            ironMountainDataExtractService.performIronMountainExtractForDate(fromDate);
+            log.info("Perform iron mountain data extract from date finished");
+        } catch (ApiClientException e) {
+            log.error(e.getMessage());
+        } catch (Exception e) {
+            log.error("Error on IronMountainExtractTask Scheduler {}", e.getMessage());
+        }
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/probate/schedule/IronMountainExtractTaskTest.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.probate.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.probate.service.dataextract.DataExtractDateValidator;
+import uk.gov.hmcts.probate.service.dataextract.IronMountainDataExtractService;
+import uk.gov.hmcts.reform.probate.model.client.ApiClientException;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static uk.gov.hmcts.probate.model.Constants.DATE_FORMAT;
+
+@ExtendWith(SpringExtension.class)
+class IronMountainExtractTaskTest {
+
+    @Mock
+    private DataExtractDateValidator dataExtractDateValidator;
+
+    @Mock
+    private IronMountainDataExtractService ironMountainDataExtractServiceData;
+
+    @InjectMocks
+    private IronMountainExtractTask ironMountainExtractTask;
+    private static final String date = DATE_FORMAT.format(LocalDate.now().minusDays(1L));
+    private String adhocDate = "2022-09-05";
+
+    @Test
+    void shouldPerformIronMountainExtractYesterdayDate() {
+        ResponseEntity<String> responseEntity = ResponseEntity.accepted()
+                .body("Perform iron mountain data extract from date finished");
+        ironMountainExtractTask.run();
+        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
+        assertEquals("Perform iron mountain data extract from date finished", responseEntity.getBody());
+        verify(dataExtractDateValidator).dateValidator(date);
+        verify(ironMountainDataExtractServiceData).performIronMountainExtractForDate(date);
+    }
+
+    @Test
+    void shouldPerformIronMountainExtractForAdhocDate() {
+        ironMountainExtractTask.adHocJobFromDate = "2022-09-05";
+        ResponseEntity<String> responseEntity = ResponseEntity.accepted()
+                .body("Perform iron mountain data extract from date finished");
+        ironMountainExtractTask.run();
+        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
+        assertEquals("Perform iron mountain data extract from date finished", responseEntity.getBody());
+        verify(dataExtractDateValidator).dateValidator(adhocDate);
+        verify(ironMountainDataExtractServiceData).performIronMountainExtractForDate(adhocDate);
+    }
+
+    @Test
+    void shouldThrowClientExceptionWithBadRequestForIronMountainExtractWithIncorrectDateFormat() {
+        doThrow(new ApiClientException(HttpStatus.BAD_REQUEST.value(), null)).when(dataExtractDateValidator)
+                .dateValidator(date);
+        ironMountainExtractTask.run();
+        verify(dataExtractDateValidator).dateValidator(date);
+        verifyNoInteractions(ironMountainDataExtractServiceData);
+    }
+
+    @Test
+    void shouldThrowExceptionForIronMountainExtract() {
+        doThrow(new NullPointerException()).when(dataExtractDateValidator)
+                .dateValidator(date);
+        ironMountainExtractTask.run();
+        verify(dataExtractDateValidator).dateValidator(date);
+        verifyNoInteractions(ironMountainDataExtractServiceData);
+    }
+
+}
