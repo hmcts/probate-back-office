a063ce7d2544d1679ac7cffd1c831b30b0e6e406
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-02-13 16:25:13 +0000

    DTSPB-4551: Make call to clear additional exec list

--- src/main/java/uk/gov/hmcts/probate/transformer/solicitorexecutors/SolicitorApplicationCompletionTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/solicitorexecutors/SolicitorApplicationCompletionTransformer.java
@@ -97,4 +97,8 @@ public class SolicitorApplicationCompletionTransformer extends LegalStatementExe
             }
         }
     }
+
+    public void clearAdditionalExecutorWhenUpdatingApplicantDetails(CaseDetails caseDetails) {
+        caseDetails.getData().clearAdditionalExecutorList();
+    }
 }
