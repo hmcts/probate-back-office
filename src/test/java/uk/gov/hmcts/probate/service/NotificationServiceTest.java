17a67f744667a875aff87e3b4afb748ead5f5169
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-02-28 16:12:50 +0000

    DTSPB-4617 Fix import

--- src/test/java/uk/gov/hmcts/probate/service/NotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/NotificationServiceTest.java
@@ -36,9 +36,8 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class NotificationServiceTest {
 
