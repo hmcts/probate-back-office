42ac0c0f41224c0aefb9c9fcdf0e58f4666df318
Tom Saunders <tom.saunders@justice.gov.uk>
2025-03-05 13:37:08 +0000

    scratch demo changes.

--- src/main/java/uk/gov/hmcts/probate/service/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/probate/service/FeatureToggleService.java
@@ -47,7 +47,7 @@ public class FeatureToggleService {
     }
 
     public boolean enableNewAliasTransformation() {
-        return this.isFeatureToggleOn("probate-enable-new-alias-transformation", false);
+        return this.isFeatureToggleOn("probate-enable-new-alias-transformation", true);
     }
 
     public boolean enableAmendLegalStatementFiletypeCheck() {
