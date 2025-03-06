6465a2832fca0fbdf50d82eca4701c6f3f8d74cf
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-03-06 18:35:17 +0000

    DTSPB-4551: Improve coverage and add new happy path test

--- src/test/java/uk/gov/hmcts/probate/validator/ZeroApplyingExecutorsValidationRuleTest.java
+++ src/test/java/uk/gov/hmcts/probate/validator/ZeroApplyingExecutorsValidationRuleTest.java
@@ -11,9 +11,12 @@ import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.hmcts.probate.service.BusinessValidationMessageRetriever;
 
+import java.util.Locale;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -29,30 +32,37 @@ class ZeroApplyingExecutorsValidationRuleTest {
     private BusinessValidationMessageRetriever businessValidationMessageRetriever;
     private CaseData caseDataMock;
 
+    private static final String NO_EXECUTORS = "zeroExecutors";
+    private static final String NO_EXECUTORS_WELSH = "zeroExecutorsWelsh";
+
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-
-        caseDataMock = CaseData.builder()
-            .applicationType(ApplicationType.SOLICITOR)
-            .solsSolicitorIsExec("No")
-            .numberOfExecutors(0L)
-            .otherExecutorExists("No")
-            .solsSolicitorIsApplying("No")
-            .titleAndClearingType("TCTPartSuccPowerRes")
-            .primaryApplicantForenames("Probate")
-            .primaryApplicantSurname("Practitioner")
-            .anyOtherApplyingPartners("No")
-            .registryLocation("Bristol")
-            .build();
     }
 
     @Test
     void shouldReturnErrorForZeroExecutors() {
+        caseDataMock = CaseData.builder()
+                .applicationType(ApplicationType.SOLICITOR)
+                .solsSolicitorIsExec("No")
+                .numberOfExecutors(0L)
+                .otherExecutorExists("No")
+                .solsSolicitorIsApplying("No")
+                .titleAndClearingType("TCTPartSuccPowerRes")
+                .primaryApplicantForenames("Probate")
+                .primaryApplicantSurname("Practitioner")
+                .anyOtherApplyingPartners("No")
+                .registryLocation("Bristol")
+                .build();
+
         when(caseDetailsMock.getData()).thenReturn(caseDataMock);
-        assertEquals("No", caseDataMock.getOtherExecutorExists());
-        assertEquals("No", caseDataMock.getSolsSolicitorIsExec());
-        assertEquals("No", caseDataMock.getSolsSolicitorIsApplying());
+        when(caseDetailsMock.getId()).thenReturn(0L);
+
+        String[] args = {"0"};
+        when(businessValidationMessageRetriever.getMessage(NO_EXECUTORS, args, Locale.UK))
+                .thenReturn("User message for no executors");
+        when(businessValidationMessageRetriever.getMessage(NO_EXECUTORS_WELSH, args, Locale.UK))
+                .thenReturn("Welsh user message for no executors");
 
         BusinessValidationException bve = assertThrows(BusinessValidationException.class, () -> {
             underTest.validate(caseDetailsMock);
@@ -60,5 +70,26 @@ class ZeroApplyingExecutorsValidationRuleTest {
         assertThat(bve.getMessage(),
                 containsString("There must be at least one executor applying."
                         + " You have not added an applying probate practitioner or any executors for case id 0"));
+    }
+
+    @Test
+    void shouldNotThrowExceptionWhenExecutorsExist() {
+        // Override the default setup for this test
+        caseDataMock = CaseData.builder()
+                .applicationType(ApplicationType.SOLICITOR)
+                .solsSolicitorIsExec("No")
+                .numberOfExecutors(1L)
+                .otherExecutorExists("No")
+                .solsSolicitorIsApplying("No")
+                .titleAndClearingType("TCTPartSuccPowerRes")
+                .primaryApplicantForenames("Probate")
+                .primaryApplicantSurname("Practitioner")
+                .anyOtherApplyingPartners("No")
+                .registryLocation("Bristol")
+                .build();
+
+        when(caseDetailsMock.getData()).thenReturn(caseDataMock);
+
+        assertDoesNotThrow(() -> underTest.validate(caseDetailsMock));
     }
 }
