72a19fdff2684176a242e00d621c0a58ab47fb37
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-03-14 13:43:59 +0000

    DTSPB-4551: Update rule to use existing error message

--- src/main/java/uk/gov/hmcts/probate/validator/ZeroApplyingExecutorsValidationRule.java
+++ src/main/java/uk/gov/hmcts/probate/validator/ZeroApplyingExecutorsValidationRule.java
@@ -18,8 +18,8 @@ import static uk.gov.hmcts.probate.model.Constants.NO;
 @RequiredArgsConstructor
 public class ZeroApplyingExecutorsValidationRule {
 
-    private static final String NO_EXECUTORS = "zeroExecutors";
-    private static final String NO_EXECUTORS_WELSH = "zeroExecutorsWelsh";
+    private static final String NO_EXECUTORS = "partnersNeeded";
+    private static final String NO_EXECUTORS_WELSH = "partnersNeededWelsh";
     private final BusinessValidationMessageRetriever businessValidationMessageRetriever;
 
     public void validate(CaseDetails caseDetails) {
@@ -36,8 +36,7 @@ public class ZeroApplyingExecutorsValidationRule {
                 && NO.equals(caseData.getOtherExecutorExists())
                 && (YES.equals(caseData.getAppointExec()) || YES.equals(caseData.getAppointExecNo()))) {
             throw new BusinessValidationException(userMessage,
-                "There must be at least one executor applying. You have not added "
-                        + "an applying probate practitioner or any executors for case id "
+                "You need to add at least 1 other partner that acts as an executor for case id "
                         + caseDetails.getId(), userMessageWelsh);
         }
     }
