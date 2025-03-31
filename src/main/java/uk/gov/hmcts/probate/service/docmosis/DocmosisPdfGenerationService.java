f232c262376fb9556f5628732b3ed1d78ce49862
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-03-31 19:40:48 +0100

    point to docmosis preveiw for testing

--- src/main/java/uk/gov/hmcts/probate/service/docmosis/DocmosisPdfGenerationService.java
+++ src/main/java/uk/gov/hmcts/probate/service/docmosis/DocmosisPdfGenerationService.java
@@ -30,7 +30,7 @@ public class DocmosisPdfGenerationService {
     private TemplateProperties templateProperties;
 
 
-    @Value("${docmosis.service.endpoint}/rs/render")
+    @Value("${docmosis.service.endpoint}/api/render")
     private String pdfServiceEndpoint;
 
     @Value("${docmosis.service.accessKey}")
