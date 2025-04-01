fcc61fcc79afc938f2d85103cbc3a44e6226a7a6
Feli Tam <feli.tam@justice.gov.uk>
2025-04-01 16:32:04 +0100

    Revert "Dtspb 4557 remove pence value from smee and ford data (#3036)" (#3054)
    This reverts commit 00e43bf3de384fc8a8d13ebd7126aa085fc1c724.

--- src/main/java/uk/gov/hmcts/probate/service/notification/SmeeAndFordPersonalisationService.java
+++ src/main/java/uk/gov/hmcts/probate/service/notification/SmeeAndFordPersonalisationService.java
@@ -26,13 +26,13 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 import static uk.gov.hmcts.probate.model.ApplicationType.SOLICITOR;
+import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_WILL;
 import static uk.gov.hmcts.probate.model.Constants.DOC_SUBTYPE_ORIGINAL_WILL;
 import static uk.gov.hmcts.probate.model.Constants.DOC_SUBTYPE_WILL;
 import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_OTHER;
-import static uk.gov.hmcts.probate.model.Constants.DOC_TYPE_WILL;
 import static uk.gov.hmcts.probate.model.Constants.YES;
-import static uk.gov.hmcts.probate.model.DocumentType.ADMON_WILL_GRANT;
 import static uk.gov.hmcts.probate.model.DocumentType.AD_COLLIGENDA_BONA_GRANT;
+import static uk.gov.hmcts.probate.model.DocumentType.ADMON_WILL_GRANT;
 import static uk.gov.hmcts.probate.model.DocumentType.DIGITAL_GRANT;
 import static uk.gov.hmcts.probate.model.DocumentType.INTESTACY_GRANT;
 import static uk.gov.hmcts.probate.model.DocumentType.WELSH_ADMON_WILL_GRANT;
@@ -108,9 +108,9 @@ public class SmeeAndFordPersonalisationService {
                 data.append(getPrimaryApplicantName(currentCaseData));
                 data.append(DELIMITER);
                 data.append(getFullAddress(currentCaseData.getPrimaryApplicantAddress()));
-                data.append(currentCaseData.getIhtGrossValuePounds());
+                data.append(currentCaseData.getIhtGrossValue().toString());
                 data.append(DELIMITER);
-                data.append(currentCaseData.getIhtNetValuePounds());
+                data.append(currentCaseData.getIhtNetValue().toString());
                 data.append(DELIMITER);
                 data.append(getSolicitorDetails(currentCaseData));
                 data.append(CONTENT_DATE.format(currentCaseData.getDeceasedDateOfBirth()));
