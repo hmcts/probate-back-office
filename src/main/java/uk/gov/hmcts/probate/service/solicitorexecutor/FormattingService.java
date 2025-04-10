acb409c3848a868c6befddd6f57ed7ff99785e11
Tom Saunders <tom.saunders@justice.gov.uk>
2025-04-10 15:55:52 +0100

    fixup checkstyle for FormattingService.

--- src/main/java/uk/gov/hmcts/probate/service/solicitorexecutor/FormattingService.java
+++ src/main/java/uk/gov/hmcts/probate/service/solicitorexecutor/FormattingService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.probate.service.solicitorexecutor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.exception.BusinessValidationException;
 import uk.gov.hmcts.probate.model.ccd.raw.AdditionalExecutorApplying;
