eb83f27e6ce4ca40b7fc936bbe1fb0539c2ea660
Tom Saunders <tom.saunders@justice.gov.uk>
2025-04-16 13:55:50 +0100

    surely this isn't the role determining thing?

--- src/main/java/uk/gov/hmcts/probate/config/AuthCheckerConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/config/AuthCheckerConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.probate.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +13,13 @@ import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 @Configuration
 public class AuthCheckerConfiguration {
 
     @Bean
     public Function<HttpServletRequest, Collection<String>> authorizedRolesExtractor() {
+        log.info("authorizedRolesExtractor - return empty list");
         return any -> Collections.emptyList();
     }
 
@@ -28,6 +31,7 @@ public class AuthCheckerConfiguration {
         return request -> {
             Matcher matcher = pattern.matcher(request.getRequestURI());
             boolean matched = matcher.find();
+            log.info("userIdExtractor with uri: [{}], matched: [{}], returning: [{}]", request.getRequestURI(), matched, matched ? matcher.group(1) : null);
             return Optional.ofNullable(matched ? matcher.group(1) : null);
         };
     }
