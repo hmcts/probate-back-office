105d8a226b1f42e72b76e87406c9bea092b58a88
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-04-24 16:55:54 +0100

    DTSPB-4434: Add generalised functions for calls from erRequest

--- src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/utils/OCRFieldExtractor.java
+++ src/main/java/uk/gov/hmcts/probate/service/exceptionrecord/utils/OCRFieldExtractor.java
@@ -66,20 +66,271 @@ public class OCRFieldExtractor {
                 .findFirst()
                 .orElse(null);
 
-        if (null == response || isInValidPostCode(response)) {
+        if (null == response || isInvalidPostCode(response)) {
             log.warn("Invalid or missing postcode for field '{}'", name);
             ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
                     .fieldName(name)
                     .originalValue(response)
                     .build();
-            modifiedFields.add(new CollectionMember(null, modifiedOCRField));
-            return bulkScanConfig.getPostcode();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getDeceasedAddressPostCode();
         }
 
         return response;
     }
 
-    private static boolean isInValidPostCode(final String postCode) {
+    //For any name, e.g sols, applicant, deceased, town
+    public static String getDefaultNameIfInvalid(List<OCRField> ocrFields, String name,
+                                                 List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing name for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getName();
+        }
+
+        return response;
+    }
+
+    //For any pairs of names, such as forenames
+    public static String getDefaultNamesIfInvalid(List<OCRField> ocrFields, String name, String name2,
+                                                  List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing names for fields '{}' and {}", name, name2);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .fieldName(name2)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getName();
+        }
+
+        return response;
+    }
+
+    //Serves all address lines
+    public static String getDefaultAddressLineIfInvalid(List<OCRField> ocrFields, String name,
+                                          List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing address line for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getAddressLine();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultDateOfBirthIfInvalid(List<OCRField> ocrFields, String name,
+                                          List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response || isInvalidDateOfBirth(response)) {
+            log.warn("Invalid or missing date of birth for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getDob();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultSolsAppReferenceIfInvalid(List<OCRField> ocrFields, String name,
+                                                        List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing solicitor application reference for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getDob();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultRepresentativeNameIfInvalid(List<OCRField> ocrFields, String name,
+                                                               List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing representative name for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getName();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultEmailIfInvalid(List<OCRField> ocrFields, String name,
+                                                               List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing email for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getEmail();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultPhoneNumberIfInvalid(List<OCRField> ocrFields, String name,
+                                                               List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing phone number for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getSolsSolicitorPhoneNumber();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultDomiciledInEngWalesIfInvalid(List<OCRField> ocrFields, String name,
+                                                        List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing domiciled in Eng/Wales value for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getDeceasedDomicileInEngWales();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultLegalRepresentativeIfInvalid(List<OCRField> ocrFields, String name,
+                                                        List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing legal representative for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getLegalRepresentative();
+        }
+
+        return response;
+    }
+
+    public static String getDefaultSolsSolicitorAddressTownIfInvalid(List<OCRField> ocrFields, String name,
+                                                        List<CollectionMember<ModifiedOCRField>> modifiedFields) {
+        String response = ocrFields
+                .stream()
+                .filter(it -> it.getName().equals(name) && it.getValue() != null)
+                .map(it -> it.getValue().trim().toUpperCase())
+                .findFirst()
+                .orElse(null);
+
+        if (null == response) {
+            log.warn("Invalid or missing town/city for field '{}'", name);
+            ModifiedOCRField modifiedOCRField = ModifiedOCRField.builder()
+                    .fieldName(name)
+                    .originalValue(response)
+                    .build();
+            modifiedFields.add(new CollectionMember<>(null, modifiedOCRField));
+            return bulkScanConfig.getLegalRepresentative();
+        }
+
+        return response;
+    }
+
+    private static boolean isInvalidPostCode(final String postCode) {
         return !postCode.matches(POSTCODE_REGEX_PATTERN);
+    }
+
+    private static boolean isInvalidDateOfBirth(final String dob) {
+        return false;
+        //return !dob.matches(DATE_OF_BIRTH_REGEX_PATTERN);
     }
 }
