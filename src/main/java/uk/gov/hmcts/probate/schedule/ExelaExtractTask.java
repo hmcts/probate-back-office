5651bb8d22d2e4217f915d197323299b80b63330
Tom Saunders <tom.saunders@justice.gov.uk>
2025-04-28 08:57:41 +0100

    Regularise log statements so we can use them for alerting.

--- src/main/java/uk/gov/hmcts/probate/schedule/ExelaExtractTask.java
+++ src/main/java/uk/gov/hmcts/probate/schedule/ExelaExtractTask.java
@@ -23,30 +23,38 @@ public class ExelaExtractTask implements Runnable {
         this.scheduleDates = scheduleExtractDates;
     }
 
+    /// Note that the log messages here are used for alerting/monitoring
     @Override
     public void run() {
-        log.info("Scheduled task ExelaExtractTask started to extract data for Exela");
+        log.info("Scheduled task ExelaExtractTask");
 
+        final String descr;
         final String fromDate;
         final String toDate;
         if (scheduleDates.hasValue()) {
+            descr = "ad hoc dates";
             fromDate = scheduleDates.getFromDate();
             toDate = scheduleDates.getToDate();
-            log.info("Running ExelaDataExtractTask with ad hoc dates {} - {}", fromDate, toDate);
         } else {
+            descr = "default dates (yesterday)";
             fromDate = scheduleDates.getYesterday();
             toDate = fromDate;
-            log.info("Running ExelaDataExtractTask with default dates (yesterday): {} - {}", fromDate, toDate);
         }
+        log.info("Running ExelaDataExtractTask with {}: {} - {}", descr, fromDate, toDate);
 
         try {
             dataExtractJobDateValidator.dateValidator(fromDate, toDate);
-            log.info("Perform Exela data extract from date started");
+
+            log.info("Starting ExelaExtractTask with {}: {} - {}", descr, fromDate, toDate);
             exelaDataExtractService.performExelaExtractForDateRange(fromDate, toDate);
-            log.info("Perform Exela data extract from date finished");
+            log.info("Finished ExelaExtractTask with {}: {} - {}", descr, fromDate, toDate);
         } catch (RuntimeException e) {
-            log.error("Error on ExelaExtractTask Scheduler", e);
+            final String errMsg = String.format(
+                    "Exception in ExelaExtractTask with %s: %s - %s",
+                    descr,
+                    fromDate,
+                    toDate);
+            log.error(errMsg, e);
         }
     }
-
 }
