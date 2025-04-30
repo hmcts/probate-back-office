0f37da5560027382c6966d9fc6e61039598a17af
marian.irudyanathan <marian.irudayanathan@justice.gov.uk>
2025-04-30 14:03:05 +0100

    Update slowMo to 200

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
         //* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://127.0.0.1:3000',
         launchoptions: {
-            slowMo: 200,
+            slowMo: 250,
         },
 
         //* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
