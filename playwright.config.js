24906de7f99bb5c441407c1d1aafe784e6287b6b
FeliTam <feli.tam@justice.gov.uk>
2025-04-30 12:45:28 +0100

    update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
         //* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://127.0.0.1:3000',
         launchoptions: {
-            slowMo: 250,
+            slowMo: 400,
         },
 
         //* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
