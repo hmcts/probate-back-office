8ac0fbab7fcbe84bfd1c09fef88d66f1b98bc85b
marian.irudyanathan <marian.irudayanathan@justice.gov.uk>
2025-05-01 11:38:12 +0100

    Update playwright.config.js to have screenshots and videos on error

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
         //* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
         screenshot: 'on', // Options: 'on', 'off', 'only-on-failure'
-        video: 'retain-on-failure',    // Options: 'on', 'off', 'retain-on-failure'
+        video: 'retain-on-failure', // Options: 'on', 'off', 'retain-on-failure'
     },
 
     //* Configure projects for major browsers */
