3bf0fb4be7a12b60b4d023264671febef2941ef6
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-05-07 18:53:35 +0100

    DTSPB-4434 Removed mandatory as we are setting dummy values

--- src/main/java/uk/gov/hmcts/probate/model/ccd/ocr/GORCitizenMandatoryFields.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/ocr/GORCitizenMandatoryFields.java
@@ -3,19 +3,10 @@ package uk.gov.hmcts.probate.model.ccd.ocr;
 import java.util.Arrays;
 
 public enum GORCitizenMandatoryFields {
-    PRIMARY_APPLICANT_FORENAMES("primaryApplicantForenames", "Primary applicant first names"),
-    PRIMARY_APPLICANT_SURNAME("primaryApplicantSurname", "Primary applicant last name"),
-    PRIMARY_APPLICANT_ADDRESS_LINE1("primaryApplicantAddressLine1", "Primary applicant address line 1"),
-    PRIMARY_APPLICANT_ADDRESS_POSTCODE("primaryApplicantAddressPostCode", "Primary applicant postcode"),
     PRIMARY_APPLICANT_HAS_ALIAS("primaryApplicantHasAlias", "Primary applicant has alias?"),
     DECEASED_FORENAMES("deceasedForenames", "Deceased first names"),
     DECEASED_SURNAME("deceasedSurname", "Deceased last name"),
-    DECEASED_ADDRESS_LINE1("deceasedAddressLine1", "Deceased address line 1"),
-    DECEASED_DOB("deceasedDateOfBirth", "Deceased date of birth"),
     DECEASED_DOD("deceasedDateOfDeath", "Deceased date of death"),
-    DECEASED_ANY_OTHER_NAMES("deceasedAnyOtherNames", "Did the deceased have assets in any other names?"),
-    DECEASED_DOMICILE_IN_ENG_WALES("deceasedDomicileInEngWales",
-            "Was the deceased domiciled in England or Wales at the time of their death?"),
     EXCEPTED_ESTATE("exceptedEstate", "I did not have to submit any forms to HMRC.", "0"),
     FORM_VERSION("formVersion", "Form version"),
     IHT_GROSS_VALUE("ihtGrossValue", "Gross value of the estate", "1"),
--- src/main/java/uk/gov/hmcts/probate/model/ccd/ocr/IntestacyCitizenMandatoryFields.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/ocr/IntestacyCitizenMandatoryFields.java
@@ -3,22 +3,12 @@ package uk.gov.hmcts.probate.model.ccd.ocr;
 import java.util.Arrays;
 
 public enum IntestacyCitizenMandatoryFields {
-    PRIMARY_APPLICANT_FORENAMES("primaryApplicantForenames", "Primary applicant first names"),
-    PRIMARY_APPLICANT_SURNAME("primaryApplicantSurname", "Primary applicant last name"),
-    PRIMARY_APPLICANT_ADDRESS_LINE1("primaryApplicantAddressLine1", "Primary applicant address line 1"),
-    PRIMARY_APPLICANT_ADDRESS_POSTCODE("primaryApplicantAddressPostCode", "Primary applicant postcode"),
     DECEASED_FORENAMES("deceasedForenames", "Deceased first names"),
     DECEASED_SURNAME("deceasedSurname", "Deceased last name"),
-    DECEASED_ADDRESS_LINE1("deceasedAddressLine1", "Deceased address line 1"),
-    DECEASED_DOB("deceasedDateOfBirth", "Deceased date of birth"),
     DECEASED_DOD("deceasedDateOfDeath", "Deceased date of death"),
-    DECEASED_ANY_OTHER_NAMES("deceasedAnyOtherNames", "Did the deceased have assets in any other names?"),
-    DECEASED_DOMICILE_IN_ENG_WALES("deceasedDomicileInEngWales",
-            "Was the deceased domiciled in England or Wales at the time of their death?"),
     FORM_VERSION("formVersion", "Form version"),
     IHT_GROSS_VALUE("ihtGrossValue", "Gross value of the estate", "1"),
     IHT_NET_VALUE("ihtNetValue", "Net value of the estate", "1"),
-    SOLICITOR_APPLYING("solsSolicitorIsApplying","Do you have legal representative acting for you?"),
     IHT_400421_COMPLETED("iht400421completed", "Did you complete an IHT400 and IHT421 form?", "2"),
     IHT_IDENTIFIER("ihtReferenceNumber", "IHT identifier", "0"),
     IHT_ESTATE_GROSS("ihtEstateGrossValue", "gross value of the estate for inheritance tax", "0"),
