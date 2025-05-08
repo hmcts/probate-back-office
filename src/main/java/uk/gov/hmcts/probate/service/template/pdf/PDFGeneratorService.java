50cba1b4662f860b814d0c6c24c8dee4e68b7c26
Tom Saunders <tom.saunders@justice.gov.uk>
2025-05-08 16:28:47 +0100

    add spring config.

--- src/main/java/uk/gov/hmcts/probate/service/template/pdf/PDFGeneratorService.java
+++ src/main/java/uk/gov/hmcts/probate/service/template/pdf/PDFGeneratorService.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.probate.service.template.pdf;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,13 +15,15 @@ import uk.gov.hmcts.probate.service.docmosis.DocmosisPdfGenerationService;
 import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.hmcts.reform.pdf.service.client.exception.PDFServiceClientException;
 
+import uk.gov.hmcts.reform.probate.service.PdfTemplateService;
+
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class PDFGeneratorService {
 
     public static final String TEMPLATE_EXTENSION = ".html";
@@ -31,13 +32,39 @@ public class PDFGeneratorService {
     private final ObjectMapper objectMapper;
     private final PDFServiceClient pdfServiceClient;
     private final DocmosisPdfGenerationService docmosisPdfGenerationService;
+    private final PdfTemplateService pdfTemplateService;
+
+    public PDFGeneratorService(
+            final FileSystemResourceService fileSystemResourceService,
+            final PDFServiceConfiguration pdfServiceConfiguration,
+            final ObjectMapper objectMapper,
+            final PDFServiceClient pdfServiceClient,
+            final DocmosisPdfGenerationService docmosisPdfGenerationService,
+            final PdfTemplateService pdfTemplateService) {
+        this.fileSystemResourceService = fileSystemResourceService;
+        this.pdfServiceConfiguration = pdfServiceConfiguration;
+        this.objectMapper = objectMapper;
+        this.pdfServiceClient = pdfServiceClient;
+        this.docmosisPdfGenerationService = docmosisPdfGenerationService;
+        this.pdfTemplateService = pdfTemplateService;
+    }
 
     public EvidenceManagementFileUpload generatePdf(DocumentType documentType, String pdfGenerationData) {
         byte[] postResult;
         try {
-            log.info("Generate pdf from template {}", documentType.getTemplateName());
-            postResult = generateFromHtml(documentType.getTemplateName(), pdfGenerationData);
-            log.info("Generated from templates with bytes size {}", postResult != null ? postResult.length : "0");
+            final String templateName = documentType.getTemplateName();
+            log.info("Generate pdf from template {}", templateName);
+
+            if (templateName.equals("digitalGrant.html")) {
+                log.info("using alternate templating?");
+                postResult = pdfTemplateService.generate(
+                        "digital_grant/main.html",
+                        Locale.UK,
+                        asMap(pdfGenerationData));
+            } else {
+                postResult = generateFromHtml(templateName, pdfGenerationData);
+                log.info("Generated from templates with bytes size {}", postResult != null ? postResult.length : "0");
+            }
         } catch (IOException | PDFServiceClientException e) {
             log.error(e.getMessage(), e);
             throw new ClientException(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
