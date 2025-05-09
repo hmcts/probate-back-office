8d77c572a20dfd79efaa06141d5c0ba40b78b4de
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-05-09 15:33:14 +0100

    DTSPB-4434: Remove unneeded tests

--- src/test/java/uk/gov/hmcts/probate/service/ocr/pa1p/PA1PSolicitorMandatoryFieldsValidatorTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/ocr/pa1p/PA1PSolicitorMandatoryFieldsValidatorTest.java
@@ -52,25 +52,6 @@ class PA1PSolicitorMandatoryFieldsValidatorTest {
         assertEquals(0, warnings.size());
     }
 
-    /*
-    @Test
-    void testSolicitorMissingMandatoryFieldsPA1P() {
-        List<OCRField> ocrFields = ocrFieldTestUtils.addAllMandatoryGORCitizenFields();
-        HashMap<String, String> ocrFieldValues = ocrFieldTestUtils.addAllFields(ocrFields);
-        ocrFieldValues.put("solsSolicitorIsApplying", "True");
-
-        pa1PSolicitorMandatoryFieldsValidator.addWarnings(ocrFieldValues, warnings);
-
-        assertEquals(4, warnings.size());
-        assertEquals("Solicitor representative name (solsSolicitorRepresentativeName) is mandatory.",
-            warnings.get(0));
-        assertEquals("Solicitors Firm name (solsSolicitorFirmName) is mandatory.", warnings.get(1));
-        assertEquals("Solictor application reference (solsSolicitorAppReference) is mandatory.",
-            warnings.get(2));
-        assertEquals("Solictor email address (solsSolicitorEmail) is mandatory.", warnings.get(3));
-    }
-     */
-
     @Test
     void testSolicitorMissingPaymentMethodFieldsPA1P() {
         List<OCRField> ocrFields = ocrFieldTestUtils.addAllMandatoryGORSolicitorFields();
