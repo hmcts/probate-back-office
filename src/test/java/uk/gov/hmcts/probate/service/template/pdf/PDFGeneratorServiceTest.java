779495842217c336c6c7b288f2092354d4dccfb4
Tom Saunders <tom.saunders@justice.gov.uk>
2025-05-16 11:42:46 +0100

    add additional testing.

--- src/test/java/uk/gov/hmcts/probate/service/template/pdf/PDFGeneratorServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/template/pdf/PDFGeneratorServiceTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.probate.service.template.pdf;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,16 +12,19 @@ import org.springframework.http.MediaType;
 import uk.gov.hmcts.probate.commons.service.PdfTemplateService;
 import uk.gov.hmcts.probate.config.PDFServiceConfiguration;
 import uk.gov.hmcts.probate.exception.ClientException;
+import uk.gov.hmcts.probate.model.DocumentType;
 import uk.gov.hmcts.probate.model.evidencemanagement.EvidenceManagementFileUpload;
 import uk.gov.hmcts.probate.service.FeatureToggleService;
 import uk.gov.hmcts.probate.service.FileSystemResourceService;
 import uk.gov.hmcts.probate.service.docmosis.DocmosisPdfGenerationService;
 import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.hmcts.reform.pdf.service.client.exception.PDFServiceClientException;
+import uk.gov.hmcts.reform.probate.exception.ProbateRuntimeException;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,6 +33,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.probate.model.DocumentType.CAVEAT_RAISED;
 import static uk.gov.hmcts.probate.model.DocumentType.LEGAL_STATEMENT_PROBATE;
@@ -39,6 +47,9 @@ class PDFGeneratorServiceTest {
 
     @Mock
     private PDFServiceConfiguration pdfServiceConfigurationMock;
+
+    @Mock
+    ObjectMapper objectMapperMock;
 
     @Mock
     private PDFServiceClient pdfServiceClientMock;
@@ -63,7 +74,7 @@ class PDFGeneratorServiceTest {
         underTest = new PDFGeneratorService(
                 fileSystemResourceServiceMock,
                 pdfServiceConfigurationMock,
-                new ObjectMapper(),
+                objectMapperMock,
                 pdfServiceClientMock,
                 docmosisPdfGenerationServiceMock,
                 pdfTemplateServiceMock,
@@ -129,24 +140,6 @@ class PDFGeneratorServiceTest {
 
         when(fileSystemResourceServiceMock.getFileFromResourceAsString(anyString()))
                 .thenReturn("<htmlTemplate>");
-        when(pdfServiceClientMock.generateFromHtml(any(byte[].class), anyMap()))
-                .thenThrow(pdfServiceClientException);
-
-        assertThrows(ClientException.class, () -> {
-            underTest.generatePdf(LEGAL_STATEMENT_PROBATE, "{\"data\":\"value\"}");
-        });
-    }
-
-    // this test seems to be identical to shouldThrowClientException() ?
-    @Test
-    void shouldThrowPDFConnectionException() {
-        final PDFServiceClientException pdfServiceClientException = new PDFServiceClientException("blah", null);
-
-        when(featureToggleServiceMock.useCommonsPdfGen())
-                .thenReturn(false);
-
-        when(fileSystemResourceServiceMock.getFileFromResourceAsString(anyString()))
-                .thenReturn("<htmlTemplate>");
         when(pdfServiceClientMock.generateFromHtml(any(), any()))
                 .thenThrow(pdfServiceClientException);
 
@@ -180,6 +173,53 @@ class PDFGeneratorServiceTest {
             when(docmosisPdfGenerationServiceMock.generateDocFrom(any(), any()))
                     .thenThrow(pdfServiceClientException);
             underTest.generateDocmosisDocumentFrom(CAVEAT_RAISED.getTemplateName(), placeholders);
+        });
+    }
+
+    @Test
+    void shouldThrowClientExceptionWhenObjectMapperProcessingException() throws JsonProcessingException {
+        JsonProcessingException jsonProcessingExceptionMock = mock();
+        when(objectMapperMock.readValue(anyString(), any(TypeReference.class)))
+                .thenThrow(jsonProcessingExceptionMock);
+
+        assertThrows(ClientException.class, () -> {
+            underTest.generatePdf(LEGAL_STATEMENT_PROBATE, "");
+        });
+
+        // Since every failure throws the same exception we can only really identify that this
+        // failed before this point.
+        verify(featureToggleServiceMock, never()).useCommonsPdfGen();
+    }
+
+    @Test
+    void shouldThrowClientExceptionWhenNoCommonsTemplate() {
+        when(featureToggleServiceMock.useCommonsPdfGen())
+                .thenReturn(true);
+
+        DocumentType documentTypeMock = mock(DocumentType.class);
+        when(documentTypeMock.getCommonsTemplateName())
+                .thenReturn(Optional.empty());
+
+        assertThrows(ClientException.class, () -> {
+            underTest.generatePdf(documentTypeMock, "");
+        });
+    }
+
+    @Test
+    void shouldThrowClientExceptionWhenCommonsThrowsProbateRuntimeException() {
+        when(featureToggleServiceMock.useCommonsPdfGen())
+                .thenReturn(true);
+
+        DocumentType documentTypeMock = mock(DocumentType.class);
+        when(documentTypeMock.getCommonsTemplateName())
+                .thenReturn(Optional.of("mock"));
+
+        ProbateRuntimeException probateRuntimeExceptionMock = mock(ProbateRuntimeException.class);
+        when(pdfTemplateServiceMock.generate(any(), any(), any()))
+                .thenThrow(probateRuntimeExceptionMock);
+
+        assertThrows(ClientException.class, () -> {
+            underTest.generatePdf(documentTypeMock, "");
         });
     }
 }
