936b8293974315445d4a5d15ea887a95bc1b4d4c
FeliTam <feli.tam@justice.gov.uk>
2025-05-21 08:36:37 +0100

    DTSPB-4718 add clock and update logging

--- src/test/java/uk/gov/hmcts/probate/schedule/CaveatExpiryTaskTest.java
+++ src/test/java/uk/gov/hmcts/probate/schedule/CaveatExpiryTaskTest.java
@@ -1,26 +1,30 @@
 package uk.gov.hmcts.probate.schedule;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.probate.service.CaveatExpiryService;
 import uk.gov.hmcts.probate.service.dataextract.DataExtractDateValidator;
 import uk.gov.hmcts.reform.probate.model.client.ApiClientException;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.probate.model.Constants.DATE_FORMAT;
 
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class CaveatExpiryTaskTest {
 
     @Mock
@@ -29,49 +33,63 @@ class CaveatExpiryTaskTest {
     @Mock
     private CaveatExpiryService caveatExpiryService;
 
+    @Mock
+    private Clock clock;
+
     @InjectMocks
-    private CaveatExpiryTask caveatExpiryTask;
-    private static final String DATE = DATE_FORMAT.format(LocalDate.now().minusDays(1L));
+    private CaveatExpiryTask caveatExpiryTaskMock;
+
+    private String yesterday;
+
+    @BeforeEach
+    void init() {
+        LocalDate fixedDate = LocalDate.of(2025, 5, 19);
+        Instant fixedInstant = fixedDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+        when(clock.instant()).thenReturn(fixedInstant);
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        yesterday = LocalDate.now(clock).minusDays(1).format(DATE_FORMAT);
+
+        caveatExpiryTaskMock.adHocJobDate = null;
+        ReflectionTestUtils.setField(caveatExpiryTaskMock, "clock", clock);
+    }
 
     @Test
-    void shouldExpireCaveatCases() {
-        ResponseEntity<String> responseEntity = ResponseEntity.accepted()
-                .body("Perform caveat expiry finished");
-        caveatExpiryTask.run();
-        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
-        assertEquals("Perform caveat expiry finished", responseEntity.getBody());
-        verify(dataExtractDateValidator).dateValidator(DATE);
-        verify(caveatExpiryService).expireCaveats(DATE);
+    void shouldExpireCaveatCasesForYesterday() {
+        caveatExpiryTaskMock.run();
+
+        verify(dataExtractDateValidator).dateValidator(yesterday);
+        verify(caveatExpiryService).expireCaveats(yesterday);
     }
 
     @Test
     void shouldExpireCaveatCasesForAdhocDate() {
-        caveatExpiryTask.adHocJobDate = "2022-09-05";
-        ResponseEntity<String> responseEntity = ResponseEntity.accepted()
-                .body("Perform caveat expiry finished");
-        caveatExpiryTask.run();
-        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
-        assertEquals("Perform caveat expiry finished", responseEntity.getBody());
-        final String adhocDate = "2022-09-05";
-        verify(dataExtractDateValidator).dateValidator(adhocDate);
-        verify(caveatExpiryService).expireCaveats(adhocDate);
+        caveatExpiryTaskMock.adHocJobDate = "2022-09-05";
+
+        caveatExpiryTaskMock.run();
+
+        verify(dataExtractDateValidator).dateValidator("2022-09-05");
+        verify(caveatExpiryService).expireCaveats("2022-09-05");
     }
 
     @Test
     void shouldThrowClientExceptionWithBadRequestForExpireCaveatCasesWithIncorrectDateFormat() {
         doThrow(new ApiClientException(HttpStatus.BAD_REQUEST.value(), null)).when(dataExtractDateValidator)
-                .dateValidator(DATE);
-        caveatExpiryTask.run();
-        verify(dataExtractDateValidator).dateValidator(DATE);
+                .dateValidator(yesterday);
+
+        caveatExpiryTaskMock.run();
+
+        verify(dataExtractDateValidator).dateValidator(yesterday);
         verifyNoInteractions(caveatExpiryService);
     }
 
     @Test
     void shouldThrowExceptionForExpireCaveatCases() {
         doThrow(new NullPointerException()).when(dataExtractDateValidator)
-                .dateValidator(DATE);
-        caveatExpiryTask.run();
-        verify(dataExtractDateValidator).dateValidator(DATE);
+                .dateValidator(yesterday);
+
+        caveatExpiryTaskMock.run();
+
+        verify(dataExtractDateValidator).dateValidator(yesterday);
         verifyNoInteractions(caveatExpiryService);
     }
 }
