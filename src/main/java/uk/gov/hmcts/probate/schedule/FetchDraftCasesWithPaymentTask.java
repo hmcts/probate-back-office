9e3836e58deba3741224ebb798692458f78931e0
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2025-05-29 12:17:35 +0100

    DTSPB-4659: Remove unused import

--- src/main/java/uk/gov/hmcts/probate/schedule/FetchDraftCasesWithPaymentTask.java
+++ src/main/java/uk/gov/hmcts/probate/schedule/FetchDraftCasesWithPaymentTask.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.probate.schedule;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.service.FetchDraftCaseService;
