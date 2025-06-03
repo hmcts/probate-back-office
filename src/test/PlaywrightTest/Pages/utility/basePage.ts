9319b6107b44ed7d355a4775dcf80fc8644629b7
Jay Ashton <jay.ashton@justice.gov.uk>
2025-06-03 14:35:13 +0100

    Initial move to TS

---
+++ src/test/PlaywrightTest/Pages/utility/basePage.ts
@@ -0,0 +1,287 @@
+import { AxeUtils } from "@hmcts/playwright-common";
+import { expect, Page } from "@playwright/test";
+import { testConfig } from "../../Configs/config";
+
+export class BasePage {
+  readonly rejectLocator = this.page.getByRole("button", {
+    name: "Reject analytics cookies",
+  });
+  readonly continueButtonLocator = this.page.getByRole("button", {
+    name: "Continue",
+  });
+  readonly saveAndContinueButtonLocator = this.page.getByRole("button", {
+    name: "Save and continue",
+  });
+  readonly submitButtonLocator = this.page.getByRole("button", {
+    name: "Submit",
+  });
+  readonly goButtonLocator = this.page.getByRole("button", { name: "Go" });
+
+  constructor(public readonly page: Page) {}
+
+  async logInfo(scenarioName, log, caseRef) {
+    let ret = scenarioName;
+    await this.page.waitForTimeout(testConfig.GetCaseRefFromUrlDelay);
+    if (log) {
+      ret = ret + " : " + log;
+    }
+    if (caseRef) {
+      ret = ret + " : " + caseRef;
+    }
+    console.info(ret);
+  }
+
+  async runAccessibilityTest() {
+    await new AxeUtils(this.page).audit();
+  }
+
+  async rejectCookies() {
+    if (testConfig.RejectCookies) {
+      try {
+        //const rejectLocator = {css: 'button.govuk-button[value="reject"]'};
+        const numVisibleCookieBannerEls = await this.rejectLocator.count();
+        if (numVisibleCookieBannerEls > 0) {
+          // just reject additional cookies
+          await expect(this.rejectLocator).toBeEnabled();
+          await this.rejectLocator.click();
+          await this.page.waitForTimeout(testConfig.RejectCookieDelay);
+        }
+      } catch (e) {
+        console.error(`error trying to close cookie banner: ${e.message}`);
+      }
+    }
+  }
+
+  async getCaseRefFromUrl() {
+    await this.page.waitForTimeout(testConfig.GetCaseRefFromUrlDelay);
+    const url = await this.page.url();
+    return url
+      .replace("#Event%20History", "")
+      .replace("#Case%20Progress", "")
+      .split("/")
+      .pop()
+      .match(/.{4}/g)
+      .join("-");
+  }
+
+  async waitForNavigationToComplete() {
+    const navigationPromise = this.page.waitForNavigation();
+    await expect(this.continueButtonLocator).toBeVisible();
+    await expect(this.continueButtonLocator).toBeEnabled();
+    this.continueButtonLocator.click();
+    await navigationPromise;
+  }
+
+  async waitForSubmitNavigationToComplete(buttonName) {
+    const navigationPromise = this.page.waitForNavigation();
+    if (buttonName === "Save and continue") {
+      await expect(this.saveAndContinueButtonLocator).toBeVisible();
+      await expect(this.saveAndContinueButtonLocator).toBeEnabled();
+      this.saveAndContinueButtonLocator.click();
+    } else {
+      await expect(this.submitButtonLocator).toBeVisible();
+      await expect(this.submitButtonLocator).toBeEnabled();
+      this.submitButtonLocator.click();
+    }
+    await navigationPromise;
+  }
+
+  async waitForGoNavigationToComplete() {
+    await expect(this.goButtonLocator).toBeVisible();
+    await expect(this.goButtonLocator).toBeEnabled();
+    await Promise.all([
+      this.goButtonLocator.waitFor({ state: "visible" }),
+      this.goButtonLocator.click(),
+      this.goButtonLocator.waitFor({ state: "detached" }),
+    ]);
+  }
+
+  async waitForSignOutNavigationToComplete(signOutLocator) {
+    const navigationPromise = this.page.waitForNavigation();
+    await expect(this.page.locator(`${signOutLocator}`)).toBeVisible();
+    await expect(this.page.locator(`${signOutLocator}`)).toBeEnabled();
+    this.page.locator(`${signOutLocator}`).click();
+    await navigationPromise;
+  }
+
+  async seeCaseDetails(
+    testInfo,
+    caseRef,
+    tabConfigFile,
+    dataConfigFile,
+    nextStep,
+    endState,
+    delay = testConfig.CaseDetailsDelayDefault
+  ) {
+    if (tabConfigFile.tabName && tabConfigFile.tabName !== "Documents") {
+      await expect(
+        this.page.getByLabel(`${tabConfigFile.tabName}`, { exact: true })
+      ).toBeVisible();
+    }
+
+    await expect(
+      this.page.getByRole("heading", { name: caseRef })
+    ).toBeVisible();
+    await this.page.getByRole("tab", { name: tabConfigFile.tabName }).focus();
+    await this.page.getByRole("tab", { name: tabConfigFile.tabName }).click();
+    await this.page.waitForTimeout(delay);
+
+    // *****Need to comment this until accessibility script is completed*****/
+    await this.runAccessibilityTest();
+
+    if (tabConfigFile.waitForText) {
+      const tabLocator = this.page.getByLabel(tabConfigFile.waitForText);
+      await expect(tabLocator).toBeVisible();
+    }
+
+    /* eslint-disable no-await-in-loop */
+    for (let i = 0; i < tabConfigFile.fields.length; i++) {
+      if (tabConfigFile.fields[i] && tabConfigFile.fields[i] !== "") {
+        const textCount = await this.page
+          .getByText(tabConfigFile.fields[i])
+          .count();
+        if (textCount > 1) {
+          if (tabConfigFile.fields[i] === "Caveat not matched") {
+            await expect(
+              this.page.getByText(tabConfigFile.fields[i]).nth(2)
+            ).toBeVisible();
+          }
+          await expect(
+            this.page.getByText(tabConfigFile.fields[i], { exact: true })
+          ).toBeVisible();
+        } else if (tabConfigFile.tabName === "Event History") {
+          await expect(
+            this.page.getByRole("table", { name: "Details" })
+          ).toContainText(tabConfigFile.fields[i]);
+        } else {
+          await expect(
+            this.page.getByRole("table", { name: "case viewer table" })
+          ).toContainText(tabConfigFile.fields[i]);
+        }
+      }
+    }
+
+    const dataConfigKeys = tabConfigFile.dataKeys;
+    // If 'Event History' tab, then check Next Step (Event), End State, Summary and Comment
+    if (tabConfigFile.tabName === "Event History") {
+      if (nextStep === endState) {
+        await expect(this.page.getByText(nextStep).nth(2)).toBeVisible();
+        await expect(this.page.getByText(endState).nth(3)).toBeVisible();
+      } else if (
+        endState === "Caveat created" ||
+        nextStep === "Apply for probate" ||
+        endState === "Grant of probate created"
+      ) {
+        await expect(
+          this.page.getByRole("cell", { name: endState, exact: true })
+        ).toBeVisible();
+        await expect(this.page.getByLabel(nextStep).nth(1)).toBeVisible();
+        // await expect(this.page.getByLabel(nextStep), {exact: true}).toBeVisible();
+      } else {
+        await expect(
+          this.page
+            .getByRole("cell", { name: endState, exact: true })
+            .locator("span")
+        ).toBeVisible();
+        await expect(
+          this.page
+            .getByRole("cell", { name: nextStep, exact: true })
+            .locator("span")
+        ).toBeVisible();
+      }
+      let eventSummaryPrefix = nextStep;
+      eventSummaryPrefix =
+        eventSummaryPrefix.replace(/\s+/g, "_").toLowerCase() + "_";
+      if (dataConfigKeys && nextStep !== "Change state") {
+        await expect(
+          this.page.getByText(eventSummaryPrefix + dataConfigFile.summary)
+        ).toBeVisible();
+        await expect(
+          this.page.getByText(eventSummaryPrefix + dataConfigFile.comment)
+        ).toBeVisible();
+      }
+    } else if (dataConfigKeys) {
+      for (let i = 0; i < tabConfigFile.dataKeys.length; i++) {
+        const textCount = await this.page
+          .getByText(dataConfigFile[tabConfigFile.dataKeys[i]])
+          .count();
+        if (textCount > 1) {
+          await expect(
+            this.page.getByText(dataConfigFile[tabConfigFile.dataKeys[i]], {
+              exact: true,
+            })
+          ).toBeVisible();
+        } else {
+          await expect(
+            this.page.getByRole("table", { name: "case viewer table" })
+          ).toContainText(dataConfigFile[tabConfigFile.dataKeys[i]]);
+        }
+      }
+    }
+  }
+
+  async seeUpdatesOnCase(
+    testInfo,
+    caseRef,
+    tabConfigFile,
+    tabUpdates,
+    tabUpdatesConfigFile,
+    forUpdateApplication
+  ) {
+    await expect(
+      this.page.getByRole("heading", { name: caseRef })
+    ).toBeVisible();
+    await this.page.getByRole("tab", { name: tabConfigFile.tabName }).focus();
+    await this.page.getByRole("tab", { name: tabConfigFile.tabName }).click();
+    await this.runAccessibilityTest();
+
+    if (tabUpdates) {
+      const updatedConfig = tabConfigFile[tabUpdates];
+      let fields = updatedConfig.fields;
+      let keys = updatedConfig.dataKeys;
+      if (forUpdateApplication) {
+        fields = fields.concat(updatedConfig.updateAppFields);
+        keys = keys.concat(updatedConfig.updateAppDataKeys);
+      }
+
+      for (let i = 0; i < fields.length; i++) {
+        // eslint-disable-next-line
+        await expect(this.page.getByText(fields[i]).first()).toBeVisible();
+      }
+
+      for (let i = 0; i < keys.length; i++) {
+        // eslint-disable-next-line
+        const textLocator = this.page.getByText(tabUpdatesConfigFile[keys[i]], {
+          exact: true,
+        });
+        const locatorCount = await textLocator.count();
+        if (locatorCount > 0) {
+          await textLocator.first().isVisible();
+        }
+      }
+    }
+  }
+
+  async dontSeeCaseDetails(fieldLabelsNotToBeShown) {
+    let visibleElements;
+    let numElements;
+
+    for (let i = 0; i < fieldLabelsNotToBeShown.length; i++) {
+      // eslint-disable-next-line
+      visibleElements = await this.page
+        .locator(
+          `xpath=//div[contains(@class, 'case-viewer-label')][text()='${fieldLabelsNotToBeShown[i]}']`
+        )
+        .filter({ visible: true });
+      numElements = await visibleElements.count();
+      expect(numElements).toBe(0);
+    }
+  }
+
+  async waitForUploadToBeCompleted() {
+    const locs = await this.page.getByText("Cancel upload").all();
+    for (let i = 0; i < locs.length; i++) {
+      await expect(locs[i]).toBeDisabled();
+    }
+  }
+}
---
+++ src/test/PlaywrightTest/Path/caseworker/caveatPath.spec.ts
@@ -0,0 +1,391 @@
+import dateFns from "date-fns";
+import { test } from "../../Fixtures/fixtures";
+
+import createCaseConfig from "../../Pages/createCase/createCaseConfig.json" with { type: "json" };
+import eventSummaryConfig from "../../Pages/eventSummary/eventSummaryConfig.json" with { type: "json" };
+
+import caseMatchesTabConfig from "../../Pages/caseDetails/caveat/caseMatchesTabConfig.json" with { type: "json" };
+import caseMatchesConfig from "../../Pages/caseMatches/caveat/caseMatchesConfig.json" with { type: "json" };
+import createCaveatConfig from "../../Pages/createCaveat/createCaveatConfig.json" with { type: "json" };
+import emailCaveatorConfig from "../../Pages/emailNotifications/caveat/emailCaveatorConfig.json" with { type: "json" };
+
+import documentUploadConfig from "../../Pages/documentUpload/caveat/documentUploadConfig.json" with { type: "json" };
+
+import historyTabConfig from "../../Pages/caseDetails/caveat/historyTabConfig.json";
+
+import caseDetailsTabConfig from "../../Pages/caseDetails/caveat/caseDetailsTabConfig.json" with { type: "json" };
+import caveatDetailsTabConfig from "../../Pages/caseDetails/caveat/caveatDetailsTabConfig.json" with { type: "json" };
+import caveatorDetailsTabConfig from "../../Pages/caseDetails/caveat/caveatorDetailsTabConfig.json" with { type: "json" };
+import deceasedDetailsTabConfig from "../../Pages/caseDetails/caveat/deceasedDetailsTabConfig.json" with { type: "json" };
+
+import caseDetailsTabUpdateConfig from "../../Pages/caseDetails/caveat/caseDetailsTabUpdateConfig.json" with { type: "json" };
+import caveatDetailsTabUpdateConfig from "../../Pages/caseDetails/caveat/caveatDetailsTabUpdateConfig.json" with { type: "json" };
+import caveatorDetailsTabUpdateConfig from "../../Pages/caseDetails/caveat/caveatorDetailsTabUpdateConfig.json" with { type: "json" };
+import deceasedDetailsTabUpdateConfig from "../../Pages/caseDetails/caveat/deceasedDetailsTabUpdateConfig.json" with { type: "json" };
+
+import documentsTabEmailCaveatorConfig from "../../Pages/caseDetails/caveat/documentsTabEmailCaveatorConfig.json" with { type: "json" };
+import documentsTabUploadDocumentConfig from "../../Pages/caseDetails/caveat/documentsTabUploadDocumentConfig.json" with { type: "json" };
+
+import registrarsDecisionConfig from "../../Pages/caseDetails/caveat/registrarsDecisionConfig.json" with { type: "json" };
+import registrarsDecisionTabConfig from "../../Pages/caseDetails/caveat/registrarsDecisionTabConfig.json" with { type: "json" };
+
+import { convertTokens, legacyParse } from "@date-fns/upgrade/v2";
+
+test.describe("Caseworker Caveat1 - Order summons", () => {
+  test("Caseworker Caveat1 - Order summons", async ({
+    basePage,
+    signInPage,
+    createCasePage,
+    cwEventActionsPage,
+  }, testInfo) => {
+    const scenarioName = "Caseworker Caveat1 - Order summons";
+    // BO Caveat (Personal): Raise a caveat -> Caveat not matched -> Order summons
+
+    // get unique suffix for names - in order to match only against 1 case
+    const unique_deceased_user = Date.now();
+
+    await basePage.logInfo(scenarioName, "Login as Caseworker", null);
+    await signInPage.authenticateWithIdamIfAvailable(false);
+
+    // FIRST case is only needed for case-matching with SECOND one
+
+    let nextStepName = "Raise a caveat";
+    await basePage.logInfo(scenarioName, nextStepName, null);
+    await createCasePage.selectNewCase();
+    await createCasePage.selectCaseTypeOptions(
+      createCaseConfig.list2_text_caveat,
+      createCaseConfig.list3_text_caveat
+    );
+    await createCasePage.enterCaveatPage1("create");
+    await createCasePage.enterCaveatPage2("create", unique_deceased_user);
+    await createCasePage.enterCaveatPage3("create");
+    await createCasePage.enterCaveatPage4("create");
+    // TODO: Expects 2 arguments
+    await createCasePage.checkMyAnswers(nextStepName);
+
+    // SECOND case - the main test case
+
+    await basePage.logInfo(scenarioName, nextStepName, null);
+    await createCasePage.selectNewCase();
+    await createCasePage.selectCaseTypeOptions(
+      createCaseConfig.list2_text_caveat,
+      createCaseConfig.list3_text_caveat
+    );
+    await createCasePage.enterCaveatPage1("create");
+    await createCasePage.enterCaveatPage2("create", unique_deceased_user);
+    await createCasePage.enterCaveatPage3("create");
+    await createCasePage.enterCaveatPage4("create");
+    // TODO: Expects 2 arguments
+    await createCasePage.checkMyAnswers(nextStepName);
+    let endState = "Caveat raised";
+
+    const caseRef = await basePage.getCaseRefFromUrl();
+
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caseDetailsTabConfig,
+      createCaveatConfig
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      deceasedDetailsTabConfig,
+      createCaveatConfig
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caveatorDetailsTabConfig,
+      createCaveatConfig
+    );
+
+    // When raising a caveat, Caveat Expiry Date is automatically set to today + 6 months
+    createCaveatConfig.caveat_expiry_date = dateFns.format(
+      legacyParse(dateFns.addMonths(new Date(), 6)),
+      convertTokens("D MMM YYYY")
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caveatDetailsTabConfig,
+      createCaveatConfig
+    );
+
+    nextStepName = "Registrar's decision";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.registrarsDecision(caseRef);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      registrarsDecisionTabConfig,
+      registrarsDecisionConfig
+    );
+
+    nextStepName = "Email caveator"; // When in state 'Caveat raised'
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.emailCaveator(caseRef);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    // Note that End State does not change when emailing the caveator.
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+    // When emailing the caveator, the Date added for the email document is set to today
+    emailCaveatorConfig.dateAdded = dateFns.format(
+      legacyParse(new Date()),
+      convertTokens("D MMM YYYY")
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      documentsTabEmailCaveatorConfig,
+      emailCaveatorConfig
+    );
+
+    nextStepName = "Caveat match";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.selectCaseMatches(
+      caseRef,
+      nextStepName,
+      true,
+      caseMatchesConfig.addNewButton
+    );
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Caveat matching";
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caseMatchesTabConfig,
+      caseMatchesConfig
+    );
+
+    nextStepName = "Email caveator"; // When in state 'Caveat closed'
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.emailCaveator(caseRef);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    // Note that End State does not change when emailing the caveator.
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+    // When emailing the caveator, the Date added for the email document is set to today
+    emailCaveatorConfig.dateAdded = dateFns.format(
+      legacyParse(new Date()),
+      convertTokens("D MMM YYYY")
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      documentsTabEmailCaveatorConfig,
+      emailCaveatorConfig
+    );
+
+    nextStepName = "Caveat not matched";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Caveat not matched";
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    nextStepName = "Upload document";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.uploadDocument(caseRef, documentUploadConfig);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    // Note that End State does not change when uploading a document.
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      documentsTabUploadDocumentConfig,
+      documentUploadConfig
+    );
+
+    nextStepName = "Add comment";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    // Note that End State does not change when adding a comment.
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    nextStepName = "Await caveat resolution";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Awaiting caveat resolution";
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    nextStepName = "Warning requested";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Warning validation";
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    nextStepName = "Issue caveat warning";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Awaiting warning response";
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    nextStepName = "Order summons";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Summons ordered";
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    nextStepName = "Amend caveat details";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await createCasePage.enterCaveatPage1("update");
+    await createCasePage.enterCaveatPage2("update", unique_deceased_user);
+    await createCasePage.enterCaveatPage3("update");
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+
+    // Note that End State does not change when amending the caveat details.
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caseDetailsTabUpdateConfig,
+      createCaveatConfig
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      deceasedDetailsTabUpdateConfig,
+      createCaveatConfig
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caveatorDetailsTabUpdateConfig,
+      createCaveatConfig
+    );
+    // TODO: Expects 6-7 arguments
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      caveatDetailsTabUpdateConfig,
+      createCaveatConfig
+    );
+
+    nextStepName = "Withdraw caveat";
+    await basePage.logInfo(scenarioName, nextStepName, caseRef);
+    await cwEventActionsPage.chooseNextStep(nextStepName);
+    await cwEventActionsPage.withdrawCaveatPage1();
+    await cwEventActionsPage.enterEventSummary(caseRef, nextStepName);
+    endState = "Caveat closed";
+    await basePage.logInfo(scenarioName, endState, null);
+    await basePage.seeCaseDetails(
+      testInfo,
+      caseRef,
+      historyTabConfig,
+      eventSummaryConfig,
+      nextStepName,
+      endState
+    );
+
+    await signInPage.signOut();
+  });
+});
