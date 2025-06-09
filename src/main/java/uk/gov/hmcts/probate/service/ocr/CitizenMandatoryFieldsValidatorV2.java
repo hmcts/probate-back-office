95a2b7397be51fb3dcf8a7effa5b8432fdd3fd78
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-06-09 15:16:17 +0100

    DTSPB-4434 Removed deceased validations

--- src/main/java/uk/gov/hmcts/probate/service/ocr/CitizenMandatoryFieldsValidatorV2.java
+++ src/main/java/uk/gov/hmcts/probate/service/ocr/CitizenMandatoryFieldsValidatorV2.java
@@ -4,17 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.keyvalue.DefaultKeyValue;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.probate.service.ExceptedEstateDateOfDeathChecker;
 import java.util.List;
 import java.util.Map;
 
-import static org.apache.commons.lang3.BooleanUtils.toBoolean;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static uk.gov.hmcts.probate.model.ccd.ocr.GORCitizenMandatoryFields.IHT_400421_COMPLETED;
-import static uk.gov.hmcts.probate.model.ccd.ocr.GORCitizenMandatoryFields.IHT_ESTATE_GROSS;
-import static uk.gov.hmcts.probate.model.ccd.ocr.GORCitizenMandatoryFields.IHT_ESTATE_NET;
-import static uk.gov.hmcts.probate.model.ccd.ocr.GORCitizenMandatoryFields.IHT_ESTATE_NQV;
-import static uk.gov.hmcts.probate.model.ccd.ocr.GORCitizenMandatoryFields.IHT_UNUSED_ALLOWANCE;
 
 @Slf4j
 @Service
@@ -29,7 +22,6 @@ public class CitizenMandatoryFieldsValidatorV2 {
     private static final String FALSE = "false";
 
 
-    private final ExceptedEstateDateOfDeathChecker exceptedEstateDateOfDeathChecker;
     private final MandatoryFieldsValidatorUtils mandatoryFieldsValidatorUtils;
 
     public void addWarnings(Map<String, String> ocrFieldValues, List<String> warnings) {
@@ -37,38 +29,7 @@ public class CitizenMandatoryFieldsValidatorV2 {
             mandatoryFieldsValidatorUtils.addWarningIfEmpty(ocrFieldValues, warnings, IHT_207_COMPLETED);
             if (FALSE.equalsIgnoreCase(ocrFieldValues.get(IHT_207_COMPLETED.getKey()))) {
                 mandatoryFieldsValidatorUtils.addWarningIfEmpty(ocrFieldValues, warnings, DIED_AFTER_SWITCH_DATE);
-
-                boolean deceasedDiedOnAfterSwitchDate = toBoolean(ocrFieldValues.get(DIED_AFTER_SWITCH_DATE.getKey()));
-
-                if (isNotBlank(ocrFieldValues.get(DIED_AFTER_SWITCH_DATE.getKey()))
-                    && deceasedDiedOnAfterSwitchDate != exceptedEstateDateOfDeathChecker
-                    .isOnOrAfterSwitchDate(ocrFieldValues.get("deceasedDateOfDeath"))) {
-                    mandatoryFieldsValidatorUtils.addWarning(
-                        "Deceased date of death not consistent with the question: "
-                            + "Did the deceased die on or after 1 January 2022? (deceasedDiedOnAfterSwitchDate)",
-                        warnings);
-                } else {
-                    addWarningsForDeceasedDiedOnAfterSwitchDate(deceasedDiedOnAfterSwitchDate, ocrFieldValues,
-                            warnings);
-                }
             }
-        }
-    }
-
-    private void addWarningsForDeceasedDiedOnAfterSwitchDate(boolean deceasedDiedOnAfterSwitchDate,
-                                                             Map<String, String> ocrFieldValues,
-                                                             List<String> warnings) {
-        if (deceasedDiedOnAfterSwitchDate) {
-            if (mandatoryFieldsValidatorUtils.hasLateSpouseCivilPartner(ocrFieldValues)
-                    && mandatoryFieldsValidatorUtils.nqvBetweenThresholds(ocrFieldValues)) {
-                mandatoryFieldsValidatorUtils.addWarningsForConditionalFields(ocrFieldValues, warnings,
-                        IHT_UNUSED_ALLOWANCE);
-            }
-            mandatoryFieldsValidatorUtils.addWarningsForConditionalFields(ocrFieldValues, warnings,
-                    IHT_ESTATE_GROSS, IHT_ESTATE_NET, IHT_ESTATE_NQV);
-        } else if (isNotBlank(ocrFieldValues.get(DIED_AFTER_SWITCH_DATE.getKey()))) {
-            mandatoryFieldsValidatorUtils.addWarningIfEmpty(ocrFieldValues, warnings,
-                    IHT_205_COMPLETED_ONLINE);
         }
     }
 }
