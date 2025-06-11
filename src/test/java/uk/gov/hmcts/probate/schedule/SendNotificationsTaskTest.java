b5b62bca178d475c76f37bb2720060d4f94ec202
FeliTam <feli.tam@justice.gov.uk>
2025-06-11 09:00:44 +0100

    DTSPB-4595 checkstyle

--- src/test/java/uk/gov/hmcts/probate/schedule/SendNotificationsTaskTest.java
+++ src/test/java/uk/gov/hmcts/probate/schedule/SendNotificationsTaskTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.probate.exception.ClientException;
