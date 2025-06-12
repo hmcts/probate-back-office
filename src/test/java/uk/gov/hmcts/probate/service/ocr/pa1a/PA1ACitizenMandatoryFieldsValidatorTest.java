85cc1964c5a32c23a38af924221272cb50cfac0a
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-06-12 14:01:53 +0100

    DTSPB-4434 Fix Check style

--- src/test/java/uk/gov/hmcts/probate/service/ocr/pa1a/PA1ACitizenMandatoryFieldsValidatorTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/ocr/pa1a/PA1ACitizenMandatoryFieldsValidatorTest.java
@@ -15,8 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class PA1ACitizenMandatoryFieldsValidatorTest {
