ed485e2eb5332717e81bd54c99d16f48c53c457f
marian.irudayanathan <marian.irudayanathan@justice.gov.uk>
2025-06-13 11:42:28 +0100

    DTSPB-4793: Update the css value for Awaiting documentation state in case stop/escalate/reissue test

--- src/test/end-to-end/pages/caseProgressStopEscalateIssue/addResolveStopDetails.js
+++ src/test/end-to-end/pages/caseProgressStopEscalateIssue/addResolveStopDetails.js
@@ -2,9 +2,9 @@
 const commonConfig = require('src/test/end-to-end/pages/common/commonConfig');
 
 // CW confirm case stopped
-module.exports = async function () {
+module.exports = async function (chooseState) {
     const I = this;
     await I.waitForElement({css: '#resolveStopState'});
-    await I.selectOption({css: '#resolveStopState'}, '2: CasePrinted');
+    await I.selectOption({css: '#resolveStopState'}, chooseState);
     await I.waitForNavigationToComplete(commonConfig.continueButton);
 };
