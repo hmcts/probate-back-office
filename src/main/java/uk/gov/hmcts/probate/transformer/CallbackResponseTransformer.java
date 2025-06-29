0ee8e7350b0abb296ecc6b480a7a840e1f7a8d69
Feli Tam <feli.tam@justice.gov.uk>
2025-06-29 21:51:33 +0100

    Update CallbackResponseTransformer.java

--- src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
+++ src/main/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformer.java
@@ -135,8 +135,7 @@ public class CallbackResponseTransformer {
     private static final String POLICY_ROLE_APPLICANT_SOLICITOR = "[APPLICANTSOLICITOR]";
     private static final String IHT400 = "IHT400";
     private static final List<String> EXCLUDED_EVENT_LIST = Arrays.asList("boHistoryCorrection",
-            "boCorrection",
-            "automatedNotification");
+            "boCorrection");
     private static final List<String> ROLLBACK_STATE_LIST = List.of("Pending", "CasePaymentFailed", "SolAdmonCreated",
             "SolAppCreatedDeceasedDtls", "SolAppCreatedSolicitorDtls", "SolAppUpdated", "SolProbateCreated",
             "SolIntestacyCreated", "Deleted", "Stopped");
@@ -1420,7 +1419,8 @@ public class CallbackResponseTransformer {
             .isSaveAndClose(caseData.getIsSaveAndClose())
             .executorsNamed(caseData.getExecutorsNamed())
             .ttl(caseData.getTtl())
-            .firstStopReminderSentDate(caseData.getFirstStopReminderSentDate());
+            .firstStopReminderSentDate(caseData.getFirstStopReminderSentDate())
+            .evidenceHandledDate(caseData.getEvidenceHandledDate());
 
         handleDeceasedAliases(
                 builder,
