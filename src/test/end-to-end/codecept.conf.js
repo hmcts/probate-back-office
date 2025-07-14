39d056a43cc6e058754ad8fee70f69e3c0e3f830
Jay Ashton <jay.ashton@justice.gov.uk>
2025-07-14 13:57:28 +0100

    Update and add reporter for nightly execution

--- src/test/end-to-end/codecept.conf.js
+++ src/test/end-to-end/codecept.conf.js
@@ -1,79 +1,81 @@
-const testConfig = require('src/test/config.js');
+const testConfig = require("src/test/config.js");
 
 exports.config = {
-    'tests': testConfig.TestPathToRun,
-    'output': testConfig.TestOutputDir,
-    'helpers': {
-        'Playwright': {
-            'waitForTimeout': 60000,
-            'getPageTimeout': 60000,
-            'waitForAction': 1000,
-            'show': testConfig.TestShowBrowserWindow,
-            'waitForNavigation': 'domcontentloaded',
-            'headless': 'true',
-            'chrome': {
-                'ignoreHTTPSErrors': true,
-                'ignore-certificate-errors': true,
-                'defaultViewport': {
-                    'width': 1280,
-                    'height': 960
-                },
-                args: [
-                    '--headless=new',
-                    '--disable-gpu',
-                    '--no-sandbox',
-                    '--allow-running-insecure-content',
-                    '--ignore-certificate-errors',
-                    // '--proxy-server=proxyout.reform.hmcts.net:8080',
-                    // '--proxy-bypass-list=*beta*LB.reform.hmcts.net',
-                    '--window-size=1440,1400'
-                ]
-            },
-
+  tests: testConfig.TestPathToRun,
+  output: testConfig.TestOutputDir,
+  helpers: {
+    Playwright: {
+      waitForTimeout: 60000,
+      getPageTimeout: 60000,
+      waitForAction: 1000,
+      show: testConfig.TestShowBrowserWindow,
+      waitForNavigation: "domcontentloaded",
+      headless: "true",
+      chrome: {
+        ignoreHTTPSErrors: true,
+        "ignore-certificate-errors": true,
+        defaultViewport: {
+          width: 1280,
+          height: 960,
         },
-        'PlaywrightHelper': {
-            'require': './helpers/PlaywrightHelper.js'
+        args: [
+          "--headless=new",
+          "--disable-gpu",
+          "--no-sandbox",
+          "--allow-running-insecure-content",
+          "--ignore-certificate-errors",
+          // '--proxy-server=proxyout.reform.hmcts.net:8080',
+          // '--proxy-bypass-list=*beta*LB.reform.hmcts.net',
+          "--window-size=1440,1400",
+        ],
+      },
+    },
+    PlaywrightHelper: {
+      require: "./helpers/PlaywrightHelper.js",
+    },
+    JSWait: {
+      require: "./helpers/JSWait.js",
+    },
+    Mochawesome: {
+      uniqueScreenshotNames: "true",
+    },
+  },
+  include: {
+    I: "./pages/steps.js",
+  },
+  plugins: {
+    autoDelay: {
+      enabled: testConfig.TestAutoDelayEnabled,
+    },
+    screenshotOnFail: {
+      enabled: true,
+      fullPageScreenshots: "true",
+    },
+  },
+  mocha: {
+    reporterOptions: {
+      "codeceptjs-cli-reporter": {
+        stdout: "-",
+        options: { steps: true },
+      },
+      "mocha-junit-reporter": {
+        stdout: "-",
+        options: {
+          mochaFile: `${testConfig.TestOutputDir}/result.xml`,
         },
-        'JSWait': {
-            require: './helpers/JSWait.js'
+      },
+      mochawesome: {
+        stdout: `${testConfig.TestOutputDir}/console.log`,
+        options: {
+          reportDir: testConfig.TestOutputDir,
+          reportName: "index",
+          inlineAssets: true,
+          overwrite: false,
+          html: true,
+          json: false,
         },
-        'Mochawesome': {
-            uniqueScreenshotNames: 'true'
-        }
+      },
     },
-    'include': {
-        'I': './pages/steps.js'
-    },
-    'plugins': {
-        'autoDelay': {
-            'enabled': testConfig.TestAutoDelayEnabled
-        },
-        screenshotOnFail: {
-            enabled: true,
-            fullPageScreenshots: 'true'
-        }
-    },
-    'mocha': {
-        'reporterOptions': {
-            'codeceptjs-cli-reporter': {
-                stdout: '-',
-                options: {steps: true}
-            },
-            'mocha-junit-reporter': {
-                stdout: '-',
-                options: {
-                    mochaFile: `${testConfig.TestOutputDir}/result.xml`
-                }
-            },
-            'mochawesome': {
-                stdout: `${testConfig.TestOutputDir}/console.log`,
-                options: {
-                    'reportDir': testConfig.TestOutputDir,
-                    'reportName': 'index',
-                    'inlineAssets': true
-                }
-            }
-        }
-    },
-    'name': 'Codecept Tests'
+  },
+  name: "Codecept Tests",
 };
