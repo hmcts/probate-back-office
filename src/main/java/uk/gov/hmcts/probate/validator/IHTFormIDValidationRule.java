746c1945e47957df9271bc359ff8d60bd1ed9ee0
Tom Saunders <tom.saunders@justice.gov.uk>
2025-07-15 13:50:40 +0100

    Updated test coverage for IHTForm validation.
    This now validates two different fields which can have slightly different values.

--- src/main/java/uk/gov/hmcts/probate/validator/IHTFormIDValidationRule.java
+++ src/main/java/uk/gov/hmcts/probate/validator/IHTFormIDValidationRule.java
@@ -12,14 +12,21 @@ import java.util.Locale;
 
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_APPLICABLE_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT400421_VALUE;
+import static uk.gov.hmcts.probate.model.Constants.YES;
 
 
 @Component
-@RequiredArgsConstructor
 public class IHTFormIDValidationRule {
 
     private final BusinessValidationMessageRetriever businessValidationMessageRetriever;
     private final ExceptedEstateDateOfDeathChecker exceptedEstateDateOfDeathChecker;
+
+    public IHTFormIDValidationRule(
+            final BusinessValidationMessageRetriever businessValidationMessageRetriever,
+            final ExceptedEstateDateOfDeathChecker exceptedEstateDateOfDeathChecker) {
+        this.businessValidationMessageRetriever = businessValidationMessageRetriever;
+        this.exceptedEstateDateOfDeathChecker = exceptedEstateDateOfDeathChecker;
+    }
 
     public void validate(CaseDetails caseDetails) {
         CaseData caseData = caseDetails.getData();
@@ -34,15 +41,18 @@ public class IHTFormIDValidationRule {
                         "IHTFormID is invalid: " + caseDetails.getId(), userMessageWelsh);
             }
         } else {
-            final String ihtFormEstate = caseData.getIhtFormEstate();
-            if (ihtFormEstate.equalsIgnoreCase(NOT_APPLICABLE_VALUE)
-            || ihtFormEstate.equalsIgnoreCase(IHT400421_VALUE)) {
-                String userMessage = businessValidationMessageRetriever
-                        .getMessage("ihtFormEstateInvalid", null, Locale.UK);
-                String userMessageWelsh = businessValidationMessageRetriever
-                        .getMessage("ihtFormEstateInvalidWelsh", null, Locale.UK);
-                throw new BusinessValidationException(userMessage,
-                        "IHTFormEstate is invalid: " + caseDetails.getId(), userMessageWelsh);
+            final String ihtFormEstateValuesCompleted = caseData.getIhtFormEstateValuesCompleted();
+            if (YES.equalsIgnoreCase(ihtFormEstateValuesCompleted)) {
+                final String ihtFormEstate = caseData.getIhtFormEstate();
+                if (NOT_APPLICABLE_VALUE.equalsIgnoreCase(ihtFormEstate)
+                        || IHT400421_VALUE.equalsIgnoreCase(ihtFormEstate)) {
+                    String userMessage = businessValidationMessageRetriever
+                            .getMessage("ihtFormEstateInvalid", null, Locale.UK);
+                    String userMessageWelsh = businessValidationMessageRetriever
+                            .getMessage("ihtFormEstateInvalidWelsh", null, Locale.UK);
+                    throw new BusinessValidationException(userMessage,
+                            "IHTFormEstate is invalid: " + caseDetails.getId(), userMessageWelsh);
+                }
             }
         }
     }
