2b111fa27d096a14f485bc921f8432d7dfd95380
Jay Ashton <jay.ashton@justice.gov.uk>
2025-07-24 10:41:40 +0100

    Remove secrets from config

---
+++ src/test/end-to-end/get-secrets.js
@@ -0,0 +1,5 @@
+import { populateSecrets } from "@hmcts/playwright-common";
+const vaultName = "probate-aat";
+const exampleEnvFilePath = "./src/test/end-to-end/.env.example";
+const envFilePath = "./src/test/end-to-end/.env";
+populateSecrets(vaultName, exampleEnvFilePath, envFilePath);
