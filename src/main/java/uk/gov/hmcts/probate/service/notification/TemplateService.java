4f78e8f4bad0895d61b1e002796d2e0c9660505a
Tom Saunders <tom.saunders@justice.gov.uk>
2025-08-01 14:02:38 +0100

    add handling for new notification template(s?).

--- src/main/java/uk/gov/hmcts/probate/service/notification/TemplateService.java
+++ src/main/java/uk/gov/hmcts/probate/service/notification/TemplateService.java
@@ -156,6 +156,26 @@ public class TemplateService {
             return emailTemplates.getDeclarationNotSignedExecutors();
         }
     }
+
+    public String getPostGrantIssueTemplateId(
+            final LanguagePreference languagePreference,
+            final ApplicationType applicationType) {
+        final EmailTemplates emailTemplates = notificationTemplates.getEmail()
+                .get(languagePreference)
+                .get(applicationType);
+
+        return emailTemplates.getPostGrantIssuedNotification();
+    }
+    public String getPostGrantIssueFailedTemplateId(
+            final LanguagePreference languagePreference,
+            final ApplicationType applicationType) {
+        final EmailTemplates emailTemplates = notificationTemplates.getEmail()
+                .get(languagePreference)
+                .get(applicationType);
+
+        return emailTemplates.getPostGrantIssuedFailedNotification();
+    }
+
 }
 
 
