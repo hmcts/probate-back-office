e66e0a03bc115f195be71bf9ac9b32bd15d583f6
Tom Saunders <tom.saunders@justice.gov.uk>
2025-08-05 11:07:34 +0100

    now should store responses.
    also adds handling for welsh dates, and begins to prep tests.

--- src/main/java/uk/gov/hmcts/probate/service/notification/TemplateService.java
+++ src/main/java/uk/gov/hmcts/probate/service/notification/TemplateService.java
@@ -166,6 +166,7 @@ public class TemplateService {
 
         return emailTemplates.getPostGrantIssuedNotification();
     }
+
     public String getPostGrantIssueFailedTemplateId(
             final LanguagePreference languagePreference,
             final ApplicationType applicationType) {
