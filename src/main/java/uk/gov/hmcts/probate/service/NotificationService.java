4b9b48c00fb5b46a7a7aec2e07151a6411fa643d
Tom Saunders <tom.saunders@justice.gov.uk>
2025-08-12 16:54:33 +0100

    fixup checkstyle issue.

--- src/main/java/uk/gov/hmcts/probate/service/NotificationService.java
+++ src/main/java/uk/gov/hmcts/probate/service/NotificationService.java
@@ -1045,7 +1045,7 @@ public class NotificationService {
 
         final SendEmailResponse response;
         try {
-             response = notificationClientService.sendEmail(
+            response = notificationClientService.sendEmail(
                     templateId,
                     recipientEmail,
                     personalisation,
