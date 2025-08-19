25b80b1155da1954b46360c372fd86272bbcf329
FeliTam <feli.tam@justice.gov.uk>
2025-08-19 09:29:46 +0100

    DTSPB-4856 update ccd def and mapping

--- src/main/java/uk/gov/hmcts/probate/model/ccd/raw/DeathRecord.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/raw/DeathRecord.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.probate.model.ccd.raw;
 
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.Alias;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Builder
@@ -14,5 +16,6 @@ public class DeathRecord {
     private final String sex;
     private final String address;
     private final LocalDate dateOfDeath;
+    private final List<Alias> aliases;
     private final String valid;
 }
