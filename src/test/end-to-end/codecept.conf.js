3fa693b7dd1a9c3bb7693d1fff69c67fc65322c2
marian.irudayanathan <marian.irudayanathan@justice.gov.uk>
2025-08-28 11:17:22 +0100

    DTSPB-4859: Update yarn.lock file for mochawsome updates

--- src/test/end-to-end/codecept.conf.js
+++ src/test/end-to-end/codecept.conf.js
@@ -69,6 +69,8 @@ exports.config = {
             'mochawesome': {
                 stdout: `${testConfig.TestOutputDir}/console.log`,
                 options: {
+                    reportDir: testConfig.TestOutputDir,
+                    reportName: 'index',
                     reportDir: './temp-reports',
                     inlineAssets: true,
                     overwrite: false,
