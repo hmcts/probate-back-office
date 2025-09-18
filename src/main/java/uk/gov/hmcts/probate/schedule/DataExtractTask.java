77e034a6c0feb1c36bbf46c9baa16bd2f606c253
FeliTam <feli.tam@justice.gov.uk>
2025-09-18 16:46:51 +0100

    DTSPB-4893 add adhocSchedulerJobToDate to DataExtractTask; add fetchSource to findAllCasesWithGrantIssuedDate

--- src/main/java/uk/gov/hmcts/probate/schedule/DataExtractTask.java
+++ src/main/java/uk/gov/hmcts/probate/schedule/DataExtractTask.java
@@ -23,36 +23,41 @@ public class DataExtractTask implements Runnable {
     private final DataExtractDateValidator dataExtractDateValidator;
     private final FeatureToggleService featureToggleService;
     private final DataExtractService dataExtractService;
-    public final String adHocJobDate;
+    public final String adHocJobFromDate;
+    public final String adHocJobToDate;
 
     public DataExtractTask(Clock clock,
                            DataExtractDateValidator dataExtractDateValidator,
                            FeatureToggleService featureToggleService,
                            DataExtractService dataExtractService,
-                           @Value("${adhocSchedulerJobDate}") String adHocJobDate) {
+                           @Value("${adhocSchedulerJobDate}") String adHocJobFromDate,
+                           @Value("${adhocSchedulerJobToDate}") String adHocJobToDate) {
         this.clock = clock;
         this.dataExtractDateValidator = dataExtractDateValidator;
         this.featureToggleService = featureToggleService;
         this.dataExtractService = dataExtractService;
-        this.adHocJobDate = adHocJobDate;
+        this.adHocJobFromDate = adHocJobFromDate;
+        this.adHocJobToDate = adHocJobToDate;
     }
 
     @Override
     public void run() {
         log.info("Scheduled task DataExtractTask started");
-        String date = DATE_FORMAT.format(LocalDate.now(clock).minusDays(1L));
-        if (StringUtils.isNotEmpty(adHocJobDate)) {
-            log.info("Running DataExtractTask with Adhoc dates {}", adHocJobDate);
-            date = adHocJobDate;
+        String fromDate = DATE_FORMAT.format(LocalDate.now(clock).minusDays(1L));
+        String toDate = fromDate;
+        if (StringUtils.isNotEmpty(adHocJobFromDate)) {
+            fromDate = adHocJobFromDate;
+            toDate = StringUtils.isNotEmpty(adHocJobToDate) ? adHocJobToDate : adHocJobFromDate;
+            log.info("Running DataExtractTask with with Adhoc dates {} {}", fromDate, toDate);
         }
-        log.info("Calling perform data extract from date, to date {} {}", date, date);
+        log.info("Calling perform data extract from date, to date {} {}", fromDate, toDate);
         try {
             if (!featureToggleService.isNfiDataExtractFeatureToggleOn()) {
                 log.info("NFI data extract feature is disabled, skipping task");
             } else {
-                dataExtractDateValidator.dateValidator(date, date);
+                dataExtractDateValidator.dateValidator(fromDate, toDate);
                 log.info("Perform NFI data extract from date started");
-                dataExtractService.performExtractForDateRange(date, date, NATIONAL_FRAUD_INITIATIVE);
+                dataExtractService.performExtractForDateRange(fromDate, toDate, NATIONAL_FRAUD_INITIATIVE);
                 log.info("Perform NFI data extract from date finished");
             }
         } catch (ApiClientException e) {
