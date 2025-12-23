f1c7e03aead02c113aa584b8a0660357fc0a8e04
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-12-23 14:37:16 +0000

    DTSPB-5137 check style fix

--- src/test/java/uk/gov/hmcts/probate/service/notification/CaseStopReasonHelperTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/notification/CaseStopReasonHelperTest.java
@@ -8,7 +8,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CaseStopReasonHelperTest {
     @Test
