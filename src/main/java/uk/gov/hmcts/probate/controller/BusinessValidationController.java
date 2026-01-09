cf8b9bcd3f2e4296c4207a488621fa942cc1786e
Tom Saunders <tom.saunders@justice.gov.uk>
2026-01-09 15:44:40 +0000

    rough changes to add metadata for callback.

--- src/main/java/uk/gov/hmcts/probate/controller/BusinessValidationController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/BusinessValidationController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
