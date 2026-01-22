ec196ce019e1c5c7025570dc6c460b5329f26065
SomeshRA <somesh.acharya1@hmcts.net>
2026-01-22 17:07:39 +0000

    DTSPB-5329: Change Probate XLS creation process to allow env specific json inclusions/exclusions

--- bin/build-release-ccd-definitions.sh
+++ bin/build-release-ccd-definitions.sh
@@ -8,4 +8,11 @@ if [[ ${environment} != "prod" && ${environment} != "aat" && ${environment} != "
   echo "Environment '${environment}' is not supported!"
   exit 1
 fi
-.././ccdImports/conversionScripts/createAllXLS-pipeline.sh probate-back-office-${1}.service.core-compute-${1}.internal ${1} ${2} aac-manage-case-assignment-${1}.service.core-compute-${1}.internal
+# Set up with non prod exclusion
+excludedFilenamePatterns="*-nonprod.json"
+
+if [[ ${environment} == "aat" && ${environment} == "demo" && ${environment} == "ithc" && ${environment} == "perftest" ]]; then
+  excludedFilenamePatterns="*-prod.json"
+fi
+echo "excludedFilenamePatterns $excludedFilenamePatterns"
+.././ccdImports/conversionScripts/createAllXLS-pipeline.sh probate-back-office-${1}.service.core-compute-${1}.internal ${1} ${2} aac-manage-case-assignment-${1}.service.core-compute-${1}.internal excludedFilenamePatterns
--- ccdImports/conversionScripts/createAllXLS-pipeline.sh
+++ ccdImports/conversionScripts/createAllXLS-pipeline.sh
@@ -7,6 +7,7 @@ configFolder=${conversionFolder}/../configFiles
 environment="$2"
 shutterOption=${3:-false}
 acc="$4"
+envExcludedFilenamePatterns="$5"
 
 if [ -z "$1" ]
   then
@@ -26,6 +27,9 @@ else
   echo Creating unshuttered CCD Definition
   excludedFilenamePatterns="-e *-shutter.json"
 fi
+
+excludedFilenamePatterns="$excludedFilenamePatterns  $envExcludedFilenamePatterns"
+
 echo excludedFilenamePatterns = $excludedFilenamePatterns
 
 export CCD_DEF_CASE_SERVICE_BASE_URL=$1
