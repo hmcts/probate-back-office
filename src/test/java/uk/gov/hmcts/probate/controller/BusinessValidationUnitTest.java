9b93fadc4cfa19f55d6df57ab972d5332be39d82
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-01-29 16:52:43 +0000

    DTSPB-5219: update config and code for GS4

--- src/test/java/uk/gov/hmcts/probate/controller/BusinessValidationUnitTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/BusinessValidationUnitTest.java
@@ -84,6 +84,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
