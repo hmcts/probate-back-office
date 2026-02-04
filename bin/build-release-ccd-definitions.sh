c56f8542776054d345393b54d83daa6f7e921c1e
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-02-04 10:44:06 +0000

    DTSPB-5329: updated exclusions

--- bin/build-release-ccd-definitions.sh
+++ bin/build-release-ccd-definitions.sh
@@ -3,16 +3,11 @@
 set -eu
 
 environment=${1:-prod}
+excludedFilenamePatterns=${2:-""}
 
 if [[ ${environment} != "prod" && ${environment} != "aat" && ${environment} != "demo" && ${environment} != "ithc" && ${environment} != "perftest" ]]; then
   echo "Environment '${environment}' is not supported!"
   exit 1
-fi
-
-excludedFilenamePatterns=""
-
-if [[ ${environment} == "prod" ]]; then
-  excludedFilenamePatterns="*-nonprod.json"
 fi
 
 .././ccdImports/conversionScripts/createAllXLS-pipeline.sh probate-back-office-${1}.service.core-compute-${1}.internal ${1} ${2} aac-manage-case-assignment-${1}.service.core-compute-${1}.internal $excludedFilenamePatterns
