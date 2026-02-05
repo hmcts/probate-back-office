b17bb775613b938fbe3eea1f29c35cca752a4b46
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-02-05 10:16:10 +0000

    DTSP-5329: rearranged jenkins xls generation3

--- ccdImports/conversionScripts/createAllXLS.sh
+++ ccdImports/conversionScripts/createAllXLS.sh
@@ -7,6 +7,11 @@ configFolder=${conversionFolder}/../configFiles
 shutterOption=${2:-false}
 extraExclusions=${3:-",*-wa.json"}
 
+
+if [ $WA_ENABLED == true ]; then
+  extraExclusions=""
+fi
+
 if [ -z "$1" ]
   then
     echo "Usage: ./ccdImports/conversionScripts/createAllXLS.sh CCD_DEF_CASE_SERVICE_BASE_URL CCD_DEF_AAC_URL"
@@ -29,10 +34,10 @@ echo CCD_DEF_AAC_URL=$CCD_DEF_AAC_URL
 echo using url = $CCD_DEF_CASE_SERVICE_BASE_URL
 
 ${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Backoffice/ "${excludedFilenamePatterns}"
-${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Caveat/ "${excludedFilenamePatterns}"
-${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Legacy_Cases/ "${excludedFilenamePatterns}"
-${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Legacy_Search/ "${excludedFilenamePatterns}"
-${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Will_Lodgement/ "${excludedFilenamePatterns}"
-${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Standing_Search/ "${excludedFilenamePatterns}"
+#${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Caveat/ "${excludedFilenamePatterns}"
+#${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Legacy_Cases/ "${excludedFilenamePatterns}"
+#${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Legacy_Search/ "${excludedFilenamePatterns}"
+#${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Will_Lodgement/ "${excludedFilenamePatterns}"
+#${conversionFolder}/convertJsonToXLS.sh ${configFolder}/CCD_Probate_Standing_Search/ "${excludedFilenamePatterns}"
 
 echo XLS files placed in /jsonToXLS folder
