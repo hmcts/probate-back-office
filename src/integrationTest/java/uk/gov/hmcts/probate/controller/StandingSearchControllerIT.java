3e958b8f465400ca8f81a094b0d05d3effc57a18
Mandeep Kapoor <mandeep.kapoor@hmcts.net>
2026-02-26 18:07:08 +0000

    Standing Search Supplementary Data related changes

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/StandingSearchControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/StandingSearchControllerIT.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.probate.util.TestUtils;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.probate.service.CcdSupplementaryDataService;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -34,6 +35,9 @@ class StandingSearchControllerIT {
 
     @MockitoBean
     private CoreCaseDataApi coreCaseDataApi;
+
+    @MockitoBean
+    private CcdSupplementaryDataService ccdSupplementaryDataService;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
