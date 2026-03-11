34cb46f0c6c289571d108637d5975628bf2fa6cb
Tom Saunders <tom.saunders@justice.gov.uk>
2026-03-11 11:55:57 +0000

    add logging.

--- src/main/java/uk/gov/hmcts/probate/service/ElasticSearchService.java
+++ src/main/java/uk/gov/hmcts/probate/service/ElasticSearchService.java
@@ -28,6 +28,7 @@ public class ElasticSearchService {
     private final HttpHeadersFactory headers;
 
     public MatchedCases runQuery(CaseType caseType, String jsonQuery) {
+        log.info("ElasticSearchService runQuery caseType: {} jsonQuery:\nv\n{}\n^", caseType, jsonQuery);
         log.debug("ElasticSearchService runQuery: " + jsonQuery);
         URI uri = UriComponentsBuilder
             .fromHttpUrl(ccdDataStoreAPIConfiguration.getHost() + ccdDataStoreAPIConfiguration.getCaseMatchingPath())
