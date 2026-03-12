eb0f0992973ff8f8c1e5d0841033cafddd52c6dc
Mandeep Kapoor <mandeep.kapoor@hmcts.net>
2026-03-12 19:01:37 +0000

    DTSPB-5389: Made changes to all controllers, ccd call to add data based on workAllocationToggleService.isProbateGSEnabled env variable

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/BusinessValidationControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/BusinessValidationControllerIT.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.util.ReflectionTestUtils;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
