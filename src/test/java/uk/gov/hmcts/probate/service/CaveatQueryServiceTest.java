a772388090f4aff8cac25702a2a1f69f42489830
Imran <Imran.Ali@justice.gov.uk>
2026-03-17 17:57:27 +0000

    checkstyletest fixed

--- src/test/java/uk/gov/hmcts/probate/service/CaveatQueryServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/CaveatQueryServiceTest.java
@@ -31,7 +31,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.probate.model.cases.CaseState.CAVEAT_NOT_MATCHED;
