6596f4825bd7a2c0a33ed08be05879676fc23c27
Tom Saunders <tom.saunders@justice.gov.uk>
2026-03-24 10:39:26 +0000

    Testing for CaseMatchingJsonService.

--- src/test/java/uk/gov/hmcts/probate/service/ElasticSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/ElasticSearchServiceTest.java
@@ -24,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
