b8b7b42de32b6fb21560ccd3739ecaf52dd0fc43
HBrac3 <harrycbrace@gmail.com>
2026-03-30 09:55:28 +0100

    DTSPB 5505: Remove link check

--- src/test/PlaywrightTest/Path/caseworker/pr_specific/globalsearch/createGrantOfProbateCases.spec.ts
+++ src/test/PlaywrightTest/Path/caseworker/pr_specific/globalsearch/createGrantOfProbateCases.spec.ts
@@ -8,34 +8,32 @@ import grantOfProbateCases from "../../../../Pages/globalSearchGrantOfProbateCas
 test.describe("Data Creation for Global Search Testing - Grant of Representation", () => {
     test("Data Creation for Global Search Testing - Grant of Representation @chromium", async ({ basePage, signInPage, createCasePage }) => {
         const scenarioName = "Data Creation for Global Search Testing - Grant of Representation";
-            
-        if (testConfig.TestBackOfficeUrl.includes("demo") || testConfig.TestBackOfficeUrl.includes("aat") ) {
-            await basePage.logInfo(scenarioName, "Login as Caseworker");
-            await signInPage.authenticateWithIdamIfAvailable(false);
 
-            for(const caseConfig of grantOfProbateCases) {
-                let nextStepName = "PA1P/PA1A/Solicitors Manual";
-                await basePage.logInfo(scenarioName, nextStepName);
-                await createCasePage.selectNewCase();
-                await createCasePage.selectCaseTypeOptions(
-                    createCaseConfig.list2_text_gor,
-                    createCaseConfig.list3_text_gor_manual
-                );
-                await createCasePage.enterGrantOfProbateManualPage1(
-                    "create",
-                    caseConfig
-                );
-                await createCasePage.enterGrantOfProbateManualPage2("createIHT400");
-                await createCasePage.enterIhtDetails(
-                    caseProgressConfig,
-                    caseProgressConfig.optionYes
-                );
-                await createCasePage.enterGrantOfProbateManualPage3(
-                    "create",
-                    caseConfig
-                );
-                await createCasePage.checkMyAnswers(nextStepName);
-            }
+        await basePage.logInfo(scenarioName, "Login as Caseworker");
+        await signInPage.authenticateWithIdamIfAvailable(false);
+
+        for (const caseConfig of grantOfProbateCases) {
+            let nextStepName = "PA1P/PA1A/Solicitors Manual";
+            await basePage.logInfo(scenarioName, nextStepName);
+            await createCasePage.selectNewCase();
+            await createCasePage.selectCaseTypeOptions(
+                createCaseConfig.list2_text_gor,
+                createCaseConfig.list3_text_gor_manual
+            );
+            await createCasePage.enterGrantOfProbateManualPage1(
+                "create",
+                caseConfig
+            );
+            await createCasePage.enterGrantOfProbateManualPage2("createIHT400");
+            await createCasePage.enterIhtDetails(
+                caseProgressConfig,
+                caseProgressConfig.optionYes
+            );
+            await createCasePage.enterGrantOfProbateManualPage3(
+                "create",
+                caseConfig
+            );
+            await createCasePage.checkMyAnswers(nextStepName);
         }
     });
 });
--- src/test/PlaywrightTest/Path/caseworker/pr_specific/globalsearch/createStandingSearchCases.spec.ts
+++ src/test/PlaywrightTest/Path/caseworker/pr_specific/globalsearch/createStandingSearchCases.spec.ts
@@ -7,32 +7,30 @@ import standingSearchCases from "../../../../Pages/globalSearchStandingSearchCas
 test.describe("Data Creation for Global Search Testing - Standing Search", () => {
     test("Data Creation for Global Search Testing  - Standing Search @chromium", async ({ basePage, signInPage, createCasePage }) => {
         const scenarioName = "Data Creation for Global Search Testing  - Standing Search";
-            
-        if (testConfig.TestBackOfficeUrl.includes("demo") || testConfig.TestBackOfficeUrl.includes("aat") ) {
-            await basePage.logInfo(scenarioName, "Login as Caseworker");
-            await signInPage.authenticateWithIdamIfAvailable(false);
 
-            for(const caseConfig of standingSearchCases) {
-                let nextStepName = "Create a standing search";
-                await basePage.logInfo(scenarioName, nextStepName);
-                await createCasePage.selectNewCase();
-                await createCasePage.selectCaseTypeOptions(
-                    createCaseConfig.list2_text_stan_search,
-                    createCaseConfig.list3_text_stan_search
-                );
-                await createCasePage.enterStandingSearchPage1("create");
-                await createCasePage.enterStandingSearchPage2(
-                    "create",
-                    caseConfig
-                );
+        await basePage.logInfo(scenarioName, "Login as Caseworker");
+        await signInPage.authenticateWithIdamIfAvailable(false);
 
-                await createCasePage.enterStandingSearchPage3(
-                    "create",
-                    caseConfig
-                );
-                await createCasePage.enterStandingSearchPage4("create");
-                await createCasePage.checkMyAnswers(nextStepName);
-            }
+        for (const caseConfig of standingSearchCases) {
+            let nextStepName = "Create a standing search";
+            await basePage.logInfo(scenarioName, nextStepName);
+            await createCasePage.selectNewCase();
+            await createCasePage.selectCaseTypeOptions(
+                createCaseConfig.list2_text_stan_search,
+                createCaseConfig.list3_text_stan_search
+            );
+            await createCasePage.enterStandingSearchPage1("create");
+            await createCasePage.enterStandingSearchPage2(
+                "create",
+                caseConfig
+            );
+
+            await createCasePage.enterStandingSearchPage3(
+                "create",
+                caseConfig
+            );
+            await createCasePage.enterStandingSearchPage4("create");
+            await createCasePage.checkMyAnswers(nextStepName);
         }
     });
 });
