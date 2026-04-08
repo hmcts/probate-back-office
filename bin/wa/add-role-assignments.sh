9148b20c133df23b5e260a4ba5e394baa6e03bc6
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-04-08 13:42:35 +0100

    DTSPB-5097: setup role assigments on preview

---
+++ bin/wa/add-role-assignments.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eu
+
+dir=$(dirname ${0})
+
+#${dir}/utils/add-system-user-case-allocator-role.sh
+#${dir}/utils/add-hearing-system-user.sh
+
+jq -c '(.[])' preview-am-role-assignments.json | while read user; do
+  email=$(jq -r '.email' <<< $user)
+  idamId=$(jq -r '.id' <<< $user)
+  password=Monday01
+
+  jq -c '(.roleAssignments[])' <<< $user | while read assignment; do
+    roleName=$(jq -r '.roleName' <<< $assignment)
+    roleCategory=$(jq -r '.roleCategory' <<< $assignment)
+    classification=$(jq -r '.classification' <<< $assignment)
+    grantType=$(jq -r '.grantType' <<< $assignment)
+    readOnly=$(jq -r '.readOnly' <<< $assignment)
+    attributes=$(jq -r '.attributes | tostring' <<< $assignment)
+
+    authorisations=$(jq -r '.authorisations | tostring' <<< $assignment)
+
+    echo "Creating '${roleName}' assignment for user ${email}"
+    ${dir}/utils/organisational-role-assignment.sh $email $password $classification $roleName $attributes $roleCategory $authorisations $grantType
+  done
+  echo
+done
