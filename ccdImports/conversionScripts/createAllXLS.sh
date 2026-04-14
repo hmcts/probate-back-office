f1349f7bea39e6b37501d6df00373c5df3c85102
Mandeep Kapoor <mandeep.kapoor@hmcts.net>
2026-04-14 15:41:11 +0100

    DTSPB-5566 : Updating the Probate GOP Global Search Specific Files extension to GS from WA and updated the script to add GS exclusion when feature is enabled

--- ccdImports/conversionScripts/createAllXLS.sh
+++ ccdImports/conversionScripts/createAllXLS.sh
@@ -5,12 +5,39 @@ set -eu
 conversionFolder=$(dirname "$0")
 configFolder=${conversionFolder}/../configFiles
 shutterOption=${2:-false}
-extraExclusions=${3:-",*-wa.json"}
+extraExclusions=${3:-}
 
 waEnabledVar=${PROBATE_WA_ENABLED:-false}
-if [ ${waEnabledVar} == true ]; then
-  extraExclusions=""
+gsEnabledVar=${PROBATE_GS_ENABLED:-false}
+
+append_exclusion() {
+  if [ -z "$extraExclusions" ]; then
+    extraExclusions="$1"
+  else
+    extraExclusions="${extraExclusions},$1"
+  fi
+}
+
+echo "[INFO] Initial extraExclusions: $extraExclusions"
+
+# WA flag
+if [[ "${waEnabledVar}" != true ]]; then
+  echo "[INFO] WA feature is DISABLED adding *-wa.json to exclusions"
+  append_exclusion "*-wa.json"
+else
+  echo "[INFO] WA feature is ENABLED no exclusion added"
 fi
+
+# GS flag
+if [[ "${gsEnabledVar}" != true ]]; then
+  echo "[INFO] GS feature is DISABLED adding *-gs.json to exclusions"
+  append_exclusion "*-gs.json"
+else
+  echo "[INFO] GS feature is ENABLED no exclusion added"
+fi
+
+echo "[INFO] Final extraExclusions: $extraExclusions"
+
 
 if [ -z "$1" ]
   then
