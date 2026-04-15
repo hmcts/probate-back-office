007e5e0f6021ad075f068b0ed544b647d3881230
sadaque-khan-hmcts <Sadaque.Khan@justice.gov.uk>
2026-04-15 22:20:04 +0100

    [DTSPB-5228] fix: resolve Checkstyle line length violations in NotificationServiceIT

--- src/integrationTest/java/uk/gov/hmcts/probate/service/NotificationServiceIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/service/NotificationServiceIT.java
@@ -1270,13 +1270,25 @@ class NotificationServiceIT {
 
         HashMap<String, String> personalisation = new HashMap<>();
         personalisation.put(PERSONALISATION_APPLICANT_NAME, SOLS_CAVEATS_NAME);
-        personalisation.put(PERSONALISATION_DECEASED_NAME, solicitorCaveatRaisedCaseDataWelsh.getData().getDeceasedFullName());
+        personalisation.put(
+            PERSONALISATION_DECEASED_NAME,
+            solicitorCaveatRaisedCaseDataWelsh.getData().getDeceasedFullName()
+        );
         personalisation.put(PERSONALISATION_CCD_REFERENCE, solicitorCaveatRaisedCaseDataWelsh.getId().toString());
-        personalisation.put(PERSONALISATION_MESSAGE_CONTENT, solicitorCaveatRaisedCaseDataWelsh.getData().getMessageContent());
+        personalisation.put(
+            PERSONALISATION_MESSAGE_CONTENT,
+            solicitorCaveatRaisedCaseDataWelsh.getData().getMessageContent()
+        );
         personalisation.put(PERSONALISATION_REGISTRY_NAME, "CTSC");
         personalisation.put(PERSONALISATION_REGISTRY_PHONE, "0300 303 0648");
-        personalisation.put(PERSONALISATION_CAVEATOR_NAME, solicitorCaveatRaisedCaseDataWelsh.getData().getCaveatorFullName());
-        personalisation.put(PERSONALISATION_SOLICITOR_REFERENCE, solicitorCaveatRaisedCaseDataWelsh.getData().getSolsSolicitorAppReference());
+        personalisation.put(
+            PERSONALISATION_CAVEATOR_NAME,
+            solicitorCaveatRaisedCaseDataWelsh.getData().getCaveatorFullName()
+        );
+        personalisation.put(
+            PERSONALISATION_SOLICITOR_REFERENCE,
+            solicitorCaveatRaisedCaseDataWelsh.getData().getSolsSolicitorAppReference()
+        );
         personalisation.put(PERSONALISATION_CAVEAT_EXPIRY_DATE, "1st January 2019");
         personalisation.put(PERSONALISATION_WELSH_CAVEAT_EXPIRY_DATE, "1 Ionawr 2019");
         personalisation.put(PERSONALISATION_DATE_OF_DEATH, "12th December 2000");
