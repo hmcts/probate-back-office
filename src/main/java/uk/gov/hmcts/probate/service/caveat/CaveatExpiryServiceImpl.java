cee694b49e1ae44939541737ac96836453b0aa9c
Imran <Imran.Ali@justice.gov.uk>
2026-04-15 17:24:07 +0100

    changes to how submit and validation is done

--- src/main/java/uk/gov/hmcts/probate/service/caveat/CaveatExpiryServiceImpl.java
+++ src/main/java/uk/gov/hmcts/probate/service/caveat/CaveatExpiryServiceImpl.java
@@ -4,21 +4,24 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.exception.BusinessValidationException;
 import uk.gov.hmcts.probate.exception.ConcurrentDataUpdateException;
-import uk.gov.hmcts.probate.model.ccd.CcdCaseType;
+import uk.gov.hmcts.probate.exception.NotFoundException;
 import uk.gov.hmcts.probate.model.ccd.caveat.request.ReturnedCaveatDetails;
 import uk.gov.hmcts.probate.model.ccd.EventId;
 import uk.gov.hmcts.probate.security.SecurityDTO;
 import uk.gov.hmcts.probate.security.SecurityUtils;
 import uk.gov.hmcts.probate.service.CaveatExpiryService;
 import uk.gov.hmcts.probate.service.CaveatQueryService;
-import uk.gov.hmcts.probate.service.ccd.CcdClientApi;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.probate.model.cases.CaseState;
 
 import java.time.Clock;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -37,13 +40,12 @@ public class CaveatExpiryServiceImpl implements CaveatExpiryService {
     private static final String EVENT_DESCRIPTOR = "Caveat Auto Expired";
 
     private final CaveatQueryService caveatQueryService;
-    private final CcdClientApi ccdClientApi;
     private final CoreCaseDataApi coreCaseDataApi;
     private final SecurityUtils securityUtils;
     private final Clock clock;
     @Value("${data-extract.pagination.size}")
     protected int dataExtractPaginationSize;
-    private static final Set<String> ALLOWED_EXPIRY_EVENTS = Set.of(
+    private static final Set<String> ALLOWED_EXPIRY_STATES = Set.of(
             CAVEAT_EXPIRED_FOR_AWAITING_RESOLUTION.getName(),
             CAVEAT_EXPIRED_FOR_CAVEAT_NOT_MATCHED.getName(),
             CAVEAT_EXPIRED_FOR_WARNNG_VALIDATION.getName(),
@@ -74,10 +76,6 @@ public class CaveatExpiryServiceImpl implements CaveatExpiryService {
 
     private void expireCaveat(ReturnedCaveatDetails caveat, SecurityDTO securityDTO, List<String> failedCases) {
         EventId eventId = getEventIdForCaveatToExpireGivenPreconditionState(caveat.getState());
-        uk.gov.hmcts.reform.probate.model.cases.caveat.CaveatData caseData =
-                uk.gov.hmcts.reform.probate.model.cases.caveat.CaveatData.builder()
-                        .autoClosedExpiry(Boolean.TRUE)
-                        .build();
         StartEventResponse startEventResponse = coreCaseDataApi.startEventForCaseWorker(
                 securityDTO.getAuthorisation(),
                 securityDTO.getServiceAuthorisation(),
@@ -87,31 +85,82 @@ public class CaveatExpiryServiceImpl implements CaveatExpiryService {
                 caveat.getId().toString(),
                 eventId.getName()
         );
-        String actualEventId = startEventResponse.getEventId();
-        LocalDateTime actualLastModified = startEventResponse.getCaseDetails().getLastModified();
-        boolean modifiedWithinLast24Hours = actualLastModified.isAfter(LocalDateTime.now(clock).minusDays(1));
 
-        if (!ALLOWED_EXPIRY_EVENTS.contains(actualEventId) || modifiedWithinLast24Hours) {
-            throw new ConcurrentDataUpdateException(String.format("caveatId: %s not updated due to different "
-                            + "eventId. actualEventId: %s, lastModified: %s",
-                    caveat.getId(), actualEventId, actualLastModified));
-        }
         try {
-            ccdClientApi.updateCaseAsCaseworker(
-                    CcdCaseType.CAVEAT,
-                    String.valueOf(caveat.getId()),
-                    caveat.getLastModified(),
-                    caseData,
-                    eventId,
-                    securityDTO,
-                    EVENT_DESCRIPTOR,
-                    EVENT_DESCRIPTOR
+            validateStateForExpiry(startEventResponse, caveat.getId());
+            checkExpiryDate(startEventResponse, caveat.getId());
+
+            startEventResponse.getCaseDetails().getData().put("autoClosedExpiry", true);
+            CaseDataContent caseDataContent = CaseDataContent.builder()
+                    .event(Event.builder()
+                            .id(startEventResponse.getEventId())
+                            .summary(EVENT_DESCRIPTOR)
+                            .description(EVENT_DESCRIPTOR)
+                            .build())
+                    .eventToken(startEventResponse.getToken())
+                    .data(startEventResponse.getCaseDetails().getData())
+                    .build();
+            coreCaseDataApi.submitEventForCaseWorker(
+                securityDTO.getAuthorisation(),
+                securityDTO.getServiceAuthorisation(),
+                securityDTO.getUserId(),
+                PROBATE.name(),
+                "Caveat",
+                caveat.getId().toString(),
+                false,
+                caseDataContent
             );
             log.info("Caveat autoExpired: {}", caveat.getId());
-
         } catch (RuntimeException e) {
             log.info("Caveat autoExpire failure for case: {}, due to {}", caveat.getId(), e.getMessage());
             failedCases.add(String.valueOf(caveat.getId()));
+        }
+    }
+
+    private void validateStateForExpiry(StartEventResponse startEventResponse, Long caveatId) {
+        final String state = startEventResponse.getCaseDetails().getState();
+        if (!ALLOWED_EXPIRY_STATES.contains(state)) {
+            throw new ConcurrentDataUpdateException(
+                    String.format(
+                            "caveatId: %s not updated due to different eventId. actualEventId: %s, lastModified: %s",
+                            caveatId,
+                            startEventResponse.getEventId(),
+                            startEventResponse.getCaseDetails().getLastModified()
+                    )
+            );
+        }
+    }
+
+    private void checkExpiryDate(StartEventResponse startEventResponse, Long caveatId) {
+        final boolean hasExpiry = startEventResponse.getCaseDetails().getData().containsKey("expiryDate");
+        if (!hasExpiry) {
+            throw new NotFoundException(
+                    String.format("Caveat ID: %s has no expiryDate", caveatId)
+            );
+        }
+
+        final Object expiryDateObj = startEventResponse.getCaseDetails().getData().get("expiryDate");
+        if (!(expiryDateObj instanceof String)) {
+            throw new IllegalArgumentException(
+                    String.format("Expiry date object: %s is not a string", expiryDateObj)
+            );
+        }
+
+        final String expiryDateStr = (String) expiryDateObj;
+        final LocalDate expiryDate;
+        try {
+            expiryDate = LocalDate.parse(expiryDateStr);
+        } catch (DateTimeParseException e) {
+            throw new BusinessValidationException(
+                    String.format("Unable to parse expiry date: %s (expecting yyyy-MM-dd format)", expiryDateStr),
+                    e.getMessage()
+            );
+        }
+
+        if (expiryDate.isAfter(LocalDate.now(clock))) {
+            throw new IllegalArgumentException(
+                    String.format("Caveat expiryDate: %s cannot be in the future", expiryDate)
+            );
         }
     }
 
