9c2d0df6700fdebab638056603ddff7cf849fe5a
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2026-04-30 17:04:39 +0100

    DTSPB-5574 Fix tests

--- src/test/java/uk/gov/hmcts/probate/service/template/pdf/caseextra/decorator/SolicitorLegalStatementPDFDecoratorTest.java
+++ src/test/java/uk/gov/hmcts/probate/service/template/pdf/caseextra/decorator/SolicitorLegalStatementPDFDecoratorTest.java
@@ -48,8 +48,7 @@ class SolicitorLegalStatementPDFDecoratorTest {
     }
 
     @Test
-    void shouldDecorateWithEnglishAndWelshDescriptionsWhenExecutorsPresent() {
-        when(caseDataMock.getSolsWillType()).thenReturn("NoWill");
+    void shouldDecorateWithEnglishAndWelshDescriptionsWhenOneExecutorPresent() {
         List<CollectionMember<AdditionalExecutorApplying>> executors = new ArrayList<>();
 
         AdditionalExecutorApplying executor1 = AdditionalExecutorApplying.builder()
@@ -58,24 +57,54 @@ class SolicitorLegalStatementPDFDecoratorTest {
                         .relationshipToDeceased("child").build())
                 .build();
         executors.add(new CollectionMember<>("1", executor1));
+        caseDataMock = CaseData.builder().executorsApplyingLegalStatement(executors)
+                .solsApplicantRelationshipToDeceased("child").solsWillType("NoWill").build();
+        when(ihtEstateNotCompletedBusinessRule.isApplicable(caseDataMock)).thenReturn(false);
+        String caseExtraJson
+                = "{\"englishCoApplicantDescriptions\" : \"Joh Doe is the child\",\"welshCoApplicantDescriptions\" : "
+                + "\"John Doe yw plentyn\"}";
+        when(caseExtraDecorator.decorate(any(IntestacyMultipleApplicantsCaseExtra.class))).thenReturn(caseExtraJson);
+        when(caseExtraDecorator.combineDecorations("", caseExtraJson)).thenReturn(caseExtraJson);
+        String actual = solicitorLegalStatementPDFDecorator.decorate(caseDataMock);
+        assertEquals(caseExtraJson, actual);
+    }
+
+    @Test
+    void shouldDecorateWithEnglishAndWelshDescriptionsWhenMoreExecutorPresent() {
+        List<CollectionMember<AdditionalExecutorApplying>> executors = new ArrayList<>();
+
+        AdditionalExecutorApplying executor1 = AdditionalExecutorApplying.builder()
+                .applyingExecutorName("John Doe")
+                .applicantFamilyDetails(ApplicantFamilyDetails.builder()
+                        .relationshipToDeceased("child").build())
+                .build();
         AdditionalExecutorApplying executor2 = AdditionalExecutorApplying.builder()
                 .applyingExecutorName("John Doe1")
                 .applicantFamilyDetails(ApplicantFamilyDetails.builder()
-                        .relationshipToDeceased("grandchild").build())
+                        .relationshipToDeceased("child").build())
                 .build();
         executors.add(new CollectionMember<>("1", executor1));
         executors.add(new CollectionMember<>("2", executor2));
-        when(caseExtraDecorator.decorate(any())).thenReturn("decoratedJson");
-
+        caseDataMock = CaseData.builder().executorsApplyingLegalStatement(executors)
+                .solsApplicantRelationshipToDeceased("child").solsWillType("NoWill").build();
+        when(ihtEstateNotCompletedBusinessRule.isApplicable(caseDataMock)).thenReturn(false);
+        String caseExtraJson
+                = "{\"englishCoApplicantDescriptions\" : \"Joh Doe is the child\",\"welshCoApplicantDescriptions\" : "
+                + "\"John Doe yw plentyn\"}";
+        when(caseExtraDecorator.decorate(any(IntestacyMultipleApplicantsCaseExtra.class))).thenReturn(caseExtraJson);
+        when(caseExtraDecorator.combineDecorations("", caseExtraJson)).thenReturn(caseExtraJson);
         String actual = solicitorLegalStatementPDFDecorator.decorate(caseDataMock);
-
-        assertEquals("decoratedJson", actual);
+        assertEquals(caseExtraJson, actual);
     }
 
     @Test
     void shouldDecorateWithEmptyDescriptionsWhenExecutorsListIsNull() {
         when(caseDataMock.getSolsWillType()).thenReturn("NoWill");
         when(caseDataMock.getExecutorsApplyingLegalStatement()).thenReturn(null);
+        when(caseExtraDecorator.decorate(any(IntestacyMultipleApplicantsCaseExtra.class)))
+                .thenReturn("decoratedJson");
+        when(caseExtraDecorator.combineDecorations("", "decoratedJson"))
+                .thenReturn("decoratedJson");
         when(caseExtraDecorator.decorate(any())).thenReturn("decoratedJson");
 
         String actual = solicitorLegalStatementPDFDecorator.decorate(caseDataMock);
