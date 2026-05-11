329d11dd41b46a3ed0a9c080a3ae81f87b54274d
Imran <Imran.Ali@justice.gov.uk>
2026-05-11 13:05:14 +0100

    DTSPB-5186 replacement log

--- src/main/java/uk/gov/hmcts/probate/service/zip/ZipFileService.java
+++ src/main/java/uk/gov/hmcts/probate/service/zip/ZipFileService.java
@@ -256,8 +256,17 @@ public class ZipFileService {
             errorDescription = "Exception adding file from case id: " + caseDetails.getId().toString()
                     + " document id: " + documentId;
             zippedManifestData.setErrorDescription(errorDescription);
-            log.info(errorDescription);
-            log.error("Error while adding file ", e);
+            log.error("Failed to fetchAndUploadDocument. "
+                            + "Case reference: {}, "
+                            + "Document name: {}, "
+                            + "CDAM id: {}, "
+                            + "Exception: {}",
+                    caseDetails.getId().toString(),
+                    zippedManifestData.getDocumentName(),
+                    documentId,
+                    e.getMessage(),
+                    e
+            );
         }
         manifestDataList.add(zippedManifestData);
     }
