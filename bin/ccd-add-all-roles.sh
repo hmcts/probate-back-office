ad0fbf3933bc3e3b430390243ed09a7365ac93df
Mandeep Kapoor <mandeep.kapoor@hmcts.net>
2026-05-21 13:36:26 +0100

    DTSPB-5229 : Adding the HMCTS Legal Operations roles

--- bin/ccd-add-all-roles.sh
+++ bin/ccd-add-all-roles.sh
@@ -45,6 +45,6 @@ export SERVICE_TOKEN_ENV=$(${binFolder}/idam-lease-service-token.sh ccd_gw $(doc
 (${binFolder}/ccd-add-role.sh caseworker-approver)
 (${binFolder}/ccd-add-role.sh TTL_profile)
 (${binFolder}/ccd-add-role.sh GS_profile)
-(${binFolder}/ccd-add-role.sh hmcts-legal-operations
+(${binFolder}/ccd-add-role.sh hmcts-legal-operations)
 
 
