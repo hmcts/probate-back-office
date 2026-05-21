91129f70e26c836cdca66cdee4417d036764898e
FeliTam <feli.tam@justice.gov.uk>
2026-05-21 12:09:32 +0100

    DTSPB-5472 checkstyle

--- src/test/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseDataTest.java
+++ src/test/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseDataTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.probate.model.ccd.raw.request;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
