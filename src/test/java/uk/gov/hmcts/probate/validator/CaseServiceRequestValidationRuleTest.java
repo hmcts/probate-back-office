e5e0cd877a5c823dacda38acbdc2f3c6b591d141
FeliTam <feli.tam@justice.gov.uk>
2026-06-01 09:25:08 +0100

    DTSPB-5593 sonar issue

--- src/test/java/uk/gov/hmcts/probate/validator/CaseServiceRequestValidationRuleTest.java
+++ src/test/java/uk/gov/hmcts/probate/validator/CaseServiceRequestValidationRuleTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
