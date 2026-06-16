b14c7dd56150a7367f87c9fe490cc24331a20909
scott.christie <s.christie496@gmail.com>
2026-06-16 14:33:38 +0100

    Reapply "change process category identifier tests"
    This reverts commit 024a2a8495f5c3adec0ec9b252567958aec86450.

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaInitiationTest.java
@@ -57,7 +57,7 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
                 "taskId", EXAMINE_DIGITAL_CASE_PROBATE,
                 "name", "Examine Digital Case - Probate",
                 "workingDaysAllowed", 10,
-                "processCategories", "case progression",
+                "processCategories", "DUMMY_PLACEHOLDER_IDENTIFIER",
                 "workType", ROUTINE_WORK_TYPE,
                 "roleCategory", ROLE_CATEGORY_CTSC
         );
