e57a2a3f65552a558776b88bc62aeafc750be09a
mandeepkapoor <mandeep.kapoor@hmcts.net>
2026-06-30 13:57:19 +0100

    DTSPB-5539 - Added custom migration handlers for GOR and Caveat

--- src/main/java/uk/gov/hmcts/probate/service/migration/Dtspb5539CaveatMigration.java
+++ src/main/java/uk/gov/hmcts/probate/service/migration/Dtspb5539CaveatMigration.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.probate.service.migration;
 
 import org.json.JSONObject;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.model.ccd.caveat.request.CaveatCallbackRequest;
 
+@Component
 public class Dtspb5539CaveatMigration implements CaveatMigrationHandler {
     @Override
     public CaveatCallbackRequest migrate(CaveatCallbackRequest callbackRequest, JSONObject migrationData) {
--- src/main/java/uk/gov/hmcts/probate/service/migration/Dtspb5539GORMigration.java
+++ src/main/java/uk/gov/hmcts/probate/service/migration/Dtspb5539GORMigration.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.probate.service.migration;
 
 import org.json.JSONObject;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
 
+@Component
 public class Dtspb5539GORMigration implements GorMigrationHandler {
     @Override
     public CallbackRequest migrate(CallbackRequest callbackRequest, JSONObject migrationData) {
