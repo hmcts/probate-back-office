f1fbc7b1d36d3731b9fe522227624e3dc761b085
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2026-07-07 11:50:53 +0100

    DTSPB-5560: Move ExamineFiatWill permissions to separate rows

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
@@ -121,7 +121,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
         assertThat(logic.getOutputs().size(), is(7));
         assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
         //Rules
-        assertThat(logic.getRules().size(), is(6));
+        assertThat(logic.getRules().size(), is(8));
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
