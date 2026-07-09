959591ea7dcd58ed4a63d0f1920f404de48398ff
scott.christie <s.christie496@gmail.com>
2026-07-09 09:55:43 +0100

    rm unused import

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.probate.DmnDecisionTableBaseUnitTest;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
