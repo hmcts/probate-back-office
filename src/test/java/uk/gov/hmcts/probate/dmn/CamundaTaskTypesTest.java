e16abd28cdca4526bca6d0c0b3d16d6661a484f4
scott.christie <s.christie496@gmail.com>
2026-07-09 09:09:37 +0100

    resolve task types

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
@@ -44,8 +44,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
                 Map.of(
                         "taskTypeName", "Examine Digital Case - Intestacy",
                         "taskTypeId", EXAMINE_DIGITAL_CASE_INTESTACY
-                ),
-                Collections.emptyMap()
+                )
         );
         return Stream.of(
                 Arguments.of(
@@ -60,7 +59,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(4));
+        assertThat(logic.getRules().size(), is(3));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
