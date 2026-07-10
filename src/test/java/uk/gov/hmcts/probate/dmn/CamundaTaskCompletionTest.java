ecfbb9bc7dd2ac2add086618af3521217c87c090
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2026-07-10 13:34:34 +0100

    fix test

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
@@ -128,7 +128,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(6));
+        assertThat(logic.getRules().size(), is(7));
     }
 
     @ParameterizedTest(name = "event id: {0}")
