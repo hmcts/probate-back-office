58f416f716d4635c2529a6e723c7bacab41f2ff6
Scott Christie <103422729+scottiee-jc@users.noreply.github.com>
2026-07-13 12:19:05 +0100

    DTSPB-5707 - 1A Awaiting Documentation: Examine Digital Case Probate: PA1P/PA1A/Solicitors Manual  (#3901)
    * wa task initiation and cancellation rules inserted for PA1P/PA1A/Solicitors Manual event and withdraw application event
    * wa configuration of fields related to PA1P/PA1A/Solicitors Manual event

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaInitiationTest.java
@@ -123,6 +123,30 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
                         "CasePrinted",
                         additionalDataHandOffListNotEmpty,
                         Collections.emptyList()
+                ), 
+                Arguments.of(
+                  "applyforGrantPaperApplicationMan",
+                        "CasePrinted",
+                        additionalData,
+                        List.of(examineDigitalCaseProbateTaskAttributes)
+                ),
+                Arguments.of(
+                        "applyforGrantPaperApplicationMan",
+                        "CasePrinted",
+                        additionalDataEvidenceHandledTrue,
+                        Collections.emptyList()
+                ),
+                Arguments.of(
+                        "applyforGrantPaperApplicationMan",
+                        "CasePrinted",
+                        additionalDataCaseTypeOther,
+                        Collections.emptyList()
+                ),
+                Arguments.of(
+                        "applyforGrantPaperApplicationMan",
+                        "CasePrinted",
+                        additionalDataHandOffListNotEmpty,
+                        Collections.emptyList()
                 )
         );
     }
@@ -133,7 +157,7 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(5));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(4));
+        assertThat(logic.getRules().size(), is(5));
     }
 
     @ParameterizedTest(name = "event id: {0} post event state: {1} evidenceHandled: {2} caseType: {3}")
--- src/test/java/uk/gov/hmcts/probate/dmnutils/CancellationScenarioBuilder.java
+++ src/test/java/uk/gov/hmcts/probate/dmnutils/CancellationScenarioBuilder.java
@@ -1,45 +1,75 @@
 package uk.gov.hmcts.probate.dmnutils;
 
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
-public class CancellationScenarioBuilder {
+public class CancellationScenarioBuilder implements ArgumentsProvider {
 
-    private final String event;
-    private final List<Map<String,String>> results = new ArrayList<>();
-
-    private CancellationScenarioBuilder(String event) {
-        this.event = event;
+    public Map<String,String> cancelWithProperties(String eventId,
+                                                   String fromState,
+                                                   String toState, String processCategories, String action) {
+        return Map.of(
+                "event", eventId,
+                "fromState", fromState,
+                "state", toState,
+                "action", action,
+                "processCategories", processCategories
+        );
     }
 
-    public static CancellationScenarioBuilder event(String event) {
-        return new CancellationScenarioBuilder(event);
+    public Map<String,String> invalidEventOrStateEntryMap(String eventId,
+                                                   String fromState,
+                                                   String toState) {
+        return Map.of(
+                "event", eventId,
+                "fromState", fromState,
+                "state", toState
+        );
     }
 
-    public CancellationScenarioBuilder cancel(String processCategories) {
-        results.add(Map.of(
-            "action", "Cancel",
-            "processCategories", processCategories
-        ));
-        return this;
-    }
-
-    public CancellationScenarioBuilder cancelAll() {
-        results.add(Map.of(
-            "action", "Cancel"
-        ));
-        return this;
-    }
-
-    public Arguments build() {
-        return Arguments.of(
-            null,
-            event,
-            null,
-            results
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+        return Stream.of(
+                Arguments.of(
+                    cancelWithProperties(
+                            "boWithdrawApplicationForCasePrinted",
+                            "CasePrinted",
+                            "BOCaseClosed",
+                            "case progression",
+                            "Cancel"
+                    )
+                ),
+                // Withdraw application from CasePrinted ("Awaiting documentation") cancels no tasks
+                // on the case when there is an invalid event
+                Arguments.of(
+                    invalidEventOrStateEntryMap(
+                            "someInvalidEvent",
+                            "CasePrinted",
+                            "BOCaseClosed"
+                    )
+                ),
+                // Withdraw application from CasePrinted ("Awaiting documentation") cancels no tasks
+                // on the case when fromState is the wrong state
+                Arguments.of(
+                        invalidEventOrStateEntryMap(
+                        "boWithdrawApplicationForCasePrinted",
+                        "BOCaseClosed",
+                        "BOCaseClosed"
+                        )
+                ),
+                // Withdraw application from CasePrinted ("Awaiting documentation") cancels no tasks
+                // on the case when toState is a different state
+                Arguments.of(
+                        invalidEventOrStateEntryMap(
+                        "boWithdrawApplicationForCasePrinted",
+                        "CasePrinted",
+                        "CasePrinted"
+                        )
+                )
         );
     }
 }
