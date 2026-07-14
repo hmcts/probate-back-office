2e8ce72f4d8fd5fbeeab8ab5536dd270f0ab58fe
Mike Brown <michael.brown@hmcts.net>
2026-07-14 14:12:33 +0100

    permissions count test updated

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
@@ -97,7 +97,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
         assertThat(logic.getOutputs().size(), is(7));
         assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
         //Rules
-        assertThat(logic.getRules().size(), is(4));
+        assertThat(logic.getRules().size(), is(6));
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
