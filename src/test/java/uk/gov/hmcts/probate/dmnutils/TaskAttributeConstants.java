5707aa674f511ebf46863e10af1fd659bc2618f4
scott.christie <s.christie496@gmail.com>
2026-07-22 12:09:51 +0100

    add whitespaces back

--- src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
+++ src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
@@ -86,26 +86,26 @@ public class TaskAttributeConstants {
     public static final String DUE_DATE_TIME_VALUE = "16:00";
     public static final String DESCRIPTION_EXAMINE_DIGITAL_CASE_PROBATE_DEFAULT_VALUE =
             "[Amend Case Details](/cases/case-details/"
-            + "${[CASE_REFERENCE]}/trigger/boAmendCaseDetails) "
+            + "${[CASE_REFERENCE]}/trigger/boAmendCaseDetails)  "
             + "[Issue Grant](/cases/case-details/"
-            + "${[CASE_REFERENCE]}/trigger/boIssueGrantForCaseMatching) "
+            + "${[CASE_REFERENCE]}/trigger/boIssueGrantForCaseMatching)  "
             + "[Escalate to Registrar](/cases/case-details/"
-            + "${[CASE_REFERENCE]}/trigger/boEscalateToRegistrar) "
+            + "${[CASE_REFERENCE]}/trigger/boEscalateToRegistrar)  "
             + "[Select For QA](/cases/case-details/"
-            + "${[CASE_REFERENCE]}/trigger/boSelectForQA) "
+            + "${[CASE_REFERENCE]}/trigger/boSelectForQA)  "
             + "[SME Referral](/cases/case-details/"
-            + "${[CASE_REFERENCE]}/trigger/moveToCWEscalation) "
+            + "${[CASE_REFERENCE]}/trigger/moveToCWEscalation)  "
             + "[Stop Case](/cases/case-details/"
             + "${[CASE_REFERENCE]}/trigger/boStopCaseForCasePrinted)";
     public static final String DESCRIPTION_EXAMINE_DIGITAL_CASE_PROBATE_READY_TO_ISSUE_VALUE =
             "[Amend Case Details](/cases/case-details/"
-                    + "${[CASE_REFERENCE]}/trigger/boAmendCaseDetails) "
+                    + "${[CASE_REFERENCE]}/trigger/boAmendCaseDetails)  "
                     + "[Issue Grant](/cases/case-details/"
-                    + "${[CASE_REFERENCE]}/trigger/boIssueGrantForCaseMatching) "
+                    + "${[CASE_REFERENCE]}/trigger/boIssueGrantForCaseMatching)  "
                     + "[Escalate to Registrar](/cases/case-details/"
-                    + "${[CASE_REFERENCE]}/trigger/boEscalateToRegistrar) "
+                    + "${[CASE_REFERENCE]}/trigger/boEscalateToRegistrar)  "
                     + "[SME Referral](/cases/case-details/"
-                    + "${[CASE_REFERENCE]}/trigger/moveToCWEscalation) "
+                    + "${[CASE_REFERENCE]}/trigger/moveToCWEscalation)  "
                     + "[Stop Case](/cases/case-details/"
                     + "${[CASE_REFERENCE]}/trigger/boStopCaseForCaseMatchingForExamining)";
     public static final String CASE_PRINTED_STATE = "CasePrinted";
