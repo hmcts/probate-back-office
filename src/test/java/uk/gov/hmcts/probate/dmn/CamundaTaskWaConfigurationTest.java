733d31bbbb997342598a21ac8092235a06836d09
Scott Christie <103422729+scottiee-jc@users.noreply.github.com>
2026-07-27 18:11:44 +0100

    Update CamundaTaskWaConfigurationTest.java

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaConfigurationTest.java
@@ -55,27 +55,25 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
                                 Map.of("state", CASE_PRINTED_STATE)).build()
                 ),
                 Arguments.of(
-                            EXAMINE_DIGITAL_CASE_PROBATE,
-                            CaseDataBuilder.defaultWaCase().isUrgent().build(),
-                            "boAmendCaseDetailsForAwaitingDocumentation",
-                            ConfigurationExpectationBuilder.examineDigitalCaseExpectationsForConditions(
-                                    Map.of("state", CASE_PRINTED_STATE)).build()
+                        EXAMINE_DIGITAL_CASE_PROBATE,
+                        CaseDataBuilder.defaultWaCase().isUrgent().build(),
+                        "boAmendCaseDetailsForAwaitingDocumentation",
+                        ConfigurationExpectationBuilder.examineDigitalCaseExpectationsForConditions(
+                                Map.of("state", CASE_PRINTED_STATE)).build()
                 ),
                 Arguments.of(
                         EXAMINE_DIGITAL_CASE_ADMON,
                         CaseDataBuilder.defaultWaCase().isUrgent().build(),
                         "handleEvidence",
-                        ConfigurationExpectationBuilder
-                                .examineDigitalCaseExpectationsForConditions(
-                                        Map.of("state", CASE_PRINTED_STATE)).build()
+                        ConfigurationExpectationBuilder.examineDigitalCaseExpectationsForConditions(
+                                Map.of("state", CASE_PRINTED_STATE)).build()
                 ),
                 Arguments.of(
                         EXAMINE_DIGITAL_CASE_INTESTACY,
                         CaseDataBuilder.defaultWaCase().isUrgent().build(),
                         "handleEvidence",
-                        ConfigurationExpectationBuilder
-                                .examineDigitalCaseExpectationsForConditions(
-                                        Map.of("state", CASE_PRINTED_STATE)).build()
+                        ConfigurationExpectationBuilder.examineDigitalCaseExpectationsForConditions(
+                                Map.of("state", CASE_PRINTED_STATE)).build()
                 ),
                 Arguments.of(
                         EXAMINE_DE_BONIS_NON,
