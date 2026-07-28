f518dec9554e83d70f6ac06e454d0736a0c89660
Mike Brown <michael.brown@hmcts.net>
2026-07-28 18:43:39 +0100

    dmn count tests updated after resolving conflicts with master

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
@@ -224,7 +224,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(10));
+        assertThat(logic.getRules().size(), is(13));
     }
 
     @ParameterizedTest(name = "event id: {0}")
--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
@@ -88,7 +88,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(6));
+        assertThat(logic.getRules().size(), is(9));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
