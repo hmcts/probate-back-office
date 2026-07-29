64b7e21f91c723fc4a1c810ccc1f1ab93771b128
scott.christie <s.christie496@gmail.com>
2026-07-29 14:58:04 +0100

    rebuild pipeline with fixed tests

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
@@ -83,7 +83,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(7));
+        assertThat(logic.getRules().size(), is(8));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
