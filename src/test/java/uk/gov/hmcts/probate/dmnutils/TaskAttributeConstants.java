affa481a3bb23035375d74af352de1dc80a3366c
scott.christie <s.christie496@gmail.com>
2026-07-29 17:05:31 +0100

    add rules for 5454

--- src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
+++ src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
@@ -65,6 +65,7 @@ public class TaskAttributeConstants {
         = "ExamineInfectedBloodCompensationAuthority";
     public static final String EXAMINE_DOUBLE_PROBATE = "ExamineDoubleProbate";
     public static final String EXAMINE_INCAPACITY_UNDER_RULE_35 = "ExamineIncapacityUnderRule35";
+    public static final String EXAMINE_LEADING_OR_FOLLOWING_GRANTS = "ExamineLeadingOrFollowingGrants";
 
 
     public static final String CREATE_DUE_DATE = "createDueDate";
@@ -143,6 +144,7 @@ public class TaskAttributeConstants {
     public static final String AD_COLLIGENDA_BONA_EXAMINE_SKILL_CODE = "SKILL:ABA6:AdColligendaBonaExamining";
     public static final String DOUBLE_PROBATE_EXAMINE_SKILL_CODE = "SKILL:ABA6:DoubleProbate";
     public static final String INCAPACITY_UNDER_RULE_35_EXAMINE_SKILL_CODE = "SKILL:ABA6:IncapacityUnder_Rule35";
+    public static final String LEADING_FOLLOWING_GRANTS_EXAMINE_SKILL_CODE = "SKILL:ABA6:LeadingFollowingGrants";
 
     public static final String PROBATE_TASK_TYPE_NAME = "Examine Digital Case - Probate";
     public static final String ADMON_TASK_TYPE_NAME = "Examine Digital Case - Admon";
@@ -154,6 +156,7 @@ public class TaskAttributeConstants {
             = "Examine - Infected Blood Compensation Authority";
     public static final String EXAMINE_DOUBLE_PROBATE_TASK_TYPE_NAME = "Examine - Double Probate";
     public static final String EXAMINE_INCAPACITY_UNDER_RULE_35_TASK_TYPE_NAME = "Examine - Incapacity under rule 35";
+    public static final String EXAMINE_LEADING_OR_FOLLOWING_GRANTS_TASK_TYPE_NAME = "Examine - Leading or following Grants";
 
 
     public static final String HANDLE_EVIDENCE_EVENT = "handleEvidence";
