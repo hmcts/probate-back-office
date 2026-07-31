db18fde4ea822436871f3e9282d1dfd91cfe6673
scott.christie <s.christie496@gmail.com>
2026-07-31 16:03:31 +0100

    fix tests post merge

--- src/test/java/uk/gov/hmcts/probate/dmn/initiation/CamundaTaskWaInitiationBaseTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/initiation/CamundaTaskWaInitiationBaseTest.java
@@ -77,7 +77,7 @@ public class CamundaTaskWaInitiationBaseTest extends DmnDecisionTableBaseUnitTes
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(7));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(13));
+        assertThat(logic.getRules().size(), is(14));
     }
 
     @ParameterizedTest(name = "event id: {0} post event state: {1} evidenceHandled: {2} caseType: {3}")
@@ -91,6 +91,7 @@ public class CamundaTaskWaInitiationBaseTest extends DmnDecisionTableBaseUnitTes
     @ArgumentsSource(CamundaTaskWaInitiationIncapacityUnderRule35TestProvider.class)
     @ArgumentsSource(CamundaTaskWaInitiationInfectedBloodTestProvider.class)
     @ArgumentsSource(CamundaTaskWaInitiationLeadingOrFollowingGrantTestProvider.class)
+    @ArgumentsSource(CamundaTaskWaInitiationWindrushTestProvider.class)
     void given_multiple_event_ids_should_evaluate_dmn_for_probate_scenarios(String eventId,
                                                                             String postEventState,
                                                                             Map<String, Object> additionalData,
