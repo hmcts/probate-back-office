e74a537b50968442bc2ed86dad780a9a48d4174d
Mike Brown <michael.brown@hmcts.net>
2026-08-04 14:17:18 +0100

    test fixes after resolving master conflicts

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaConfigurationTest.java
@@ -140,7 +140,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(2));
         assertThat(logic.getOutputs().size(), is(3));
-        assertEquals(18, logic.getRules().size());
+        assertEquals(17, logic.getRules().size());
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
