d5a8aaf67cfa30bf5217ff7bfa45f33870ae24e9
Mike Brown <michael.brown@hmcts.net>
2026-08-05 17:17:10 +0100

    test fixes after resolving master conflicts

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
@@ -156,7 +156,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
         assertThat(logic.getOutputs().size(), is(7));
         assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
         //Rules
-        assertThat(logic.getRules().size(), is(19));
+        assertThat(logic.getRules().size(), is(20));
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
