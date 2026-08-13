d2733419756b395a25d839bcb92dd8cd15f2267c
SomeshRA <somesh.acharya1@hmcts.net>
2026-08-13 15:25:56 +0100

    DTSPG-5699: Updated WA Task controller

--- src/main/java/uk/gov/hmcts/probate/controller/WaTaskContoller.java
+++ src/main/java/uk/gov/hmcts/probate/controller/WaTaskContoller.java
@@ -8,16 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import uk.gov.hmcts.probate.controller.validation.ApplicationCreatedGroup;
-import uk.gov.hmcts.probate.controller.validation.ApplicationReviewedGroup;
-import uk.gov.hmcts.probate.controller.validation.ApplicationUpdatedGroup;
-import uk.gov.hmcts.probate.controller.validation.NextStepsConfirmationGroup;
 import uk.gov.hmcts.probate.exception.BadRequestException;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
 import uk.gov.hmcts.probate.model.ccd.raw.response.CallbackResponse;
