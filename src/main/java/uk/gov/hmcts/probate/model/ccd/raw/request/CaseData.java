7d89542421ffe14e93a8c9478ee38ea6b625fe78
SomeshRA <somesh.acharya1@hmcts.net>
2026-08-13 17:13:44 +0100

    DTSPB-5699: Added createTask and relevant auths

--- src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseData.java
+++ src/main/java/uk/gov/hmcts/probate/model/ccd/raw/request/CaseData.java
@@ -54,6 +54,7 @@ import uk.gov.hmcts.probate.model.ccd.raw.SolsAddress;
 import uk.gov.hmcts.probate.model.ccd.raw.StopReason;
 import uk.gov.hmcts.probate.model.ccd.raw.TTL;
 import uk.gov.hmcts.probate.model.ccd.raw.UploadDocument;
+import uk.gov.hmcts.reform.probate.model.YesNo;
 import uk.gov.hmcts.reform.probate.model.cases.CombinedName;
 import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.Damage;
 import uk.gov.hmcts.reform.probate.model.cases.HandoffReason;
@@ -380,6 +381,7 @@ public class CaseData extends CaseDataParent {
     private String evidenceHandled;
     private transient String attachDocuments;
     private final String caseType;
+    private final YesNo createTask;
     private final String paperForm;
     private String channelChoice;
     private final String languagePreferenceWelsh;
