8aa90daf0297592d784087e29eb7173445154aa2
SomeshRA <somesh.acharya1@hmcts.net>
2026-08-20 17:35:42 +0100

    DTSPB-5699: Fixed checkstyle

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/WaTaskControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/WaTaskControllerIT.java
@@ -113,7 +113,7 @@ public class WaTaskControllerIT {
                     }
                    }
                 }
-        """;
+            """;
 
         when(workAllocationToggleService.isProbateWAEnabled())
                 .thenReturn(true);
