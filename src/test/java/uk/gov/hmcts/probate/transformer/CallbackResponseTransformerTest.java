a240afaadecf07afb7d2221e9e5d0e18af4102c0
sadaque-khan-hmcts <Sadaque.Khan@justice.gov.uk>
2026-08-23 20:26:51 +0100

    [DTSPB-5795-5789] fix(core): hide storage-only intestacy parent fields - keep niece and nephew parent fields for CCD storage only to prevent unsupported caseworker input - align citizen parent-death labels with relationship-specific wording to reduce ambiguity - remove temporary relationship diagnostics from callbacks to keep production logs focused - restore callbackresponse transformer test to match the digitising intestacy baseline

--- src/test/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformerTest.java
+++ src/test/java/uk/gov/hmcts/probate/transformer/CallbackResponseTransformerTest.java
@@ -141,12 +141,10 @@ import static uk.gov.hmcts.probate.model.Constants.CHANNEL_CHOICE_DIGITAL;
 import static uk.gov.hmcts.probate.model.Constants.CHILD;
 import static uk.gov.hmcts.probate.model.Constants.CTSC;
 import static uk.gov.hmcts.probate.model.Constants.GRAND_CHILD;
-import static uk.gov.hmcts.probate.model.Constants.HALF_BLOOD_NIECE_OR_NEPHEW;
 import static uk.gov.hmcts.probate.model.Constants.HALF_BLOOD_SIBLING;
 import static uk.gov.hmcts.probate.model.Constants.PARENT;
 import static uk.gov.hmcts.probate.model.Constants.SIBLING;
 import static uk.gov.hmcts.probate.model.Constants.HALF_SIBLING;
-import static uk.gov.hmcts.probate.model.Constants.WHOLE_BLOOD_NIECE_OR_NEPHEW;
 import static uk.gov.hmcts.probate.model.Constants.WHOLE_BLOOD_SIBLING;
 import static uk.gov.hmcts.probate.model.Constants.WHOLE_SIBLING;
 import static uk.gov.hmcts.probate.model.DocumentType.AD_COLLIGENDA_BONA_GRANT;
@@ -5383,7 +5381,6 @@ class CallbackResponseTransformerTest {
         assertNotNull(relationshipList);
         assertEquals(2, relationshipList.getListItems().size());
         assertEquals(WHOLE_BLOOD_SIBLING, relationshipList.getListItems().getFirst().getCode());
-        assertEquals(WHOLE_BLOOD_NIECE_OR_NEPHEW, relationshipList.getListItems().get(1).getCode());
     }
 
     @Test
@@ -5410,7 +5407,6 @@ class CallbackResponseTransformerTest {
         assertNotNull(relationshipList);
         assertEquals(2, relationshipList.getListItems().size());
         assertEquals(HALF_BLOOD_SIBLING, relationshipList.getListItems().getFirst().getCode());
-        assertEquals(HALF_BLOOD_NIECE_OR_NEPHEW, relationshipList.getListItems().get(1).getCode());
     }
 
     @Test
