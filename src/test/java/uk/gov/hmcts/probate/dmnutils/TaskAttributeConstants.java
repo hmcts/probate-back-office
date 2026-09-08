9ead64f1ef3ac4c25fd2ac55a1c7bf8c1abd9d0c
Vamshidhar Chitti <vamshidhar.chitti@HMCTS.NET>
2026-09-08 15:45:44 +0100

    updated taskid for foreignDomicile

--- src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
+++ src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
@@ -226,7 +226,7 @@ public class TaskAttributeConstants {
     public static final String EXAMINE_FIAT_WILL_CASE_PRINTED_TASK_TYPE_NAME = "Examine - Fiat Will";
     public static final String EXAMINE_FIAT_WILL_CASE_PRINTED_SKILL_CODE = "SKILL:ABA6:FiatWill";
 
-    public static final String EXAMINE_FOREIGN_DOMICILE_CASE_PRINTED = "ExamineForeignDomicileReadyToIssue";
+    public static final String EXAMINE_FOREIGN_DOMICILE_CASE_PRINTED = "ExamineForeignDomicile";
     public static final String EXAMINE_FOREIGN_DOMICILE_CASE_PRINTED_TASK_TYPE_NAME = "Examine - Foreign Domicile";
     public static final String EXAMINE_FOREIGN_DOMICILE_CASE_PRINTED_SKILL_CODE = "SKILL:ABA6:ForeignDomicile";
 
