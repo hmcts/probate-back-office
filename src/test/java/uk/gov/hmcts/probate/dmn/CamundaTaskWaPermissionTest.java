8b7d64b46963c420bc743430cdfe07c49a490804
AbdirahmanHussein001 <Abdirahman.Hussein@HMCTS.NET>
2026-09-10 16:14:55 +0100

    Rebase fixes

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
@@ -308,7 +308,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
         assertThat(logic.getOutputs().size(), is(7));
         assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
         //Rules
-        assertThat(logic.getRules().size(), is(56));
+        assertThat(logic.getRules().size(), is(62));
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
