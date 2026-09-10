dcaeb7f18ac25b0111fe8bd43a77a1fee15d91c2
Vincent Ngobeh <vincentngobeh@gmail.com>
2026-09-10 11:09:33 +0100

    DTSPB-5431 Awaiting documentation merge conflict resolution for initiation and compeltion dmn.

--- src/test/java/uk/gov/hmcts/probate/dmn/initiation/CamundaTaskWaInitiationBaseTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/initiation/CamundaTaskWaInitiationBaseTest.java
@@ -90,7 +90,7 @@ public class CamundaTaskWaInitiationBaseTest extends DmnDecisionTableBaseUnitTes
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(8));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(35));
+        assertThat(logic.getRules().size(), is(36));
     }
 
     @ParameterizedTest(name = "event id: {0} post event state: {1} evidenceHandled: {2} caseType: {3}")
