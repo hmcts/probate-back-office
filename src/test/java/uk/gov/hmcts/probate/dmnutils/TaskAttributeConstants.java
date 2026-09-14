eeb3cc9013615eea2ad316c261f2607f232a7712
Vamshidhar Chitti <vamshidhar.chitti@HMCTS.NET>
2026-09-14 09:19:47 +0100

    Use correct escalation reason

--- src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
+++ src/test/java/uk/gov/hmcts/probate/dmnutils/TaskAttributeConstants.java
@@ -241,7 +241,7 @@ public class TaskAttributeConstants {
             "Resolve Registrar Escalation - Referrals";
     public static final String RESOLVE_REGISTRAR_ESCALATION_ORDERS_TASK_TYPE_NAME =
             "Resolve Registrar Escalation - Orders";
-    public static final String REGISTRAR_ESCALATION_REASON_REFERRALS = "Referrals";
-    public static final String REGISTRAR_ESCALATION_REASON_ORDERS = "Orders";
+    public static final String REGISTRAR_ESCALATION_REASON_REFERRALS = "referrals";
+    public static final String REGISTRAR_ESCALATION_REASON_ORDERS = "orders";
 }
 
