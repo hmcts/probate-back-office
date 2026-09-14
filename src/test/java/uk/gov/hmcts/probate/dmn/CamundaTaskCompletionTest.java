7f0785bf6d8980d0fef2b290a814a49e082216e5
Vincent Ngobeh <vincentngobeh@gmail.com>
2026-09-14 14:25:38 +0100

    DTSPB-5240 Amend completion test.

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
@@ -707,7 +707,8 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                                 Map.of(
                                         "completionMode", AUTO_COMPLETE_MODE,
                                         "taskType", EXAMINE_DIGITAL_CASE_AD_COLLIGENDA_BONA
-                                )
+                                ),
+                                Map.of()
                         )
                 )
         );
