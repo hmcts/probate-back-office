dbfe424a4ae6965e5edafc0454f081125a04a660
Mike Brown <michael.brown@hmcts.net>
2026-09-15 13:00:51 +0100

    test fixes after resolving master conflicts

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
@@ -232,7 +232,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(33));
+        assertThat(logic.getRules().size(), is(34));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
