13252f306439796c09312d9d50a409b1ebdf071d
Mike Brown <michael.brown@hmcts.net>
2026-09-16 16:18:46 +0100

    logging added to new mid event 2

--- src/main/java/uk/gov/hmcts/probate/controller/WaTaskContoller.java
+++ src/main/java/uk/gov/hmcts/probate/controller/WaTaskContoller.java
@@ -97,8 +97,11 @@ public class WaTaskContoller {
             Optional<String> encodedClientContext = taskUtils.setTaskCompletion(
                     clientContext,
                     callbackRequest,
-                    paramCallbackRequest -> paramCallbackRequest.getCaseDetails().getData()
-                            .getEvidenceHandled().equals(NO)
+                    paramCallbackRequest -> {
+                        String evidenceHandled = paramCallbackRequest.getCaseDetails().getData().getEvidenceHandled();
+                        log.info("Evaluating evidenceHandled value: {}", evidenceHandled);
+                        return NO.equals(evidenceHandled);
+                    }
             );
 
             encodedClientContext
