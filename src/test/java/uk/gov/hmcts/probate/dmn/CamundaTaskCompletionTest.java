9e979d5073804a29f45c1098f2495fac9b6c1dd7
AbdirahmanHussein001 <Abdirahman.Hussein@HMCTS.NET>
2026-09-17 16:10:31 +0100

    Rebase test fixes

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
@@ -940,7 +940,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(44));
+        assertThat(logic.getRules().size(), is(47));
     }
 
     @ParameterizedTest(name = "event id: {0}")
