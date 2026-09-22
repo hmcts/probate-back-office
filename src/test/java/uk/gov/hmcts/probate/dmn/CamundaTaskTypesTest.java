0c540732ed16dd1b9c188b5f9cfda4f57ec2b2f0
Vincent Ngobeh <vincentngobeh@gmail.com>
2026-09-22 09:36:20 +0100

    Merge conflicts resolution.

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskTypesTest.java
@@ -328,7 +328,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(48));
+        assertThat(logic.getRules().size(), is(51));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
