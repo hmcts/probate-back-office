138d47369a34e6af18f3dc08ef7f2a9bad619042
Vincent Ngobeh <vincentngobeh@gmail.com>
2026-09-22 10:03:53 +0100

    Permission test configuration.

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskWaPermissionTest.java
@@ -417,7 +417,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
         assertThat(logic.getOutputs().size(), is(7));
         assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
         //Rules
-        assertThat(logic.getRules().size(), is(90));
+        assertThat(logic.getRules().size(), is(96));
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
