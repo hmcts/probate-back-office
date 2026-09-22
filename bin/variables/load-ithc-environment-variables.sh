d8c3f58e4b6a279c5318ac249f5a143988f4a4d8
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2026-09-22 11:39:35 +0100

    DTSPB-5933 HMCTS Access to idam-web-public

--- bin/variables/load-ithc-environment-variables.sh
+++ bin/variables/load-ithc-environment-variables.sh
@@ -6,7 +6,7 @@ echo 'export ENVIRONMENT=ithc'
 
 # urls
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-ithc.service.core-compute-ithc.internal"
-echo "export IDAM_API_BASE_URL=https://idam-api.ithc.platform.hmcts.net"
+echo "export IDAM_API_BASE_URL=https://idam-web-public.ithc.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-ithc.service.core-compute-ithc.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://ccd-definition-store-api-ithc.service.core-compute-ithc.internal"
 
