38c1395b9e993f7830a63b579e424a14ba60f8b6
Vincent Ngobeh <vincentngobeh@gmail.com>
2026-09-22 11:31:24 +0100

    Whitespace alignments with master branch.

--- src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/probate/dmn/CamundaTaskCompletionTest.java
@@ -1043,6 +1043,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                                 Map.of(
                                         "completionMode", AUTO_COMPLETE_MODE,
                                         "taskType", REVIEW_QA_CASE_PROBATE
+
                                 )
                         )
                 )
