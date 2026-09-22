33e9f757ce0897b6ee17c95fd43e0b2d2532fa1f
Mike Brown <michael.brown@hmcts.net>
2026-09-22 10:07:41 +0100

    verify added to test shouldUpdateClientContextEvidenceHandledWhenProbateWaIsEnabled

--- src/integrationTest/java/uk/gov/hmcts/probate/controller/WaTaskControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/controller/WaTaskControllerIT.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.probate.model.ccd.raw.request.CallbackRequest;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseData;
+import uk.gov.hmcts.probate.model.ccd.raw.request.CaseDetails;
 import uk.gov.hmcts.probate.model.wa.WaMapper;
 import uk.gov.hmcts.probate.service.wa.WorkAllocationToggleService;
 import uk.gov.hmcts.probate.util.TestUtils;
@@ -19,7 +21,9 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -27,6 +31,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.probate.model.Constants.CLIENT_CONTEXT_HEADER_PARAMETER;
+import static uk.gov.hmcts.probate.util.CommonVariables.NO;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -144,6 +149,21 @@ public class WaTaskControllerIT {
                 .andExpect(header().string(
                         CLIENT_CONTEXT_HEADER_PARAMETER,
                         encodedString.get()));
+
+        verify(taskUtils).setTaskCompletion(isA(String.class), isA(CallbackRequest.class), argThat(predicate -> {
+            // Mock the CallbackRequest and its nested objects
+            CallbackRequest mockCallbackRequest = mock(CallbackRequest.class);
+            CaseDetails mockCaseDetails = mock(CaseDetails.class);
+            CaseData mockCaseData = mock(CaseData.class);
+
+            // Set up the mock behavior
+            when(mockCallbackRequest.getCaseDetails()).thenReturn(mockCaseDetails);
+            when(mockCaseDetails.getData()).thenReturn(mockCaseData);
+            when(mockCaseData.getEvidenceHandled()).thenReturn(NO);
+
+            // Test the predicate
+            return predicate.test(mockCallbackRequest);
+        }));
     }
 
     @Test
